fa7ed6a342e3c49fac421d7034ec4bfd944ce0bd
yogesh <ysa-openerp@ysa>
2009-06-30 11:11:55 +0530

    [IMP] product_qt module : changes in mrp.production view (/home/gbaconnier/code/openerp/extra-addons/lp6.1-main rev 3754.1.3)

--- product_extended/__init__.py
+++ product_extended/__init__.py
@@ -20,6 +20,5 @@
 #
 ##############################################################################
 import product_extended
-import wizard
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
