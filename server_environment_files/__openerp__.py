c59f16ff09af54e6c5b0465ba2a8711075569a2e
Joël Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2011-10-12 15:54:04 +0200

    [ADD] server evironment file + sample (lp:c2c-addons/6.1  rev 11)

---
+++ server_environment_files/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Adapted by Nicolas Bessi. Copyright Camptocamp SA
+#    Based on Florent Xicluna original code. Copyright Wingo SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "server configuration environment files repository module",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Camptocamp",
+    "description": """This module provides a file store for classical configuration by environnement file pattern into OpenERP provided by server_environment.
+     It is ment to be used by server_environment module. Please look at this module for more info and doc.
+    """,
+    "website": "http://www.camptocamp.com",
+    "category": "Tools",
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml": [],
+    "installable": True,
+    "active": False,
+}
