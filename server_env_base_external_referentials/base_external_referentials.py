15ebbc4ad0aa3f559f8976bf3df888158b17b964
Guewen Baconnier @ Camptocamp <guewen.baconnier@camptocamp.com>
2011-11-04 11:27:39 +0100

    [ADD] Server environment module for base_external_referentials. You can configure the url, api login and password in a config file for each referential and each environment (dev/test/prod, ...). No risk to synchronize your Dev / Test OpenERP with your Magento Production (lp:c2c-addons/6.1  rev 11.1.5)

---
+++ server_env_base_external_referentials/base_external_referentials.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author Guewen Baconnier. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+from server_environment import serv_config
+
+
+class external_referential(osv.osv):
+    _inherit = 'external.referential'
+
+    def _get_environment_config_by_name(self, cr, uid, ids, field_names, arg, context):
+        values = {}
+        for referential in self.browse(cr, uid, ids, context):
+            values[referential.id] = {}
+            for field_name in field_names:
+                section_name = '.'.join((self._name.replace('.', '_'), referential.name))
+                value = serv_config.get(section_name, field_name)
+                values[referential.id].update({field_name: value})
+        return values
+
+    _columns = {
+        'location': fields.function(_get_environment_config_by_name, type='char', size=200,
+                    method=True, string='Location', multi='connection_config'),
+        'apiusername': fields.function(_get_environment_config_by_name, type='char', size=64,
+                       method=True, string='User Name', multi='connection_config'),
+        'apipass': fields.function(_get_environment_config_by_name, type='char', size=64,
+                   method=True, string='Password', multi='connection_config'),
+    }
+
+external_referential()
