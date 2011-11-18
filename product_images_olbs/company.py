c4b0c17576690a0c49726714184c84956c2fca5d
sebastien beau <sebastien.beau@akretion.com.br>
2011-11-18 11:13:17 +0100

    [FIX] product_images_olbs : commit missing fields

---
+++ product_images_olbs/company.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Nicolas Bessi & Guewen Baconnier. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+from tools.translate import _
+
+class ResCompany(osv.osv):
+    """Override company to add images configuration"""
+    _inherit = "res.company"
+    _columns = {        
+        'local_media_repository':fields.char(
+                        'Images Repository Path', 
+                        size=256, 
+                        required=True,
+                        help='Local mounted path on OpenERP server where all your images are stored.'
+                    ),
+    }    
+    
+ResCompany()
