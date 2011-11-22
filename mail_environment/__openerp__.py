25acb955801c32dbc5b68d5d608bca2923367475
Guewen Baconnier @ Camptocamp <guewen.baconnier@camptocamp.com>
2011-11-22 10:45:49 +0100

    [IMP] mail_environment: add support for multi mail servers (lp:c2c-addons/6.1  rev 16)

--- mail_environment/__openerp__.py
+++ mail_environment/__openerp__.py
@@ -8,7 +8,35 @@
     'version': '0.1',
     'category': 'Tools',
     'description': """
-        extend mail and fetch mail with server env
+Extend mail and fetch mail with server environment module.
+
+In config files, sections outgoint_mail and incoming_mails are default values for all Outgoing Mail Servers and Fetchmail Servers.
+For each server, you can (re)define values with a section named "outgoing_mail.resource_name" where resource_name is the name of your server.
+
+Exemple of config file :
+
+[outgoing_mail]
+smtp_host = smtp.myserver.com
+smtp_port = 587
+smtp_user = 
+smtp_pass =
+smtp_encryption = ssl
+
+[outgoing_mail.openerp_smtp_server1]
+smtp_user = openerp
+smtp_pass = openerp
+
+[incoming_mail.openerp_pop_mail1]
+server = mail.myserver.com
+port = 110
+type = pop
+is_ssl = 0
+attach = 0
+original = 0
+user = openerp@myserver.com
+password = openerp
+
+
     """,
     'author': 'Camptocamp',
     'website': 'http://openerp.camptocamp.com',
