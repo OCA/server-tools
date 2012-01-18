68eb57dce98265acb89c02e75774deb411be28eb
unknown <nicolas.bessi@camptocamp.com>
2012-01-18 10:41:04 +0100

    [FIX] typo (lp:c2c-addons/6.1  rev 30)

--- server_env_base_external_referentials/__openerp__.py
+++ server_env_base_external_referentials/__openerp__.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Guewen Baconnier
-#    Copyright 2011-2012 Camptocamp SA
+#    Author Guewen Baconnier. Copyright Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
+#    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
@@ -24,7 +23,6 @@
     "version": "1.0",
     "depends": ["base", 'server_environment', 'base_external_referentials'],
     "author": "Camptocamp",
-    'license': 'AGPL-3',
     "description": """This module is based on the server_environment module to use files for configuration.
 Thus we can have a different file for each environment (dev, test, staging, prod).
 This module define the config variables for the base_external_referential module.
--- server_env_base_external_referentials/base_external_referentials.py
+++ server_env_base_external_referentials/base_external_referentials.py
@@ -1,28 +1,25 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Guewen Baconnier
-#    Copyright 2011-2012 Camptocamp SA
+#    Author Guewen Baconnier. Copyright Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
+#    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 from osv import fields, osv
 from server_environment import serv_config
-import logging
-
 
 
 class external_referential(osv.osv):
@@ -34,12 +31,8 @@ class external_referential(osv.osv):
             values[referential.id] = {}
             for field_name in field_names:
                 section_name = '.'.join((self._name.replace('.', '_'), referential.name))
-                try:
-                    value = serv_config.get(section_name, field_name)
-                    values[referential.id].update({field_name: value})
-                except:
-                    logger = logging.getLogger(__name__)
-                    logger.exception('error trying to read field %s in section %s', field_name, section_name)
+                value = serv_config.get(section_name, field_name)
+                values[referential.id].update({field_name: value})
         return values
 
     _columns = {
