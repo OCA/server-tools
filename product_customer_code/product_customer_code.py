8ce3bdc6a8beb6e312c46b065540ed01db177872
unknown <luis_cleto_@hotmail.com>
2012-07-02 16:35:06 -0500

    [IMP][product_customer_code]Add traducction & Constrains of product code (/home/erp_super/erp/prod/src/addons-vauxoo rev 286.1.8)

--- product_customer_code/product_customer_code.py
+++ product_customer_code/product_customer_code.py
@@ -24,6 +24,8 @@
 #
 ##############################################################################
 from osv import osv, fields
+from tools.translate import _
+
 
 #TODO List:
 #   *Hay que modificar la venta, para que tome en el descripción el product_code & product_name del cliente, en su pedido de venta.
@@ -64,10 +66,13 @@ class product_customer_code(osv.osv):
         'product_code': fields.char('Customer Product Code', size=64, required=True, help="This customer's product code will be used when printing a request for quotation."),
         'product_name': fields.char('Customer Product Name', size=128, help="This customer's product name will be used when printing a request for quotation. Keep empty to use the internal one."),
         'product_id': fields.many2one('product.product', 'Product', required=True),
-        'partner_id': fields.many2one('res.partner', 'Partner',required=True),
+        'partner_id': fields.many2one('res.partner', 'Customer',required=True),
         'company_id': fields.many2one('res.company','Company')
     }
     _defaults = {
         'company_id': lambda s, cr, uid, c: s.pool.get('res.company')._company_default_get(cr, uid, 'product.customer.code', context=c),
     }
+    _sql_constraints = [
+        ('unique_name', 'unique(product_code)', 'Product Code must be unique'),
+    ]
 product_customer_code()
