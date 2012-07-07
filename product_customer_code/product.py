678a0a8dcf2caac1345d29d9268649ab5bc40634
moylop260 <moylop260@vauxoo.com>
2012-07-07 16:21:13 -0500

    [REF][product_customer_code] Change code to file product.py. Remove comment's. Add ToDo messages (/home/erp_super/erp/prod/src/addons-vauxoo rev 286.1.15)

--- product_customer_code/product.py
+++ product_customer_code/product.py
@@ -26,27 +26,26 @@
 from osv import osv, fields
 from tools.translate import _
 
-class product_customer_code(osv.osv):
-    _name = "product.customer.code"
-    _description = "Add manies Code of Customer's"
-    
-    _rec_name = 'product_code'
+class product_product(osv.osv):
+    _inherit = "product.product"
     
     _columns = {
-        'product_code': fields.char('Customer Product Code', size=64, required=True, help="This customer's product code will be used when printing a request for quotation."),
-        'product_name': fields.char('Customer Product Name', size=128, help="This customer's product name will be used when printing a request for quotation. Keep empty to use the internal one."),
-        'product_id': fields.many2one('product.product', 'Product', required=True),
-        'partner_id': fields.many2one('res.partner', 'Customer',required=True),
-        'company_id': fields.many2one('res.company','Company')
+        'product_customer_code_ids': fields.one2many('product.customer.code', 'product_id', 'Customer Codes'),
     }
     
-    _defaults = {
-        'company_id': lambda s, cr, uid, c: s.pool.get('res.company')._company_default_get(cr, uid, 'product.customer.code', context=c),
-    }
-    
-    _sql_constraints = [
-        ('unique_code', 'unique(product_code,company_id,partner_id)', 'Product Code must be unique'),
-    ]
-    
-    #TODO: Add index to product_code, partner_id
-product_customer_code()
+    def name_search(self, cr, user, name='', args=None, operator='ilike', context=None, limit=80):
+        res = super(product_product, self).name_search(cr, user, name, args, operator, context, limit)
+        product_customer_code_obj=self.pool.get('product.customer.code')
+        if not res:
+            ids=[]
+            partner_id = context.get('partner_id', False)
+            if partner_id:
+                id_prod_code=product_customer_code_obj.search(cr, user, [('product_code','=',name),('partner_id','=',partner_id)], limit=limit, context=context)
+                #TODO: Search for product customer name
+                id_prod=id_prod_code and product_customer_code_obj.browse(cr, user, id_prod_code, context=context) or []
+                for ppu in id_prod:
+                    ids.append(ppu.product_id.id)
+            if ids:
+                res = self.name_get(cr, user, ids, context)
+        return res
+product_product()
