1b068238deeb46f5bf0d54073af40f3c3dc27c6b
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2012-10-30 16:42:30 -0400

    [FIX[ licenses and dependencies

--- mgmtsystem_kpi/__openerp__.py
+++ mgmtsystem_kpi/__openerp__.py
@@ -23,7 +23,7 @@
     "version" : "0.1",
     "author" : "Savoir-faire Linux",
     "website" : "http://www.savoirfairelinux.com",
-    "license" : "GPL-3",
+    "license" : "AGPL-3",
     "category" : "Management System",
     "complexity" : "normal",
     "description": """
