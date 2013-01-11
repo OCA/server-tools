a605d68c0dd487fd17c8335a32af85f7231209f3
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-01-11 17:02:08 +0100

    [FIX] copyright header

--- super_calendar/__init__.py
+++ super_calendar/__init__.py
@@ -3,7 +3,6 @@
 #    
 #    Copyright (C) 2012 Agile Business Group sagl (<http://www.agilebg.com>)
 #    Copyright (C) 2012 Domsense srl (<http://www.domsense.com>)
-#    All Rights Reserved
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
