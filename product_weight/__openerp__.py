1ff556187037d0a6bf884bbd306c51898efa4c76
Joao Alfredo Gama Batista <joao.gama@savoirfairelinux.com>
2013-01-15 16:19:03 -0500

    [ADD] Module initial version

---
+++ product_weight/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Product Weight Calculation",
+    "version": "0.1",
+    "author": "Savoir-faire Linux",
+    "website": "http://www.savoirfairelinux.com",
+    "license": "GPL-3",
+    "category": "Warehouse",
+    "description": """
+    This module updates product weight based on it's components weight'
+    """,
+    "depends": [
+        "base",
+        "mrp",
+    ],
+    "demo": [],
+    "data": [
+        "product_view.xml"
+    ],
+    "installable": True,
+}
