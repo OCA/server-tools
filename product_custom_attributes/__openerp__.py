c25aa6bb15a5f58848d9d320340d87cac3dd797f
Raphaël Valyi <rvalyi@gmail.com>
2013-03-01 18:55:42 +0000

    [REF][product_custom_attributes] moved again specific code from base_custom_attributes to roduct_custom_attributes module that have just been-recreated (we did all that because bzr misses a bzr cp feature)

--- product_custom_attributes/__openerp__.py
+++ product_custom_attributes/__openerp__.py
@@ -21,9 +21,26 @@
 
 
 
-import ir_model
-import product_attributes
-
-
-
+{
+    'name': 'product_custom_attributes',
+    'version': '0.1',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'description': """This module adds the possibility to easily create custom fields on products.
+Each product can be linked to an attribute set (like camera, fridge...).
+Each attribute has custom fields (for example, you don't need the same field for a frigde and a camera).
+In particular it's used by the Magento Magentoerpconnect module to match the EAV flexibility of Magento.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com/',
+    'depends': ['product', 'base_custom_attributes'],
+    'init_xml': [],
+    'update_xml': [
+           'product_view.xml',
+           'wizard/open_product_by_attribute_set.xml',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
 
