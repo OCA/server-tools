030b2a5c913f39895e282114ba209c3e859eba52
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-06-12 19:05:17 +0200

    [imp] descr

--- base_optional_quick_create/__openerp__.py
+++ base_optional_quick_create/__openerp__.py
@@ -23,8 +23,10 @@
     'version': '0.1',
     'category': 'Tools',
     'description': """
-https://twitter.com/nbessi/status/337869826028605441
+This module allows to avoid to 'quick create' new records, through many2one fields, for a specific model.
+You can configure which models should allow 'quick create'. When specified, 'quick create' option will always open the standard create form.
 
+Got the idea from https://twitter.com/nbessi/status/337869826028605441
 """,
     'author': 'Agile Business Group',
     'website': 'http://www.agilebg.com',
