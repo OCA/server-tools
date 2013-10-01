eff362ad79f3bfe4b09ef0cd5857d4a39c4fa40d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-10-01 17:17:09 +0200

    [IMP] when creating a new option on a 'selection' attribute, the attribute_id should be pre-filled with the current attribute id and should not be editable

--- base_custom_attributes/custom_attributes.py
+++ base_custom_attributes/custom_attributes.py
@@ -126,6 +126,7 @@ class attribute_attribute(orm.Model):
                     kwargs['domain'] = "[('id', 'in', %s)]" % ids
             else:
                 kwargs['domain'] = "[('attribute_id', '=', %s)]" % attribute.attribute_id.id
+        kwargs['context'] = "{'default_attribute_id': %s}" % attribute.attribute_id.id
         field = etree.SubElement(parent, 'field', **kwargs)
         orm.setup_modifiers(field, self.fields_get(cr, uid, attribute.name, context))
         return parent
