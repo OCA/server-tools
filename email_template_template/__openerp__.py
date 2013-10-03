582dc7147814f45102c8757351d5fc773aa70f1e
Holger Brunn <hbrunn@therp.nl>
2013-10-03 16:50:48 +0200

    [FIX] rst-fy email_template_template's description

--- email_template_template/__openerp__.py
+++ email_template_template/__openerp__.py
@@ -32,8 +32,9 @@ This way we can put the layout parts into the template template and only content
 in the other templates. Changing the layout is then only a matter of changing
 the template template.
 
-
-Usage:
+-----
+Usage
+-----
 Create an email template with the related document model 'Email Templates'. Now
 most of the fields gray out and you can only edit body_text and body_html. Be
 sure to use ${body_text} and ${body_html} respectively in your template
@@ -43,48 +44,50 @@ Then select this newly created template templates in one of your actual
 templates.
 
 For example, create a template template
------
-Example Corp logo
-Example Corp header
-${object.body_text} <- this gets evaluated to the body_text of a template using this template template
-Example Corp
-Example street 42
-Example city
-Example Corp footer
------
+
+::
+
+   Example Corp logo
+   Example Corp header
+   ${object.body_text} <- this gets evaluated to the body_text of a template using this template template
+   Example Corp
+   Example street 42
+   Example city
+   Example Corp footer
 
 Then in your template you write
 
------
-Dear ${object.partner_id.name},
+::
 
-Your order has been booked on date ${object.date} for a total amount of ${object.sum}.
------
+   Dear ${object.partner_id.name},
+   
+   Your order has been booked on date ${object.date} for a total amount of ${object.sum}.
 
 And it will be evaluated to
 
------
-Example Corp logo
-Example Corp header
-Dear Jane Doe,
+::
 
-Your order has been booked on date 04/17/2013 for a total amount of 42.
-Example Corp
-Example street 42
-Example city
-Example Corp footer
------
+    Example Corp logo
+    Example Corp header
+    Dear Jane Doe,
+
+    Your order has been booked on date 04/17/2013 for a total amount of 42.
+    Example Corp
+    Example street 42
+    Example city
+    Example Corp footer
 
 Given the way evaluation works internally (body_text of the template template is evaluated two times, first with the instance of email.template of your own template, then with the object your template refers to), you can do some trickery if you know that a template template is always used with the same kind of model (that is, models that have the same field name):
 
 In your template template:
 
-------
-Dear ${'${object.name}'}, <-- gets evaluated to "${object.name}" in the first step, then to the content of object.name
-${object.body_html}
-Best,
-Example Corp
-------""",
+::
+
+    Dear ${'${object.name}'}, <-- gets evaluated to "${object.name}" in the first step, then to the content of object.name
+    ${object.body_html}
+    Best,
+    Example Corp
+""",
     'website': 'http://therp.nl',
     'images': [],
     'depends': ['email_template'],
