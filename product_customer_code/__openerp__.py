d8b5643152e76cfb83807d5b104748a6977557ef
Nhomar - Vauxoo <nhomar@gmail.com>
2013-12-13 08:08:26 -0530

    [IMP] Better documentation with image.

--- product_customer_code/__openerp__.py
+++ product_customer_code/__openerp__.py
@@ -41,6 +41,13 @@ Customer' codes in product
 
 This module does just like the product.supplierinfo but for customers instead. For instance it
 allows to have different references for the same product according to the customer.
+
+.. image:: product_customer_code/static/src/img/screenshot1.png
+
+.. tip::
+
+    You will need install some of the Apps which enable the product menu to see this module in
+    action, like Sales, Purchase or Warehouse Management
      """,
     "data": [
         "security/product_customer_code_security.xml",
