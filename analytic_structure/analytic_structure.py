1e09d882e1bfc539a25b36b61a0cd05c5e398765
Jérémie Gavrel <jeremie.gavrel@xcg-consulting.fr>
2014-03-20 14:49:11 +0100

    Added methods analytic_fields_get and analytic_fields_view_get to the analytic_structure class.
    These methods should be called by models that implement analytic fields, respectively in their fields_get and fields_view_get methods.

--- analytic_structure/analytic_structure.py
+++ analytic_structure/analytic_structure.py
@@ -19,7 +19,8 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
-from openerp.tools.translate import _
+import re
+from lxml import etree
 
 
 ORDER_SELECTION = (
@@ -30,15 +31,89 @@ ORDER_SELECTION = (
     ('5', 'Analysis 5')
 )
 
+
 class analytic_structure(osv.Model):
+
     _name = "analytic.structure"
 
     _columns = dict(
         model_name=fields.char("Object", size=128, required=True, select="1"),
         nd_id=fields.many2one(
-            "analytic.dimension", "Related Dimension", ondelete="restrict", required=True, select="1"),
-        ordering=fields.selection(ORDER_SELECTION, 'Analysis slot', required=True),
+            "analytic.dimension",
+            "Related Dimension",
+            ondelete="restrict",
+            required=True,
+            select="1"
+        ),
+        ordering=fields.selection(
+            ORDER_SELECTION,
+            'Analysis slot',
+            required=True),
     )
     _sql_constraints = [
-        ('unique_ordering', 'unique(model_name,ordering)', 'One dimension per Analysis slot per object'),
+        (
+            'unique_ordering',
+            'unique(model_name,ordering)',
+            'One dimension per Analysis slot per object'
+        ),
     ]
+
+    def _get_dimensions_names(self, cr, uid, model, context=None):
+        """Get the names of the analysis dimensions linked to a given model."""
+
+        ans_ids = self.search(
+            cr, uid,
+            [('model_name', '=', model)],
+            context=context
+        )
+        ans_brs = self.browse(cr, uid, ans_ids, context=context)
+
+        return {
+            ans.ordering: ans.nd_id.name
+            for ans in ans_brs
+        }
+
+    def analytic_fields_get(
+        self, cr, uid, model, fields, prefix='a', context=None
+    ):
+        """Set the label values for the analytic fields."""
+
+        ans_dict = self._get_dimensions_names(cr, uid, model, context=context)
+
+        regex = '{0}(\d+)_id'.format(prefix)
+        match_fct = re.compile(regex).search
+        matches = filter(None, map(match_fct, fields.keys()))
+
+        for match in matches:
+            field = match.group(0)
+            slot = match.group(1)
+            fields[field]['string'] = ans_dict.get(
+                '{0}'.format(slot),
+                '{0}{1}'.format(prefix.upper(), slot)
+            )
+
+        return fields
+
+    def analytic_fields_view_get(
+        self, cr, uid, model, view, prefix='a', context=None
+    ):
+        """Show or hide used/unused analytic fields."""
+
+        ans_dict = self._get_dimensions_names(cr, uid, model, context=context)
+
+        regex = '{0}(\d+)_id'.format(prefix)
+        path = "//field[re:match(@name, '{0}')]".format(regex)
+        ns = {"re": "http://exslt.org/regular-expressions"}
+
+        pattern = '{{"invisible":{0}, "tree_invisible":{0}}}'
+        doc = etree.XML(view['arch'])
+        matches = doc.xpath(path, namespaces=ns)
+
+        for match in matches:
+            name = match.get('name')
+            slot = re.search(regex, name).group(1)
+            is_invisible = str(not slot in ans_dict).lower()
+            match.set('modifiers', pattern.format(is_invisible))
+
+        view['arch'] = etree.tostring(doc)
+        return view
