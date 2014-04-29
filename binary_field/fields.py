6d363ca6847657e453eb988c9815cf9ee575736b
Sebastien Beau <sebastien.beau@akretion.com>
2014-04-29 16:56:34 +0200

    [IMP] add cache for resized image

--- binary_field/fields.py
+++ binary_field/fields.py
@@ -22,20 +22,23 @@
 
 from openerp.osv import fields, orm
 from openerp.tools import image_resize_image
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class BinaryField(fields.function):
 
     def __init__(self, string, filters=None, **kwargs):
-        if not kwargs.get('type'):
-            kwargs['type'] = 'binary'
+        new_kwargs = {
+            'type': 'binary',
+            'string': string,
+            'fnct': self._fnct_read,
+            'fnct_inv': self._fnct_write,
+            'multi': False, 
+            }
+        new_kwargs.update(kwargs)
         self.filters = filters
-        super(BinaryField, self).__init__(
-            string=string,
-            fnct=self._fnct_read,
-            fnct_inv=self._fnct_write,
-            multi=False,
-            **kwargs)
+        super(BinaryField, self).__init__(**new_kwargs)
 
     def _get_binary_id(self, cr, uid, obj, field_name, record_id, context=None):
         cr.execute(
@@ -54,21 +57,21 @@ class BinaryField(fields.function):
 
     def _fnct_write(self, obj, cr, uid, ids, field_name, value, args,
                     context=None):
+        if not isinstance(ids, (list, tuple)):
+            ids = [ids]
         binary_obj = obj.pool['binary.binary']
-        if type(ids) in (list, tuple):
-            assert len(ids) == 1, 'multi mode is not supported'
-            record_id = ids[0]
-        else:
-            record_id = ids
-        field_key = "%s-%s" % (obj._name, field_name)
-        binary_id = self._get_binary_id(
-            cr, uid, obj, field_name, record_id, context=context)
-        if binary_id:
-            res_id = binary_obj.update(
-                cr, uid, binary_id, value, field_key, context=context)
-        else:
-            res_id = binary_obj.add(cr, uid, value, field_key, context=context)
-            obj.write(cr, uid, record_id, {'%s_info_id' % field_name: res_id}, context=context)
+        for record_id in ids:
+            field_key = "%s-%s" % (obj._name, field_name)
+            binary_id = self._get_binary_id(
+                cr, uid, obj, field_name, record_id, context=context)
+            if binary_id:
+                res_id = binary_obj.update(
+                    cr, uid, binary_id, value, field_key, context=context)
+            else:
+                res_id = binary_obj.add(cr, uid, value, field_key, context=context)
+                obj.write(cr, uid, record_id, {
+                    '%s_info_id' % field_name: res_id,
+                    }, context=context)
         return True
 
     def _fnct_read(self, obj, cr, uid, ids, field_name, args, context=None):
@@ -92,6 +95,18 @@ class ImageField(BinaryField):
             string=string,
             **kwargs)
 
+    def _fnct_write(self, obj, cr, uid, ids, field_name, value, args,
+                    context=None):
+        super(ImageField, self)._fnct_write(
+            obj, cr, uid, ids, field_name, value, args, context=context)
+        for name, field in obj._columns.items():
+            if isinstance(field, ImageResizeField) \
+                and field.related_field == field_name:
+                    field._refresh_cache(
+                        obj, cr, uid, ids, name, context=context)
+        return True
+
+
 class ImageResizeField(ImageField):
 
     def __init__(self, string, related_field, height, width, compute='on_write', filters=None, **kwargs):
@@ -110,23 +125,36 @@ class ImageResizeField(ImageField):
         #soit je les vire et je les regénère
         if context.get('bin_size'):
             return 2
-        resize_field = obj._columns[field_name]
-        record = obj.browse(cr, uid, record_id, context=context)
-        original_image = record[resize_field.related_field]
-        size = (resize_field.height, resize_field.width)
-        return image_resize_image(original_image, size)
- 
+        return super(ImageResizeField, self)._get_binary(
+                cr, uid, obj, field_name, record_id, context=context)
 
-#        binary_obj = obj.pool['binary.binary']
-#        binary_id = self._get_binary_id(
-#            cr, uid, obj, field_name, record_id, context=context)
-#        if not binary_id:
-#            return None
-#        field_key = "%s-%s" % (obj._name, field_name)
-#        return binary_obj.get_content(cr, uid, field_key, binary_id, context=context)
-#
+    def _refresh_cache(self, obj, cr, uid, ids, field_name, context=None):
+        if not isinstance(ids, (list, tuple)):
+            ids = [ids]
+        for record_id in ids:
+            _logger.debug('Refresh Image Cache from the field %s of object %s '
+                         'id : %s' % (field_name, obj._name, record_id))
+            field = obj._columns[field_name]
+            record = obj.browse(cr, uid, record_id, context=context)
+            original_image = record[field.related_field]
+            if original_image:
+                size = (field.height, field.width)
+                resized_image = image_resize_image(original_image, size)
+            else:
+                resized_image = None
+            ctx = context.copy()
+            ctx['refresh_image_cache'] = True
+            self._fnct_write(obj, cr, uid, [record_id], field_name,
+                    resized_image, None, context=ctx)
 
-
+    def _fnct_write(self, obj, cr, uid, ids, field_name, value, args,
+                    context=None):
+        if context is not None and context.get('refresh_image_cache'):
+            field = field_name
+        else:
+            field = obj._columns[field_name].related_field
+        return super(ImageResizeField, self)._fnct_write(
+                obj, cr, uid, ids, field, value, args, context=context)
 
 
 fields.BinaryField = BinaryField
