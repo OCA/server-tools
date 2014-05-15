d7befbf6d76d3946dd81b988a1cc2a26c9d0cbd8
Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
2014-05-15 15:45:20 +0200

    1.2

--- auth_saml/__openerp__.py
+++ auth_saml/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Saml2 Authentication',
-    'version': '1.1.1',
+    'version': '1.2',
     'category': 'Tools',
     'description': """
 Allow users to login through Saml2 Provider.
