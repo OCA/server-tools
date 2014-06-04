41de0f8476500e19b5ac1ba8567863b98d4871ed
Jérémie Gavrel <jeremie.gavrel@xcg-consulting.fr>
2014-06-04 18:03:16 +0200

    Bumped version to 1.3.2 (stable)

--- analytic_structure/__openerp__.py
+++ analytic_structure/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     "name" : "Analytic Structure",
-    "version" : "1.3.1",
+    "version" : "1.3.2",
     "author" : "XCG Consulting",
     "category": 'Dependency',
     "description": """
