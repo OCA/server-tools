42a6bed82add90c6de0d86179c81e4df9bbb2db0
Sebastien Beau <sebastien.beau@akretion.com>
2014-06-06 13:03:04 +0200

    [IMP] Update example module

--- binary_field_example/res_company.py
+++ binary_field_example/res_company.py
@@ -28,7 +28,11 @@ class ResCompany(orm.Model):
     
     _columns = {
         'binary_test': fields.BinaryField('Test Binary'),
-        'image_test': fields.ImageField('Test Image'),
+        'image_test': fields.ImageField('Test Image',
+            config={
+                'field_key': 'StoreMeHere',
+                'base_location': 'file:///testpath',
+                }),
         'image_test_resize': fields.ImageResizeField(
             related_field='image_test',
             string='Test Image small',
