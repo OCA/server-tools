941a0e2cae90b9ca32c2a0fef0d5bf3831535d77
unknown <alexandre.fayolle@camptocamp.com>
2014-06-10 12:33:33 +0200

    start porting to v7 OCA standards

--- product_gtin/product_gtin.py
+++ product_gtin/product_gtin.py
@@ -19,38 +19,39 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import fields, osv
+from openerp.osv import fields, osv, orm
 import operator
 
 def is_pair(x):
     return not x%2
 
 def check_ean(eancode):
-     if not eancode:
-         return True
-     if not len(eancode) in [8,12,13,14]:
-         return False
-     try:
-         int(eancode)
-     except:
-         return False
-     sum=0
-     ean13_len= int(len(eancode))
-     for i in range(ean13_len-1):
-         pos=int(ean13_len-2-i)
-         if is_pair(i): 
-             sum += 3 * int(eancode[pos])
-         else:
-             sum += int(eancode[pos])
-     check = 10 - operator.mod(sum,10)
-     if check == 10 :
-         check = 0
-     if check != int(eancode[ean13_len-1]): # last digit
-         return False
-     return True
+    if not eancode:
+        return True
+    if not len(eancode) in [8,12,13,14]:
+        return False
+    try:
+        int(eancode)
+    except:
+        return False
+    sum=0
+    ean13_len= int(len(eancode))
+    for i in range(ean13_len-1):
+        pos=int(ean13_len-2-i)
+        if is_pair(i):
+            sum += 3 * int(eancode[pos])
+        else:
+            sum += int(eancode[pos])
+    check = 10 - operator.mod(sum,10)
+    if check == 10 :
+        check = 0
+    if check != int(eancode[ean13_len-1]): # last digit
+        return False
+    return True
 
-# need to replace the check_ean13_key function 
-class product_product(osv.osv):
+
+# need to replace the check_ean13_key function
+class product_product(orm.Model):
     _inherit = "product.product"
 
     def _check_ean_key(self, cr, uid, ids, context=None):
@@ -59,15 +60,16 @@ class product_product(osv.osv):
         return res
 
     _columns = {
-        'ean13': fields.char('EAN', help ='Code for EAN8 EAN13 UPC JPC GTIN http://de.wikipedia.org/wiki/Global_Trade_Item_Number', size=14),
+        'ean13': fields.char('EAN',
+                             help='Code for EAN8 EAN13 UPC JPC GTIN '
+                             'http://de.wikipedia.org/wiki/Global_Trade_Item_Number',
+                             size=14),
     }
 
     _constraints = [(_check_ean_key, 'Error: Invalid EAN code', ['ean13'])]
 
-product_product()
 
-
-class product_packaging(osv.osv):
+class product_packaging(orm.Model):
     _inherit = "product.packaging"
 
     def _check_ean_key(self, cr, uid, ids, context=None):
@@ -76,14 +78,14 @@ class product_packaging(osv.osv):
         return res
 
     _columns = {
-        'ean':    fields.char('EAN', help ='Barcode number for EAN8 EAN13 UPC JPC GTIN', size=14),
+        'ean':    fields.char('EAN',
+                              help='Barcode number for EAN8 EAN13 UPC JPC GTIN',
+                              size=14),
     }
     _constraints = [(_check_ean_key, 'Error: Invalid EAN code', ['ean'])]
 
-product_packaging()
 
-
-class res_partner(osv.osv):
+class res_partner(orm.Model):
     _inherit = "res.partner"
 
     def _check_ean_key(self, cr, uid, ids, context=None):
@@ -92,12 +94,10 @@ class res_partner(osv.osv):
         return res
 
     _columns = {
-        'ean13':    fields.char('EAN', help ='Code for EAN8 EAN13 UPC JPC GTIN http://de.wikipedia.org/wiki/Global_Trade_Item_Number', size=14),
+        'ean13':    fields.char('EAN',
+                                help='Code for EAN8 EAN13 UPC JPC GTIN '
+                                'http://de.wikipedia.org/wiki/Global_Trade_Item_Number',
+                                size=14),
     }
 
     _constraints = [(_check_ean_key, 'Error: Invalid EAN code', ['ean13'])]
-
-res_partner()
-
-#class wiz_ean13_check(wizard.interface):
-#wiz_ean13_check()
