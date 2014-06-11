48fa12ffcfd2c524172cb633badbd2e209af86db
unknown <alexis@via.ecp.fr>
2014-06-11 10:39:47 +0200

    removed useless view file

--- product_gtin/__openerp__.py
+++ product_gtin/__openerp__.py
@@ -34,6 +34,6 @@ Product GTIN module
 Replaces the EAN13 field on products, partners and packaging by a field with the same technical name that accepts EAN13, EAN8, JPC, UPC and GTIN.
     """,
     "demo": [],
-    "data": ["product_gtin_view.xml"],
+    "data": [],
     "installable": True,
 }
