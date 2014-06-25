e7b22454afa54b00e931cfd79a08205a1d1b79af
Anael Lorimier <anael.lorimier@xcg-consulting.fr>
2014-06-25 15:40:56 +0200

    1.3.7

--- analytic_structure/__openerp__.py
+++ analytic_structure/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     "name" : "Analytic Structure",
-    "version" : "1.3.6",
+    "version" : "1.3.7",
     "author" : "XCG Consulting",
     "category": 'Dependency',
     "description": """
