f7891087da4f6d4a0332be01cebca9f6bfee8c81
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2014-07-12 12:19:38 +0200

    port dbfilter_from_header to 8.0

---
+++ dbfilter_from_header/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Therp BV (<http://therp.nl>).
+#    This module copyright (C) 2014 ACSONE SA/NV (<http://acsone.eu>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import re
+from openerp import http
+
+db_filter_org = http.db_filter
+
+
+def db_filter(dbs, httprequest=None):
+    dbs = db_filter_org(dbs, httprequest)
+    httprequest = httprequest or http.request.httprequest
+    db_filter_hdr = \
+        httprequest.environ.get('HTTP_X_ODOO_DBFILTER') or \
+        httprequest.environ.get('HTTP_X_OPENERP_DBFILTER')
+    if db_filter_hdr:
+        dbs = [db for db in dbs if re.match(db_filter_hdr, db)]
+    return dbs
+
+http.db_filter = db_filter
