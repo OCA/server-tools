b8e08093244f64badad61030c07b3dc06c0d9c4a
Дмитро Катюха <Dmytro.Katyukha@jbm.com.ua>
2014-07-30 12:07:49 +0300

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,30 @@
+{
+    "name": "GrayLog2 Handler",
+    "version": "0.0.1",
+    "author": "JBM",
+    "website": "",
+    "summary": "Provides ability to send log messages to graylog2 server",
+    "category": "Added functionality",
+    "depends": [
+    ],
+    "description": """
+Aditional options available for config file:
+    gelf_enabled: bool
+    gelf_host: string, required if gelf_enabled=True
+    gelf_port: integer, required if gelf_enabled=True
+    gelf_localname: string
+
+    """,
+    "init_xml": [
+    ],
+    "demo": [
+    ],
+    "test": [
+    ],
+    "update_xml": [
+    ],
+    "active": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
