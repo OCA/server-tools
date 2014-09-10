5acadd404d8cd04e6e136f8d5a8f5b0c8ace4566
Alex Comba <alex.comba@agilebg.com>
2014-09-10 17:18:53 +0200

    [ADD] Copyright (C) 2014 Agile Business Group sagl

--- google_meeting/__init__.py
+++ google_meeting/__init__.py
@@ -2,7 +2,8 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013 solutions2use (<http://www.solutions2.com>).
+#    Copyright (C) 2013 solutions2use (<http://www.solutions2.com>)
+#    Copyright (C) 2014 Agile Business Group sagl (<http://www.agilebg.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
