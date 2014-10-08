ce71f5a771c5f30158671b84d4b9dbb93ae4f6d4
Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
2014-10-08 12:37:59 +0200

    [pep8] E203

--- analytic_structure/__openerp__.py
+++ analytic_structure/__openerp__.py
@@ -20,9 +20,9 @@
 ##############################################################################
 
 {
-    "name" : "Analytic Structure",
-    "version" : "1.6.2",
-    "author" : "XCG Consulting",
+    "name": "Analytic Structure",
+    "version": "1.6.2",
+    "author": "XCG Consulting",
     "category": 'Dependency',
     "description": """
 This module allows to use several analytic dimensions through a structure
