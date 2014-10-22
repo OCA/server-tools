3b196613343bae227ca82c3e78b2de5cfdbbc951
Alexis de Lattre <alexis@via.ecp.fr>
2014-10-22 17:57:55 +0200

    Port to v8 PEP-8 compliant

--- base_optional_quick_create/__init__.py
+++ base_optional_quick_create/__init__.py
@@ -18,30 +18,4 @@
 #
 ##############################################################################
 
-{
-    'name': "Optional quick create",
-    'version': '0.1',
-    'category': 'Tools',
-    'summary': "Avoid 'quick create' on m2o fields, on a 'by model' basis",
-    'description': """
-This module allows to avoid to 'quick create' new records, through many2one
-fields, for a specific model.
-You can configure which models should allow 'quick create'.
-When specified, the 'quick create' option will always open the standard create
-form.
-
-Got the idea from https://twitter.com/nbessi/status/337869826028605441
-""",
-    'author': 'Agile Business Group',
-    'website': 'http://www.agilebg.com',
-    'license': 'AGPL-3',
-    "depends": ['base'],
-    "data": [
-        'model_view.xml',
-    ],
-    "demo": [],
-    'test': [
-    ],
-    "active": False,
-    "installable": True
-}
+from . import model
