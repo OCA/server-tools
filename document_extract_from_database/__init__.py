be13c759c4cb18e92b09c3deb00eb417479ab69b
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-19 23:11:58 +0100

    [REF] 'document_extract_from_database' flake8;

--- document_extract_from_database/__init__.py
+++ document_extract_from_database/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#################################################################################
+###############################################################################
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2012 Julius Network Solutions SARL <contact@julius.fr>
@@ -17,6 +17,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#################################################################################
+###############################################################################
 
 from . import model
--- document_extract_from_database/model/__init__.py
+++ document_extract_from_database/model/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#################################################################################
+###############################################################################
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2012 Julius Network Solutions SARL <contact@julius.fr>
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#################################################################################
+###############################################################################
 
 from . import ir_attachment
 from . import document_multiple_action
