3cd91e013dbf5a15b30d0b59112a604787307b1f
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-12-09 14:00:03 +0100

    [ADD] 'auth_generate_password' module to generate randomly user's password

---
+++ auth_generate_password/__openerp__.py
@@ -0,0 +1,70 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Authentification - Generate Password module for Odoo
+#    Copyright (C) 2014 GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Password Secure',
+    'version': '2.0',
+    'category': 'GRAP - Custom',
+    'description': """
+Password Secure
+===============
+
+Functionnality:
+---------------
+    * Forbid users to change them password;
+        * Only members of base.group_system can change password; OK.
+    * Add a button to the res_users form:
+        * to change the password (randomly generated);
+        * send an email to each users;
+
+Settings:
+---------
+    * Once the module installed, you have to set two settings:
+        * password size: the size of the password generated (6 by default);
+        * password chars: the list of allowed chars (by default ASCII letters
+          and digits); You can:
+            * set a list of chars like 'abcdef';
+            * use string function like string.ascii_letters;
+    * Be sure that an Outgoing Email Server is correctly configured;
+
+Copyright, Author and Licence :
+-------------------------------
+    * Copyright : 2014, Groupement Régional Alimentaire de Proximité;
+    * Author : Sylvain LE GAL (https://twitter.com/legalsylvain);
+    * Licence : AGPL-3 (http://www.gnu.org/licenses/)
+    """,
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'email_template',
+    ],
+    'data': [
+        'data/ir_config_parameter.yml',
+        'data/email_template.xml',
+        'view/view.xml',
+    ],
+    'demo': [
+        'demo/res_groups.yml',
+        'demo/res_users.yml',
+    ],
+}
---
+++ auth_generate_password/model/res_users.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Authentification - Generate Password module for Odoo
+#    Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+# This line is noqa flaged, to allow users to use string functions written
+# in ir_config_parameter
+import string # flake8: noqa
+import random
+
+from openerp.osv.orm import Model
+from openerp.osv.osv import except_osv
+from openerp.tools.translate import _
+
+
+class res_users(Model):
+    _inherit = 'res.users'
+
+    def generate_password(self, cr, uid, ids, context=None):
+        mm_obj = self.pool['mail.mail']
+        icp_obj = self.pool['ir.config_parameter']
+        imd_obj = self.pool['ir.model.data']
+        et_obj = self.pool['email.template']
+        password_size = eval(icp_obj.get_param(
+            cr, uid, 'auth_generate_password.password_size'))
+        password_chars = eval(icp_obj.get_param(
+            cr, uid, 'auth_generate_password.password_chars'))
+        for ru in self.browse(cr, uid, ids, context=context):
+            if not ru.email:
+                raise osv.except_osv(
+                    _("Cannot send email: user has no email address."),
+                    user.name)
+            # Generate Password
+            password = "".join([random.choice(
+                password_chars) for n in xrange(password_size)])
+            self._set_new_password(
+                cr, uid, ru.id, None, password, None, context=None)
+            # Send Mail
+            et = imd_obj.get_object(
+                cr, uid, 'auth_generate_password',
+                'generate_password_template')
+            mail_id = et_obj.send_mail(
+                cr, uid, et.id, ru.id, True, context=context)
+        return {}
