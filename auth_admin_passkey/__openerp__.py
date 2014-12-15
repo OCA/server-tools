1f12893c8d03f81a6aa7fd741acc263e6ec63140
Anthony Muschang <anthony.muschang@acsone.eu>
2014-12-15 09:25:06 +0100

    [CHG] auth_admin_passkey: move description to README.md

--- auth_admin_passkey/__openerp__.py
+++ auth_admin_passkey/__openerp__.py
@@ -24,26 +24,6 @@
     'name': 'Authentification - Admin Passkey',
     'version': '2.1.1',
     'category': 'base',
-    'description': """
-Admin password become a passkey for all active logins
-=====================================================
-
-Functionality :
----------------
-    * Administrator has now the possibility to login in with any login;
-    * By default, OpenERP will send a mail to user and admin to indicate them;
-    * If a user and the admin have the same password, admin will be informed;
-
-Technical information :
------------------------
-    * Create two ir_config_parameter to enable / disable mail sending;
-
-Copyright, Author and Licence :
--------------------------------
-    * Copyright : 2014, Groupement Régional Alimentaire de Proximité;
-    * Author : Sylvain LE GAL (https://twitter.com/legalsylvain);
-    * Licence : AGPL-3 (http://www.gnu.org/licenses/)
-    """,
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
