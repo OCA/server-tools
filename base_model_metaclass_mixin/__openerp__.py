c32aab464f3c96e1cf4c3970e63275b82075415a
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2015-01-15 17:55:24 +0100

    Update and Add License (AGPL) and (XCG)

--- base_model_metaclass_mixin/__openerp__.py
+++ base_model_metaclass_mixin/__openerp__.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-# BaseModelMetaclassMixin
 #
-# Author: Anael LORIMIER <anael.lorimier@xcg-consulting.fr>
-#         Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
+#    BaseModelMetaclassMixin, for OpenERP
+#    Copyright (C) 2013 XCG Consulting (http://odoo.consulting)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Author: Anael LORIMIER <anael.lorimier@xcg-consulting.fr>
+#            Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
+#
 ##############################################################################
+
 {
     'name': "BaseModelMetaclassMixin",
     'version': '0.5',
