5f47177089f9367a75950095040c7d38d5031f95
vrms <vrms@netcologne.de>
2015-01-24 20:58:55 +0800

    move description in README.rst
    Use of OCA template

--- product_brand/__openerp__.py
+++ product_brand/__openerp__.py
@@ -33,26 +33,11 @@
     'version': '0.1',
     'category': 'Product',
     'summary': ' brand',
-    'description': """
-Product Brand Manager
-=====================
-
-This module allows your user to easily manage product brands. You can
-define brands, attach a logo and a description to them.  It also allows to
-attach a partner to a brand.  Once installed, check the menu
-Product/configuration/brand
-
-To do / To come:
-
-- A view to seeing products by brand.
-
-    """,
-    'author': 'Prisnet Telecommunications SA ',
-    'website': 'http://www.prisnet.ch ',
+    'author': 'NetAndCo, Akretion, Prisnet Telecommunications SA',
     'depends': ['product'],
     'data': [
         'product_brand_view.xml',
         'security/ir.model.access.csv'
-    ],
+        ],
     'installable': True,
 }
