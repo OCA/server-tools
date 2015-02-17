8cb55937dd7d296ae76e8af8adf3ce2f1d859af9
Leonardo Donelli <learts92@gmail.com>
2015-02-17 18:31:39 +0100

    Convert model to new APIs.

--- product_brand/product_brand.py
+++ product_brand/product_brand.py
@@ -1,70 +1,66 @@
 # -*- encoding: utf-8 -*-
 ###############################################################################
-# #                                                                           #
-# product_brand for Odoo #                                                    #
-# Copyright (C) 2009 NetAndCo (<http://www.netandco.net>). #                  #
-# Copyright (C) 2011 Akretion Benoît Guillot <benoit.guillot@akretion.com> #  #
-# Copyright (C) 2014 prisnet.ch Seraphine Lantible <s.lantible@gmail.com> #   #
+#                                                                             #
+# product_brand for Odoo                                                      #
+# Copyright (C) 2009 NetAndCo (<http://www.netandco.net>).                    #
+# Copyright (C) 2011 Akretion Benoît Guillot <benoit.guillot@akretion.com>    #
+# Copyright (C) 2014 prisnet.ch Seraphine Lantible <s.lantible@gmail.com>     #
+# Copyright (C) 2015 Leonardo Donelli                                         #
 # Contributors                                                                #
-# Mathieu Lemercier, mathieu@netandco.net, #                                  #
-# Franck Bret, franck@netandco.net #                                          #
+# Mathieu Lemercier, mathieu@netandco.net                                     #
+# Franck Bret, franck@netandco.net                                            #
 # Seraphine Lantible, s.lantible@gmail.com, http://www.prisnet.ch             #
-# #                                                                           #
-# This program is free software: you can redistribute it and/or modify #      #
-# it under the terms of the GNU Affero General Public License as #            #
-# published by the Free Software Foundation, either version 3 of the #        #
-# License, or (at your option) any later version. #                           #
-# #                                                                           #
-# This program is distributed in the hope that it will be useful, #           #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of #            #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the #              #
-# GNU Affero General Public License for more details. #                       #
-# #                                                                           #
-# You should have received a copy of the GNU Affero General Public License #  #
-# along with this program. If not, see <http://www.gnu.org/licenses/>. #      #
-# #                                                                           #
+# Leonardo Donelli, donelli@webmonks.it, http://www.wearemonk.com             #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as              #
+# published by the Free Software Foundation, either version 3 of the          #
+# License, or (at your option) any later version.                             #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                #
+# GNU Affero General Public License for more details.                         #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with this program. If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                             #
 ###############################################################################
 ###############################################################################
 # Product Brand is an Openobject module wich enable Brand management for      #
 # products                                                                    #
 ###############################################################################
-from openerp.osv import orm, fields
+from openerp import models, fields
 
 
-class product_brand(orm.Model):
+class ProductBrand(models.Model):
     _name = 'product.brand'
 
-    def _get_products_count(self, cr, uid, ids, fields, arg, context=None):
-        product_model = self.pool['product.template']
-        res = {}
-        for brand in self.browse(cr, uid, ids, context=context):
-            n_products = product_model.search(
-                cr, uid, [('product_brand_id', '=', brand.id)], count=True)
-            res[brand.id] = n_products
-        return res
+    name = fields.Char('Brand Name')
+    description = fields.Text('Description', translate=True)
+    partner_id = fields.Many2one(
+        'res.partner',
+        string='Partner',
+        help='Select a partner for this brand if it exists',
+        ondelete='restrict'
+    )
+    logo = fields.Binary('Logo File')
+    products_count = fields.Integer(
+        string='Number of products',
+        compute='_get_products_count',
+    )
 
-    _columns = {
-        'name': fields.char('Brand Name'),
-        'description': fields.text('Description', translate=True),
-        'partner_id': fields.many2one(
-            'res.partner', 'partner',
-            help='Select a partner for this brand if it exists.',
-            ondelete='restrict'
-        ),
-        'logo': fields.binary('Logo File'),
-        'products_count': fields.function(
-            _get_products_count,
-            string='Branded Products',
-            type='integer',
-        ),
-    }
+    def _get_products_count(self):
+        for brand in self:
+            brand.products_count = self.env['product.template'].search_count(
+                [('product_brand_id', '=', brand.id)]
+            )
 
-
-class product_template(orm.Model):
+class ProductTemplate(models.Model):
     _inherit = 'product.template'
-    _columns = {
-        'product_brand_id': fields.many2one(
-            'product.brand', 'Brand',
-            help='Select a brand for this product.'
-        ),
-    }
+
+    product_brand_id = fields.Many2one(
+        'product.brand',
+        string='Brand',
+        help='Select a brand for this product'
+    )
