09f0a0e759fdab67c5944fc395bda46d257fff49
Dhaval Patel <dhaval@deneroteam.com>
2015-03-12 16:19:59 +0530

    fix: import statement as in v8

--- auth_password_settings/models/res_config.py
+++ auth_password_settings/models/res_config.py
@@ -18,7 +18,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp.osv import osv, fields
+from openerp.osv import (
+    osv,
+    fields)
 from openerp.tools.safe_eval import safe_eval
 from openerp.tools.translate import _
 
