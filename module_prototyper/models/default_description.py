a1c2f7a617d5269d8659a16523a0ccdadd0e2c3f
EL HADJI DEM <elhadji.dem@savoirfairelinux.com>
2015-04-07 16:44:52 -0400

    [UPD] Add a new file for default value decription field

--- module_prototyper/models/default_description.py
+++ module_prototyper/models/default_description.py
@@ -22,6 +22,7 @@
 
 
 DEFAULT_DESCRIPTION = """
+===========
 Module name
 ===========
 
@@ -48,6 +49,13 @@ Usage
 To use this module, you need to:
 
  * go to ...
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/{repo_id}/{branch}
+
+.. repo_id is available in https://github.com/OCA/maintainer-tools/blob/master/tools/repos_with_ids.txt
+.. branch is "8.0" for example
 
 For further information, please visit:
 
