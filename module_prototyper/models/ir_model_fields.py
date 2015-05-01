60f327b8d3df74b249bb7242820ce9c104c50ae4
Vincent Vinet <vincent.vinet@savoirfairelinux.com>
2015-05-01 08:03:13 -0400

    fix template description being indented
    [FIX] Use human name and english category name for manifest
    
    [IMP] multi line notes, unprefix names, pep8 py files
    
    [IMP] whitespace cleanup in templates
    
    [FIX] base models get _name, custom get _inherit
    
    flake fix
    
    add missing spaces
    
    prevent crash on menus with no action
    
    switch GPL-2 to LGPL-3
    
    generate license headers with license name
    
    fix pep/flake errors in generated files
    
    add m2m and relation field options in fields

--- module_prototyper/models/ir_model_fields.py
+++ module_prototyper/models/ir_model_fields.py
@@ -19,7 +19,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 from openerp import fields, models
+from openerp.tools.translate import _
 
 
 class ir_model_fields(models.Model):
@@ -28,3 +30,20 @@ class ir_model_fields(models.Model):
 
     notes = fields.Text('Notes to developers.')
     helper = fields.Text('Helper')
+    # TODO: Make column1 and 2 required if a model has a m2m to itself
+    column1 = fields.Char(
+        'Column1',
+        help=_("name of the column referring to 'these' records in the "
+               "relation table"),
+    )
+    column2 = fields.Char(
+        'Column2',
+        help=_("name of the column referring to 'those' records in the "
+               "relation table"),
+    )
+    limit = fields.Integer('Read limit', help=_("Read limit"))
+    context = fields.Char(
+        'Context',
+        help=_("Context to use on the client side when handling the field "
+               "(python dictionary)"),
+    )
