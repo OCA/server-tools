d8e7332ca457b4f13a791857a59a626375ffd2c3
Sandy Carter <sandy.carter@savoirfairelinux.com>
2015-05-08 14:19:58 -0400

    Use OCA template

--- admin_technical_features/__openerp__.py
+++ admin_technical_features/__openerp__.py
@@ -30,11 +30,18 @@
     'category': 'Administration',
     'summary': 'Checks the technical features box for admin user.',
     'description': """
-Base Partner Compute Field
-==========================
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License: AGPL-3
+
+Admin Technical Features
+========================
+
 Checks the technical features box for administrator user.
 
 .. image:: /admin_technical_features/static/admin_technical_features.png
+
+Credits
+=======
 
 Contributors
 ------------
@@ -42,11 +49,20 @@ Contributors
 * Sandy Carter <sandy.carter@savoirfairelinux.com>
 * Jordi Riera<jordi.riera@savoirfairelinux.com>
 
-More information
-----------------
-Module developed and tested with Odoo version 7.0
-For questions, please contact our support services \
-(support@savoirfairelinux.com)
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
 """,
     'depends': ['base_import'],
     'external_dependencies': {
