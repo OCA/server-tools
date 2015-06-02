0efae140552b6a6cd1b2c7b2ddbd23da05f12a20
oihane <oihanecruce@gmail.com>
2015-06-02 15:12:15 +0200

    [IMP] <base_report_auto_create_qweb> Duplication of report added

--- base_report_auto_create_qweb/models/report_xml.py
+++ base_report_auto_create_qweb/models/report_xml.py
@@ -18,26 +18,76 @@ class IrActionsReport(models.Model):
                 _("Template Name must contain at least a dot in it's name"))
         report_xml = super(IrActionsReport, self).create(values)
         if values.get('report_type') in ['qweb-pdf', 'qweb-html']:
-            qweb_view_data = {
-                'name': values['report_name'].split('.')[1],
-                'mode': 'primary',
-                'type': 'qweb',
-                'arch': '<?xml version="1.0"?>\n'
-                        '<t t-name="%s">\n</t>' % values['report_name'],
-            }
-            qweb_view = self.env['ir.ui.view'].create(qweb_view_data)
-            model_data_data = {
-                'module': values['report_name'].split('.')[0],
-                'name': values['report_name'].split('.')[1],
-                'res_id': qweb_view.id,
-                'model': 'ir.ui.view',
-            }
-            self.env['ir.model.data'].create(model_data_data)
-            value_view_data = {
-                'name': values['name'],
-                'model': values['model'],
-                'key2': 'client_print_multi',
-                'value_unpickle': 'ir.actions.report.xml,%s' % report_xml.id,
-            }
-            self.env['ir.values'].create(value_view_data)
+            report_views = self.env.context.get('report_views')
+            suffix = self.env.context.get('suffix', 'copy')
+            report_name = values['report_name']
+            module = report_name.split('.')[0]
+            name = report_name.split('.')[1]
+            for report_view in report_views:
+                origin_name = name.replace(('_%s' % suffix), '')
+                new_report_name = '%s_%s' % (origin_name, suffix)
+                qweb_view_data = {
+                    'name': report_view.name.replace(
+                        origin_name, new_report_name),
+                    'mode': 'primary',
+                    'type': 'qweb',
+                    'arch': report_view.arch.replace(
+                        origin_name, new_report_name),
+                }
+                qweb_view = self.env['ir.ui.view'].create(qweb_view_data)
+                model_data_data = {
+                    'module': module,
+                    'name': report_view.name.replace(
+                        origin_name, new_report_name),
+                    'res_id': qweb_view.id,
+                    'model': 'ir.ui.view',
+                }
+                self.env['ir.model.data'].create(model_data_data)
+                value_view_data = {
+                    'name': values['name'],
+                    'model': values['model'],
+                    'key2': 'client_print_multi',
+                    'value_unpickle': ('ir.actions.report.xml,%s' %
+                                       report_xml.id),
+                }
+                self.env['ir.values'].create(value_view_data)
+            if not report_views:
+                qweb_view_data = {
+                    'name': name,
+                    'mode': 'primary',
+                    'type': 'qweb',
+                    'arch': '<?xml version="1.0"?>\n'
+                            '<t t-name="%s">\n</t>' % report_name,
+                }
+                qweb_view = self.env['ir.ui.view'].create(qweb_view_data)
+                model_data_data = {
+                    'module': module,
+                    'name': name,
+                    'res_id': qweb_view.id,
+                    'model': 'ir.ui.view',
+                }
+                self.env['ir.model.data'].create(model_data_data)
+                value_view_data = {
+                    'name': values['name'],
+                    'model': values['model'],
+                    'key2': 'client_print_multi',
+                    'value_unpickle': (
+                        'ir.actions.report.xml,%s' % report_xml.id),
+                }
+                self.env['ir.values'].create(value_view_data)
         return report_xml
+
+    @api.one
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        suffix = self.env.context.get('suffix', 'copy')
+        default['name'] = '%s (%s)' % (self.name, suffix)
+        default['report_name'] = '%s_%s' % (self.report_name, suffix.lower())
+        report_views = self.env['ir.ui.view'].search([
+            ('name', 'ilike', self.report_name.split('.')[1]),
+            ('type', '=', 'qweb')])
+        return super(IrActionsReport,
+                     self.with_context(
+                         report_views=report_views,
+                         suffix=suffix.lower())).copy(default=default)
