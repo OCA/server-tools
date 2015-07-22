e7473ccf01e73ff9afd746dadc5e529f72e0e846
Jairo Llopis <j.llopis@grupoesoc.es>
2015-07-22 13:34:23 +0200

    Add module datetime_formatter.
    This module allows the developer to format any date/time combination in
    the most comfortable way for the user, just with a quick call to

---
+++ datetime_formatter/tests/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import test_best_matcher, test_formatter
