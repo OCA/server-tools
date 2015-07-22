a054b0930d41c6248d886620bb6dd913cab5a487
Jairo Llopis <j.llopis@grupoesoc.es>
2015-07-22 13:34:23 +0200

    Add module datetime_formatter.
    This module allows the developer to format any date/time combination in
    the most comfortable way for the user, just with a quick call to
    :meth:`~.datetime_formatter` under any context.

---
+++ datetime_formatter/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Date & Time Formatter",
+    "summary": "Helper functions to give correct format to date[time] fields",
+    "version": "8.0.1.0.0",
+    "category": "Tools",
+    "website": "https://grupoesoc.es/",
+    "author": "Grupo ESOC Ingeniería de Servicios, S.L.U., "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+}
