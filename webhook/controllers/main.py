c1c0645135fde2d88a952d0ee56bde8dbdf1288d
Moises Lopez <moylop260@vauxoo.com>
2015-07-23 05:53:53 -0500

    [REF] webhook: Add dynamic url with name of webhook record

--- webhook/controllers/main.py
+++ webhook/controllers/main.py
@@ -20,26 +20,32 @@ from openerp.tools.translate import _
 
 class WebhookController(http.Controller):
 
-    @http.route('/webhook', type='json', auth='none', method=['POST'])
-    def webhook(self, *args, **kwargs):
+    @http.route(['/webhook/<webhook_name>'], type='json',
+                auth='none', method=['POST'])
+    def webhook(self, webhook_name, **post):
         '''
+        :params string webhook_name: Name of webhook to use
         Webhook odoo controller to receive json request and send to
         driver method.
         You will need create your webhook with http://0.0.0.0:0000/webhook
         NOTE: Important use --db-filter params in odoo start.
         '''
-        cr, context = request.cr, request.context
-        webhook_registry = request.registry['webhook']
-        webhook = webhook_registry.search_with_request(
-            cr, SUPERUSER_ID, request, context=context)
+        # Deprecated by webhook_name dynamic name
+        # webhook = webhook_registry.search_with_request(
+        #     cr, SUPERUSER_ID, request, context=context)
+        webhook = request.env['webhook'].with_env(
+            request.env(user=SUPERUSER_ID)).search(
+                [('name', '=', webhook_name)], limit=1)
+        # TODO: Add security by secret string  or/and ip consumer
         if not webhook:
             remote_addr = ''
             if hasattr(request, 'httprequest'):
                 if hasattr(request.httprequest, 'remote_addr'):
                     remote_addr = request.httprequest.remote_addr
             raise exceptions.ValidationError(_(
-                'webhook consumer from remote address [%s] '
+                'webhook consumer [%s] from remote address [%s] '
                 'not found jsonrequest [%s]' % (
+                    webhook_name,
                     remote_addr,
                     pprint.pformat(request.jsonrequest)[:450]
                 )))
