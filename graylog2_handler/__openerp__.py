9e41f4dae70c0d1b9f80fb8575d89cc737536750
Дмитро Катюха <Dmytro.Katyukha@jbm.com.ua>
2015-09-07 16:06:21 +0300

    [FIX] graylog2_handler: Readme and __openerp__ fixes

--- graylog2_handler/__openerp__.py
+++ graylog2_handler/__openerp__.py
@@ -1,28 +1,13 @@
 {
     "name": "GrayLog2 Handler",
     "version": "0.0.1",
-    "author": "Management and Accounting On-line",
+    "author": ("Management and Accounting On-line, "
+               "Odoo Community Association (OCA)"),
     "website": "https://github.com/OCA/server-tools/",
     "summary": "Provides ability to send log messages to graylog2 server",
     "category": "Added functionality",
     "depends": [
         'base',
-    ],
-    "description": """
-Aditional options available for config file:
-    gelf_enabled: bool
-    gelf_host: string, required if gelf_enabled=True
-    gelf_port: integer, required if gelf_enabled=True
-    gelf_localname: string
-
-    """,
-    "init_xml": [
-    ],
-    "demo": [
-    ],
-    "test": [
-    ],
-    "update_xml": [
     ],
     "active": True,
 }
