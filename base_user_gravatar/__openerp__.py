d75fc90618dcbdae992201d96ef58d3d05bd2b82
Endika Iglesias García <eiglesias@qdqmedia.com>
2015-09-18 13:24:21 +0200

    [UPDATE]

--- base_user_gravatar/__openerp__.py
+++ base_user_gravatar/__openerp__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2015 Endika Iglesias
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'Synchronize Gravatar image',
     'version': '1',
