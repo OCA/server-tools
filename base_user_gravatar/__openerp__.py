990d57c99e7cd0a4844da36c3b57dbf65cdc5de2
Endika Iglesias García <eiglesias@qdqmedia.com>
2015-09-18 13:19:59 +0200

    [UPDATE]

---
+++ base_user_gravatar/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2015 Endika Iglesias
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Synchronize Gravatar image',
+    'version': '1',
+    'author': 'Endika Iglesias, Odoo Community Association (OCA)',
+    'category': 'Tools',
+    'website': 'http://www.endikaiglesias.com/',
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    'depends': ['base'],
+    'data': [
+        'views/res_users_view.xml',
+    ],
+}
