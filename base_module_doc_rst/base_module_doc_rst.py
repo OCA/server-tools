d464ad2e69adf13635de7f3ee14d2ae12dadd8cd
Matjaž Mozetič <info@matmoz.si>
2015-09-18 07:49:03 +0200

    Narrot pep-8 check from 120 to 80 columns

--- base_module_doc_rst/base_module_doc_rst.py
+++ base_module_doc_rst/base_module_doc_rst.py
@@ -36,19 +36,48 @@ class Module(models.Model):
 
     file_graph = fields.Binary('Relationship Graph')
 
-    def _get_graphical_representation(self, cr, uid, model_ids, level=1, context=None):
+    def _get_graphical_representation(
+        self, cr, uid, model_ids, level=1, context=None
+    ):
         obj_model = self.pool.get('ir.model')
         if level == 0:
             return tuple()
         relation = []
         for id in model_ids:
             model_data = obj_model.browse(cr, uid, id, context=context)
-            for field in (f for f in model_data.field_id if f.ttype in ('many2many', 'many2one', 'one2many')):
-                relation.append((model_data.model, field.name, field.ttype, field.relation, field.field_description))
-                new_model_ids = obj_model.search(cr, uid, [('model', '=', field.relation)], context=context)
+            for field in (
+                f for f in model_data.field_id if f.ttype in (
+                    'many2many', 'many2one', 'one2many'
+                )
+            ):
+                relation.append(
+                    (
+                        model_data.model,
+                        field.name,
+                        field.ttype,
+                        field.relation,
+                        field.field_description
+                    )
+                )
+                new_model_ids = obj_model.search(
+                    cr,
+                    uid,
+                    [('model', '=', field.relation)],
+                    context=context
+                )
                 if new_model_ids:
-                    model = obj_model.read(cr, uid, new_model_ids, ['id', 'name'], context=context)[0]
-                    relation.extend(self._get_graphical_representation(cr, uid, model['id'], level - 1))
+                    model = obj_model.read(
+                        cr,
+                        uid,
+                        new_model_ids,
+                        ['id', 'name'],
+                        context=context
+                    )[0]
+                    relation.extend(
+                        self._get_graphical_representation(
+                            cr, uid, model['id'], level - 1
+                        )
+                    )
         return tuple(relation)
 
     def _get_structure(self, relations, main_element):
@@ -62,17 +91,26 @@ class Module(models.Model):
         for obj, fields in res.items():
             val.append('"%s" [%s label="{<id>%s|%s}"];' % (
                 obj,
-                obj in main_element and 'fillcolor=yellow, style="filled,rounded"' or "",
+                obj in
+                main_element and
+                'fillcolor=yellow, style="filled,rounded"' or
+                "",
                 obj,
-                "|".join(["<%s> %s" % (fn, fn) for fn in fields]))
-            )
+                "|".join(["<%s> %s" % (fn, fn) for fn in fields])
+            ))
         return "\n".join(val)
 
     def _get_arrow(self, field_type='many2one'):
         return {
-            'many2one': 'arrowtail="none" arrowhead="normal" color="red" label="m2o"',
-            'many2many': 'arrowtail="crow" arrowhead="crow" color="green" label="m2m"',
-            'one2many': 'arrowtail="none" arrowhead="crow" color="blue" label="o2m"',
+            'many2one': (
+                'arrowtail="none" arrowhead="normal" color="red" label="m2o"'
+            ),
+            'many2many': (
+                'arrowtail="crow" arrowhead="crow" color="green" label="m2m"'
+            ),
+            'one2many': (
+                'arrowtail="none" arrowhead="crow" color="blue" label="o2m"'
+            ),
         }[field_type]
 
     def get_graphical_representation(self, cr, uid, model_ids, context=None):
@@ -83,12 +121,18 @@ class Module(models.Model):
         models = []
         for obj in obj_model.browse(cr, uid, model_ids, context=context):
             models.append(obj.model)
-        relations = set(self._get_graphical_representation(cr, uid, model_ids, context.get('level', 1)))
-        res[obj.model] = "digraph G {\nnode [style=rounded, shape=record];\n%s\n%s }" % (
-            self._get_structure(relations, models),
-            ''.join('"%s":%s -> "%s":id:n [%s]; // %s\n' % (
-                m, fn, fr, self._get_arrow(ft), ft
-            ) for m, fn, ft, fr, fl in relations),
+        relations = set(
+            self._get_graphical_representation(
+                cr, uid, model_ids, context.get('level', 1)
+            )
+        )
+        res[obj.model] = (
+            "digraph G {\nnode [style=rounded, shape=record];\n%s\n%s }" % (
+                self._get_structure(relations, models),
+                ''.join('"%s":%s -> "%s":id:n [%s]; // %s\n' % (
+                    m, fn, fr, self._get_arrow(ft), ft
+                ) for m, fn, ft, fr, fl in relations),
+            )
         )
         return res
 
@@ -97,10 +141,14 @@ class Module(models.Model):
         obj_mod_data = self.pool.get('ir.model.data')
         obj_ids = []
         model_data_ids = obj_mod_data.search(
-            cr, uid, [('module', '=', module), ('model', '=', 'ir.model')], context=context
+            cr, uid,
+            [('module', '=', module), ('model', '=', 'ir.model')],
+            context=context
         )
         model_ids = []
-        for mod in obj_mod_data.browse(cr, uid, model_data_ids, context=context):
+        for mod in obj_mod_data.browse(
+            cr, uid, model_data_ids, context=context
+        ):
             model_ids.append(mod.res_id)
         models = obj_model.browse(cr, uid, model_ids, context=context)
         map(lambda x: obj_ids.append(x.id), models)
@@ -109,13 +157,19 @@ class Module(models.Model):
     def get_relation_graph(self, cr, uid, module_name, context=None):
         if context is None:
             context = {}
-        object_ids = self._get_module_objects(cr, uid, module_name, context=context)
+        object_ids = self._get_module_objects(
+            cr, uid, module_name, context=context
+        )
         if not object_ids:
             return {'module_file': False}
         # context.update({'level': 1})
-        dots = self.get_graphical_representation(cr, uid, object_ids, context=context)
+        dots = self.get_graphical_representation(
+            cr, uid, object_ids, context=context
+        )
         # todo: use os.realpath
-        file_path = openerp.modules.module.get_module_path('base_module_doc_rst')
+        file_path = openerp.modules.module.get_module_path(
+            'base_module_doc_rst'
+        )
         path_png = file_path + "/module.png"
         for key, val in dots.items():
             path_dotfile = file_path + "/%s.dot" % (key,)
