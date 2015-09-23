6511f9ccf19780ef0345ce207f6edde9da00e614
Matjaž Mozetič <info@matmoz.si>
2015-09-23 06:45:37 +0200

    Revert "Merge pull request #248 from savoirfairelinux/8.0-transifex-config"
    This reverts commit 005dd7a8b7b004eab89146a0902c779103e2a62a, reversing
    changes made to fae982bfbf32b0b9a7f4643052dd202de9e052e0.

--- base_report_auto_create_qweb/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-
-from . import models
-from . import wizard
