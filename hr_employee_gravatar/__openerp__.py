10ee8c3824275573ed5d29d57bd7842740fbcec9
Endika Iglesias García <eiglesias@qdqmedia.com>
2015-09-24 09:25:15 +0200

    [UPDATE] Add hr_employee_gravatar

---
+++ hr_employee_gravatar/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2015 Endika Iglesias
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Employees Synchronize Gravatar image',
+    'version': '8.0.1.0.0',
+    'author': 'Endika Iglesias, Odoo Community Association (OCA)',
+    'category': 'Tools',
+    'website': 'http://www.endikaiglesias.com/',
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    'depends': ['hr', 'base_user_gravatar']
+}
---
+++ hr_employee_gravatar/models/res_users.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015 Endika Iglesias
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    @api.one
+    def get_gravatar_image(self):
+        super(ResUsers, self).get_gravatar_image()
+        hr_employee_list = self.env['hr.employee'].search(
+            [('user_id', '=', self.id)])
+        for hr_employee in hr_employee_list:
+            hr_employee.write({'image': self.image})
+        return True
