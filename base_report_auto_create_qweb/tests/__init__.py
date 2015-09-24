d61d1e26546cea829ca21bf3a3a99858f3f8d555
oihane <oihanecruce@gmail.com>
2015-09-24 14:48:09 +0200

    [IMP] <base_report_auto_create_qweb> added test and some fixes

---
+++ base_report_auto_create_qweb/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import test_base_report_auto_create_qweb
