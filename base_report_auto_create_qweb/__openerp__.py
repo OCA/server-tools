c8d01cd271eec2de3ff439943e28e2293b00791e
oihane <oihanecruce@gmail.com>
2015-09-28 11:25:55 +0200

    [IMP] <base_report_auto_create_qweb> Fixed reported problems * Warning raised when template name does not contain '.' * Template name was not same as template ID * Dependecies added to __openerp__.py file

--- base_report_auto_create_qweb/__openerp__.py
+++ base_report_auto_create_qweb/__openerp__.py
@@ -22,12 +22,17 @@
     "depends": [
         "report",
     ],
+    "external_dependencies": {
+        "python": [
+            "unidecode",
+        ],
+    },
     "author": "OdooMRP team, "
               "AvanzOSC, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA), ",
     "website": "http://www.odoomrp.com",
-    'license': 'AGPL-3',
+    "license": "AGPL-3",
     "contributors": [
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
         "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
