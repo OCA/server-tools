763d4503606353d14387627d30269861a7ccca2e
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-09-29 21:37:54 +0200

    [ADD] Add reset_user_access_right module

---
+++ reset_user_access_right/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#     This file is part of reset_user_access_right,
+#     an Odoo module.
+#
+#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
+#
+#     reset_user_access_right is free software:
+#     you can redistribute it and/or modify it under the terms of the GNU
+#     Affero General Public License as published by the Free Software
+#     Foundation,either version 3 of the License, or (at your option) any
+#     later version.
+#
+#     reset_user_access_right is distributed
+#     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+#     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#     PURPOSE.  See the GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with reset_user_access_right.
+#     If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "Reset User Access Right",
+    'summary': """Reset User Access Right""",
+    'author': "ACSONE SA/NV,Odoo Community Association (OCA)",
+    'website': "http://acsone.eu",
+    'category': 'Uncategorized',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_users_view.xml',
+    ],
+}
---
+++ reset_user_access_right/tests/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import test_reset_user_access_right
