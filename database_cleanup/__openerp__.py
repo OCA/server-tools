aea2c28b589dda50778aa09873314ba08e777dd3
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:03:09 +0200

    [UPD] prefix versions with 8.0

--- database_cleanup/__openerp__.py
+++ database_cleanup/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Database cleanup',
-    'version': '0.1',
+    'version': '8.0.0.1.0',
     'author': "Therp BV,Odoo Community Association (OCA)",
     'depends': ['base'],
     'license': 'AGPL-3',
