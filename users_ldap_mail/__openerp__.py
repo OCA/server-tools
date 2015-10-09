9732400591be6e4932ed0ab49686ae406caef685
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:03:09 +0200

    [UPD] prefix versions with 8.0

--- users_ldap_mail/__openerp__.py
+++ users_ldap_mail/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': "LDAP mapping for user name and e-mail",
-    'version': "1.0",
+    'version': "8.0.1.0.0",
     'depends': ["auth_ldap"],
     'author': "Daniel Reis (https://launchpad.com/~dreis-pt),"
               "Odoo Community Association (OCA)",
