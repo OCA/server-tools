d65ca1e91a84e66ddd71419fe54987391b859b97
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:03:09 +0200

    [UPD] prefix versions with 8.0

--- auditlog/__openerp__.py
+++ auditlog/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': "Audit Log",
-    'version': "1.0",
+    'version': "8.0.1.0.0",
     'author': "ABF OSIELL,Odoo Community Association (OCA)",
     'website': "http://www.osiell.com",
     'category': "Tools",
