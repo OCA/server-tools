xcgd/oemetasl

Transformed into a module

__openerp__.py
@@ -1,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# OEmetaSL
+#
+# Author: Anael LORIMIER <anael.lorimier@xcg-consulting.fr>
+#         Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
+##############################################################################
+{
+    'name': "OEmetaSL",
+    'version': "0.2",
+    'author': "XCG Consulting",
+    'category': "Custom Module",
+    'description': """This module handles methods to modify OpenERP base
+    methods.
+    """,
+    'website': "http://www.openerp-experts.com/",
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

OCA/server-tools

base_model_metaclass_mixin/__openerp__.py
@@ -1,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# BaseModelMetaclassMixin
+#
+# Author: Anael LORIMIER <anael.lorimier@xcg-consulting.fr>
+#         Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
+##############################################################################
+{
+    'name': "BaseModelMetaclassMixin",
+    'version': "0.2",
+    'author': "XCG Consulting",
+    'category': "Custom Module",
+    'description': """This module handles methods to modify OpenERP base
+    methods.
+    """,
+    'website': "http://www.openerp-experts.com/",
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4: