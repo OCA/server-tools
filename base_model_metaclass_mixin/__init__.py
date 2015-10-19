xcgd/oemetasl

Fix for odoo8 (MetaModel in orm)

__init__.py
@@ -1,4 +1,4 @@
-from openerp.osv import osv
+from openerp.osv import orm
 from openerp.tools.translate import _
 
 __all__ = ['OEMetaSL']
@@ -23,7 +23,7 @@ def get_overrides():
         yield func_name, func
 
 
-class OEMetaSL(osv.MetaModel):
+class OEMetaSL(orm.MetaModel):
     def __init__(cls, name, bases, nmspc):
         super(OEMetaSL, cls).__init__(name, bases, nmspc)
 

OCA/server-tools

base_model_metaclass_mixin/__init__.py
@@ -1,4 +1,4 @@
-from openerp.osv import osv
+from openerp.osv import orm
 from openerp.tools.translate import _
 
 __all__ = ['BaseModelMetaclassMixin']
@@ -23,7 +23,7 @@ def get_overrides():
         yield func_name, func
 
 
-class BaseModelMetaclassMixin(osv.MetaModel):
+class BaseModelMetaclassMixin(orm.MetaModel):
     def __init__(cls, name, bases, nmspc):
         super(BaseModelMetaclassMixin, cls).__init__(name, bases, nmspc)
 