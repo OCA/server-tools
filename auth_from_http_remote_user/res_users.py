105e4efece7f12f1c6c7fb0dd9075553519f8f99
Jairo Llopis <yajo.sk8@gmail.com>
2015-10-28 16:46:58 +0100

    Another typo.

--- auth_from_http_remote_user/res_users.py
+++ auth_from_http_remote_user/res_users.py
@@ -34,10 +34,10 @@ class res_users(orm.Model):
                                readonly=True),
     }
 
-    def copy(self, cr, uid, rid, default=None, context=None):
+    def copy(self, cr, uid, id, default=None, context=None):
         default = default or {}
         default['sso_key'] = False
-        return super(res_users, self).copy(cr, uid, rid, default, context)
+        return super(res_users, self).copy(cr, uid, id, default, context)
 
     def check_credentials(self, cr, uid, password):
         try:
