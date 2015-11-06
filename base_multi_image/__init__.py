d8f10e2a353c68bae056ec41cc958e85553e6a51
Jairo Llopis <yajo.sk8@gmail.com>
2015-11-06 17:45:53 +0100

    Split model in abstract & concrete.

---
+++ base_multi_image/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+#        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
