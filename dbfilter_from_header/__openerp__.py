399c640306e2262a3431cafaf276b101f2cfb993
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-11-18 22:16:10 +0100

    [ADD] - cherry picked 5737ebc

--- dbfilter_from_header/__openerp__.py
+++ dbfilter_from_header/__openerp__.py
@@ -30,8 +30,9 @@
     This is interesting for setups where database names can't be mapped to
     proxied host names.
 
-    In nginx, use
+    In nginx, use one of
     proxy_set_header X-OpenERP-dbfilter [your filter];
+    proxy_set_header X-Odoo-dbfilter [your filter];
 
     The addon has to be loaded as server-wide module.
     """,
