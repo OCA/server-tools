c485e5f8b5734b1a681f9f9a6b8934b44ea00fa4
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2015-11-19 20:14:36 -0500

    [FIX] C8104(class-camelcase)

--- module_prototyper/models/ir_model_fields.py
+++ module_prototyper/models/ir_model_fields.py
@@ -24,7 +24,7 @@ from openerp import fields, models
 from openerp.tools.translate import _
 
 
-class ir_model_fields(models.Model):
+class IrModelFields(models.Model):
     """Addition of text fields to fields."""
     _inherit = "ir.model.fields"
 
--- module_prototyper/tests/test_prototype_module_export.py
+++ module_prototyper/tests/test_prototype_module_export.py
@@ -24,9 +24,9 @@ import zipfile
 import StringIO
 
 
-class test_prototype_module_export(common.TransactionCase):
+class TestPrototypeModuleExport(common.TransactionCase):
     def setUp(self):
-        super(test_prototype_module_export, self).setUp()
+        super(TestPrototypeModuleExport, self).setUp()
         self.main_model = self.env['module_prototyper.module.export']
         self.prototype_model = self.env['module_prototyper']
         self.module_category_model = self.env[
