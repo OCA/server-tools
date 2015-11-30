9a0759de43ce27942ea0ceb83c3447cbc009d5af
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-11-30 17:53:22 +0100

    [DEL] remove shell which is in official 9.0

--- shell/__openerp__.py
+++
@@ -1,7 +0,0 @@
-{
-    'name': 'Shell command backport',
-    'summary': 'Backport of the v9 shell CLI command.',
-    'author': "Daniel Reis,Odoo Community Association (OCA)",
-    'version': '8.0.1.0.0',
-    'installable': False,
-}
