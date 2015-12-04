47fd9ced05347f1b65b1b0ff4fa43a739f0a710a
Carlos Incaser <carlos@incaser.es>
2015-12-04 19:12:24 +0100

    [ADD][8.0] base_custom_info: Init develop

---
+++ base_custom_info/models/custom_info_template.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
+# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
