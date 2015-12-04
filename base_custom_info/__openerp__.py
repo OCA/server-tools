86dc11674f0627cf067af7ed2de634dec451cd94
Carlos Incaser <carlos@incaser.es>
2015-12-04 19:12:24 +0100

    [ADD][8.0] base_custom_info: Init develop

---
+++ base_custom_info/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
+# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': "Base Custom Info",
+    'summary': "Add custom field in models",
+    'category': 'Customize',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'author': 'Antiun Ingeniería S.L., '
+              'Incaser Informatica S.L., ',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'installable': True,
+}
