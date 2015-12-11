e162df2e121a4506a85b33f55acf0aed462592b9
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-12-11 16:57:02 +0100

    [CHG] Rename reset_user_access_right to base_user_reset_access

--- base_user_reset_access/README.rst
+++ base_user_reset_access/README.rst
@@ -2,11 +2,11 @@
     :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
     :alt: License: AGPL-3
 
-=======================
-Reset User Access Right
-=======================
+======================
+Base User Reset Access
+======================
 
-This module add a button to reset the user's access rights to the minimum configuration.
+This module adds a button to reset the user's access rights to the minimum configuration.
 
 Installation
 ============
@@ -21,7 +21,7 @@ Bug Tracker
 Bugs are tracked on `GitHub Issues <https://github.com/OCA/server-tools/issues>`_.
 In case of trouble, please check there if your issue has already been reported.
 If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
-`here <https://github.com/OCA/server-tools/issues/new?body=module:%20reset_user_access_right%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+`here <https://github.com/OCA/server-tools/issues/new?body=module:%20base_user_reset_access%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
 
 Credits
 =======
--- base_user_reset_access/models/res_users.py
+++ base_user_reset_access/models/res_users.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#     This file is part of reset_user_access_right,
+#     This file is part of base_user_reset_access,
 #     an Odoo module.
 #
 #     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
 #
-#     reset_user_access_right is free software:
+#     base_user_reset_access is free software:
 #     you can redistribute it and/or modify it under the terms of the GNU
 #     Affero General Public License as published by the Free Software
 #     Foundation,either version 3 of the License, or (at your option) any
 #     later version.
 #
-#     reset_user_access_right is distributed
+#     base_user_reset_access is distributed
 #     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
 #     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 #     PURPOSE.  See the GNU Affero General Public License for more details.
 #
 #     You should have received a copy of the GNU Affero General Public License
-#     along with reset_user_access_right.
+#     along with base_user_reset_access.
 #     If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
