d5702800e5f923db45fe545c2d0698cb1e46b961
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-15 17:16:06 +0100

    Fix many bugs and styling, and prepare for OCA.

--- base_custom_info/__openerp__.py
+++ base_custom_info/__openerp__.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
-# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# © 2015 Antiun Ingeniería S.L. - Sergio Teruel
+# © 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': "Base Custom Info",
     'summary': "Add custom field in models",
-    'category': 'Customize',
+    'category': 'Tools',
     'version': '8.0.1.0.0',
     'depends': [
         'base',
     ],
     'data': [
         'views/custom_info_template_view.xml',
-        'views/custom_info_template_line_view.xml',
+        'views/custom_info_property_view.xml',
         'views/custom_info_value_view.xml',
         'views/menu.xml',
         'security/ir.model.access.csv',
     ],
+    "images": [
+        "images/menu.png",
+        "images/properties.png",
+        "images/templates.png",
+        "images/values.png",
+    ],
     'author': 'Antiun Ingeniería S.L., '
-              'Incaser Informatica S.L., ',
+              'Incaser Informatica S.L., '
+              'Odoo Community Association (OCA)',
     'website': 'http://www.antiun.com',
     'license': 'AGPL-3',
     'installable': True,
