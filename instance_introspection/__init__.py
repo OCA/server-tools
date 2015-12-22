6d651fe5c1747073424016be89347f30f433be44
Nhomar Hernández <nhomar@vauxoo.com>
2015-12-22 04:08:13 -0600

    [REM] Clean up old version files.

--- instance_introspection/__init__.py
+++ instance_introspection/__init__.py
@@ -1,3 +1,2 @@
-from . import wizard
 from . import models
 from . import controllers
--- instance_introspection/__openerp__.py
+++ instance_introspection/__openerp__.py
@@ -1,31 +1,12 @@
-# -*- encoding: utf-8 -*-
-###########################################################################
-#    Module Writen to OpenERP, Open Source Management Solution
-#
-#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com/
-#    All Rights Reserved.
-#    info Vauxoo (info@vauxoo.com)
-############################################################################
-#    Coded by: moylop260 (moylop260@vauxoo.com)
-############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# coding: utf-8
+# © 2015 Vauxoo - http://www.vauxoo.com
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# info Vauxoo (info@vauxoo.com)
+# coded by: nhomar@vauxoo.com
+# planned by: nhomar@vauxoo.com
 
 {
-    "name": "Branch Info",
+    "name": "Instance Instrospection",
     "version": "1.5",
     "author": "Vauxoo",
     "category": "Tecnhical Features",
