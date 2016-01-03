9dfde4d687c1c4be9ab994ecbe2e7961c30c7357
Stefan Rijnhart <stefan@opener.am>
2016-01-03 17:36:46 +0100

    [RFR] Expose as a user preference

--- base_technical_features/__openerp__.py
+++ base_technical_features/__openerp__.py
@@ -1,15 +1,17 @@
-# -*- coding: utf-8 -*-
-# © 2016 Opener B.V.
+# coding: utf-8
+# © 2016 Opener B.V. (<https://opener.am>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Technical features group",
     "summary": "Access to technical features without activating debug mode",
     "version": "9.0.1.0.0",
     "category": "Usability",
-    "website": "https://odoo-community.org/",
+    "website": "https://github.com/oca/server-tools",
     "author": "Opener B.V., Odoo Community Association (OCA)",
     "data": [
         'security/res_groups.xml',
+        'views/res_users.xml',
+        'data/res_users.xml',
     ],
     "license": "AGPL-3",
     "installable": True,
