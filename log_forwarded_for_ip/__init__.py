3629ef468462b0f784a47d60d8cc0fe28b971905
Alexis de Lattre <alexis.delattre@akretion.com>
2016-01-28 15:14:50 +0100

    FIX new API

--- log_forwarded_for_ip/__init__.py
+++ log_forwarded_for_ip/__init__.py
@@ -12,7 +12,7 @@ class log_forwarded_for_ip_installed(models.AbstractModel):
 address_string_original = WSGIRequestHandler.address_string
 
 def address_string(self):
-    if self.pool.get('log.forwarded.for.ip.installed'):
+    if self.env.get('log.forwarded.for.ip.installed'):
         # Code executed if the module log_forwarded_for_ip is installed
         if self.headers and isinstance(self.headers, dict):
             forwarded_for = self.headers.get('X-Forwarded-For', '').split(',')
