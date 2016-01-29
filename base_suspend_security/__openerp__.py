abce37075e0eb8dde8cfd348fbb138c9988b80f5
Laurent Mignon <laurent.mignon@acsone.eu>
2016-01-29 17:40:19 +0100

    [FIX] Bump version to 9.0.1.0.0

--- base_suspend_security/__openerp__.py
+++ base_suspend_security/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Suspend security",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Therp BV",
     "license": "AGPL-3",
     "category": "Hidden/Dependency",
