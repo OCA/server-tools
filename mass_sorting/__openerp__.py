184fe0b5ff6ad3fb381fa08ee54eba07e6ad9ccb
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-02-01 11:55:36 +0100

    [REF] simplify header;

--- mass_sorting/__openerp__.py
+++ mass_sorting/__openerp__.py
@@ -1,30 +1,16 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Mass Sorting Module for Odoo
-#    Copyright (C) 2016-Today GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-##############################################################################
+# Copyright (C) 2016-Today GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
 {
-    'name': "Mass Sorting",
-    'version': "1.0",
-    'author': "GRAP,Odoo Community Association (OCA)",
-    'category': "Tools",
-    'website': "http://www.grap.coop",
+    'name': 'Mass Sorting',
+    'version': '1.0',
+    'author': 'GRAP,Odoo Community Association (OCA)',
+    'category': 'Tools',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
     "description": """
 Mass Sorting
 ============
@@ -41,7 +27,7 @@ This module is highly inspired by 'mass_editing' module. (by OCA and SerpentCS)
         'base',
     ],
     'data': [
-        "security/ir.model.access.csv",
+        'security/ir.model.access.csv',
         'views/mass_sort_config_view.xml',
         'views/mass_sort_wizard_view.xml',
         'views/action.xml',
