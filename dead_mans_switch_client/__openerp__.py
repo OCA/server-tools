5dea9d45c13d19aebcd1e550a38ffd1d2e59a5b4
Holger Brunn <hbrunn@therp.nl>
2016-02-04 05:19:41 +0100

    [FIX] version number

--- dead_mans_switch_client/__openerp__.py
+++ dead_mans_switch_client/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Dead man's switch (client)",
-    "version": "1.0.0",
+    "version": "6.1.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Monitoring",
