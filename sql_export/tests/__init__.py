966e97ece5099cc94da6b021329f1cf9b7fa60bd
Florian da Costa <florian.dacosta@akretion.com>
2016-02-05 14:36:11 +0100

    Add sql export module (migration from v7)

---
+++ sql_export/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import test_sql_query
+checks = [
+    test_sql_query
+]
---
+++ sql_export/wizard/wizard_file.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 Akretion (<http://www.akretion.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields
+
+
+class SqlFileWizard(models.TransientModel):
+    _name = "sql.file.wizard"
+    _description = "Allow the user to save the file with sql request's data"
+
+    binary_file = fields.Binary('File', required=True, readonly=True)
+    file_name = fields.Char('File Name', readonly=True)
