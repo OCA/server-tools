51a25a04086bdb31b961741074c8098ba34f6711
Tony Galmiche <tony.galmiche@infosaone.com>
2016-02-06 14:36:47 +0100

    flake8 treeview2csv

--- treeview2csv/__init__.py
+++ treeview2csv/__init__.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# (c) 2016 Tony Galmiche / InfoSaône
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import controllers
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- treeview2csv/__openerp__.py
+++ treeview2csv/__openerp__.py
@@ -1,35 +1,20 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# (c) 2016 Tony Galmiche / InfoSaône
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    'name' : 'Export tree View - CSV',
-    'version' : '1.1',
-    'author' : 'ShadowErp',
-    'category' : 'web',
-    'description' : """
+    'name': 'Export tree View - CSV',
+    'version': '1.0',
+    'author': 'Tony Galmiche',
+    'category': 'Tools',
+    'description': """
 Export TreeView Directly.
     """,
-    'website': 'http://www.openerp.com',
-    'depends' : ['base'],
-    'data': ['view/web_export_treeview.xml',],
+    'maintainer': 'InfoSaône',
+    'website': 'http://www.infosaone.com',
+    'depends': ['base'],
+    'data': ['view/web_export_treeview.xml'],
+    'license': 'AGPL-3',
     'installable': True,
-    'auto_install': False,
+    'auto_install': False
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
