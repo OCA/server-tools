9e7c9c9110309730aa88c4bc6539c71faffa076f
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2016-02-08 19:13:43 +0100

    Cleanup

--- auth_password_settings/__openerp__.py
+++ auth_password_settings/__openerp__.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Dhaval Patel
-#    Copyright (C) 2011 - TODAY Denero Team. (<http://www.deneroteam.com>)
-#                  2015 - TODAY Hans Henrik Gabelgaard
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © Denero Team. (<http://www.deneroteam.com>)
+# © 2016 Hans Henrik Gabelgaard www.steingabelgaard.dk
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'User Password Settings',
     'version': '1.0',
@@ -33,21 +16,13 @@
     'depends': [
         'base', 'base_setup',
     ],
-    'images': ['static/description/auth_password_settings_config.png'],
+    'images': [
+        'static/description/auth_password_settings_config.png'
+        ],
     'data': [
-        # Datas
         "data/auth_password_settings_data.xml",
-        # Security
-
-        # Views
         "views/res_config_view.xml",
-        # Menus
-
-        # Wizards
-
-    ],
-    'demo': [
-    ],
+        ],
     'installable': True,
     'application': True,
     'auto_install': False,
