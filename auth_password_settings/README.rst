b9c4d4ed8e3a828c21ac56db33fdf9b0c2d24da5
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2016-02-08 18:08:25 +0100

    Web controllers added as some ValidationErros wasn't shown to the user

--- auth_password_settings/README.rst
+++ auth_password_settings/README.rst
@@ -45,13 +45,10 @@ Bug Tracker
 ===========
 
 Bugs are tracked on `GitHub Issues
-<https://github.com/OCA/{project_repo}/issues>`_. In case of trouble, please
+<https://github.com/OCA/server_tools/issues>`_. In case of trouble, please
 check there if your issue has already been reported. If you spotted it first,
 help us smashing it by providing a detailed and welcomed `feedback
-<https://github.com/OCA/
-{project_repo}/issues/new?body=module:%20
-{module_name}%0Aversion:%20
-{branch}%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+<https://github.com/OCA/server-tools/issues/new?body=module:%20auth_password_settings%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
 
 Credits
 =======
--- auth_password_settings/models/res_users.py
+++ auth_password_settings/models/res_users.py
@@ -1,35 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Dhaval Patel
-#    Copyright (C) 2011 - TODAY Denero Team. (<http://www.deneroteam.com>)
-#                  2015 - TODAY Hans Henrik Gabelgaard
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © Denero Team. (<http://www.deneroteam.com>)
+# © 2016 Hans Henrik Gabelgaard www.steingabelgaard.dk
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import string
 
 from openerp import models, api
 from openerp.tools.translate import _
 from openerp.exceptions import ValidationError, AccessDenied
-
-import operator
-from openerp import http
-from openerp.http import request
-from openerp.addons.web.controllers.main import Session
 
 class ResUsers(models.Model):
     _inherit = "res.users"
@@ -85,21 +62,3 @@ class ResUsers(models.Model):
         if password:
             self._validate_password(password, raise_=True)
         return super(ResUsers, self)._set_password(password)
-    
-class SessionEx(Session):
-    
-    @http.route('/web/session/change_password', type='json', auth="user")
-    def change_password(self, fields):
-        old_password, new_password,confirm_password = operator.itemgetter('old_pwd', 'new_password','confirm_pwd')(
-                dict(map(operator.itemgetter('name', 'value'), fields)))
-        if not (old_password.strip() and new_password.strip() and confirm_password.strip()):
-            return {'error':_('You cannot leave any password empty.'),'title': _('Change Password')}
-        if new_password != confirm_password:
-            return {'error': _('The new password and its confirmation must be identical.'),'title': _('Change Password')}
-        try:
-            if request.session.model('res.users').change_password(
-                old_password, new_password):
-                return {'new_password':new_password}
-        except AccessDenied:
-            return {'error': _('The old password you provided is incorrect, your password was not changed.'), 'title': _('Change Password')}
-        return {'error': _('Error, password not changed !'), 'title': _('Change Password')}
