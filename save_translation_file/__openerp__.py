54c016786bde5f40ddcbc9055e06797ba9cca9d7
Sylvain Calador <sylvain.calador@akretion.com>
2016-02-09 12:16:40 +0100

    [IMP] Add 'save_translation_file' module

---
+++ save_translation_file/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2016-TODAY Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Save translation file",
+    "summary": "Allows developpers to easily generate i18n files",
+    "version": "8.0.1.0.0",
+    "category": "Tools",
+    "website": "https://www.akretion.com/",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "save_translation_file_view.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
