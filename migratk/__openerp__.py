0b04c168ffa62cbf875754c50d96af7399b3b3b1
Jairo Llopis <j.llopis@grupoesoc.es>
2016-02-16 10:11:28 +0100

    [migratk] Migration tools collection.
    This module provides an object-oriented toolkit to perform migrations
    easily.

---
+++ migratk/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Migration Toolkit",
+    "summary": "Tools for migrating modules",
+    "version": "8.0.1.0.0",
+    "category": "Hidden",
+    "website": "https://grupoesoc.es",
+    "author": "Antiun Ingeniería S.L., "
+              "Grupo ESOC Ingeniería de Servicios, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": False,
+    "external_dependencies": {
+        "python": ["psycopg2"],
+    },
+    "depends": [
+        "base",
+    ],
+}
