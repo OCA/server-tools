d03cc62a2a885e97a8b7be502ae2c8f1c253fbe3
David Beal <david.beal@akretion.com>
2016-02-23 21:51:34 +0100

    [FIX] python header in akretion modules

--- file_email/attachment_metadata.py
+++ file_email/attachment_metadata.py
@@ -1,24 +1,6 @@
-# -*- coding: utf-8 -*-
-###############################################################################
-#
-#   file_email for OpenERP
-#   Copyright (C) 2012-TODAY Akretion <http://www.akretion.com>.
+# coding: utf-8
 #   @author Sébastien BEAU <sebastien.beau@akretion.com>
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api
 import base64
@@ -63,8 +45,8 @@ class IrAttachmentMetadata(models.Model):
     @api.model
     def prepare_data_from_basic_condition(self, condition, msg):
         vals = {}
-        if (condition.from_email in msg['from']
-                and condition.mail_subject in msg['subject']):
+        if (condition.from_email in msg['from'] and
+                condition.mail_subject in msg['subject']):
             for att in msg['attachments']:
                 if condition.file_extension in att[0]:
                     vals = self._get_attachment_metadata_data(condition,
