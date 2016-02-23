85ff3ccccd601c8bbba900b37e6e270574614143
David Beal <david.beal@akretion.com>
2016-02-23 21:51:34 +0100

    [FIX] python header in akretion modules

--- external_file_location/helper.py
+++ external_file_location/helper.py
@@ -1,23 +1,7 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
+# coding: utf-8
 # Author: Joel Grand-Guillaume
 # Copyright 2011-2012 Camptocamp SA
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
 def itersubclasses(cls, _seen=None):
