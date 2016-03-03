f627d90d525eebcfbcc855662d126930d07bd279
Jairo Llopis <yajo.sk8@gmail.com>
2016-03-03 16:37:56 +0100

    Remove wrong copyright.

--- base_multi_image/models/__init__.py
+++ base_multi_image/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2009 Sharoon Thomas Open Labs Business Solutions
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
