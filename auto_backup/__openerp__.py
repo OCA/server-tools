0b110c3d592b9068df36cbd006c829df611dd134
Jairo Llopis <j.llopis@grupoesoc.es>
2016-03-09 09:21:23 +0100

    [auto_backup] Refactor.
    - Follow template README.
    - Remove HTML README.
    - Move models to models folder.
    - Model and view file names follow guidelines.
    - Unused methods cleanup.
    - Remove unneeded `.pot` file.
    - Fix permissons.
    - Follow PEP8 in names everywhere.
    - Set more descriptive field names.
    - Disable backups for other databases, for security.
    - Remove db name from generated file, for easier cleanup.
    - EAFP logic everywhere.
    - More descriptive name.
    - Data files moved to YAML, with cleaner ir.cron record creation.
    - Add permissions for db.backup model.
    - Icons.
    - Update tests with new format.
    - Storage method is a selectable, for easier extensibility.
    - Instead of custom mailing, it just has a mail thread where you can subscribe.
    - Should fix almost all comments in https://github.com/OCA/server-tools/pull/203.
    - Reduce headers.
    
    This respects the upstream license choice (GPL/AGPL) but reduces
    verbosity.
    
    It would be ideal to have everything under AGPL though.

--- auto_backup/__openerp__.py
+++ auto_backup/__openerp__.py
@@ -1,43 +1,31 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    $Id$
-#    Copyright (C) 2015 Agile Business Group <http://www.agilebg.com>
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2004-2009 Tiny SPRL (<http://tiny.be>).
+# © 2015 Agile Business Group <http://www.agilebg.com>
+# © 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
 {
     "name": "Database Auto-Backup",
+    "summary": "Backups database",
     "version": "8.0.1.0.0",
     "author": (
         "VanRoey.be - Yenthe Van Ginneken, Agile Business Group,"
+        " Grupo ESOC Ingeniería de Servicios,"
         " Odoo Community Association (OCA)"
     ),
     'license': "AGPL-3",
     "website": "http://www.vanroey.be/applications/bedrijfsbeheer/odoo",
     "category": "Tools",
-    "summary": "Backups data base",
     "depends": ['email_template'],
     "demo": [],
     "data": [
-        "view/bkp_conf_view.xml",
-        "data/backup_data.xml",
-        "security/ir.model.access.csv"
+        "data/backup_data.yml",
+        "security/ir.model.access.csv",
+        "view/db_backup_view.xml",
     ],
     "application": True,
-    "installable": True
+    "installable": True,
+    "external_dependencies": {
+        "python": ["pysftp"],
+    },
 }
--- auto_backup/tests/test_auto_backup.py
+++ auto_backup/tests/test_auto_backup.py
@@ -1,56 +1,28 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright 2015 Agile Business Group <http://www.agilebg.com>
-#    Copyright (C) 2015 Alessio Gerace <alesiso.gerace@agilebg.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Agile Business Group <http://www.agilebg.com>
+# © 2015 Alessio Gerace <alesiso.gerace@agilebg.com>
+# © 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
-from openerp.exceptions import except_orm
 import os
-import time
+from datetime import datetime
+from openerp.tests import common
 
 
 class TestsAutoBackup(common.TransactionCase):
 
     def setUp(self):
         super(TestsAutoBackup, self).setUp()
-        self.abk_model = self.env["db.backup"]
-        self.cron_model = self.env["ir.cron"]
-
-    def test_0(self):
-        with self.assertRaises(except_orm):
-            self.abk_model.create(
-                {
-                    'name': 'abcd',
-                    'adminpassword': 'admin'
-                }
-            )
-
-    def test_1(self):
-        this = self.abk_model.create(
+        self.abk = self.env["db.backup"].create(
             {
-                'bkp_dir': '/tmp'
+                'name': u'Têst backup',
             }
         )
-        self.assertEqual(this.bkp_dir, '/tmp')
-        bkp_file = '%s_%s.dump.zip' % (
-            time.strftime('%d_%m_%Y_%H_%M_%S'),
-            this.name)
-        file_path = os.path.join(this.bkp_dir, bkp_file)
-        this.schedule_backup()
-        self.assertTrue(os.path.isfile(file_path))
+
+    def test_local(self):
+        """A local database is backed up."""
+        filename = self.abk.filename(datetime.now())
+        self.abk.action_backup()
+        generated_backup = [f for f in os.listdir(self.abk.folder)
+                            if f >= filename]
+        self.assertEqual(len(generated_backup), 1)
