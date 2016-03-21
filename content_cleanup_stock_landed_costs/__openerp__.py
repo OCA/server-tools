6e16ca1d07fc9f15e4619babd8556ae857308053
Mike Telahun <miket@clearict.com>
2016-03-21 09:31:29 +0300

    Additional db content cleanup modules for: account_asset, hr_timesheet, stock_landed_costs
    o content_cleanup_account_asset - remove asset account valuation records
    o content_cleanup_hr_timesheet - remove timesheet analytica accounting records
    o content_cleanup_stock_landed_costs - remove stock landed costs related records

---
+++ content_cleanup_stock_landed_costs/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2016 Clear ICT Solutions (<info@clearict.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Database content cleanup - Stock Landed Costs',
+    'summary': 'Remove stock landed costs related content from the database',
+    'version': '8.0',
+    'author': "Clear ICT Solutions,Odoo Community Association (OCA)",
+    'depends': [
+        'content_cleanup',
+        'stock_landed_costs',
+    ],
+    'license': 'AGPL-3',
+    'category': 'Tools',
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
