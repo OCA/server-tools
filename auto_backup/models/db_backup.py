b2ae0fe0de53f9d1a0c5d0d86aca4967964f9bf2
archetipo <alessio.gerace@gmail.com>
2016-03-22 17:01:27 +0100

    FIX License type

--- auto_backup/models/db_backup.py
+++ auto_backup/models/db_backup.py
@@ -2,7 +2,7 @@
 # © 2004-2009 Tiny SPRL (<http://tiny.be>).
 # © 2015 Agile Business Group <http://www.agilebg.com>
 # © 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
-# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
 import os
 import shutil
