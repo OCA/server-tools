04995d84b962fb989f129a73a46018efdab809c9
Atchuthan, Sodexis <atchuthan@sodexis.com>
2016-03-30 15:49:45 +0530

    [MIG][9.0] base_multi_image

--- base_multi_image/__openerp__.py
+++ base_multi_image/__openerp__.py
@@ -2,14 +2,15 @@
 # © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Multiple images base",
     "summary": "Allow multiple images for database objects",
-    "version": "8.0.2.0.0",
+    "version": "9.0.1.0.0",
     "author": "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
-              "Antiun Ingeniería, S.L., "
+              "Antiun Ingeniería, S.L., Sodexis, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "http://www.antiun.com",
