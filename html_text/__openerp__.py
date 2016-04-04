5a1a82a26e959b09bbced3067498e683c64038a2
Yajo <Yajo@users.noreply.github.com>
2016-04-04 16:58:49 +0200

    Excerpt generator.
    * [8.0][html_text] Excerpt generator.
    
    This module adds a technical utility to allow the developer to get an excerpt from any HTML chunk.
    
    You can choose the maximum amount of words or characters if you want.
    
    See the README and inline docstrings for further details.

---
+++ html_text/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Text from HTML field",
+    "summary": "Generate excerpts from any HTML field",
+    "version": "8.0.1.0.0",
+    "category": "Tools",
+    "website": "https://grupoesoc.es",
+    "author": "Grupo ESOC Ingeniería de Servicios, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [
+            "lxml.html",
+        ],
+    },
+    "depends": [
+        "base",
+    ],
+}
---
+++ html_text/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import ir_fields_converter
