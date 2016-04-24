9dc090fe2c685b87f4ee580acd1ab5bef3a55af0
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2016-04-24 11:55:00 +0200

    Pylint: Version number

--- auth_password_settings/__openerp__.py
+++ auth_password_settings/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'User Password Settings',
-    'version': '8.0.1.0',
+    'version': '8.0.1.0.0',
     'category': 'tools',
     'sequence': 3,
     'summary': 'User Password Settings',
