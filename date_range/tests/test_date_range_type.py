52712d4088c0690612b9d4cce1cf6a0dda0dfdaf
Laurent Mignon <laurent.mignon@acsone.eu>
2016-04-28 09:46:23 +0200

    [PYLINT] remove unused import

--- date_range/tests/test_date_range_type.py
+++ date_range/tests/test_date_range_type.py
@@ -3,7 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp.tests.common import TransactionCase
-from openerp.exceptions import ValidationError
 
 
 class DateRangeTypeTest(TransactionCase):
