d2b1272aa089211dd721f73fc2bb32b1c8b3f6f9
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-04-29 11:52:01 +0200

    9.0 add date range Sorrento Delivery
    * [ADD] Basic structure for the new date range module
    
    * [IMP] Add a basic description into the README
    
    * [IMP] Basic implementation
    
    * [IMP] First working implementation
    
    * [IMP] Improve datamodel
    
    * [ADD] Add basic tests for date.range
    
    * [PEP8]
    
    * [PYLINT]
    
    * [DEL] Remove unused code
    
    * [IMP] Remove unsused dependencies into the JS
    
    * [IMP] Better operator label for date range
    
    * [DEL] Remove unused file
    
    * [IMP] Better user experience by showing the select input only once empty
    
    * [FIX]Try to fix tests that fails only on travis by adding an explicit cast on the daterange methods parameters
    
    * [FIX]Try to fix tests that fails only on travis by adding an explicit cast on the daterange methods parameters
    
    * [FIX]Try to fix tests that fails only on travis by using postgresql 9.4
    
    * [FIX]Try with postgresql 9.2 since the daterange method has appeared in 9.2
    
    * [IMP] Add a limitation into the module description to warm about the minimal version of postgresql to use
    
    * [IMP]Add multi-company rules
    
    * [IMP]Remove unused files
    
    * [FIX] Add missing brackets into JS
    
    * [FIX] Overlap detection when company_id is False
    
    * [IMP] Add default order for date.range
    
    * [IMP] Add date range generator
    
    * [FIX] OE compatibility
    
    * [FIX] Travis
    
    * [IMP] Code cleanup and improves test coverage
    
    * [FIX] Add missing dependency on 'web'
    
    * [PYLINT] remove unused import
    
    * [FIX] Add missing copyright
    
    * [FIX] Limits are included into the range
    
    * [IMP][date_range] Security
    
    * [IMP] Improve module description
    
    * [IMP] Spelling

---
+++ date_range/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Date Range",
+    "summary": "Manage all kind of date range",
+    "version": "9.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://odoo-community.org/",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/date_range_security.xml",
+        "views/assets.xml",
+        "views/date_range_view.xml",
+        "wizard/date_range_generator.xml",
+    ],
+    "qweb": [
+        "static/src/xml/date_range.xml",
+    ]
+}
