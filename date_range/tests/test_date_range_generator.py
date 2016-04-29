cbc3d54163e1b4678e4aabd3369744cb2d55bdcd
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-04-29 11:52:01 +0200

    9.0 add date range Sorrento Delivery
    * [ADD] Basic structure for the new date range module
    
    * [IMP] Add a basic description into the README
    
    * [IMP] Basic implementation
    
    * [IMP] First working implementation
    
    * [IMP] Improve datamodel
    
    * [ADD] Add basic tests for date.range
    
    * [PEP8]
    
    * [PYLINT]
    
    * [DEL] Remove unused code
    
    * [IMP] Remove unsused dependencies into the JS
    
    * [IMP] Better operator label for date range
    
    * [DEL] Remove unused file
    
    * [IMP] Better user experience by showing the select input only once empty
    
    * [FIX]Try to fix tests that fails only on travis by adding an explicit cast on the daterange methods parameters
    
    * [FIX]Try to fix tests that fails only on travis by adding an explicit cast on the daterange methods parameters
    
    * [FIX]Try to fix tests that fails only on travis by using postgresql 9.4
    
    * [FIX]Try with postgresql 9.2 since the daterange method has appeared in 9.2
    
    * [IMP] Add a limitation into the module description to warm about the minimal version of postgresql to use
    
    * [IMP]Add multi-company rules
    
    * [IMP]Remove unused files
    
    * [FIX] Add missing brackets into JS
    
    * [FIX] Overlap detection when company_id is False
    
    * [IMP] Add default order for date.range
    
    * [IMP] Add date range generator
    
    * [FIX] OE compatibility
    
    * [FIX] Travis
    
    * [IMP] Code cleanup and improves test coverage
    
    * [FIX] Add missing dependency on 'web'
    
    * [PYLINT] remove unused import
    
    * [FIX] Add missing copyright
    
    * [FIX] Limits are included into the range
    
    * [IMP][date_range] Security
    
    * [IMP] Improve module description
    
    * [IMP] Spelling

---
+++ date_range/tests/test_date_range_generator.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)nses/agpl).
+
+from openerp.tests.common import TransactionCase
+from dateutil.rrule import MONTHLY
+
+
+class DateRangeGeneratorTest(TransactionCase):
+
+    def setUp(self):
+        super(DateRangeGeneratorTest, self).setUp()
+        self.type = self.env['date.range.type'].create(
+            {'name': 'Fiscal year',
+             'company_id': False,
+             'allow_overlap': False})
+
+    def test_generate(self):
+        generator = self.env['date.range.generator']
+        generator = generator.create({
+            'date_start': '1943-01-01',
+            'name_prefix': '1943-',
+            'type_id': self.type.id,
+            'duration_count': 3,
+            'unit_of_time': MONTHLY,
+            'count': 4})
+        generator.action_apply()
+        ranges = self.env['date.range'].search(
+            [('type_id', '=', self.type.id)])
+        self.assertEquals(len(ranges), 4)
+        range4 = ranges[3]
+        self.assertEqual(range4.date_start, '1943-10-01')
+        self.assertEqual(range4.date_end, '1943-12-31')
+        self.assertEqual(range4.type_id, self.type)
