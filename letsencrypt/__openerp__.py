bfab104054953f2a7cf9bc8e8ed584fadc4caca0
Dave Lasley <dave@laslabs.com>
2016-05-17 13:45:59 -0700

    Migrate letsencrypt to v9

--- letsencrypt/__openerp__.py
+++ letsencrypt/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Let's encrypt",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Therp BV,"
               "Tecnativa,"
               "Odoo Community Association (OCA)",
