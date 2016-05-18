ecf9ba7281200343c30aeb05eb1efc730d23cc21
Jesus Alan Ramos Rodriguez <alan.ramos@jarsa.com.mx>
2016-05-18 12:53:36 -0500

    [FIX] Fix pylint errors

--- auth_signup_verify_email/static/src/js/test_main.js
+++ auth_signup_verify_email/static/src/js/test_main.js
@@ -1,29 +1,29 @@
 odoo.define('auth_signup_verify_email.tour_register_with_email', function (require) {
-	'use strict';
-	var core = require('web.core');
-	var Tour = require('web.Tour');
-	var _t = core._t;
+    'use strict';
+    var core = require('web.core');
+    var Tour = require('web.Tour');
+    var _t = core._t;
 
-	Tour.register({
-		id: 'register_with_email',
-		name: _t('Register with email'),
-		path: '/web/signup',
-		mode: 'test',
-		steps: [
-			{
-				title: 'Input email',
-				element: 'input[name="login"][value="john@got.com"]',
-				sampleText:'john@got.com',
-			},
-			{
-				title: 'Input name',
-				element: 'input[name="name"][value="John Snow"]',
-				sampleText:'John Snow',
-			},
-			{
-				title: 'Click Sign up button',
-				element: 'button.btn.btn-primary.pull-left',
-			},
-		],
-	});
-};
+    Tour.register({
+        id: 'register_with_email',
+        name: _t('Register with email'),
+        path: '/web/signup',
+        mode: 'test',
+        steps: [
+            {
+                title: 'Input email',
+                element: 'input[name="login"][value="john@got.com"]',
+                sampleText:'john@got.com',
+            },
+            {
+                title: 'Input name',
+                element: 'input[name="name"][value="John Snow"]',
+                sampleText:'John Snow',
+            },
+            {
+                title: 'Click Sign up button',
+                element: 'button.btn.btn-primary.pull-left',
+            },
+        ],
+    });
+});
