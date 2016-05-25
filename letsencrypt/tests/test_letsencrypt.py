3e30d0ee2e122f012b4cc5df62c9067da998721e
Dave Lasley <dave@laslabs.com>
2016-05-25 09:24:59 -0700

    [ADD] letsencrypt (#425)
    * [ADD] letsencrypt (#347)
    
    * [ADD] letsencrypt
    
    * [ADD] write bogus restart script for tests
    
    * [IMP] exclude library call from coveralls
    
    * [IMP] try moving the library import into nocover branch
    
    * [ADD] explain how to redirect the well known uri to the odoo instance
    
    * [ADD] example for apache
    
    * [FIX] cronjob should be noupdate
    
    * [FIX] community review
    
    * [FIX] flake8
    
    * [DEL] unused imports
    
    * [UPD] chain cert
    
    * Multi-database support and other fixes (#2)
    
    [ADD] multi-database support and other fixes
    
    * [ADD] eggs necessary for letsencrypt
    
    * [IMP] readme
    
    * [ADD] ipv6 localhosts
    
    * [ADD] restrict reload command
    
    * Revert "[ADD] eggs necessary for letsencrypt"
    
    This reverts commit 642df6ba50c319dff3c2e546034c14329c2443e0.
    
    * [ADD] eggs necessary for letsencrypt
    
    Conflicts:
    	requirements.txt
    
    * Migrate letsencrypt to v9
    
    * Add AGPL target link to ReadMe in letsencrypt

---
+++ letsencrypt/tests/test_letsencrypt.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp.tests.common import TransactionCase
+
+
+class TestLetsencrypt(TransactionCase):
+    def test_letsencrypt(self):
+        from ..hooks import post_init_hook
+        post_init_hook(self.cr, None)
+        self.env.ref('letsencrypt.config_parameter_reload').write({
+            'value': '',
+        })
+        self.env['letsencrypt'].with_context(letsencrypt_dry_run=True).cron()
