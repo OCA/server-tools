6e0fd03080d4026339f9e2c5e7116eba142e87a3
Jairo Llopis <yajo.sk8@gmail.com>
2016-05-26 16:33:03 +0200

    [9.0][MIG][base_custom_info] Migration.
    Mostly no changes from 8.0 after all.

--- base_custom_info/__openerp__.py
+++ base_custom_info/__openerp__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # © 2015 Antiun Ingeniería S.L. - Sergio Teruel
 # © 2015 Antiun Ingeniería S.L. - Carlos Dauden
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2015-2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': "Base Custom Info",
     'summary': "Add custom field in models",
     'category': 'Tools',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'depends': [
         'base',
     ],
@@ -27,6 +27,7 @@
     ],
     'author': 'Antiun Ingeniería S.L., '
               'Incaser Informatica S.L., '
+              'Tecnativa, '
               'Odoo Community Association (OCA)',
     'website': 'http://www.antiun.com',
     'license': 'AGPL-3',
