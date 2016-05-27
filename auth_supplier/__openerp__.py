5b7c2eaeac495cf0f767bfa0e81b226b6514d468
Yajo <Yajo@users.noreply.github.com>
2016-05-27 19:16:51 +0200

    [MIG][9.0][auth_supplier] Migrate. (#434)
    * [ADD] auth_supplier: New Module.
    
    * [ADD] auth_supplier: New Module.
    
    * [FIX] auth_supplier: Change author order.
    
    * [MIG][9.0][auth_supplier] Migrate.
    
    Update to v9, with mostly cosmetic changes.
    
    * Update XML tags.
    
    * Inform about the need to have technical features enabled.
    
    * Don't assume website is installed.
    
    * Add known issue

---
+++ auth_supplier/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
+# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': "Auth Supplier",
+    'category': 'Tools',
+    'version': '9.0.1.0.0',
+    'depends': [
+        'auth_signup',
+    ],
+    'data': [
+        'security/auth_supplier_security.xml',
+        'views/auth_supplier_view.xml',
+    ],
+    'author': 'Antiun Ingeniería S.L., '
+              'Incaser Informatica S.L., '
+              "Tecnativa, "
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.incaser.es',
+    'license': 'AGPL-3',
+    'installable': True,
+}
