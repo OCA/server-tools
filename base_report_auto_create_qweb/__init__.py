3b546e79df5cc092d65868e06e1eaf99ec4d77e5
Damien Crier <damien.crier@camptocamp.com>
2016-06-02 11:29:39 +0200

    [FIX] year and copyright

--- base_report_auto_create_qweb/__init__.py
+++ base_report_auto_create_qweb/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# Authors: See README.RST
-# Copyright 2016 See README.rst for Authors
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
 from . import wizard
