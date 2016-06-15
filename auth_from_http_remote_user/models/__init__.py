b02f98f849fcdc5f7dae46dc01f4d9f993f67ab5
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-06-15 16:27:52 +0200

    [MIG] Migration from 8.0

--- auth_from_http_remote_user/models/__init__.py
+++ auth_from_http_remote_user/models/__init__.py
@@ -2,5 +2,5 @@
 # Copyright 2014 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import controllers
-from . import models
+from . import res_users
+from . import auth_from_http_remote_user
