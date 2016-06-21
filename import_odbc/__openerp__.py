d7fa4fef3583e5efa1d4b9f7ee523d12d4bbe3bf
Luis Felipe Miléo <mileo@kmee.com.br>
2016-06-21 11:05:31 -0300

    Make module installable
    As mentioned in migration  https://github.com/OCA/server-tools/issues/168 and pr https://github.com/OCA/server-tools/pull/41, This module should be installable.

--- import_odbc/__openerp__.py
+++ import_odbc/__openerp__.py
@@ -95,8 +95,8 @@ Contributors
         'import_odbc_demo.xml',
     ],
     'test': [],
-    'installable': False,
-    'active': False,
+    'installable': True,
+    'auto_install': False,
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
