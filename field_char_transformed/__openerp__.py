7b28d64f29309f6aa38b91d4213dd78013675836
Holger Brunn <hbrunn@therp.nl>
2016-06-27 12:53:08 +0200

    [ADD] field_char_transformed

---
+++ field_char_transformed/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Transform data on character fields",
+    "version": "8.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Allows to transform input in character fields before writing"
+    " or reading it to/from the database",
+    "depends": [
+        'base',
+    ],
+    "data": [
+    ],
+}
