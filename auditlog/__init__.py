3cffd977abcf0c1f28f4ee32a9dfa8d55083a8a3
Holden Rehg <holdenrehg@gmail.com>
2016-06-27 09:23:47 -0500

    auditlog: Remove migration from audittrail functionality, this is not required for version 9

--- auditlog/__init__.py
+++ auditlog/__init__.py
@@ -3,25 +3,3 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
-
-
-def pre_init_hook(cr):
-    cr.execute("SELECT 1 FROM pg_class WHERE relname = 'audittrail_rule'")
-    if cr.fetchall():
-        migrate_from_audittrail(cr)
-
-
-def migrate_from_audittrail(cr):
-    cr.execute('ALTER TABLE audittrail_rule RENAME TO auditlog_rule')
-    cr.execute('ALTER TABLE audittrail_rule_id_seq '
-               'RENAME TO auditlog_rule_id_seq')
-    cr.execute('ALTER TABLE auditlog_rule RENAME COLUMN object_id TO model_id')
-    cr.execute('ALTER TABLE audittrail_log RENAME TO auditlog_log')
-    cr.execute('ALTER TABLE audittrail_log_id_seq '
-               'RENAME TO auditlog_log_id_seq')
-    cr.execute('ALTER TABLE auditlog_log RENAME COLUMN object_id TO model_id')
-    cr.execute('ALTER TABLE audittrail_log_line RENAME TO auditlog_log_line')
-    cr.execute('ALTER TABLE audittrail_log_line_id_seq '
-               'RENAME TO auditlog_log_line_id_seq')
-    cr.execute("UPDATE ir_model_data SET model='auditlog.rule' "
-               "WHERE model='audittrail.rule'")
