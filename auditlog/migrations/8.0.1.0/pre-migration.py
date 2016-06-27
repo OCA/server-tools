9fdbbcacc4e5443e1d31a535f264bf6db4d4fa04
Holden Rehg <holdenrehg@gmail.com>
2016-06-27 09:23:47 -0500

    auditlog: Remove migration from audittrail functionality, this is not required for version 9

--- auditlog/migrations/8.0.1.0/pre-migration.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp.addons.auditlog import migrate_from_audittrail
-
-
-def migrate(cr):
-    """if we migrate from an older version, it's a migration from audittrail"""
-    migrate_from_audittrail(cr)
