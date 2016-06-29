24c919abbfd8dc7377a7cc73f93078c023030388
Holger Brunn <hbrunn@therp.nl>
2016-06-29 16:37:51 +0200

    [ADD] migrate database_cleanup
    [ADD] test purging modules
    
    [ADD] test purging tables

--- database_cleanup/tests/__init__.py
+++ database_cleanup/tests/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 Therp BV <http://therp.nl>
+# © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import models
+from . import test_database_cleanup
