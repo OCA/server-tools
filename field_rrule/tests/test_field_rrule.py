12a127cd12318915a4341473b53d33418a1a32b2
Holger Brunn <hbrunn@therp.nl>
2016-07-12 20:55:55 +0200

    [ADD] field_rrule

---
+++ field_rrule/tests/test_field_rrule.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp.tests.common import TransactionCase
+
+
+class TestFieldRrule(TransactionCase):
+    def test_field_rrule(self):
+        pass
