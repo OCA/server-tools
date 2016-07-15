f8f95bc9b4a10c070aca95402263b5cdb3e75a35
Holger Brunn <hbrunn@therp.nl>
2016-07-15 23:07:14 +0200

    [ADD] field_char_transformed (#466)

---
+++ field_char_transformed/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from .field_char_transformed import FieldCharTransformed
+from .field_char_stripped import FieldCharStripped
