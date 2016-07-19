c0a8b8e21de07595c7b3bd9cb85a1bfe97a75770
Holger Brunn <hbrunn@therp.nl>
2016-07-19 18:35:58 +0200

    [FIX] defuse flake8

--- field_rrule/hooks.py
+++ field_rrule/hooks.py
@@ -8,7 +8,7 @@ import inspect
 def post_load_hook():
     """do some trickery to have demo data/model on runbot, but nowhere else"""
     if socket.getfqdn().endswith('odoo-community.org'):  # pragma: nocover
-        from . import demo
+        from . import demo  # flake8: noqa
         for frame, filename, lineno, funcname, line, index in inspect.stack():
             if 'package' in frame.f_locals:
                 frame.f_locals['package'].info['demo'] =\
