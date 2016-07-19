2b32875b3f9653397801fd52992f3e158708b03d
Holger Brunn <hbrunn@therp.nl>
2016-07-19 17:28:41 +0200

    [ADD] try if we can have our cake and eat it

--- field_rrule/demo/__init__.py
+++ field_rrule/demo/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-import socket
-from .field_rrule import FieldRRule
-if socket.getfqdn().endswith('odoo-community.org'):
-    import demo
+from . import res_partner
