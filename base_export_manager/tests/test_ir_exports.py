77a44408dc62946e182466a11d06cff958de9271
Jairo Llopis <yajo.sk8@gmail.com>
2016-07-25 18:00:58 +0200

    Hardcode constraints in the `create` method instead of using normal ones.
    Depending on the context where the record is created, it gets `resource` or `model_id`. The problem is that Odoo checks constrains before inverses, so constrains would fail always.
    
    Test added to ensure future versions contemplate all use cases.

--- base_export_manager/tests/test_ir_exports.py
+++ base_export_manager/tests/test_ir_exports.py
@@ -2,6 +2,7 @@
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from openerp.exceptions import ValidationError
 from openerp.tests.common import TransactionCase
 
 
@@ -26,3 +27,28 @@ class TestIrExportsCase(TransactionCase):
         record = self.env["ir.exports"].create(data)
 
         self.assertEqual(record.model_id.model, data["resource"])
+
+    def test_create_without_model(self):
+        """Creating a record without ``model_id`` and ``resource`` fails."""
+        IrExports = self.env["ir.exports"]
+        model = IrExports._get_model_id("res.partner")
+
+        # Creating with resource
+        record = IrExports.create({
+            "name": "some",
+            "resource": model.model,
+        })
+        self.assertEqual(record.model_id, model)
+
+        # Creating with model_id
+        record = IrExports.create({
+            "name": "some",
+            "model_id": model.id,
+        })
+        self.assertEqual(record.resource, model.model)
+
+        # Creating without anyone
+        with self.assertRaises(ValidationError):
+            IrExports.create({
+                "name": "some",
+            })
