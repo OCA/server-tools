ff1896eab3b939ea9b52fcd17c3ff3b168e8ab4c
Florian da Costa <florian.dacosta@akretion.com>
2016-08-01 13:26:02 +0200

    Migrate and rename attachment_metadata to attachment_base_synchronize

--- attachment_base_synchronize/__openerp__.py
+++ attachment_base_synchronize/__openerp__.py
@@ -1,19 +1,21 @@
 # coding: utf-8
-# @ 2015 Valentin CHEMIERE @ Akretion
+# @ 2015 Florian DA COSTA @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Attachment Metadata',
-    'version': '8.0.1.0.0',
+    'name': 'Attachment Base Synchronize',
+    'version': '9.0.1.0.0',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'www.akretion.com',
     'license': 'AGPL-3',
     'category': 'Generic Modules',
     'depends': [
+        'base',
     ],
     'data': [
         'views/attachment_view.xml',
         'security/ir.model.access.csv',
+        'data/cron.xml',
     ],
     'installable': True,
     'application': False,
