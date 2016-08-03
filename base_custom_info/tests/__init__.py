b1525a515d441942c4f2670f9717ef1a01815deb
Jairo Llopis <yajo.sk8@gmail.com>
2016-08-03 17:18:46 +0200

    [IMP]base_custom_info: New types and access rules system.
    * Now you can define properties types, and access rules are inherited from the model/record
      linked to the custom info record.
    * Simplified version of computed value.
    * Implement for res.partner.
    * Add tests and fix bugs discovered in the meantime.
    * Allow to disable partner custom info tab, and custom info menu.
    * All of it can be set within general settings.
    * Now, by default, this module does not display custom info for partners unless in demo mode.
      Better fit for a base module.
    * You can disable the top menu entry too if it disturbs you, or enable it for everybody.
    * Give a special form when editing in partner custom info tab.
    * Sortable properties.
    * Sort values at onchange time.
    * Improve performance in onchange.
    * Split in several model files.

---
+++ base_custom_info/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import test_partner, test_value_conversion
---
+++ base_custom_info/wizard/base_config_settings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+from openerp import fields, models
+
+
+class BaseConfigSettings(models.TransientModel):
+    _inherit = "base.config.settings"
+
+    group_custom_info_manager = fields.Boolean(
+        string="Manage custom information",
+        implied_group="base_custom_info.group_basic",
+        help="Allow all employees to manage custom information",
+    )
+    group_custom_info_partner = fields.Boolean(
+        string="Edit custom information in partners",
+        implied_group="base_custom_info.group_partner",
+        help="Add a tab in the partners form to edit custom information",
+    )
