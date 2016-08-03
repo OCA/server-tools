8e0fbbee8c050d5413463a2d03718bcf2fc79f3d
Jairo Llopis <yajo.sk8@gmail.com>
2016-08-03 17:18:46 +0200

    [IMP]base_custom_info: New types and access rules system.
    * Now you can define properties types, and access rules are inherited from the model/record
      linked to the custom info record.
    * Simplified version of computed value.
    * Implement for res.partner.
    * Add tests and fix bugs discovered in the meantime.
    * Allow to disable partner custom info tab, and custom info menu.
    * All of it can be set within general settings.
    * Now, by default, this module does not display custom info for partners unless in demo mode.
      Better fit for a base module.
    * You can disable the top menu entry too if it disturbs you, or enable it for everybody.
    * Give a special form when editing in partner custom info tab.
    * Sortable properties.
    * Sort values at onchange time.
    * Improve performance in onchange.
    * Split in several model files.

---
+++ base_custom_info/models/res_partner.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    """Implement custom information for partners.
+
+    Besides adding some visible feature to the module, this is useful for
+    testing and example purposes.
+    """
+    _name = "res.partner"
+    _inherit = [_name, "custom.info"]
+
+    custom_info_template_id = fields.Many2one(
+        context={"default_model": _name},
+    )
+    custom_info_ids = fields.One2many(
+        context={"default_model": _name},
+    )
