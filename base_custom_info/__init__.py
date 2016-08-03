1a30d8514c6100663642d444ba56efbefd2c75a7
Jairo Llopis <yajo.sk8@gmail.com>
2016-08-03 13:00:30 +0200

    [CHG] base_custom_info: Relicense to LGPL with all authors' consent
    # Conflicts:
    #	base_custom_info/__manifest__.py

--- base_custom_info/__init__.py
+++ base_custom_info/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015 Antiun Ingeniería S.L. - Sergio Teruel
 # © 2015 Antiun Ingeniería S.L. - Carlos Dauden
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
 from . import models
