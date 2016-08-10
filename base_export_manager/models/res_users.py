cfe4984888dd7a2e1aa3158e096958b457c04c85
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-08-10 09:10:43 -0500

    [FIX] PEP8 errors

--- base_export_manager/models/res_users.py
+++ base_export_manager/models/res_users.py
@@ -8,21 +8,24 @@ from openerp import api, models
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
-    
+
     @api.v7
     def get_export_models(self, cr, uid):
         return self.fetch_export_models(cr, uid)
-    
+
     @api.v8
     def get_export_models(self):
         uid = self.id or self.env.uid
         return self.fetch_export_models(self.env.cr, uid)
-    
+
     def fetch_export_models(self, cr, uid):
         groups_id = [group.id for group in self.browse(cr, uid, uid).groups_id]
         accessobj = self.pool['ir.model.access']
-        accessobj_ids = accessobj.search(cr, uid, [('perm_export','=',True),('group_id','in',groups_id)])
-        model_names = [access_obj.model_id.model for access_obj in accessobj.browse(cr, uid, accessobj_ids)]
-        #make distinct value in list
+        accessobj_ids = accessobj.search(cr, uid, [('perm_export', '=', True),
+                                                   ('group_id', 'in',
+                                                    groups_id)])
+        model_names = [access_obj.model_id.model for access_obj in
+                       accessobj.browse(cr, uid, accessobj_ids)]
+        # Make distinct value in list
         model_names = list(set(model_names))
         return model_names
