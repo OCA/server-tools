512ce840d02ef33948eb58d133fabfab19e64edd
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2016-08-23 19:36:26 +0530

    Restored OLD Mass Editing Module

--- mass_editing/models/ir_model_fields.py
+++ mass_editing/models/ir_model_fields.py
@@ -20,22 +20,26 @@
 #
 ##############################################################################
 
-from openerp import api, models
+from openerp.osv import orm
 
 
-class IrModelFields(models.Model):
+class IrModelFields(orm.Model):
     _inherit = 'ir.model.fields'
 
-    @api.model
-    def search(self, args, offset=0, limit=0, order=None, count=False):
+    def search(
+            self, cr, uid, args, offset=0, limit=0, order=None, context=None,
+            count=False):
         model_domain = []
         for domain in args:
-            if (len(domain) > 2 and domain[0] == 'model_id' and
+            if (len(domain) > 2 and
+                    domain[0] == 'model_id' and
                     isinstance(domain[2], basestring)):
-                model_domain += [('model_id', 'in',
-                                  map(int, domain[2][1:-1].split(',')))]
+                model_domain += [
+                    ('model_id', 'in', map(int, domain[2][1:-1].split(',')))
+                ]
             else:
                 model_domain.append(domain)
-        return super(IrModelFields, self).search(model_domain, offset=offset,
-                                                 limit=limit, order=order,
-                                                 count=count)
+        return super(IrModelFields, self).search(
+            cr, uid, model_domain, offset=offset, limit=limit, order=order,
+            context=context, count=count
+        )
