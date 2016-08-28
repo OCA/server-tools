bd5c026f38129517da1c7d7b43117b4eee990100
Dave Lasley <dave@laslabs.com>
2016-08-27 17:49:08 -0700

    Fix copyright, wrong year for new file

--- res_users_password_security/models/res_users_pass_history.py
+++ res_users_password_security/models/res_users_pass_history.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 LasLabs Inc.
+# Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from openerp import fields, models
