222377f5768837b57a02c6f525c9e2ef0340081d
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2016-09-20 19:05:28 +0200

    [IMP] load server environment options from odoo rcfile
    This allows delivering the code with default configuration options for
    all environments in the server_environment_file module, but letting
    the administrator override some configuration entries in the regular
    odoo configuration file.

--- server_environment/__openerp__.py
+++ server_environment/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     "name": "server configuration environment files",
-    "version": "9.0.1.1.0",
+    "version": "9.0.1.2.0",
     "depends": ["base", "server_environment_files"],
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "summary": "move some configurations out of the database",
