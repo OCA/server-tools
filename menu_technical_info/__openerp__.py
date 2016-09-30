7e824937af49a8ab8b1fc685a92893737281476d
Dennis Sluijk <dennissluijk@outlook.com>
2016-09-30 16:17:45 +0200

    [9.0][ADD] Menu technical info (#554)
    * [ADD] Initial commit
    
    [ADD] Bootstrap tooltip
    
    [ADD] Readme
    
    [FIX] Readme grammar
    
    * [FIX] cursos -> cursor
    
    * [FIX] EOL and js requirement
    
    * [FIX] New line, clean up js

---
+++ menu_technical_info/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2016 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Menu Technical Info',
+    'category': 'Administration',
+    'version': '9.0.1.0.0',
+    'author': 'Onestein, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'website': 'http://www.onestein.eu',
+    'depends': ['web'],
+    'data': [
+        'views/menu_technical_info_templates.xml',
+    ],
+    'summary': "Fast way to look up technical info about menu item."
+}
