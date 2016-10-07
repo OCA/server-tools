ebcd1e0651eb82f3aedbc029c92d71c113700f4a
Antonio Esposito <a.esposito@onestein.nl>
2016-10-07 15:07:05 +0200

    [10.0][MIGRATION] Migrated module 'date_range' to V10

--- date_range/tests/test_date_range.py
+++ date_range/tests/test_date_range.py
@@ -2,8 +2,8 @@
 # © 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from openerp.tests.common import TransactionCase
-from openerp.exceptions import ValidationError
+from odoo.tests.common import TransactionCase
+from odoo.exceptions import ValidationError
 
 
 class DateRangeTest(TransactionCase):
