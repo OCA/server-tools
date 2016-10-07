5b189ab018ff62ac0a766e5345c7647e19a756c0
Antonio Esposito <a.esposito@onestein.nl>
2016-10-07 15:07:05 +0200

    [10.0][MIGRATION] Migrated module 'date_range' to V10

--- date_range/README.rst
+++ date_range/README.rst
@@ -91,6 +91,8 @@ Contributors
 ------------
 
 * Laurent Mignon <laurent.mignon@acsone.eu>
+* Andrea Stirpe <a.stirpe@onestein.nl>
+* Antonio Esposito <a.esposito@onestein.nl>
 
 Maintainer
 ----------
--- date_range/tests/test_date_range_type.py
+++ date_range/tests/test_date_range_type.py
@@ -2,7 +2,7 @@
 # © 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class DateRangeTypeTest(TransactionCase):
