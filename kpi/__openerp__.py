e707cd00953e5ef12e801fbac68da3a6e295a549
Gervais Naoussi <gervaisnaoussi@gmail.com>
2016-10-21 11:57:13 -0400

    Latest Ellicoidal comment implemented

--- kpi/__openerp__.py
+++ kpi/__openerp__.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright <YEAR(S)> Daniel Reis <dreis.pt@hotmail.com>
+# Copyright <YEAR(S)> Glen Dromgoole <gdromgoole@tier1engineering.com>
+# Copyright <YEAR(S)> Loic Lacroix <loic.lacroix@savoirfairelinux.com>
+# Copyright <YEAR(S)> Sandy Carter <sandy.carter@savoirfairelinux.com>
+# Copyright <YEAR(S)>Gervais Naoussi <gervaisnaoussi@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     "name": "Key Performance Indicator",
     "version": "9.0.1.0.0",
--- kpi/models/kpi_threshold_range.py
+++ kpi/models/kpi_threshold_range.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> Daniel Reis <dreis.pt@hotmail.com>
+# Copyright <YEAR(S)> Glen Dromgoole <gdromgoole@tier1engineering.com>
+# Copyright <YEAR(S)> Loic Lacroix <loic.lacroix@savoirfairelinux.com>
+# Copyright <YEAR(S)> Sandy Carter <sandy.carter@savoirfairelinux.com>
+# Copyright <YEAR(S)>Gervais Naoussi <gervaisnaoussi@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import fields, models, api
 from openerp.tools.safe_eval import safe_eval
