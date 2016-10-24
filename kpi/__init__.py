00c6959c81822786174e4b6dbd2c5838e0c401ac
Gervais Naoussi <gervaisnaoussi@gmail.com>
2016-10-24 11:24:07 -0400

    last Ellicoial recommendation done.

--- kpi/__init__.py
+++ kpi/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright <YEAR(S)> Daniel Reis <dreis.pt@hotmail.com>
-# Copyright <YEAR(S)> Glen Dromgoole <gdromgoole@tier1engineering.com>
-# Copyright <YEAR(S)> Loic Lacroix <loic.lacroix@savoirfairelinux.com>
-# Copyright <YEAR(S)> Sandy Carter <sandy.carter@savoirfairelinux.com>
-# Copyright <YEAR(S)>Gervais Naoussi <gervaisnaoussi@gmail.com>
+# Copyright <YEAR(S)> Savoir-faire Linux <https://www.savoirfairelinux.com/>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
