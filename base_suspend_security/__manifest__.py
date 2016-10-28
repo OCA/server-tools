1b1ae47b94a58aa1b804fbf9e3967f10dd325e47
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-28 09:39:52 +0200

    [IMP] Add OCA as author

--- base_suspend_security/__manifest__.py
+++ base_suspend_security/__manifest__.py
@@ -20,7 +20,7 @@
 {
     "name": "Suspend security",
     "version": "10.0.1.0.0",
-    "author": "Therp BV",
+    "author": "Therp BV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Hidden/Dependency",
     "summary": "Suspend security checks for a call",
