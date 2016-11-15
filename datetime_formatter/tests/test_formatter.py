996251166dae206dc308e7e8c770abe350916546
cubells <vicent@vcubells.net>
2016-11-15 09:48:50 +0100

    datatime_formatter migration to 9.0

--- datetime_formatter/tests/test_formatter.py
+++ datetime_formatter/tests/test_formatter.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# © 2016 Tecnativa, S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import datetime
 from random import random
