5eb911cd3ce9aa1c17e4516bb111b588c881cda9
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2016-11-16 12:42:53 +0100

    [ADD] Add oauth_provider module

---
+++ oauth_provider/tests/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 SYLEAM
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_oauth_provider_client
+from . import test_oauth_provider_token
+from . import test_oauth_provider_scope
+from . import test_oauth_provider_controller_web_application
+from . import test_oauth_provider_controller_mobile_application
+from . import test_oauth_provider_controller_legacy_application
