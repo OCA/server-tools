7feb7ec89d14c9af03701aa90cc14571fa5005b6
jeanpaul <jprobineau@yahoo.com>
2016-11-16 09:10:43 +0100

    Ported to v10.0

--- super_calendar/models/super_calendar_configurator_line.py
+++ super_calendar/models/super_calendar_configurator_line.py
@@ -26,7 +26,7 @@
 #
 ##############################################################################
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class SuperCalendarConfiguratorLine(models.Model):
