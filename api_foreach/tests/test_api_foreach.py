700a098f8278a22905b571c7d4e0076c4cebe2e7
Dave Lasley <dave@laslabs.com>
2016-11-18 15:50:20 -0800

    Add test for iteration

--- api_foreach/tests/test_api_foreach.py
+++ api_foreach/tests/test_api_foreach.py
@@ -6,10 +6,36 @@ from odoo import api
 from odoo.tests.common import TransactionCase
 
 
+class TestRecordset(object):
+    """ It provides a mock recordset for testing foreach api """
+
+    records = ['test1', 'test2']
+
+    def __iter__(self):
+        for record in self.records:
+            yield record
+
+    @api.foreach
+    def decorated_method(self):
+        """ It provides a method decorated with foreach """
+        return self
+
+
 class TestApiForeach(TransactionCase):
+
+    def setUp(self):
+        super(TestApiForeach, self).setUp()
+        self.count = 0
 
     def test_api_monkey_patch(self):
         """ It should monkey-patch api.foreach into odoo.api """
         self.assertTrue(
             callable(api.foreach),
+        )
+
+    def test_api_iterate(self):
+        """ It should iterate and return list of method results """
+        res = TestRecordset().decorated_method()
+        self.assertEqual(
+            res, TestRecordset.records,
         )
