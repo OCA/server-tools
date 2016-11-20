c9d1735c63f03e3a8146b0e574e4532c201780e6
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2016-11-20 18:14:15 +0100

    server_environment_ir_config_parameter

---
+++ server_environment_ir_config_parameter/models/ir_config_parameter.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models, _, SUPERUSER_ID
+from openerp.exceptions import UserError
+from openerp.tools import ormcache
+from openerp.addons.server_environment import serv_config
+
+
+SECTION = 'ir.config_parameter'
+CTX_NO_CHECK = 'icp_no_check'
+
+
+class IrConfigParameter(models.Model):
+
+    _inherit = 'ir.config_parameter'
+
+    @ormcache('uid', 'key')
+    def _get_param(self, cr, uid, key):
+        value = super(IrConfigParameter, self)._get_param(cr, uid, key)
+        if serv_config.has_option(SECTION, key):
+            cvalue = serv_config.get(SECTION, key)
+            if cvalue != value:
+                # we write in db on first access;
+                # should we have preloaded values in database at,
+                # server startup, modules loading their parameters
+                # from data files would break on unique key error.
+                self.set_param(
+                    cr, SUPERUSER_ID, key, cvalue,
+                    context={CTX_NO_CHECK: True})
+                return cvalue
+        return value
+
+    def _check_not_in_config(self, keys):
+        if self.env.context.get(CTX_NO_CHECK):
+            return
+        if not serv_config.has_section(SECTION):
+            return
+        config_icp_keys = set(serv_config.options(SECTION)) & set(keys)
+        if config_icp_keys:
+            raise UserError(_("System Parameter(s) %s is/are defined "
+                              "in server_environment_files.") %
+                            (config_icp_keys, ))
+
+    @api.model
+    def create(self, vals):
+        self._check_not_in_config([vals.get('key')])
+        return super(IrConfigParameter, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        self._check_not_in_config(self.mapped('key'))
+        return super(IrConfigParameter, self).write(vals)
+
+    @api.multi
+    def unlink(self):
+        self._check_not_in_config(self.mapped('key'))
+        return super(IrConfigParameter, self).unlink()
---
+++ server_environment_ir_config_parameter/tests/test_server_environment_ircp.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.exceptions import UserError
+from openerp.tests import common
+
+
+class TestEnv(common.SavepointCase):
+
+    def setUp(self):
+        super(TestEnv, self).setUp()
+        self.ICP = self.env['ir.config_parameter']
+
+    def test_get_param(self):
+        """ Get system parameter from config """
+        # it's not in db
+        res = self.ICP.search([('key', '=', 'ircp_from_config')])
+        self.assertFalse(res)
+        # read so it's created in db
+        value = self.ICP.get_param('ircp_from_config')
+        self.assertEqual(value, 'config_value')
+        # now it's in db
+        res = self.ICP.search([('key', '=', 'ircp_from_config')])
+        self.assertEqual(len(res), 1)
+        self.assertEqual(res.value, 'config_value')
+
+    def test_set_param_1(self):
+        """ We can't set parameters that are in config file """
+        # create
+        with self.assertRaises(UserError):
+            self.ICP.set_param('ircp_from_config', 'new_value')
+        # read so it's created in db
+        self.ICP.get_param('ircp_from_config')
+        # write
+        res = self.ICP.search([('key', '=', 'ircp_from_config')])
+        self.assertEqual(len(res), 1)
+        with self.assertRaises(UserError):
+            res.write({'ircp_from_config': 'new_value'})
+        # unlink
+        with self.assertRaises(UserError):
+            res.unlink()
+
+    def test_set_param_2(self):
+        """ We can set parameters that are not in config file """
+        self.ICP.set_param('some.param', 'new_value')
+        self.assertEqual(self.ICP.get_param('some.param'), 'new_value')
+        res = self.ICP.search([('key', '=', 'some.param')])
+        res.unlink()
+        res = self.ICP.search([('key', '=', 'some.param')])
+        self.assertFalse(res)
