8a078fc4ec9df1993fcb4d498d0a4508eb7a5ecf
Dave Lasley <dave@laslabs.com>
2016-11-24 07:06:05 -0800

    Revert "[9.0][MIG] auto_backup (#526)"
    This reverts commit 2798d4591bfd6d2ccf45a6e0c22ab64709eaa3d8.

--- auto_backup/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Agile Business Group <http://www.agilebg.com>
-# © 2015 Alessio Gerace <alesiso.gerace@agilebg.com>
-# © 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import test_db_backup
