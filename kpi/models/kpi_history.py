40694c42d4724c96eb707528b224f06f0474a1fa
Gervais Naoussi <gervaisnaoussi@gmail.com>
2016-12-02 08:43:56 -0500

    mgmtsystem_kpi rename to kpi (#543)
    * kpi migration to odoo 9
    * UI error connected
    * Correction base on last maxime review
    * correction on @elicoidal review
    * Warning exception improve in kpi_threshold
    * Latest Ellicoidal comment implemented
    * last Ellicoial recommendation done.
    * Copyright corrected

---
+++ kpi/models/kpi_history.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012 - Now Savoir-faire Linux <https://www.savoirfairelinux.com/>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
+
+
+class KPIHistory(models.Model):
+    """History of the KPI."""
+
+    _name = "kpi.history"
+    _description = "History of the KPI"
+    _order = "date desc"
+
+    name = fields.Char('Name', size=150, required=True,
+                       default=fields.Datetime.now(),)
+    kpi_id = fields.Many2one('kpi', 'KPI', required=True)
+    date = fields.Datetime(
+        'Execution Date',
+        required=True,
+        readonly=True,
+        default=fields.Datetime.now()
+    )
+    value = fields.Float('Value', required=True, readonly=True)
+    color = fields.Text('Color', required=True,
+                        readonly=True, default='#FFFFFF')
+    company_id = fields.Many2one(
+        'res.company', 'Company',
+        default=lambda self: self.env.user.company_id.id)
