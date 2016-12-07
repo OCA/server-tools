ff6adc6bd40b818c7f4e27a8c873bde223559598
Daniel Reis <dreis.pt@hotmail.com>
2016-12-07 22:22:01 +0000

    Relicense to LGPL
    With the agreement of authors and contributors.
    Closes #635 (https://github.com/OCA/server-tools/issues/635)

--- base_external_dbsource/models/base_external_dbsource.py
+++ base_external_dbsource/models/base_external_dbsource.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Daniel Reis
-#    2011
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2011 Daniel Reis
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import os
 import logging
-import psycopg2
+
 from odoo import models, fields, api,  _
 from odoo.exceptions import Warning as UserError
 import odoo.tools as tools
