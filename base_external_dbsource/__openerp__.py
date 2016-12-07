84b102cda878aa809c7e09a622c243417fa02493
Daniel Reis <dreis.pt@hotmail.com>
2016-12-07 22:22:01 +0000

    Relicense to LGPL
    With the agreement of authors and contributors.
    Closes #635 (https://github.com/OCA/server-tools/issues/635)

--- base_external_dbsource/__openerp__.py
+++ base_external_dbsource/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright <2011> <Daniel Reis, Maxime Chambreuil, Savoir-faire Linux>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'External Database Sources',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.0.1',
     'category': 'Tools',
     'author': "Daniel Reis,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/server-tools',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'images': [
         'images/screenshot01.png',
     ],
