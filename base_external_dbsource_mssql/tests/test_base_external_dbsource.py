ba5330b8ae8331078de76871cc0c6622564d0eac
Dave Lasley <dave@laslabs.com>
2016-12-07 18:28:41 -0800

    [IMP] base_external_dbsource: Refactor & Split by source * Heavily refactor code for reusability * Split all sources into independent modules * Add more test coverage * Add CRUD methods * Add iterator execute return to roadmap

---
+++ base_external_dbsource_mssql/tests/test_base_external_dbsource.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+
+import mock
+
+from odoo.tests import common
+
+
+ADAPTER = ('odoo.addons.base_external_dbsource_mssql.models'
+           '.base_external_dbsource.pymssql')
+
+
+class TestBaseExternalDbsource(common.TransactionCase):
+
+    def setUp(self):
+        super(TestBaseExternalDbsource, self).setUp()
+        self.dbsource = self.env.ref(
+            'base_external_dbsource_mssql.demo_mssql',
+        )
+
+    def test_connection_close_mssql(self):
+        """ It should close the connection """
+        connection = mock.MagicMock()
+        res = self.dbsource.connection_close_mssql(connection)
+        self.assertEqual(res, connection.close())
+
+    def test_connection_open_mssql(self):
+        """ It should call SQLAlchemy open """
+        with mock.patch.object(
+            self.dbsource, '_connection_open_sqlalchemy'
+        ) as parent_method:
+            self.dbsource.connection_open_mssql()
+            parent_method.assert_called_once_with()
+
+    def test_excecute_mssql(self):
+        """ It should pass args to SQLAlchemy execute """
+        expect = 'sqlquery', 'sqlparams', 'metadata'
+        with mock.patch.object(
+            self.dbsource, '_execute_sqlalchemy'
+        ) as parent_method:
+            self.dbsource.execute_mssql(*expect)
+            parent_method.assert_called_once_with(*expect)
---
+++ base_external_dbsource_oracle/models/base_external_dbsource.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011 Daniel Reis
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+import logging
+import os
+
+from odoo import api
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from odoo.addons.base_external_dbsource.models import (
+        base_external_dbsource,
+    )
+    CONNECTORS = base_external_dbsource.BaseExternalDbsource.CONNECTORS
+    try:
+        import cx_Oracle
+        CONNECTORS.append(('cx_Oracle', 'Oracle'))
+    except ImportError:
+        _logger.info('Oracle libraries not available. Please install '
+                     '"cx_Oracle" python package.')
+except ImportError:
+    _logger.info('base_external_dbsource Odoo module not found.')
+
+
+class BaseExternalDbsource(models.Model):
+    """ It provides logic for connection to an Oracle data source. """
+
+    _inherit = "base.external.dbsource"
+
+    PWD_STRING_CX_ORACLE = 'Password=%s;'
+
+    @api.multi
+    def connection_close_cx_Oracle(self, connection):
+        return connection.close()
+
+    @api.multi
+    def connection_open_cx_Oracle(self):
+        os.environ['NLS_LANG'] = 'AMERICAN_AMERICA.UTF8'
+        return cx_Oracle.connect(self.conn_string_full)
+
+    @api.multi
+    def execute_cx_Oracle(self, sqlquery, sqlparams, metadata):
+        return self._execute_generic(sqlquery, sqlparams, metadata)
