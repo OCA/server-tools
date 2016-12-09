5b55c376353cb6348252b30073c5ba04fd704182
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2016-12-09 11:35:20 +0100

    server_environment_ir_config_parameter: migrate to 10.0

--- server_environment_ir_config_parameter/models/ir_config_parameter.py
+++ server_environment_ir_config_parameter/models/ir_config_parameter.py
@@ -2,9 +2,9 @@
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, models, _, SUPERUSER_ID
-from openerp.exceptions import UserError
-from openerp.addons.server_environment import serv_config
+from odoo import api, models, _
+from odoo.exceptions import UserError
+from odoo.addons.server_environment import serv_config
 
 
 SECTION = 'ir.config_parameter'
@@ -14,9 +14,9 @@ class IrConfigParameter(models.Model):
 
     _inherit = 'ir.config_parameter'
 
-    def get_param(self, cr, uid, key, default=False, context=None):
-        value = super(IrConfigParameter, self).get_param(
-            cr, uid, key, default=None, context=context)
+    @api.model
+    def get_param(self, key, default=False):
+        value = super(IrConfigParameter, self).get_param(key, default=None)
         if serv_config.has_option(SECTION, key):
             cvalue = serv_config.get(SECTION, key)
             if not cvalue:
@@ -28,7 +28,7 @@ class IrConfigParameter(models.Model):
                 # should we have preloaded values in database at,
                 # server startup, modules loading their parameters
                 # from data files would break on unique key error.
-                self.set_param(cr, SUPERUSER_ID, key, cvalue)
+                self.set_param(key, cvalue)
                 value = cvalue
         if value is None:
             return default
