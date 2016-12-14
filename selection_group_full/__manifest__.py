d28a2f3e47d44876fc5b6fb4a727f51b67d2e827
Denis Roussel <denis.roussel@acsone.eu>
2016-12-14 10:23:18 +0100

    [10.0] Adds a property in Selection fields to show void values in kanbans

---
+++ selection_group_full/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Selection Group Full',
+    'summary': """
+        Adds a property 'group_full' in fields Selection to be able to expand
+        void Selection in kanban views""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://acsone.eu',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ selection_group_full/models/base.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class BaseModel(models.AbstractModel):
+    _inherit = 'base'
+
+    @api.model
+    def _read_group_fill_results(self, domain, groupby,
+                                 remaining_groupbys, aggregated_fields,
+                                 count_field, read_group_result,
+                                 read_group_order=None):
+        """
+        This method checks if the 'group_full' property is present
+        in Selection field.
+        It accepts:
+        True : each value of the Selection will appear
+        A list of values : the values to show
+        """
+        field = self._fields.get(groupby)
+        if field and isinstance(field, fields.Selection) and field.group_full:
+            VALUES_DICT = dict(field.selection)
+            if isinstance(field.group_full, bool):
+                remaining_groups = [key for key in dict(field.selection)]
+            elif isinstance(field.group_full, list):
+                remaining_groups = field.group_full
+            else:
+                remaining_groups = []
+            for result in read_group_result:
+                field_value = result[groupby]
+                result[groupby] = (field_value, VALUES_DICT.get(field_value))
+                if field_value in remaining_groups:
+                    remaining_groups.remove(field_value)
+
+            for group in remaining_groups:
+                # Selection field that we want to appear in group but
+                # with count == 0
+                read_group_result.append({groupby: group, 'count': 0,
+                                          'domain': [(groupby, '=', group)]})
+
+        return super(BaseModel, self)._read_group_fill_results(
+            domain, groupby, remaining_groupbys, aggregated_fields,
+            count_field, read_group_result, read_group_order
+        )
