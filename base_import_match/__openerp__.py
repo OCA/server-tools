66f8265ed990b4e14261c2742294b2a164e570c2
cubells <vicent@vcubells.net>
2016-12-15 14:13:36 +0100

    base_import_match module

--- base_import_match/__openerp__.py
+++ base_import_match/__openerp__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Base Import Match",
     "summary": "Try to avoid duplicates before importing",
-    "version": "8.0.1.0.1",
+    "version": "9.0.1.0.0",
     "category": "Tools",
-    "website": "https://grupoesoc.es",
-    "author": "Grupo ESOC Ingeniería de Servicios, "
+    "website": "https://tecnativa.com",
+    "author": "Grupo ESOC Ingeniería de Servicios,"
+              "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
@@ -17,10 +19,10 @@
     ],
     "data": [
         "security/ir.model.access.csv",
-        "data/base_import_match.yml",
+        "data/base_import_match.xml",
         "views/base_import_match_view.xml",
     ],
     "demo": [
-        "demo/base_import_match.yml",
+        "demo/base_import_match.xml",
     ],
 }
