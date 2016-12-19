06b0f6baa1b4803aa64ace94ec6c33d5f1b2ee37
Daniel Blanco <daniel@blancomartin.cl>
2016-12-19 08:28:19 -0300

    Enable installation for mass_editing

--- mass_editing/__manifest__.py
+++ mass_editing/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Mass Editing',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': 'Serpent Consulting Services Pvt. Ltd., '
               'Odoo Community Association (OCA)',
     'contributors': [
@@ -20,7 +20,7 @@
         'security/ir.model.access.csv',
         'views/mass_editing_view.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
     'auto_install': False,
 }
