e97fc0bdee7a177a8b04a26790cb8b2cd2894ad1
Denis Roussel <denis.roussel@acsone.eu>
2016-12-19 15:36:58 +0100

    [FIX] Try not to import test model for other modules tests

--- selection_group_full/tests/test_fields.py
+++ selection_group_full/tests/test_fields.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import common
+from .common import BaseTestGroupFull
 
 
-class TestGroupFullField(common.BaseTestGroupFull):
+class TestGroupFullField(BaseTestGroupFull):
 
     @classmethod
     def setUpClass(cls):
