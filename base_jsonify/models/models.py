b135375dfa581cb1b5dd93d2aad9ae7603c13b32
Sébastien BEAU <sebastien.beau@akretion.com>
2016-12-21 16:14:22 +0100

    fix complatibility with 8

--- base_jsonify/models/models.py
+++ base_jsonify/models/models.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import api, models
-from openerp.exceptions import UserError
+from openerp.exceptions import Warning as UserError
 
 
 @api.multi
