ff462688811cf5f122e52267ae540da809efd9ba
Jairo Llopis <Yajo@users.noreply.github.com>
2016-12-23 11:37:09 +0100

    [base_view_inheritance_extension] Relicense to LGPL. (#4)

--- base_view_inheritance_extension/models/__init__.py
+++ base_view_inheritance_extension/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from . import ir_ui_view
