f712ab34672498fe607009803ed88eac2204ca0a
Jairo Llopis <Yajo@users.noreply.github.com>
2016-12-23 11:37:09 +0100

    [base_view_inheritance_extension] Relicense to LGPL. (#4)

--- base_view_inheritance_extension/__openerp__.py
+++ base_view_inheritance_extension/__openerp__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Extended view inheritance",
     "version": "8.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "category": "Hidden/Dependency",
     "summary": "Adds more operators for view inheritance",
     "depends": [
