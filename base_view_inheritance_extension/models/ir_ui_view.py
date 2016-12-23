30094d5621ccbce9aadecf6b28de77a573892030
Jairo Llopis <Yajo@users.noreply.github.com>
2016-12-23 11:37:09 +0100

    [base_view_inheritance_extension] Relicense to LGPL. (#4)

--- base_view_inheritance_extension/models/ir_ui_view.py
+++ base_view_inheritance_extension/models/ir_ui_view.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from lxml import etree
 from openerp import api, models, tools
 
