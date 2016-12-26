a49bdfbbed55a17676243a36129699e73564c963
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2016-12-26 16:28:26 +0530

    Migrated base_search_fuzzy to v9

--- base_search_fuzzy/__openerp__.py
+++ base_search_fuzzy/__openerp__.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Fuzzy Search",
     'summary': "Fuzzy search with the PostgreSQL trigram extension",
     'category': 'Uncategorized',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'website': 'https://odoo-community.org/',
     'author': 'bloopark systems GmbH & Co. KG, '
+              'Eficent Business and IT Consulting Services S.L., '
+              'Serpent Consulting Services Pvt. Ltd., '
               'Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'depends': [
-        'base',
-    ],
+    'depends': ['base'],
     'data': [
         'views/trgm_index.xml',
         'security/ir.model.access.csv',
