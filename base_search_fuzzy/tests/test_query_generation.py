2e6f8c124f8404df78469b002a1dc3e6be56524e
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2016-12-26 16:28:26 +0530

    Migrated base_search_fuzzy to v9

--- base_search_fuzzy/tests/test_query_generation.py
+++ base_search_fuzzy/tests/test_query_generation.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from openerp.osv import expression
 from openerp.tests.common import TransactionCase, at_install, post_install
