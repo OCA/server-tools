0e310b7bdd4d7dfafe6a78b006606e2331876f39
Stephan <stephan.rozendaal@gmail.com>
2016-12-27 14:58:00 +0100

    10.0 migrate users_ldap_populate (#657)
    * [MIG] migrate users_ldap_populate to 10.0
    
    Changes include:
     - move views to views folder
     - move models to models folder
     - in views rename openerp to odoo
     - __openerp__.py to __manifest__.py
    
    * [FIX] Add read me to module users_ldap_populate
    
    * [FIX] users_ldap_populate: cleanup manifest file
    
    Cleanup of the author list
    
    * [FIX] users_ldap_populate: More modern copyright in .py files
    
    * [FIX] users_ldap_populate: Update read me
    
    Fixes include:
     - more human module name
     - Fix web links

--- users_ldap_populate/models/__init__.py
+++ users_ldap_populate/models/__init__.py
@@ -2,4 +2,5 @@
 # © 2012 Therp BV (<http://therp.nl>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
-from . import models
+from . import users_ldap
+from . import populate_wizard
