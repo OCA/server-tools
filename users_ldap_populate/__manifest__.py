7cf62bd2a787e0674546a58b8b9b525767676d44
Stephan <stephan.rozendaal@gmail.com>
2016-12-27 14:58:00 +0100

    10.0 migrate users_ldap_populate (#657)
    * [MIG] migrate users_ldap_populate to 10.0
    
    Changes include:
     - move views to views folder
     - move models to models folder
     - in views rename openerp to odoo
     - __openerp__.py to __manifest__.py
    
    * [FIX] Add read me to module users_ldap_populate
    
    * [FIX] users_ldap_populate: cleanup manifest file
    
    Cleanup of the author list
    
    * [FIX] users_ldap_populate: More modern copyright in .py files
    
    * [FIX] users_ldap_populate: Update read me
    
    Fixes include:
     - more human module name
     - Fix web links

--- users_ldap_populate/__manifest__.py
+++ users_ldap_populate/__manifest__.py
@@ -1,27 +1,14 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2012 Therp BV (<http://therp.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2012 Therp BV (<http://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
 {
     "name": "LDAP Populate",
-    "version": "9.0.1.0.0",
-    "author": "Therp BV,Odoo Community Association (OCA)",
+    "version": "10.0.1.0.0",
+    "author": (
+        "Therp BV",
+        "Odoo Community Association (OCA)",
+    ),
     "license": "AGPL-3",
     "category": 'Tools',
     "description": """
@@ -45,8 +32,8 @@ object you want to query.
         'python': ['ldap'],
     },
     "data": [
-        'view/users_ldap.xml',
-        'view/populate_wizard.xml',
+        'views/users_ldap.xml',
+        'views/populate_wizard.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
