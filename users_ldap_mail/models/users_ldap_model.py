e1b1b3254e9977a5d5fcdeec15ad40e2757fba18
Stephan <stephan.rozendaal@gmail.com>
2016-12-27 23:12:37 +0100

    10.0 Migrate users_ldap_mail module (#651)
    * [ADD] Add README.rst to users_ldap_mail
    
    * [MIG] Migrate users_ldap_mail to 10.0
    
    * [FIX] Fixes in code style in module  users_ldap_mail
    
    Changes in models/users_ldap_mail include:
     - remove unused imports
     - indentation of too long lines
    
    * [FIX] users_ldap_mail: Fix copyright notice
    
    * [FIX] users_ldap_mail: set UTF-8 encoding
    
    users_ldap_mail/models/__init__.py should be UTF-8 encoded
    
    * [FIX] users_ldap_mail: remove size argument from Char fields
    
    * [FIX] users_ldap_mail: return value can be combined in one line
    
    * [FIX] Update read me
    
    Fixes included:
     - Make module name human readable
     - Instead of numbering steps, use #
     - Update broken link

---
+++ users_ldap_mail/models/users_ldap_model.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# © Daniel Reis (https://launchpad.com/~dreis-pt)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from odoo import models, fields
+
+import logging
+_log = logging.getLogger(__name__)
+
+
+class CompanyLDAP(models.Model):
+    _inherit = 'res.company.ldap'
+
+    name_attribute = fields.Char(
+        'Name Attribute',  _defaults='cn',
+        help="By default 'cn' is used. "
+             "For ActiveDirectory you might use 'displayName' instead.")
+    mail_attribute = fields.Char(
+        'E-mail attribute',  _defaults='mail',
+        help="LDAP attribute to use to retrieve em-mail address.")
+
+    def get_ldap_dicts(self):
+        """
+        Copy of auth_ldap's funtion, changing only the SQL, so that it returns
+        all fields in the table.
+        """
+        return self.sudo().search([('ldap_server', '!=', False)],
+                                  order='sequence').read([])
+
+    def map_ldap_attributes(self, conf, login, ldap_entry):
+        values = super(CompanyLDAP, self).map_ldap_attributes(conf, login,
+                                                              ldap_entry)
+        mapping = [
+            ('name', 'name_attribute'),
+            ('email', 'mail_attribute'),
+        ]
+        for value_key, conf_name in mapping:
+            try:
+                if conf[conf_name]:
+                    values[value_key] = ldap_entry[1][conf[conf_name]][0]
+            except KeyError:
+                _log.warning('No LDAP attribute "%s" found for login  "%s"' % (
+                    conf.get(conf_name), values.get('login')))
+        return values
