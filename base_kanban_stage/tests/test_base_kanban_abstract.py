b8e93b7141e804e9fdad684adfa175abeeda7ac1
Dave Lasley <dave@laslabs.com>
2016-12-29 16:06:56 -0800

    [MIG] base_kanban_stage: Upgrade to v10 * Rename manifest * Change openerp references to odoo * Bump version

--- base_kanban_stage/tests/test_base_kanban_abstract.py
+++ base_kanban_stage/tests/test_base_kanban_abstract.py
@@ -2,8 +2,8 @@
 # Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from openerp import models
-from openerp.tests.common import TransactionCase
+from odoo import models
+from odoo.tests.common import TransactionCase
 
 
 class BaseKanbanAbstractTester(models.Model):
