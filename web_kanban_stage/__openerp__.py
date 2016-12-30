OCA/web

[ADD] web_kanban_stage: Stage model and abstract logic * Add Kanban-compatible stage model web.kanban.stage * Add views, menu items, actions, and access controls needed to manage web.kanban.stage records * Add abstract model web.kanban.abstract that other models can inherit from to gain Kanban stage functionality * Add base Kanban view web_kanban_abstract_view_kanban, which can be customized as needed for use with models that inherit from web.kanban.abstract * Add model web.kanban.abstract.tester, which is needed for web.kanban.abstract unit tests

web_kanban_stage/__openerp__.py
@@ -1,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Kanban - Stage Support',
+    'summary': 'Provides stage model and abstract logic for inheritance',
+    'version': '9.0.1.0.0',
+    'author': "LasLabs, Odoo Community Association (OCA)",
+    'category': 'Web',
+    'depends': [
+        'web_kanban',
+    ],
+    'website': 'https://laslabs.com',
+    'license': 'AGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/web_kanban_abstract.xml',
+        'views/web_kanban_stage.xml',
+    ],
+    'installable': True,
+    'application': False,
+}

OCA/server-tools

web_kanban_stage/__openerp__.py
@@ -1,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Kanban - Stage Support',
+    'summary': 'Provides stage model and abstract logic for inheritance',
+    'version': '9.0.1.0.0',
+    'author': "LasLabs, Odoo Community Association (OCA)",
+    'category': 'Web',
+    'depends': [
+        'web_kanban',
+    ],
+    'website': 'https://laslabs.com',
+    'license': 'LGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/web_kanban_abstract.xml',
+        'views/web_kanban_stage.xml',
+    ],
+    'installable': True,
+    'application': False,
+}