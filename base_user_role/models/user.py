d2a6e8e8e82775b660912d56954d904ac111e05d
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2017-01-02 18:09:23 +0100

    [MIG] base_user_role: Port to 10.0

--- base_user_role/models/user.py
+++ base_user_role/models/user.py
@@ -2,7 +2,7 @@
 # Copyright 2014 ABF OSIELL <http://osiell.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class ResUsers(models.Model):
