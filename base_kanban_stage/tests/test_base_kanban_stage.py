871629ca2c7c586acd1a1b80bc6e711d5abac27a
Dave Lasley <dave@laslabs.com>
2017-01-14 17:52:48 -0800

    Implement newer abstract test strategy

--- base_kanban_stage/tests/test_base_kanban_stage.py
+++ base_kanban_stage/tests/test_base_kanban_stage.py
@@ -6,23 +6,23 @@ from odoo.tests.common import TransactionCase
 
 
 class TestBaseKanbanStage(TransactionCase):
-    def test_default_res_model_no_params(self):
+    def test_default_res_model_id_no_params(self):
         '''It should return empty ir.model Recordset if no params in context'''
         test_stage = self.env['base.kanban.stage'].with_context({})
-        res_model = test_stage._default_res_model()
+        res_model_id = test_stage._default_res_model_id()
 
-        self.assertFalse(res_model)
-        self.assertEqual(res_model._name, 'ir.model')
+        self.assertFalse(res_model_id)
+        self.assertEqual(res_model_id._name, 'ir.model')
 
-    def test_default_res_model_no_action(self):
+    def test_default_res_model_id_no_action(self):
         '''It should return empty ir.model Recordset if no action in params'''
         test_stage = self.env['base.kanban.stage'].with_context(params={})
-        res_model = test_stage._default_res_model()
+        res_model_id = test_stage._default_res_model_id()
 
-        self.assertFalse(res_model)
-        self.assertEqual(res_model._name, 'ir.model')
+        self.assertFalse(res_model_id)
+        self.assertEqual(res_model_id._name, 'ir.model')
 
-    def test_default_res_model_info_in_context(self):
+    def test_default_res_model_id_info_in_context(self):
         '''It should return correct ir.model record if info in context'''
         test_action = self.env['ir.actions.act_window'].create({
             'name': 'Test Action',
@@ -33,11 +33,11 @@ class TestBaseKanbanStage(TransactionCase):
         )
 
         self.assertEqual(
-            test_stage._default_res_model(),
+            test_stage._default_res_model_id(),
             self.env['ir.model'].search([('model', '=', 'res.users')])
         )
 
-    def test_default_res_model_ignore_self(self):
+    def test_default_res_model_id_ignore_self(self):
         '''It should not return ir.model record corresponding to stage model'''
         test_action = self.env['ir.actions.act_window'].create({
             'name': 'Test Action',
@@ -47,4 +47,4 @@ class TestBaseKanbanStage(TransactionCase):
             params={'action': test_action.id},
         )
 
-        self.assertFalse(test_stage._default_res_model())
+        self.assertFalse(test_stage._default_res_model_id())
