3e7779cff1f1c65922778caa9fadf8cf47e7930b
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-01-19 17:38:33 +0100

    [PORT] from 7.0 to 10.0 version

--- mass_sorting/models/mass_sort_wizard.py
+++ mass_sorting/models/mass_sort_wizard.py
@@ -3,95 +3,83 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv import fields
-from openerp.osv.orm import TransientModel
-from openerp.tools.translate import _
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
-class MassSortWizard(TransientModel):
+class MassSortWizard(models.TransientModel):
     _name = 'mass.sort.wizard'
 
-    _columns = {
-        'description': fields.text(
-            string='Description', readonly=True),
-        'allow_custom_setting': fields.boolean(
-            string='Allow Custom Setting', readonly=True),
-        'one2many_model': fields.char(
-            string='Model Name of the Field to Sort', readonly=True),
-        'line_ids': fields.one2many(
-            'mass.sort.wizard.line', 'wizard_id', 'Sorting Criterias'),
-    }
-
-    # Constraint Section
-    def _check_line_ids(self, cr, uid, ids, context=None):
-        for wizard in self.browse(cr, uid, ids, context=context):
-            if not len(wizard.line_ids):
-                return False
-        return True
-
-    _constraints = [(
-        _check_line_ids, "Please Select at least ona Sorting Criteria.",
-        ['line_ids']),
-    ]
-
     # Default Section
-    def _default_description(self, cr, uid, context=None):
-        config_obj = self.pool['mass.sort.config']
+    def _default_config_id(self):
+        return self._context.get('mass_sort_config_id', False)
+
+    def _default_line_ids(self):
+        config_obj = self.env['mass.sort.config']
+        line_ids = []
         config = config_obj.browse(
-            cr, uid, context.get('mass_sort_config_id'), context=context)
-
-        return _(
-            "You will sort the field '%(field)s' for %(qty)d %(model)s(s)"
-            ". \n\nThe sorting will be done by %(field_list)s.") % ({
-                'field': config.one2many_field_id.field_description,
-                'qty': len(context.get('active_ids', False)),
-                'model': config.model_id.name,
-                'field_list': ', '.join(
-                    [x.field_id.field_description for x in config.line_ids])
-                })
-
-    def _default_allow_custom_setting(self, cr, uid, context=None):
-        config_obj = self.pool['mass.sort.config']
-        return config_obj.browse(
-            cr, uid, context.get('mass_sort_config_id'), context=context)\
-            .allow_custom_setting
-
-    def _default_one2many_model(self, cr, uid, context=None):
-        config_obj = self.pool['mass.sort.config']
-        return config_obj.browse(
-            cr, uid, context.get('mass_sort_config_id'), context=context)\
-            .one2many_model
-
-    def _default_line_ids(self, cr, uid, context=None):
-        config_obj = self.pool['mass.sort.config']
-        res = []
-        config = config_obj.browse(
-            cr, uid, context.get('mass_sort_config_id'), context=context)
-        for line in config.line_ids:
-                res.append((0, 0, {
+            self._context.get('mass_sort_config_id', False))
+        if config:
+            for line in config.line_ids:
+                line_ids.append((0, 0, {
                     'sequence': line.sequence,
                     'field_id': line.field_id.id,
                     'desc': line.desc}))
-        return res
+        return line_ids
 
-    _defaults = {
-        'description': _default_description,
-        'allow_custom_setting': _default_allow_custom_setting,
-        'one2many_model': _default_one2many_model,
-        'line_ids': _default_line_ids,
-    }
+    # Column Section
+    config_id = fields.Many2one(
+        comodel_name='mass.sort.config', default=_default_config_id,
+        required=True)
+
+    description = fields.Text(
+        string='Description', readonly=True, compute='_compute_description')
+
+    allow_custom_setting = fields.Boolean(
+        string='Allow Custom Setting', readonly=True,
+        related='config_id.allow_custom_setting')
+
+    one2many_model = fields.Char(
+        string='Model Name of the Field to Sort', readonly=True,
+        related='config_id.one2many_model')
+
+    line_ids = fields.One2many(
+        comodel_name='mass.sort.wizard.line', inverse_name='wizard_id',
+        string='Sorting Criterias', default=_default_line_ids)
+
+    # Compute Section
+    @api.depends('config_id')
+    def _compute_description(self):
+        for wizard in self:
+            wizard.description = _(
+                "You will sort the field '%(field)s' for %(qty)d %(model)s(s)"
+                ". \n\nThe sorting will be done by %(field_list)s.") % ({
+                    'field':
+                    wizard.config_id.one2many_field_id.field_description,
+                    'qty': len(self._context.get('active_ids', False)),
+                    'model': wizard.config_id.model_id.name,
+                    'field_list': ', '.join(
+                        [x.field_id.field_description
+                            for x in wizard.line_ids])
+                    })
+
+    # Constraint Section
+    @api.constrains('line_ids')
+    def _check_line_ids(self):
+        for wizard in self:
+            if not len(wizard.line_ids):
+                raise ValidationError(_(
+                    "Please Select at least one Sorting Criteria."))
 
     # Action Section
-    def button_apply(self, cr, uid, ids, context=None):
-        config_obj = self.pool['mass.sort.config']
-        model_obj = self.pool[context.get('active_model')]
-        active_ids = context.get('active_ids')
-        config = config_obj.browse(
-            cr, uid, context.get('mass_sort_config_id'), context=context)
-        wizard = self.browse(cr, uid, ids[0], context=context)
+    def button_apply(self):
+        self.ensure_one()
+        wizard = self
+        active_ids = self._context.get('active_ids')
 
-        one2many_obj = self.pool[config.one2many_field_id.relation]
-        parent_field = config.one2many_field_id.relation_field
+        model_obj = self.env[wizard.config_id.model_id.model]
+        one2many_obj = self.env[wizard.config_id.one2many_field_id.relation]
+        parent_field = wizard.config_id.one2many_field_id.relation_field
 
         order_list = []
         for line in wizard.line_ids:
@@ -100,15 +88,13 @@ class MassSortWizard(TransientModel):
                 '%s desc' % line.field_id.name or line.field_id.name)
         order = ', '.join(order_list)
 
-        for item in model_obj.browse(cr, uid, active_ids, context=context):
+        for item in model_obj.browse(active_ids):
             # DB Query sort by the correct order
-            line_ids = one2many_obj.search(
-                cr, uid, [(parent_field, '=', item.id)], order=order,
-                context=context)
+            lines = one2many_obj.search(
+                [(parent_field, '=', item.id)], order=order)
+
             # Write new sequence to sort lines
             sequence = 1
-            for id in line_ids:
-                one2many_obj.write(
-                    cr, uid, [id], {'sequence': sequence}, context=context)
+            for line in lines:
+                line.sequence = sequence
                 sequence += 1
-        return {'type': 'ir.actions.act_window_close'}
