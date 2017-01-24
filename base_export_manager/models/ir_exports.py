42282458dd03705a573519a8266da038f02119f8
serpentcs <serpentcs@serpentcs-abhishek.(none)>
2017-01-24 18:49:06 +0530

    [MIG]Migrated base_export_manager module.

--- base_export_manager/models/ir_exports.py
+++ base_export_manager/models/ir_exports.py
@@ -2,8 +2,8 @@
 # Copyright 2015-2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, fields, models
-from openerp.exceptions import ValidationError
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class IrExports(models.Model):
--- base_export_manager/models/ir_exports_line.py
+++ base_export_manager/models/ir_exports_line.py
@@ -3,8 +3,8 @@
 # Copyright 2015-2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, exceptions
-from openerp.tools.translate import _
+from odoo import models, fields, api, exceptions
+from odoo.tools.translate import _
 
 
 class IrExportsLine(models.Model):
