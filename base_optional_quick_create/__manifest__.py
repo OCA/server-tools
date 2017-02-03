c07f18c7d03c043e682d11ec9fe84776ab2e521c
Zakaria Makrelouf <z.makrelouf@gmail.com>
2017-02-03 10:50:53 +0100

    [FIX]base_optional_quick_create: fix manifest version

--- base_optional_quick_create/__manifest__.py
+++ base_optional_quick_create/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Optional quick create",
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Tools',
     'summary': "Avoid 'quick create' on m2o fields, on a 'by model' basis",
     'author': "Agile Business Group,Odoo Community Association (OCA)",
