343415bfd002de32e984c3571c7bcf073db91b7c
Andrea Stirpe <a.stirpe@onestein.nl>
2017-02-14 18:21:22 +0100

    Update version in manifest

--- auto_backup/__manifest__.py
+++ auto_backup/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Database Auto-Backup",
     "summary": "Backups database",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "author": (
         "Yenthe Van Ginneken, "
         "Agile Business Group, "
