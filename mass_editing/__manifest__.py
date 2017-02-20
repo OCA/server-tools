2d47eb47c4ad221ff81fb36e857c9712e939793a
Serpent Consulting Services Pvt Ltd <serpentcs.lp@gmail.com>
2017-02-20 18:17:50 +0530

    [MIG] mass_editing: Migration to v10 (#598)

--- mass_editing/__manifest__.py
+++ mass_editing/__manifest__.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     'name': 'Mass Editing',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': 'Serpent Consulting Services Pvt. Ltd., '
               'Odoo Community Association (OCA)',
     'contributors': [
         'Oihane Crucelaegui <oihanecrucelaegi@gmail.com>',
         'Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>',
+        'Jay Vora <jay.vora@serpentcs.com>'
     ],
     'category': 'Tools',
     'website': 'http://www.serpentcs.com',
@@ -20,7 +22,7 @@
         'security/ir.model.access.csv',
         'views/mass_editing_view.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
     'auto_install': False,
 }
--- mass_editing/models/__init__.py
+++ mass_editing/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import ir_model_fields
 from . import mass_object
