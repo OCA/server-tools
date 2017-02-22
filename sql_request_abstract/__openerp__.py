1e65d9592f2f6f0419fe9c25f10a052bafdad39b
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-02-22 03:58:01 +0100

    create a new module sql_request_abstract

---
+++ sql_request_abstract/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'SQL Request Abstract',
+    'version': '8.0.1.0.0',
+    'author': 'GRAP,Akretion,Odoo Community Association (OCA)',
+    'website': 'https://www.odoo-community.org',
+    'license': 'AGPL-3',
+    'category': 'Tools',
+    'summary': 'Abstract Model to manage SQL Requests',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir_module_category.xml',
+        'security/res_groups.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+}
