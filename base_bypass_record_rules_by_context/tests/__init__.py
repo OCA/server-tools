202b4c23aee9a8b906901fe5aab749034a4c8234
Roberto Onnis <roberto.onnis@innoviu.com>
2017-03-02 16:15:38 +0100

    adds module base_bypass_record_rules_by_context
    fixes README and improves code
    
    fixes Readme template
    
    adds comments in IrRule code
    
    improves IrRule test
    
    minor change in Readme.rst

---
+++ base_bypass_record_rules_by_context/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2017 innoviù Srl <http://www.innoviu.com>
+# © 2017 Agile Business Group Sagl <http://www.agilebg.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import test_ir_rule
