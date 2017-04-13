044e86316b808052978c1345e047e90de12acc5a
mreficent <miquel.raich@eficent.com>
2017-04-13 10:25:12 +0200

    [MIG] fetchmail_notify_error_to_sender

--- fetchmail_notify_error_to_sender/__openerp__.py
+++ fetchmail_notify_error_to_sender/__openerp__.py
@@ -1,40 +1,25 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+# Copyright 2017 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Send notice on fetchmail errors',
     'summary': 'If fetching mails gives error, send an email to sender',
-    'version': '8.0.1.0.1',
+    'version': '9.0.1.0.0',
     'category': 'Tools',
-    'author': "Agile Business Group,Odoo Community Association (OCA)",
-    'website': 'http://www.agilebg.com',
+    'author': "Agile Business Group,Eficent,Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/server-tools',
     'license': 'AGPL-3',
     'depends': [
         'fetchmail',
     ],
     'data': [
-        'fetchmail_view.xml',
-        'email_template_data.xml',
+        'views/fetchmail_view.xml',
+        'data/email_template_data.xml',
     ],
     'qweb': [
     ],
     'installable': True,
-    'auto_install': False,
+    'application': False,
 }
