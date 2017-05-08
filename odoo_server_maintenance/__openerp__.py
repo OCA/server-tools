c681683838554eb9683e1110cd3f970ac33180c6
Tharathip <newtratip>
2017-05-08 16:24:18 +0700

    [ADD] add testing module

--- odoo_server_maintenance/__openerp__.py
+++ odoo_server_maintenance/__openerp__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Odoo Server Lock for Maintenance',
+    'name': 'Server Lock for Maintenance Mode',
     'version': '8.0.1.0.0',
     'author': 'Ecosoft,Odoo Community Association (OCA)',
     'category': 'Hidden',
     'description': """
-This module will lock user for maintenance by selecting maintenance mode
+This module will lock server for maintenance mode by selecting maintenance mode
 in users menu cause that user can not login and show message
 'This user is in maintenance mode' but if unselect maintenance mode that user
 will normal login
