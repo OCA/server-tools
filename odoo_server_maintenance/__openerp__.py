9c4f8395c4b72908878b46c3a6a9a20d17b5411b
Tharathip <newtratip>
2017-05-10 01:26:17 +0700

    [ENH] modify manage maintenance mode by group

--- odoo_server_maintenance/__openerp__.py
+++ odoo_server_maintenance/__openerp__.py
@@ -6,10 +6,15 @@
     'author': 'Ecosoft,Odoo Community Association (OCA)',
     'category': 'Hidden',
     'description': """
-This module will lock server for maintenance mode by selecting maintenance mode
-in users menu cause that user can not login and show message
-'This user is in maintenance mode' but if unselect maintenance mode that user
-will normal login
+This module will lock server for maintenance mode by \n
+1. Set users in group manage maintenance mode. \n
+2. Select change maintenance mode under more button in users menu
+   which user is in group manage maintenance mode will not select
+   maintenance mode and if user not in group manage maintenance mode will
+   can not manage maintenance mode. \n
+3. User is in maintenance mode can not login and show message
+   'This user is in maintenance mode'. \n
+If user is not maintenance mode user will normal login.
     """,
     'website': 'http://ecosoft.co.th',
     'depends': [
@@ -18,10 +23,10 @@ will normal login
     'data': [
         'wizards/change_maintenance_mode_view.xml',
         'views/res_users_view.xml',
+        'security/odoo_server_maintenance_security.xml',
         'security/ir.model.access.csv',
     ],
     'installable': True,
     'auto_install': False,
 }
-
 # vim:expandtab:smartinden't:tabstop=4:softtabstop=4:shiftwidth=4:
--- odoo_server_maintenance/wizards/change_maintenance_mode.py
+++ odoo_server_maintenance/wizards/change_maintenance_mode.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, api, SUPERUSER_ID, _
-from openerp.exceptions import ValidationError
+from openerp import models, api, SUPERUSER_ID
 
 
 class ChangeMaintenanceModeWizard(models.TransientModel):
@@ -9,15 +8,23 @@ class ChangeMaintenanceModeWizard(models.TransientModel):
     @api.multi
     def select_maintenance_mode(self):
         ResUsers = self.env['res.users']
+        Category = self.env['ir.module.category']
+        Group = self.env['res.groups']
         context = self._context.copy()
         active_ids = context.get('active_ids', [])
+        maintenance_category = Category.search([('name', '=',
+                                                 'Server Maintenance')])
+        user_ids = []
+        for mc in maintenance_category:
+            groups = Group.search([('category_id', '=', mc.id)])
+            for group in groups:
+                for user in group.users:
+                    if user.id not in user_ids:
+                        user_ids.append(user.id)
         if len(active_ids) > 0:
             for user in ResUsers.browse(active_ids):
-                if user.id == SUPERUSER_ID:
-                    raise ValidationError(_("%s is superuser, so can not "
-                                            "select maintenance mode.")
-                                          % (user.name))
-                user.write({'maintenance_mode': True, })
+                if user.id not in user_ids and user.id != SUPERUSER_ID:
+                    user.write({'maintenance_mode': True, })
         return {'type': 'ir.actions.act_window_close'}
 
     @api.multi
