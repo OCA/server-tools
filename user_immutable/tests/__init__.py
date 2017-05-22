ea8efd35dac15893777f033ef800fa83df065ead
Ted Salmon <tsalmon@laslabs.com>
2017-05-22 13:00:18 -0700

    [10.0] [ADD] user_immutable: Add Module (#830)
    * [ADD] user_immutable: Add Module
    * Add module to support users that are immutable
    
    * [IMP] user_immutable: Fixes per PR
    * Add missing test
    * Fix README
    * Add logging
    
    * [IMP] user_immutable: Fixes per PR
    * Lower code complexity
    * Fix README
    * Fix License
    
    * [IMP] user_immutable: Fixes per PR Review
    * Call `_check_immutable` on singleton
    * Simplify check for immutable group

---
+++ user_immutable/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import test_res_groups
+from . import test_res_users
