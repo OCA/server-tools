2329ab3ca89c6e2c43a2507b79e4ca9010273c39
mara1 <raul.martin@braintec-group.com>
2017-05-31 10:21:56 +0200

    [FIX] Adapting to PEP8

--- mass_editing/wizard/mass_editing_wizard.py
+++ mass_editing/wizard/mass_editing_wizard.py
@@ -210,17 +210,21 @@ class MassEditingWizard(orm.TransientModel):
                         dict.update({split_key: vals.get(split_key, False)})
                     elif val == 'remove':
                         dict.update({split_key: False})
-                        # If field to remove is translatable, its translations have to be removed
-                        model_field_id = model_field_obj.search(cr, uid, [('model', '=', context.get('active_model')),
-                                                                          ('name', '=', split_key)])
+                        # If field to remove is translatable,
+                        # its translations have to be removed
+                        model_field_id = model_field_obj.search(cr, uid,
+                                   [('model', '=', context.get('active_model')),
+                                    ('name', '=', split_key)])
                         if model_field_id and \
-                                model_field_obj.browse(cr, uid, model_field_id, context=context).translate:
+                                model_field_obj.browse(cr, uid, model_field_id,
+                                                     context=context).translate:
                             translation_ids = translation_obj.search(cr, uid, [
-                                                                        ('res_id', 'in', context.get('active_ids')),
-                                                                        ('type', '=', 'model'),
-                                                                        ('name', '=', u"{0},{1}".format(
-                                                                            context.get('active_model'), split_key))])
-                            translation_obj.unlink(cr, uid, translation_ids, context=context)
+                                    ('res_id', 'in', context.get('active_ids')),
+                                    ('type', '=', 'model'),
+                                    ('name', '=', u"{0},{1}".format(
+                                      context.get('active_model'), split_key))])
+                            translation_obj.unlink(cr, uid, translation_ids,
+                                                   context=context)
 
                     elif val == 'remove_m2m':
                         dict.update({split_key: [
