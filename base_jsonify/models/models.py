dd2a3b9a1f42034ee89d8f8a47465f0343d21d1f
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-05-31 21:45:10 +0200

    base_jsonify: Allow fields aliasing in jsonify

--- base_jsonify/models/models.py
+++ base_jsonify/models/models.py
@@ -8,6 +8,17 @@ from openerp.exceptions import Warning as UserError
 from openerp.tools.translate import _
 
 
+def __parse_field(parser_field):
+    field_name = parser_field
+    subparser = None
+    if isinstance(parser_field, tuple):
+        field_name, subparser = parser_field
+    json_key = field_name
+    if ':' in field_name:
+        field_name, json_key = field_name.split(':')
+    return field_name, json_key, subparser
+
+
 @api.multi
 def jsonify(self, parser):
     """ Convert the record according to the parser given
@@ -22,26 +33,35 @@ def jsonify(self, parser):
 
     In order to be consitent with the odoo api the jsonify method always
     return a list of object even if there is only one element in input
+
+    By default the key into the json is the name of the field extracted
+    from the model. If you need to specify an alternate name to use as key, you
+    can definne your mapping as follow into the parser definition:
+
+    parser = [
+         'field_name:json_key'
+    ]
+
     """
     result = []
     for rec in self:
         res = {}
         for field in parser:
-            if isinstance(field, tuple):
-                field_name, subparser = field
+            field_name, json_key, subparser = __parse_field(field)
+            if subparser:
                 field_type = rec._fields[field_name].type
                 if field_type in ('one2many', 'many2many'):
-                    res[field_name] = rec[field_name].jsonify(subparser)
+                    res[json_key] = rec[field_name].jsonify(subparser)
                 elif field_type == 'many2one':
                     data = rec[field_name].jsonify(subparser)
                     if data:
-                        res[field_name] = data[0]
+                        res[json_key] = data[0]
                     else:
-                        res[field_name] = None
+                        res[json_key] = None
                 else:
                     raise UserError(_('Wrong parser configuration'))
             else:
-                res[field] = rec[field]
+                res[json_key] = rec[field_name]
         result.append(res)
     return result
 
