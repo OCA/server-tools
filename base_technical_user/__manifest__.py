c41ce8eb0e9be7ec35efebb04ea357475d1dff58
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-06-14 16:49:01 +0200

    [10.0] add new module to manage technical user on company
    [CHG] add name to wml tag to make override easier
    
    [FIX] remove domain on active field

---
+++ base_technical_user/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "Base Technical User",
+    'summary': """
+        Add a technical user parameter on the company """,
+    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
+    'website': "http://acsone.eu",
+    'category': 'Hidden/Dependency',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_company_view.xml'
+    ],
+    'installable': True
+}
