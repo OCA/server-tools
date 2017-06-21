ff6cd3c743c10ccf253321052582b63d13748847
Eric Antones <eantones@nuobit.com>
2017-06-21 14:05:52 +0200

    [MIG] database_cleanup: Migration to version 10.0
    Using new base model inheritance.

--- database_cleanup/__openerp__.py
+++ database_cleanup/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Database cleanup',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': "Therp BV,Odoo Community Association (OCA)",
     'depends': ['base'],
     'license': 'AGPL-3',
