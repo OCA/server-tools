05a0a20e8ce6d04c50c5e3d43fdac755ef0daf20
Jairo Llopis <yajo.sk8@gmail.com>
2017-06-22 13:02:51 +0200

    [10.0][MIG][html_image_url_extractor] Migration

--- html_image_url_extractor/__manifest__.py
+++ html_image_url_extractor/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Image URLs from HTML field",
     "summary": "Extract images found in any HTML field",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Tools",
-    "website": "https://tecnativa.com",
+    "website": "https://www.tecnativa.com",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
