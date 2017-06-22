7d833275da832ecfb4cce950099c733301d94076
Jairo Llopis <yajo.sk8@gmail.com>
2017-06-22 13:02:51 +0200

    [10.0][MIG][html_image_url_extractor] Migration
    OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex

--- html_image_url_extractor/models/__init__.py
+++ html_image_url_extractor/models/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
-# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import ir_fields_converter
