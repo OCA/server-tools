5fde481d6e3309b22b2ffc534bb38a738c0f982c
Jairo Llopis <yajo.sk8@gmail.com>
2017-06-22 13:08:16 +0200

    [MIG][10.0][html_text] Migrate
    OCA Transbot updated translations from Transifex

--- html_text/__manifest__.py
+++ html_text/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Text from HTML field",
     "summary": "Generate excerpts from any HTML field",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Tools",
     "website": "https://tecnativa.com",
     "author": "Grupo ESOC Ingeniería de Servicios, "
