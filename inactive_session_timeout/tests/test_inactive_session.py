c32b5308088e736c2347965276235a6c1878287d
David Vidal <chienandalu@gmail.com>
2017-06-30 11:52:53 +0200

    Rewrite test

--- inactive_session_timeout/tests/test_inactive_session.py
+++ inactive_session_timeout/tests/test_inactive_session.py
@@ -7,18 +7,20 @@ from ..models.res_users import DELAY_KEY
 import time
 
 
-class TestInactiveSession(common.SavepointCase):
+class TestInactiveSession(common.HttpCase):
 
-    @classmethod
-    def setUpClass(cls):
-        super(TestInactiveSession, cls).setUpClass()
-        cls.ipm = cls.env['ir.config_parameter']
-        cls.rus = cls.env['res.users']
-        cls.db = common.get_db_name()
+    def setUp(self):
+        super(TestInactiveSession, self).setUp()
+        with self.registry.cursor() as test_cursor:
+            env = self.env(test_cursor)
+            self.ipm = env['ir.config_parameter']
+            self.rus = env['res.users']
+        self.db = common.get_db_name()
 
     def test_01_timeout(self):
         """ Test timeout after delay """
         self.ipm.set_param(DELAY_KEY, 1)
-        res = self.rus.authenticate(self.db, 'admin', 'admin', {})
+        self.assertTrue(self.ipm.get_param(DELAY_KEY), 1)
+        self.authenticate('admin', 'admin')
         time.sleep(1.1)
-        res = self.rus.browse(res).check(self.db, res, 'admin')
+        res = self.rus.browse(1).check(self.db, 1, 'admin')
