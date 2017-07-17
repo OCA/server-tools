8b0b40858dde9c6cd00a1b6ad6fd0ff808aeae8d
mreficent <miquel.raich@eficent.com>
2017-07-17 12:43:39 +0200

    [MIG] fetchmail_notify_error_to_sender: Migration to 10.0

--- fetchmail_notify_error_to_sender/models/fetchmail.py
+++ fetchmail_notify_error_to_sender/models/fetchmail.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Eficent <http://www.eficent.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class FetchmailServer(models.Model):
