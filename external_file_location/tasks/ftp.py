91a520aba1e72895b43834613d66c4468a3cd285
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2017-08-07 19:52:32 +0000

    add direcory tasks

---
+++ external_file_location/tasks/ftp.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+# @ 2016 Florian DA COSTA @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import logging
+_logger = logging.getLogger(__name__)
+
+try:
+    from fs import ftpfs
+except ImportError:
+    _logger.debug('Cannot `import fs`.')
+
+
+class FtpTask(ftpfs.FTPFS):
+
+    _key = 'sftp'
+    _name = 'SFTP'
+    _synchronize_type = None
+    _default_port = 22
+    _hide_login = False
+    _hide_password = False
+    _hide_port = False
+
+    @staticmethod
+    def connect(location):
+        conn = FtpTask(location.address,
+                       location.login,
+                       location.password,
+                       location.port)
+        return conn
