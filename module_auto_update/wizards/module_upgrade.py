4ec9662a54105478880936f1367017234a24dd58
Dave Lasley <dave@laslabs.com>
2017-08-11 09:10:12 -0700

    [FIX] module_auto_update: Don't compute upgrade on uninstall * `button_uninstall` calls `get_module_list`, which subsequently forces module upgrades. Add a context validation to prevent that * Add test for button_immediate_uninstall * Mock commit for immediate uninstall * Fix immediate uninstall test * Switch nesting * Bump module version

--- module_auto_update/wizards/module_upgrade.py
+++ module_auto_update/wizards/module_upgrade.py
@@ -10,12 +10,13 @@ class ModuleUpgrade(models.TransientModel):
 
     @api.model
     def get_module_list(self):
-        Module = self.env["ir.module.module"]
-        installed_modules = Module.search([('state', '=', 'installed')])
-        upgradeable_modules = installed_modules.filtered(
-            lambda r: r.checksum_dir != r.checksum_installed,
-        )
-        upgradeable_modules.button_upgrade()
+        if not self.env.context.get('module_uninstall'):
+            Module = self.env["ir.module.module"]
+            installed_modules = Module.search([('state', '=', 'installed')])
+            upgradeable_modules = installed_modules.filtered(
+                lambda r: r.checksum_dir != r.checksum_installed,
+            )
+            upgradeable_modules.button_upgrade()
         return super(ModuleUpgrade, self).get_module_list()
 
     @api.multi
