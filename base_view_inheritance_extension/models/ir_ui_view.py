b2ef6d860ac8e83a990f0fb66360ad394bc259c1
lfreeke <lfreeke@therp.nl>
2017-08-31 14:11:29 +0200

    [MIG] base_view_inheritance_extension to 10.0

--- base_view_inheritance_extension/models/ir_ui_view.py
+++ base_view_inheritance_extension/models/ir_ui_view.py
@@ -2,7 +2,7 @@
 # © 2016 Therp BV <http://therp.nl>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from lxml import etree
-from openerp import api, models, tools
+from odoo import api, models, tools
 
 
 class UnquoteObject(str):
