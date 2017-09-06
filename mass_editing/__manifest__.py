97b55889629991585b53bd56cd2bec99de7c6cb8
Jairo Llopis <yajo.sk8@gmail.com>
2017-09-06 11:08:01 +0200

    [FIX][mass_editing] Allow to delete models
    Without this fix, it is impossible to uninstall an addon that removes a model for which we had a `mass.object`.

--- mass_editing/__manifest__.py
+++ mass_editing/__manifest__.py
@@ -3,8 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Mass Editing',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'author': 'Serpent Consulting Services Pvt. Ltd., '
+              'Tecnativa, '
               'Odoo Community Association (OCA)',
     'contributors': [
         'Oihane Crucelaegui <oihanecrucelaegi@gmail.com>',
