d5426ea6d92f6a4b701505d60917eb0a2f3aea3b
Oleg Bulkin <o.bulkin@gmail.com>
2017-09-13 13:29:09 -0700

    [ADD] base_manifest_extension: rdepends_if_installed * Add support for rdepends_if_installed key, which allows a module to specify other installed modules that should list it as a dependency * Add unit tests for new logic

--- base_manifest_extension/tests/__init__.py
+++ base_manifest_extension/tests/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Therp BV <http://therp.nl>
+# Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-from . import test_base_manifest_extension
+from . import test_hooks
