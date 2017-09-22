539472f72e84bf4511fea7b4653dff6c6d077b06
felixvillafranca <f.villafranca@ingetive.com>
2017-09-22 22:28:49 +0200

    Little changes

--- dbf2csv/wizard/dbf2csv.py
+++ dbf2csv/wizard/dbf2csv.py
@@ -24,16 +24,16 @@ class Conversion(models.TransientModel):
     _name = 'dbftocsv.conversion'
     _description = 'File DBF to convert to CSV'
 
-    data_file = fields.Binary(
+    data_dbf = fields.Binary(
         string='DBF file',
         required=True,
-        help='Get you DBF file.')
-    filename = fields.Char(string='DBF file')
+        help='The DBF file that you want to transform.')
+    filename_dbf = fields.Char(string='DBF file')
 
-    data_file_csv = fields.Binary(
+    data_csv = fields.Binary(
         string='CSV file',
         readonly=True,
-        help='Get you CSV file.')
+        help='CSV file transformed and ready to download')
     filename_csv = fields.Char(string='CSV file')
 
     def reopen_wizard(self):
@@ -48,7 +48,7 @@ class Conversion(models.TransientModel):
         }
 
     def process_file(self):
-        dbf_buf = cStringIO.StringIO(base64.b64decode(self.data_file))
+        dbf_buf = cStringIO.StringIO(base64.b64decode(self.data_dbf))
         csv_buf = cStringIO.StringIO()
         writer = csv.writer(csv_buf)
         try:
@@ -57,9 +57,10 @@ class Conversion(models.TransientModel):
                 for rec in in_db:
                     writer.writerow(rec.fieldData)
             self.write({
-                'data_file_csv': base64.b64encode(csv_buf.getvalue()),
-                'filename_csv': os.path.splitext(self.filename)[0] + '.csv',
+                'data_csv': base64.b64encode(csv_buf.getvalue()),
+                'filename_csv': os.path.splitext(self.filename_dbf)[0] + '.csv',
             })
             return self.reopen_wizard()
         except ValueError:
+            _logger.error("Could not convert to CSV", exc_info=True)
             raise UserError(_("Unable to convert to csv"))
