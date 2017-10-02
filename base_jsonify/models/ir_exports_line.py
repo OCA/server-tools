36d8adea960c41d538bbaa192a19c02d0ab54831
hpar <raph@hpar.fr>
2017-10-02 12:08:12 +0200

    base_jsonify: cleanups
    remove copyrights from init and update copyrights

--- base_jsonify/models/ir_exports_line.py
+++ base_jsonify/models/ir_exports_line.py
@@ -2,8 +2,8 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.exceptions import ValidationError
-from openerp import api, fields, models, _
+from odoo.exceptions import ValidationError
+from odoo import api, fields, models, _
 
 
 class IrExportsLine(models.Model):
