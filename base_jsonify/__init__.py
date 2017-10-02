acded6d05c49fa26046cdb98a98ff10e201abdd8
hpar <raph@hpar.fr>
2017-10-02 12:08:12 +0200

    base_jsonify: cleanups
    remove copyrights from init and update copyrights

--- base_jsonify/__init__.py
+++ base_jsonify/__init__.py
@@ -1,7 +1,2 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com)
-# Sébastien BEAU <sebastien.beau@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-
 from . import models
--- base_jsonify/models/ir_export.py
+++ base_jsonify/models/ir_export.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com)
+# © 2017 Akretion (http://www.akretion.com)
 # Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from collections import OrderedDict
-from openerp import api, models
+from odoo import api, models
 
 
 def update_dict(data, fields):
