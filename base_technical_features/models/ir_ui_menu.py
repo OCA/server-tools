c18c4f3a0be4801e3274670fd9bc2b4ee47c34a5
Stefan Rijnhart <stefan@opener.am>
2017-10-05 16:43:33 +0200

    [UPG] 11.0, base_technical_features

--- base_technical_features/models/ir_ui_menu.py
+++ base_technical_features/models/ir_ui_menu.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # © 2016 Opener B.V. (<https://opener.am>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, models
+from odoo import api, models
 
 
 class IrUiMenu(models.Model):
--- base_technical_features/models/res_users.py
+++ base_technical_features/models/res_users.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 # © 2016 Opener B.V. (<https://opener.am>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models
-from openerp.exceptions import AccessError
-from openerp.tools.translate import _
+from odoo import api, fields, models
+from odoo.exceptions import AccessError
+from odoo.tools.translate import _
 
 
 class ResUsers(models.Model):
