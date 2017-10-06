5a0409cad8db3c0e24bfc47aa39ba6882d830a72
Simone Orsi <simahawk@users.noreply.github.com>
2017-10-06 10:56:00 +0200

    [add] base_fontawesome (#866)

---
+++ base_fontawesome/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Simone Orsi
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Base Fontawesome',
+    'summary': """Up to date Fontawesome resources.""",
+    'version': '9.0.4.7.0',
+    'license': 'LGPL-3',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'templates/assets.xml',
+    ],
+}
