6cadd64b656550375fe924f4acb9196bd2cb2402
Holger Brunn <hbrunn@therp.nl>
2017-10-11 18:38:32 +0200

    [ADD] allow mapping records by equal field values

--- base_import_odoo/models/import_odoo_database_field.py
+++ base_import_odoo/models/import_odoo_database_field.py
@@ -7,7 +7,9 @@ from openerp import fields, models
 class ImportOdooDatabaseField(models.Model):
     _name = 'import.odoo.database.field'
     _description = 'A field mapping for records in the remote database'
+    _order = 'database_id, sequence'
 
+    sequence = fields.Integer()
     database_id = fields.Many2one(
         'import.odoo.database', string='Database', required=True,
         ondelete='cascade',
@@ -15,13 +17,10 @@ class ImportOdooDatabaseField(models.Model):
     model_id = fields.Many2one(
         'ir.model', string='Model', required=True, ondelete='cascade',
     )
-    fields_id = fields.Many2one(
+    model = fields.Char(related=['model_id', 'model'])
+    field_ids = fields.Many2many(
         'ir.model.fields', string='Field', help='If set, the mapping is only '
         'effective when setting said field', ondelete='cascade',
-    )
-    unique = fields.Boolean(
-        'Unique', help='If set on a char field, a number is appended until '
-        'the value is unique. Set this for fields with uniqueness constraints',
     )
     # TODO: create a reference function field to set this conveniently
     local_id = fields.Integer(
@@ -32,4 +31,12 @@ class ImportOdooDatabaseField(models.Model):
     remote_id = fields.Integer(
         'Remote ID', help='If you leave this empty, every (set) field value '
         'will be mapped to the local ID'
+    )
+    mapping_type = fields.Selection(
+        [
+            ('fixed', 'Fixed'),
+            ('by_field', 'Based on equal fields'),
+            ('unique', 'Unique'),
+        ],
+        string='Type', required=True, default='fixed',
     )
