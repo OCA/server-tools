fa9116a9a23389edf8614b6ae7eca2a967813c83
Holger Brunn <hbrunn@therp.nl>
2017-10-11 04:51:35 +0200

    [FIX] don't break when an attribute node in an xpath makes the
    expression not match the node any more. Fixes #885

--- base_view_inheritance_extension/__manifest__.py
+++ base_view_inheritance_extension/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Extended view inheritance",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Hidden/Dependency",
