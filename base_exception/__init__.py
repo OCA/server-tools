321175e2e3b6ec72730011c375868ad2f980c3da
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-10-13 10:00:13 +0200

    [MIG] base_exception: Migration to 11.0

--- base_exception/__init__.py
+++ base_exception/__init__.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2011 Raphaël Valyi, Renato Lima, Guewen Baconnier, Sodexis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2011 Raphaël Valyi, Renato Lima, Guewen Baconnier, Sodexis
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import wizard, models
+from .tests import test_tmp_model
