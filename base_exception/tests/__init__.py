6074c36918a5f0f7b43a17f51464d61e4e3f9757
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-10-13 10:00:13 +0200

    [MIG] base_exception: Migration to 11.0

---
+++ base_exception/tests/__init__.py
@@ -0,0 +1,3 @@
+
+from . import test_tmp_model
+from . import test_base_exception
--- base_exception/wizard/base_exception_confirm.py
+++ base_exception/wizard/base_exception_confirm.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-# © 2011 Raphaël Valyi, Renato Lima, Guewen Baconnier, Sodexis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2011 Raphaël Valyi, Renato Lima, Guewen Baconnier, Sodexis
+# Copyright 2017 Akretion (http://www.akretion.com)
+# Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
 
