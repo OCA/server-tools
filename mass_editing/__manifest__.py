9f28e1200fba846a9479e412c252e7fc24ac81b5
Leonardo Donelli <learts92@gmail.com>
2017-10-16 18:15:20 +0200

    Bump to new major version
    As a new mass_editing operation has been introduced and one of the old ones
    renamed, bump to new major version to indicate breakage of backward compatibility.

--- mass_editing/__manifest__.py
+++ mass_editing/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Mass Editing',
-    'version': '10.0.1.1.1',
+    'version': '10.0.2.0.0',
     'author': 'Serpent Consulting Services Pvt. Ltd., '
               'Tecnativa, '
               'MONK Software, '
