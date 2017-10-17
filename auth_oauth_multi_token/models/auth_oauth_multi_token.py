7f080a4b01e1bfb99c6bcd3da91a1470e3375ba2
Simone Orsi <simahawk@gmail.com>
2017-10-17 18:38:31 +0200

    [MIG+IMP][10.0] auth_oauth_multi_token
    * cleanup, improve, docstrings
    * add tests

--- auth_oauth_multi_token/models/auth_oauth_multi_token.py
+++ auth_oauth_multi_token/models/auth_oauth_multi_token.py
@@ -1,29 +1,63 @@
 # -*- coding: utf-8 -*-
-# Florent de Labarre - 2016
+# Copyright 2016 Florent de Labarre
+# Copyright 2017 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
-class auth_oauth_multi_token(models.Model):
-    """Class defining list of tokens"""
+class AuthOauthMultiToken(models.Model):
+    """Define a set of tokens."""
 
     _name = 'auth.oauth.multi.token'
     _description = 'OAuth2 token'
-    _order = "id desc"
+    _order = 'id desc'
 
-    oauth_access_token = fields.Char('OAuth Access Token', readonly=True, copy=False)
-    user_id = fields.Many2one('res.users', 'User', required=True)
+    EMPTY_OAUTH_TOKEN = '****************************'
+
+    oauth_access_token = fields.Char(
+        string='OAuth Access Token',
+        readonly=True,
+        copy=False
+    )
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='User',
+        required=True
+    )
     active_token = fields.Boolean('Active')
 
     @api.model
     def create(self, vals):
-        res = super(auth_oauth_multi_token, self).create(vals)
-        oauth_access_token_ids = self.search([('user_id', '=', vals['user_id']), ('active_token', '=', True)], ).ids
-        oauth_access_max_token = self.env['res.users'].search([('id', '=', vals['user_id'])], limit=1).oauth_access_max_token
-        if len(oauth_access_token_ids) >= oauth_access_max_token:
-            self.browse(oauth_access_token_ids[oauth_access_max_token]).write({
-                'oauth_access_token': "****************************",
-                'active_token': False})
-        return res
+        """Override to validate tokens."""
+        token = super(AuthOauthMultiToken, self).create(vals)
+        token._oauth_validate_multi_token()
+        return token
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    @api.model
+    def _oauth_user_tokens(self, user_id, active=True):
+        """Retrieve tokens for given user.
+
+        :param user_id: Odoo ID of the user
+        :param active: retrieve active or inactive tokens
+        """
+        return self.search([
+            ('user_id', '=', user_id),
+            ('active_token', '=', active)
+        ])
+
+    def _oauth_validate_multi_token(self):
+        """Check current user's token and clear them if max number reached."""
+        user_tokens = self._oauth_user_tokens(self.user_id.id)
+        max_token = self.user_id.oauth_access_max_token
+        if user_tokens and len(user_tokens) > max_token:
+            # clear last token
+            user_tokens[max_token - 1]._oauth_clear_token()
+
+    @api.multi
+    def _oauth_clear_token(self):
+        """Disable current token records."""
+        self.write({
+            'oauth_access_token': self.EMPTY_OAUTH_TOKEN,
+            'active_token': False
+        })
---
+++ auth_oauth_multi_token/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_multi_token
