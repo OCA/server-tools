e90e0fc3945291dd5d44c26e198b66ea038c2d7a
Enric Tobella <etobella@creublanca.es>
2017-11-03 09:13:46 +0100

    [MIG] auditlog: Migration to 11.0

--- auditlog/models/http_session.py
+++ auditlog/models/http_session.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2015 ABF OSIELL <http://osiell.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -8,16 +7,16 @@ from odoo.http import request
 
 class AuditlogtHTTPSession(models.Model):
     _name = 'auditlog.http.session'
-    _description = u"Auditlog - HTTP User session log"
+    _description = "Auditlog - HTTP User session log"
     _order = "create_date DESC"
 
     display_name = fields.Char(
-        u"Name", compute="_compute_display_name", store=True)
-    name = fields.Char(u"Session ID", index=True)
+        "Name", compute="_compute_display_name", store=True)
+    name = fields.Char("Session ID", index=True)
     user_id = fields.Many2one(
-        'res.users', string=u"User", index=True)
+        'res.users', string="User", index=True)
     http_request_ids = fields.One2many(
-        'auditlog.http.request', 'http_session_id', string=u"HTTP Requests")
+        'auditlog.http.request', 'http_session_id', string="HTTP Requests")
 
     @api.depends('create_date', 'user_id')
     def _compute_display_name(self):
@@ -25,7 +24,7 @@ class AuditlogtHTTPSession(models.Model):
             create_date = fields.Datetime.from_string(httpsession.create_date)
             tz_create_date = fields.Datetime.context_timestamp(
                 httpsession, create_date)
-            httpsession.display_name = u"%s (%s)" % (
+            httpsession.display_name = "%s (%s)" % (
                 httpsession.user_id and httpsession.user_id.name or '?',
                 fields.Datetime.to_string(tz_create_date))
 
