b265568ba119c504991209fb564d760d1b2eb0c7
rgarnau <rgarnau20@gmail.com>
2017-11-14 16:17:19 +0100

    [add] https

--- auditlog/models/autovacuum.py
+++ auditlog/models/autovacuum.py
@@ -1,5 +1,5 @@
-# © 2016 ABF OSIELL <http://osiell.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# © 2016 ABF OSIELL <https://osiell.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import logging
 from datetime import datetime, timedelta
 
--- auditlog/models/rule.py
+++ auditlog/models/rule.py
@@ -1,5 +1,5 @@
-# © 2015 ABF OSIELL <http://osiell.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# © 2015 ABF OSIELL <https://osiell.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api, modules, _
 
