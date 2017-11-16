3c70bb320c8b178a26ad999ac20d67e2b8c3484f
mehdi-ghezal <mehdi.ghezal@gmail.com>
2017-11-16 14:56:34 +0100

    Update module version: non-breaking features added

--- base_view_inheritance_extension/__openerp__.py
+++ base_view_inheritance_extension/__openerp__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Extended view inheritance",
-    "version": "9.0.1.1.0",
+    "version": "9.0.1.2.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Hidden/Dependency",
