83fcd8b33a9fe94af49bbace3db0ff48cb802960
Oleg Bulkin <o.bulkin@gmail.com>
2017-11-16 19:23:02 -0800

    [FIX] auth_totp: Various issues * Restructure controller and res.users logic to prevent RPC authentication for users with MFA enabled and add support for multiple simultaneous MFA sessions * Switch trusted device cookies from using the DB secret to user-level secret keys, thereby increasing security * Remove MFA login tokens and trusted device model, which are now redundant * Add migration logic that generates a trusted device cookie key for every user with MFA enabled and cleans up device model ir records to prevent warnings * Update unit tests and remainder of module accordingly

--- auth_totp/tests/__init__.py
+++ auth_totp/tests/__init__.py
@@ -3,6 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from . import test_main
+from . import test_post_migrate
 from . import test_res_users
 from . import test_res_users_authenticator
 from . import test_res_users_authenticator_create
