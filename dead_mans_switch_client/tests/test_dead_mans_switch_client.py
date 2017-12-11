69b175105c37477c0e50e5aeba27587bc37c3617
Holger Brunn <hbrunn@therp.nl>
2017-12-11 12:11:11 +0100

    [FIX] squelch expected error log

--- dead_mans_switch_client/tests/test_dead_mans_switch_client.py
+++ dead_mans_switch_client/tests/test_dead_mans_switch_client.py
@@ -2,6 +2,7 @@
 # © 2015 Therp BV <http://therp.nl>
 # © 2017 Avoin.Systems - Miku Laitinen
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo.tools import mute_logger
 from odoo.tests.common import TransactionCase
 
 
@@ -10,7 +11,11 @@ class TestDeadMansSwitchClient(TransactionCase):
         # test unconfigured case
         self.env['ir.config_parameter'].search([
             ('key', '=', 'dead_mans_switch_client.url')]).unlink()
-        self.env['dead.mans.switch.client'].alive()
+        with mute_logger(
+                'odoo.addons.dead_mans_switch_client.models'
+                '.dead_mans_switch_client'
+        ):
+            self.env['dead.mans.switch.client'].alive()
         # test configured case
         self.env['ir.config_parameter'].set_param(
             'dead_mans_switch_client.url', 'fake_url')
