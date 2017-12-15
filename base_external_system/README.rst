dbb4e7c812657ea8bb05cfd278d251f7af668e68
Dave Lasley <dave@laslabs.com>
2017-12-15 15:42:32 -0800

    [MIG] base_external_system: Upgrade to v11 * Perform standard v11 migration

--- base_external_system/README.rst
+++ base_external_system/README.rst
@@ -57,7 +57,7 @@ Configure external systems in Settings => Technical => External Systems
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/149/10.0
+   :target: https://runbot.odoo-community.org/runbot/149/11.0
 
 Bug Tracker
 ===========
--- base_external_system/__manifest__.py
+++ base_external_system/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Base External System",
     "summary": "Data models allowing for connection to external systems.",
-    "version": "10.0.1.0.1",
+    "version": "11.0.1.0.0",
     "category": "Base",
     "website": "https://laslabs.com/",
     "author": "LasLabs, Odoo Community Association (OCA)",
