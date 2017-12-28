be62320a8a64606b4d7fd484389d4dae2adde525
Jordi Ballester <jordi.ballester@eficent.com>
2017-12-28 18:01:56 +0100

    Refactor tests of module 'base_exception'. Defines a new mechanism to build odoo classes that are only defined during testing.

--- base_exception/__init__.py
+++ base_exception/__init__.py
@@ -1,5 +1,2 @@
-# Copyright 2011 Raphaël Valyi, Renato Lima, Guewen Baconnier, Sodexis
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import wizard, models
-from .tests import test_tmp_model
