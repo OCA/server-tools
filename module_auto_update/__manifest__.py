fb4e43e1d192d5d377bc7bb60f1343d5033cb54f
Jairo Llopis <yajo.sk8@gmail.com>
2018-01-05 13:55:52 +0000

    [MIG] module_auto_update: Migrate to v11

--- module_auto_update/__manifest__.py
+++ module_auto_update/__manifest__.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Module Auto Update',
     'summary': 'Automatically update Odoo modules',
-    'version': '10.0.1.0.1',
+    'version': '11.0.1.0.0',
     'category': 'Extra Tools',
     'website': 'https://odoo-community.org/',
     'author': 'LasLabs, '
               'Juan José Scarafía, '
+              'Tecnativa, '
               'Odoo Community Association (OCA)',
     'license': 'LGPL-3',
     'application': False,
