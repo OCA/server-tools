0a95a51f9f9c5f3e18b2b165cf74d79037267b4a
Manu Varghese(Codeware) <manu@codewareuae.com>
2018-01-14 15:56:56 +0400

    [DEV]mass_editing: Migrated 10.0 to 11.0

---
+++ mass_editing/models/ir_model_fields.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class IrModelFields(models.Model):
+    _inherit = 'ir.model.fields'
+
+    @api.model
+    def search(self, args, offset=0, limit=0, order=None, count=False):
+        model_domain = []
+        for domain in args:
+            if (len(domain) > 2 and domain[0] == 'model_id' and
+                    isinstance(domain[2], str) and
+                    list(domain[2][1:-1])):
+                model_domain += [('model_id', 'in',
+                                  list(map(int, domain[2][1:-1].split(','))))]
+            else:
+                model_domain.append(domain)
+        return super(IrModelFields, self).search(model_domain, offset=offset,
+                                                 limit=limit, order=order,
+                                                 count=count)
