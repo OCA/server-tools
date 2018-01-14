e490c27352b61070011fd09967d646fb305c9c01
Andrea <a.stirpe@onestein.nl>
2018-01-14 12:24:54 +0100

    [11.0][MIG] onchange_helper + add tests

--- onchange_helper/__manifest__.py
+++ onchange_helper/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# © 2016-2017 Akretion (http://www.akretion.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {'name': 'Onchange Helper',
- 'version': '10.0.1.0.0',
+ 'version': '11.0.1.0.0',
  'summary': 'Technical module that ease execution of onchange in Python code',
  'author': 'Akretion,Camptocamp,Odoo Community Association (OCA)',
- 'website': 'http://www.akretion.com',
+ 'website': 'https://github.com/OCA/server-tools',
  'license': 'AGPL-3',
  'category': 'Generic Modules',
  'depends': ['base'],
