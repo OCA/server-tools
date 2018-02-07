a29ecd2d88b08d022c4a8db81bec65ef07605e80
Hugo Santos <hugo.santos@factorlibre.com>
2018-02-07 18:13:20 +0100

    [MIG] base_technical_user to V11

--- base_technical_user/__manifest__.py
+++ base_technical_user/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
@@ -8,7 +7,7 @@
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': "http://acsone.eu",
     'category': 'Hidden/Dependency',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'base',
