da3cb14bde3839fd9da7659b38df81c08ff43b9d
tarteo <d.sluijk@onestein.nl>
2018-02-14 13:39:27 +0100

    [MIG] html_image_url_extractor: Migration to 11.0

--- html_image_url_extractor/tests/test_extractor.py
+++ html_image_url_extractor/tests/test_extractor.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from lxml import etree
 from odoo.tools import mute_logger
