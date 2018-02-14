6df2b59dec9464e22074edd443d3cfd327c8af65
tarteo <d.sluijk@onestein.nl>
2018-02-14 13:39:27 +0100

    [MIG] html_image_url_extractor: Migration to 11.0

--- html_image_url_extractor/__manifest__.py
+++ html_image_url_extractor/__manifest__.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2016 Tecnativa - Vicent Cubells
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Image URLs from HTML field",
     "summary": "Extract images found in any HTML field",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Tools",
-    "website": "https://www.tecnativa.com",
+    "website": "https://github.com/OCA/server-tools",
     "author": "Tecnativa, "
+              "Onestein, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
