14b5aa75350a45257a753a87fd914aeb77982013
tarteo <d.sluijk@onestein.nl>
2018-02-14 13:15:58 +0100

    [MIG] html_text: Migration to 11.0
    [FIX] Tests
    
    [FIX] Do not test the specific exception
    
    [FIX] Do not test the specific exception

--- html_text/__manifest__.py
+++ html_text/__manifest__.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2016 Tecnativa - Vicent Cubells
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Text from HTML field",
     "summary": "Generate excerpts from any HTML field",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Tools",
-    "website": "https://tecnativa.com",
+    "website": "https://github.com/OCA/server-tools",
     "author": "Grupo ESOC Ingeniería de Servicios, "
               "Tecnativa, "
+              "Onestein, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
