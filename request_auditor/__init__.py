fb609adfa9a575427affd38ca8dea91da3db8426
andreparames <github@andreparames.com>
2018-02-15 17:30:06 +0100

    [9.0][ADD] request_auditor

---
+++ request_auditor/__init__.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+from datetime import datetime
+
+import openerp
+
+from . import config
+from .config import MAX_VALUE_SIZE
+
+audit_logger = config.logger()
+filter_methods = config.filter_methods()
+
+
+def restrict_dict(o):
+    new = {}
+    for key, value in o.iteritems():
+        if isinstance(value, basestring) and 'passw' in key:
+            new[key] = 'PASSWORD_CENSORED'
+        elif isinstance(value, basestring) and len(value) > MAX_VALUE_SIZE:
+            new[key] = '%s...' % value[:MAX_VALUE_SIZE]
+        elif isinstance(value, dict):
+            new[key] = restrict_dict(value)
+        else:
+            new[key] = value
+    return new
+
+
+if audit_logger:
+    old_call_function = openerp.http.WebRequest._call_function
+
+    def call_function(self, *args, **kw):
+        try:
+            exception = None
+            return old_call_function(self, *args, **kw)
+        except Exception as e:
+            exception = repr(e)
+            raise
+        finally:
+            method = self.endpoint.method.__name__
+            if method in filter_methods:
+                log_record = {
+                    'dbname': self.db,
+                    'method': method,
+                    'params': restrict_dict(self.params),
+                    'uid': self.session.uid,
+                    'timestamp': datetime.utcnow().isoformat(),
+                    'exception': exception,
+                }
+                audit_logger.info("AUDIT", extra=log_record)
+
+    openerp.http.WebRequest._call_function = call_function
