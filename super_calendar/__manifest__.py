6f73622a7f91cac6c34419db9c07534165edf53a
David Dufresne <dufresne.david.2@gmail.com>
2018-02-26 23:04:38 -0500

    Bump module version to 11

--- super_calendar/__manifest__.py
+++ super_calendar/__manifest__.py
@@ -8,7 +8,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Super Calendar',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Generic Modules/Others',
     'summary': 'This module allows to create configurable calendars.',
     'author': ('Agile Business Group, '
