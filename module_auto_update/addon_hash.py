6755b810ec1c108543c867536c72144e89d5d410
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-02-27 23:45:55 +0100

    [REF] module_auto_update: Step 2, add new API
    This code comes from the module_checksum_upgrade proposal
    at https://github.com/OCA/server-tools/pull/1176.
    
    * [ADD] module_checksum_upgrade
    
    It provides the core mechanism of module_auto_update without
    the cron nor any change to the standard upgrade mechanism.
    Instead it provides an API on which module_auto_update can build,
    as well as a method which can be called from a script to run
    the upgrade of modules for which the checksum has changed.
    
    * [IMP] refactor module_auto_update
    
    Make it depend on module_checksum_upgrade which provides
    the core mechanisms of managing the checksums. module_auto_update
    makes it automatic.
    
    * [IMP] module_checksum_upgrade: better exclusion mechanism
    
    Ignore files based on exclude patterns.
    Ignore uninstalled languages.
    Better default for patterns to ignore (*.pyc,*.pyo,*.pot,static/*)
    
    For better control on the hashing mechanism implement our own:
    it's quite easy, and the checksumdir module used previously had
    no test.
    
    * [MIG] module_auto_update: adapt to new checksum mechanism
    
    * [IMP] module_checksum_upgrade: raise in case of
     incomplete upgrade
    
    * [IMP] module_checksum_upgrade: improve default exclusion
     pattern
    
    * [IMP] module_checksum_upgrade: control translations
     overwrite
    
    * [IMP] module_checksum_upgrade: one more test
    
    * [IMP] module_checksum_upgrade: credits [ci skip]

---
+++ module_auto_update/addon_hash.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from fnmatch import fnmatch
+import hashlib
+import os
+
+
+def _fnmatch(filename, patterns):
+    for pattern in patterns:
+        if fnmatch(filename, pattern):
+            return True
+    return False
+
+
+def _walk(top, exclude_patterns, keep_langs):
+    keep_langs = {l.split('_')[0] for l in keep_langs}
+    for dirpath, dirnames, filenames in os.walk(top):
+        dirnames.sort()
+        reldir = os.path.relpath(dirpath, top)
+        if reldir == '.':
+            reldir = ''
+        for filename in sorted(filenames):
+            filepath = os.path.join(reldir, filename)
+            if _fnmatch(filepath, exclude_patterns):
+                continue
+            if keep_langs and reldir in {'i18n', 'i18n_extra'}:
+                basename, ext = os.path.splitext(filename)
+                if ext == '.po':
+                    if basename.split('_')[0] not in keep_langs:
+                        continue
+            yield filepath
+
+
+def addon_hash(top, exclude_patterns, keep_langs):
+    """Compute a sha1 digest of file contents."""
+    m = hashlib.sha1()
+    for filepath in _walk(top, exclude_patterns, keep_langs):
+        # hash filename so empty files influence the hash
+        m.update(filepath.encode('utf-8'))
+        # hash file content
+        with open(os.path.join(top, filepath), 'rb') as f:
+            m.update(f.read())
+    return m.hexdigest()
