b9689e7b23295c95a3bbe58de796492d5fa2c93a
Ronald Portier <ronald@therp.nl>
2018-03-01 16:01:28 +0100

    [MIG] fetchmail_attach_from_folder. Migration to 10.0.

--- fetchmail_attach_from_folder/__manifest__.py
+++ fetchmail_attach_from_folder/__manifest__.py
@@ -1,39 +1,25 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2013 Therp BV (<http://therp.nl>)
-#    All Rights Reserved
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# Copyright - 2013-2018 Therp BV <https://therp.nl>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Email gateway - folders',
     'summary': 'Attach mails in an IMAP folder to existing objects',
-    'version': '8.0.1.0.1',
-    'author': "Therp BV,Odoo Community Association (OCA)",
+    'version': '10.0.1.0.0',
+    'author': 'Therp BV,Odoo Community Association (OCA)',
     'website': 'http://www.therp.nl',
     'license': 'AGPL-3',
-    "category": "Tools",
-    "depends": ['fetchmail'],
+    'category': 'Tools',
+    'depends': ['fetchmail'],
     'data': [
-        'view/fetchmail_server.xml',
+        'views/fetchmail_server.xml',
         'wizard/attach_mail_manually.xml',
         'security/ir.model.access.csv',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
+    'external_dependencies': {
+        'python': [
+            'simplejson',
+        ],
+    },
 }
