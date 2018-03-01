d68acec11ded67395ab259cdf22ea6fa88f50ce8
Ronald Portier <ronald@therp.nl>
2018-03-01 16:01:28 +0100

    [MIG] fetchmail_attach_from_folder. Migration to 10.0.

--- fetchmail_attach_from_folder/models/__init__.py
+++ fetchmail_attach_from_folder/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright - 2013-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import match_algorithm
-from . import models
-from . import wizard
+from . import fetchmail_server
+from . import fetchmail_server_folder
--- fetchmail_attach_from_folder/tests/test_match_algorithms.py
+++ fetchmail_attach_from_folder/tests/test_match_algorithms.py
@@ -1,41 +1,70 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models
-from openerp.tests.common import TransactionCase
-from openerp.addons.fetchmail_attach_from_folder.match_algorithm import (
-    email_exact, email_domain, openerp_standard)
+# Copyright - 2015-2018 Therp BV <https://acme.com>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models
+from odoo.tests.common import TransactionCase
+
+from ..match_algorithm import email_exact, email_domain, odoo_standard
+
+MSG_BODY = [
+    ('1 (RFC822 {1149}',
+     'Return-Path: <ronald@acme.com>\r\n'
+     'Delivered-To: demo@yourcompany.example.com\r\n'
+     'Received: from localhost (localhost [127.0.0.1])\r\n'
+     '\tby vanaheim.acme.com (Postfix) with ESMTP id 14A3183163\r\n'
+     '\tfor <demo@yourcompany.example.com>;'
+     ' Mon, 26 Mar 2018 16:03:52 +0200 (CEST)\r\n'
+     'To: Test User <demo@yourcompany.example.com>\r\n'
+     'From: Ronald Portier <ronald@acme.com>\r\n'
+     'Subject: test\r\n'
+     'Message-ID: <485a8041-d560-a981-5afc-d31c1f136748@acme.com>\r\n'
+     'Date: Mon, 26 Mar 2018 16:03:51 +0200\r\n'
+     'User-Agent: Mock Test\r\n'
+     'MIME-Version: 1.0\r\n'
+     'Content-Type: text/plain; charset=utf-8\r\n'
+     'Content-Language: en-US\r\n'
+     'Content-Transfer-Encoding: 7bit\r\n\r\n'
+     'Hallo Wereld!\r\n'),
+    ')']
+
+
+class MockConnection():
+
+    def store(self, msgid, msg_item, value):
+        """Mock store command."""
+        return 'OK'
+
+    def fetch(self, msgid, parts):
+        """Return RFC822 formatted message."""
+        return ('OK', MSG_BODY)
 
 
 class TestMatchAlgorithms(TransactionCase):
-    def do_matching(self, match_algorithm, expected_xmlid, conf, mail_message,
-                    mail_message_org=None):
+
+    def _get_base_folder(self):
+        server_model = self.env['fetchmail.server']
+        folder_model = self.env['fetchmail.server.folder']
+        folder = folder_model.browse([models.NewId()])
+        folder.model_id = self.env.ref('base.model_res_partner').id
+        folder.model_field = 'email'
+        folder.match_algorithm = 'EmailExact'
+        folder.mail_field = 'to,from'
+        folder.server_id = server_model.browse([models.NewId()])
+        return folder
+
+    def do_matching(
+            self, match_algorithm, expected_xmlid, folder, mail_message,
+            mail_message_org=None):
         matcher = match_algorithm()
         matches = matcher.search_matches(
-            self.env.cr, self.env.uid, conf, mail_message, mail_message_org)
+            folder, mail_message, mail_message_org)
         self.assertEqual(len(matches), 1)
         self.assertEqual(
-            matches[0], self.env.ref(expected_xmlid).id)
+            matches[0], self.env.ref(expected_xmlid))
+        connection = MockConnection()
         matcher.handle_match(
-            self.env.cr, self.env.uid, None, matches[0], conf, mail_message,
-            mail_message_org, None)
+            connection, matches[0], folder, mail_message, mail_message_org,
+            None)
 
     def test_email_exact(self):
         mail_message = {
@@ -43,15 +72,11 @@ class TestMatchAlgorithms(TransactionCase):
             'to': 'demo@yourcompany.example.com',
             'from': 'someone@else.com',
         }
-        conf = self.env['fetchmail.server.folder'].browse([models.NewId()])
-        conf.model_id = self.env.ref('base.model_res_partner').id
-        conf.model_field = 'email'
-        conf.match_algorithm = 'email_exact'
-        conf.mail_field = 'to,from'
-        conf.server_id = self.env['fetchmail.server'].browse([models.NewId()])
+        folder = self._get_base_folder()
+        folder.match_algorithm = 'EmailExact'
         self.do_matching(
-            email_exact.email_exact, 'base.user_demo_res_partner',
-            conf, mail_message)
+            email_exact.EmailExact, 'base.user_demo_res_partner',
+            folder, mail_message)
         self.assertEqual(
             self.env.ref('base.user_demo_res_partner').message_ids.subject,
             mail_message['subject'])
@@ -61,22 +86,18 @@ class TestMatchAlgorithms(TransactionCase):
             'subject': 'Testsubject',
             'to': 'test@seagate.com',
             'from': 'someone@else.com',
-        }
-        conf = self.env['fetchmail.server.folder'].browse([models.NewId()])
-        conf.model_id = self.env.ref('base.model_res_partner').id
-        conf.model_field = 'email'
-        conf.match_algorithm = 'email_domain'
-        conf.mail_field = 'to,from'
-        conf.use_first_match = True
-        conf.server_id = self.env['fetchmail.server'].browse([models.NewId()])
+            'attachments': [('hello.txt', 'Hello World!')]}
+        folder = self._get_base_folder()
+        folder.match_algorithm = 'EmailDomain'
+        folder.use_first_match = True
         self.do_matching(
-            email_domain.email_domain, 'base.res_partner_address_31',
-            conf, mail_message)
+            email_domain.EmailDomain, 'base.res_partner_address_31',
+            folder, mail_message)
         self.assertEqual(
             self.env.ref('base.res_partner_address_31').message_ids.subject,
             mail_message['subject'])
 
-    def test_openerp_standard(self):
+    def test_odoo_standard(self):
         mail_message_org = (
             "To: demo@yourcompany.example.com\n"
             "From: someone@else.com\n"
@@ -84,20 +105,32 @@ class TestMatchAlgorithms(TransactionCase):
             "Message-Id: 42\n"
             "Hello world"
         )
-        conf = self.env['fetchmail.server.folder'].browse([models.NewId()])
-        conf.model_id = self.env.ref('base.model_res_partner').id
-        conf.model_field = 'email'
-        conf.match_algorithm = 'openerp_standard'
-        conf.mail_field = 'to,from'
-        conf.server_id = self.env['fetchmail.server'].browse([models.NewId()])
-        matcher = openerp_standard.openerp_standard()
+        folder = self._get_base_folder()
+        folder.match_algorithm = 'OdooStandard'
+        matcher = odoo_standard.OdooStandard()
         matches = matcher.search_matches(
-            self.env.cr, self.env.uid, conf, None, mail_message_org)
+            folder, None, mail_message_org)
         self.assertEqual(len(matches), 1)
         matcher.handle_match(
-            self.env.cr, self.env.uid, None, matches[0], conf, None,
-            mail_message_org, None, None)
+            None, matches[0], folder, None, mail_message_org, None)
         self.assertIn(
             'Hello world',
             self.env['mail.message']
                 .search([('subject', '=', 'testsubject')]).body)
+
+    def test_apply_matching_exact(self):
+        folder = self._get_base_folder()
+        folder.match_algorithm = 'EmailExact'
+        connection = MockConnection()
+        msgid = "<485a8041-d560-a981-5afc-d31c1f136748@acme.com>"
+        matcher = email_exact.EmailExact()
+        folder.apply_matching(connection, msgid, matcher)
+
+    def test_field_view_get(self):
+        """For the moment just check execution withouth errors."""
+        server_model = self.env['fetchmail.server']
+        view = server_model.fields_view_get()
+        self.assertTrue(view)
+        self.assertIn(
+            'match_algorithm',
+            view['fields']['folder_ids']['views']['form']['arch'])
