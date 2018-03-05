3930fdb2d7acb0d48287c3b6ce16e94b79d1eac7
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-03-05 15:48:30 +0100

    [ADD] setup.py

--- base_sparse_field_list_support/__manifest__.py
+++ base_sparse_field_list_support/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Base Sparse Field List Support",
     "summary": "add list support to convert_to_cache()",
-    "version": "8.0.1.0.0",
+    "version": "10.0.1.0.0",
     'category': 'Technical Settings',
     "website": "www.akretion.com",
     "author": " Akretion",
