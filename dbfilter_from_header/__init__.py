a862f34c20df359e3751e281ff1b58ed0cea8554
Ivan Yelizariev <yelizariev@it-projects.info>
2018-03-08 05:07:17 +0500

    [11.0][MIG] dbfilter_from_header
    updates were made in #1137 by @TimLai125
    
    and small lint fixes by @yelizariev
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- dbfilter_from_header/__init__.py
+++ dbfilter_from_header/__init__.py
@@ -1,20 +1,2 @@
-# -*- coding: utf-8 -*-
-# © 2013  Therp BV
-# © 2014  ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import re
-from odoo import http
-
-db_filter_org = http.db_filter
-
-
-def db_filter(dbs, httprequest=None):
-    dbs = db_filter_org(dbs, httprequest)
-    httprequest = httprequest or http.request.httprequest
-    db_filter_hdr = httprequest.environ.get('HTTP_X_ODOO_DBFILTER')
-    if db_filter_hdr:
-        dbs = [db for db in dbs if re.match(db_filter_hdr, db)]
-    return dbs
-
-http.db_filter = db_filter
+from . import override
