d8bb981dfdf4f415bc1b0f8ac58370c61dbd7361
Jairo Llopis <yajo.sk8@gmail.com>
2018-03-16 11:47:39 +0000

    [REF] module_auto_update: Step 1, move all deprecated stuff to deprecated files
    - Files are clearly suffixed with `_deprecated` so we know those features have no support nor migrations.
    - Views are removed, since updating from UI was too buggy to support it anymore.

--- module_auto_update/tests/__init__.py
+++ module_auto_update/tests/__init__.py
@@ -1,4 +1,4 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from . import test_module
-from . import test_module_upgrade
+from . import test_module_deprecated
+from . import test_module_upgrade_deprecated
