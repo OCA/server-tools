a9ae5af37733432c92cfebb1b988ace7e46efc4c
Ronald Portier <ronald@therp.nl>
2018-03-30 14:56:35 +0200

    [ADD] fetchmail_inbox. Add OCA to authors.

--- fetchmail_inbox/__manifest__.py
+++ fetchmail_inbox/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 Therp BV <http://therp.nl>
+# Copyright 2014-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Fetch mail into inbox",
     "version": "10.0.1.0.0",
-    "author": "Therp BV",
-    "category": "Dependency",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "category": "Tools",
     "depends": [
         'mail',
         'fetchmail',
--- fetchmail_inbox_invoice/__manifest__.py
+++ fetchmail_inbox_invoice/__manifest__.py
@@ -1,11 +1,11 @@
 # coding: utf-8
-# Copyright (C) 2014 Therp BV (<http://therp.nl>)
+# Copyright - 2014-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Fetch mail into inbox (invoice specific)",
     "version": "10.0.1.0.0",
-    "author": "Therp BV",
-    "category": "Accounting & Finance",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "category": "Tools",
     "depends": [
         'account',
         'fetchmail_inbox',
