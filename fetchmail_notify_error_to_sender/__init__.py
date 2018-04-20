f3ea5d364e7899f197697d14df761e09692bff3f
Nedas <nedas.zilinskas@gmail.com>
2018-04-20 15:00:50 +0300

    [MIG][11.0] fetchmail_notify_error_to_sender

---
+++ fetchmail_notify_error_to_sender/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+# Copyright 2017 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
