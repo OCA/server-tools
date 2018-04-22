91f2dc1e13dff0185e83ca702782a168478b6f03
Mohamed M. Hagag <mohamedhagag@users.noreply.github.com>
2018-04-22 04:35:50 +0200

    [10] auth_admin_passkey Fix Issue #1227
    this is a fix for https://github.com/OCA/server-tools/issues/1227 based on the missing PR by akretion for 8 and 9 .
    https://github.com/OCA/server-tools/pull/519

--- auth_admin_passkey/models/res_config.py
+++ auth_admin_passkey/models/res_config.py
@@ -4,55 +4,49 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import api, fields, models
-from odoo.tools import safe_eval
-
 
 class BaseConfigSettings(models.TransientModel):
     _inherit = 'base.config.settings'
 
+    # Getter / Setter Section
     @api.model
     def get_default_auth_admin_passkey_send_to_admin(self, fields):
-        icp = self.env['ir.config_parameter']
         return {
-            'auth_admin_passkey_send_to_admin': safe_eval(icp.get_param(
-                'auth_admin_passkey.send_to_admin', 'True')),
+            'auth_admin_passkey_send_to_admin':
+            self.env["ir.config_parameter"].get_param(
+                "auth_admin_passkey.send_to_admin")
         }
-
-    @api.model
-    def get_default_auth_admin_passkey_send_to_user(self, fields):
-        icp = self.env['ir.config_parameter']
-        return {
-            'auth_admin_passkey_send_to_user': safe_eval(icp.get_param(
-                'auth_admin_passkey.send_to_user', 'True')),
-        }
-
-    auth_admin_passkey_send_to_admin = fields.Boolean(
-        'Send email to admin user.',
-        help=('When the administrator use his password to login in '
-              'with a different account, Odoo will send an email '
-              'to the admin user.'),
-    )
-    auth_admin_passkey_send_to_user = fields.Boolean(
-        string='Send email to user.',
-        help=('When the administrator use his password to login in '
-              'with a different account, Odoo will send an email '
-              'to the account user.'),
-    )
 
     @api.multi
     def set_auth_admin_passkey_send_to_admin(self):
-        self.ensure_one()
+        for config in self:
+            self.env['ir.config_parameter'].set_param(
+                "auth_admin_passkey.send_to_admin",
+                config.auth_admin_passkey_send_to_admin or '')
 
-        icp = self.env['ir.config_parameter']
-        icp.set_param(
-            'auth_admin_passkey.send_to_admin',
-            repr(self.auth_admin_passkey_send_to_admin))
+    @api.model
+    def get_default_auth_admin_passkey_send_to_user(self, fields):
+        return {
+            'auth_admin_passkey_send_to_user':
+            self.env["ir.config_parameter"].get_param(
+                "auth_admin_passkey.send_to_user")
+        }
 
     @api.multi
     def set_auth_admin_passkey_send_to_user(self):
-        self.ensure_one()
+        for config in self:
+            self.env['ir.config_parameter'].set_param(
+                "auth_admin_passkey.send_to_user",
+                config.auth_admin_passkey_send_to_user or '')
 
-        icp = self.env['ir.config_parameter']
-        icp.set_param(
-            'auth_admin_passkey.send_to_user',
-            repr(self.auth_admin_passkey_send_to_user))
+    auth_admin_passkey_send_to_admin = fields.Boolean(
+        string='Send email to admin user.',
+        help="""When the administrator use his password to login in """
+             """with a different account, OpenERP will send an email """
+             """to the admin user.""")
+
+    auth_admin_passkey_send_to_user = fields.Boolean(
+        string='Send email to user.',
+        help="""When the administrator use his password to login in """
+             """with a different account, OpenERP will send an email """
+             """to the account user.""")
