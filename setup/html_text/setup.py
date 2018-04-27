69e82e0722efb9c425d502dc74abe3566937eb01
Jaume Planas <jaume.planas@minorisa.net>
2018-04-27 17:53:07 +0200

    Reverting first commit. Only change in manifest is left.

--- setup/html_text/setup.py
+++ setup/html_text/setup.py
@@ -2,11 +2,5 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon={
-        'external_dependencies_override': {
-            'python': {
-                'lxml.html': 'lxml'
-            }
-        }
-    },
+    odoo_addon=True,
 )
