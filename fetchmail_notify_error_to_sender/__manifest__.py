0e6322b386e0b3d258693f96bd423bbe58aaa2c6
HaiDD <haidd.uit@gmail.com>
2018-05-01 16:15:52 +0700

    [MIG] fetchmail_notify_error_to_sender: Migration to 11.0

--- fetchmail_notify_error_to_sender/__manifest__.py
+++ fetchmail_notify_error_to_sender/__manifest__.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
 # Copyright 2017 Eficent <http://www.eficent.com>
+# Copyright 2018 Hai Dinh <haidd.uit@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Fetchmail Notify Error to Sender',
     'summary': 'If fetching mails gives error, send an email to sender',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
-    'author': "Agile Business Group,Eficent,Odoo Community Association (OCA)",
+    'author':
+        "Agile Business Group,Eficent,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/server-tools',
     'license': 'AGPL-3',
     'depends': [
