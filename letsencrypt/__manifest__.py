b8d906ef9ae0ed25eda65601e4816d4f8f69b1ab
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2018-05-13 12:08:30 +0200

    [MIG] letsencrypt: migration to 11.0

--- letsencrypt/__manifest__.py
+++ letsencrypt/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Let's encrypt",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Therp BV,"
               "Tecnativa,"
+              "Acysos S.L,"
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Hidden/Dependency",
