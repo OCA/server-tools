7bbb4c960669f12b142fb7a2c16fc0e33176c172
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-05-17 19:06:36 +0200

    module_auto_update: bump version

--- module_auto_update/__openerp__.py
+++ module_auto_update/__openerp__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Module Auto Update',
     'summary': 'Automatically update Odoo modules',
-    'version': '9.0.2.0.1',
+    'version': '9.0.2.0.2',
     'category': 'Extra Tools',
     'website': 'https://github.com/OCA/server-tools',
     'author': 'LasLabs, '
