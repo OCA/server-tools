4c79fef97a1e854cb13cc921a9e0391a85def1a5
HaiDD <haidd.uit@gmail.com>
2018-05-23 18:12:29 +0700

    [REF] Refactor code

--- fetchmail_notify_error_to_sender/models/fetchmail.py
+++ fetchmail_notify_error_to_sender/models/fetchmail.py
@@ -7,8 +7,8 @@ from odoo import fields, models
 
 
 class FetchmailServer(models.Model):
-
     _inherit = 'fetchmail.server'
+
     error_notice_template_id = fields.Many2one(
         'mail.template', string="Error notice template",
         help="Set here the template to use to send notice to sender when "
