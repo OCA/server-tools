40ec9fc4eee5fcb7c6de7c46773d5a0255ade1b5
Jairo Llopis <yajo.sk8@gmail.com>
2018-05-25 10:28:13 +0100

    [FIX] auth_brute_force: Avoid storing false login attempts
    In Odoo v9, every request calls `res.users.check()`, which stores one authentication attempt per request, which is false.
    
    Besides, it easily leads to hitting ip-api.com rate limits, so now that API is only asked when seeing in form view (simply by setting the computed field as not stored).
    
    Also, form view was hidden, so it's now visible.

--- auth_brute_force/__openerp__.py
+++ auth_brute_force/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Authentification - Brute-Force Filter',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'category': 'Tools',
     'summary': "Track Authentication Attempts and Prevent Brute-force Attacks",
     'author': "GRAP, "
