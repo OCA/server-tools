ae7a2d494790afa1dee33be77b78724326bff8bb
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-05-29 14:20:11 +0200

    [MIG] configuration_helper: Migration to 11.0

--- configuration_helper/__manifest__.py
+++ configuration_helper/__manifest__.py
@@ -1,18 +1,13 @@
-# -*- coding: utf-8 -*-
-# © 2014 David BEAL Akretion
+# Copyright 2014 David BEAL Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {'name': 'Configuration Helper',
- 'version': '10.0.1.0.0',
+ 'version': '11.0.1.0.0',
  'author': "Akretion,Odoo Community Association (OCA)",
  'maintainer': 'Akretion',
  'category': 'server',
- 'complexity': 'normal',
  'depends': ['base'],
- 'website': 'http://www.akretion.com/',
- 'data': [],
- 'tests': [],
+ 'website': 'https://github.com/OCA/server-tools',
  'installable': True,
  'auto_install': False,
  'license': 'AGPL-3',
- 'application': False,
  }
