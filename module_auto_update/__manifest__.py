4cc73f43db27e65a449cb789a59c631fa880e485
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-06-04 15:39:12 +0200

    module_auto_update: 11.0.2.0.4

--- module_auto_update/__manifest__.py
+++ module_auto_update/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Module Auto Update',
     'summary': 'Automatically update Odoo modules',
-    'version': '11.0.2.0.3',
+    'version': '11.0.2.0.4',
     'category': 'Extra Tools',
     'website': 'https://github.com/OCA/server-tools',
     'author': 'LasLabs, '
