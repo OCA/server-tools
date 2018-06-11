89a1e24a3d817bb111910acc884ca42da8ef8fb8
cubells <vicent@vcubells.net>
2018-06-11 17:26:25 +0200

    base_search_fuzzy: Migration to 11.0

--- base_search_fuzzy/models/__init__.py
+++ base_search_fuzzy/models/__init__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L.
-# © 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import ir_model
 from . import trgm_index
