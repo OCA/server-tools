fcdd4c04af9273612b7054dd1830a0d6a3139548
Sébastien BEAU <sebastien.beau@akretion.com>
2018-06-12 00:06:37 +0200

    [REF] migrate to new api, inherit base class instead of patching, add test and make improve play_onchange

---
+++ onchange_helper/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_onchange
