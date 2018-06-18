0f2eab5c74a01aa3e0abf990a1294f7d16843a7f
Enric Tobella <etobella@creublanca.es>
2018-06-18 18:34:40 +0200

    [ADD] remote_base

---
+++ remote_base/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2018 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': "Remote Base",
+    'version': '11.0.1.0.0',
+    'category': 'Generic Modules/Base',
+    'author': "Creu Blanca, Odoo Community Association (OCA)",
+    'website': 'http://github.com/OCA/server-tools',
+    'license': 'AGPL-3',
+    "depends": ['web', 'base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_remote_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
