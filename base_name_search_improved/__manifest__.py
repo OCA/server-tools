a2cc3de0b6efccad4d190b298b6961c82f6dcab5
Katherine Zaoral <kz@adhoc.com.ar>
2018-07-05 10:32:27 -0300

    [MIG] base_name_search_improved: Migration to 11.0
    - Descriptor: change __openerp__.py file to __manifest__.py also update module
      version from 9.0.1.0.0 to 11.0.1.0.0
    - Update README using guideline
    - Views: replace view tag openerp to odoo and rename files to match guideline
    - Models
    
      - update imports from openerp to odoo
      - remove enconding line # -*- coding: utf-8 -*-
      - update to make it compatible
      - remove use of SUPERUSER_ID use sudo instead.
      - rename class name to make it match with guideline.
      - update methods to match api used in version 11.0
      - fix pylint errors
      - replace use of non exist self._model with self._name
      - use `Model `is not None instead of if `Model` this becuase the last one is
        a empty recordset and this one was evaluate to False and never was
        patching the method.
    
    - apply changes resquested/suggested in the PR by the reviewers.

--- base_name_search_improved/__manifest__.py
+++ base_name_search_improved/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
-# © 2016 Daniel Reis
+# Copyright 2016 Daniel Reis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Improved Name Search',
     'summary': 'Friendlier search when typing in relation fields',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Uncategorized',
-    'website': 'https://odoo-community.org/',
+    'website': 'https://github.com/OCA/server-tools',
     'author': 'Daniel Reis, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'data': [
-        'views/ir_model.xml',
+        'views/ir_model_views.xml',
     ],
     'depends': [
         'base',
