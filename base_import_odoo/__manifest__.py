0c023b8e5ca92e2b04cbafb106c9befae2957b70
Holger Brunn <hbrunn@therp.nl>
2018-07-19 09:20:08 +0200

    [MIG] base_import_odoo

--- base_import_odoo/__manifest__.py
+++ base_import_odoo/__manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-# © 2017 Therp BV <http://therp.nl>
+# Copyright 2017-2018 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Import from Odoo",
-    "version": "8.0.1.0.1",
+    "version": "10.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Tools",
     "summary": "Import records from another Odoo instance",
+    "website": "https://github.com/OCA/server-tools",
     "depends": [
         'mail',
     ],
