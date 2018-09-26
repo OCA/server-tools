Therp/Therp-Addons

Fetchmail inbox invoice. Leaving out the integration with fetchmail_invoice. It can be implemented in a dedicated glue module.

fetchmail_inbox_invoice/models/fetchmail_inbox_invoice.py
@@ -1,0 +1,11 @@
+# coding: utf-8
+# Copyright (C) 2014 Therp BV (<http://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import models
+
+
+class FetchmailInboxInvoice(models.Model):
+    _inherit = 'fetchmail.inbox'
+    _name = 'fetchmail.inbox.invoice'
+    _table = 'fetchmail_inbox'
+    _description = 'Fetchmail inbox for invoices'

fetchmail_inbox_invoice/models/mail_message.py
@@ -1,0 +1,14 @@
+# coding: utf-8
+# Copyright (C) 2014 Therp BV (<http://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import api, models
+
+
+class MailMessage(models.Model):
+    _inherit = 'mail.message'
+
+    @api.model
+    def _needaction_count(self, domain):
+        if domain == [('model', '=', 'fetchmail.inbox.invoice')]:
+            return len(self.search(domain))
+        return super(MailMessage, self)._needaction_count(domain)

fetchmail_inbox_invoice/wizard/fetchmail_inbox_attach_existing_wizard.py
@@ -1,0 +1,29 @@
+# coding: utf-8
+# Copyright (C) 2014 Therp BV (<http://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import api, models
+
+
+class FetchmailInboxAttachExistingWizard(models.TransientModel):
+    _inherit = 'fetchmail.inbox.attach.existing.wizard'
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form',
+                        toolbar=False, submenu=False):
+        result = super(
+            FetchmailInboxAttachExistingWizard, self).fields_view_get(
+                view_id=view_id, view_type=view_type,
+                toolbar=toolbar, submenu=submenu)
+        if (self.env.context.get('default_mail_id') and
+                self.env.context.get(
+                    'set_default_res_model') == 'account.invoice'):
+            mail = self.env['mail.message'].browse(
+                self.env.context['default_mail_id'])
+            result['fields']['res_id']['domain'] = [
+                ('type', 'in', ('in_invoice', 'in_refund'))]
+            result['fields']['res_id']['options'] = '{"no_create": True}'
+            if mail.author_id:
+                partner = mail.author_id.commercial_partner_id
+                result['fields']['res_id']['context'].update(
+                    search_default_partner_id=partner.id)
+        return result

OCA/server-tools

models/fetchmail_inbox_invoice.py
@@ -1,0 +1,11 @@
+# coding: utf-8
+# Copyright (C) 2014 Therp BV (<http://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models
+
+
+class FetchmailInboxInvoice(models.Model):
+    _inherit = 'fetchmail.inbox'
+    _name = 'fetchmail.inbox.invoice'
+    _table = 'fetchmail_inbox'
+    _description = 'Fetchmail inbox for invoices'

models/mail_message.py
@@ -1,0 +1,14 @@
+# coding: utf-8
+# Copyright (C) 2014 Therp BV (<http://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, models
+
+
+class MailMessage(models.Model):
+    _inherit = 'mail.message'
+
+    @api.model
+    def _needaction_count(self, domain):
+        if domain == [('model', '=', 'fetchmail.inbox.invoice')]:
+            return len(self.search(domain))
+        return super(MailMessage, self)._needaction_count(domain)

wizard/fetchmail_inbox_attach_existing_wizard.py
@@ -1,0 +1,29 @@
+# coding: utf-8
+# Copyright (C) 2014 Therp BV (<http://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, models
+
+
+class FetchmailInboxAttachExistingWizard(models.TransientModel):
+    _inherit = 'fetchmail.inbox.attach.existing.wizard'
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form',
+                        toolbar=False, submenu=False):
+        result = super(
+            FetchmailInboxAttachExistingWizard, self).fields_view_get(
+                view_id=view_id, view_type=view_type,
+                toolbar=toolbar, submenu=submenu)
+        if (self.env.context.get('default_mail_id') and
+                self.env.context.get(
+                    'set_default_res_model') == 'account.invoice'):
+            mail = self.env['mail.message'].browse(
+                self.env.context['default_mail_id'])
+            result['fields']['res_id']['domain'] = [
+                ('type', 'in', ('in_invoice', 'in_refund'))]
+            result['fields']['res_id']['options'] = '{"no_create": True}'
+            if mail.author_id:
+                partner = mail.author_id.commercial_partner_id
+                result['fields']['res_id']['context'].update(
+                    search_default_partner_id=partner.id)
+        return result