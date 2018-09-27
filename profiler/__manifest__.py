7fc14e243a8bb59100303914423e207b3293cf2a
Moisés López <moylop260@vauxoo.com>
2018-09-27 12:09:41 +0200

    [ADD] profiler: Integration of python cprofile and postgresql logging collector for Odoo

--- profiler/__manifest__.py
+++ profiler/__manifest__.py
@@ -1,9 +1,9 @@
 {
     'name': "profiler",
-    'author': "Vauxoo",
-    'website': "http://www.vauxoo.com",
+    'author': "Vauxoo, Odoo Community Association (OCA)",
+    'website': "https://github.com/OCA/server-tools/tree/12.0/profiler",
     'category': 'Tests',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': ["document"],
     'data': [
