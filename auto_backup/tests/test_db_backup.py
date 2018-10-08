193968149f9cd61e7f7cb4a3d8e0ccdea65813d2
Aitor Bouzas <bouzasnaveira@gmail.com>
2018-10-08 15:04:31 +0200

    [MIG] auto_backup: Migration to 12.0 Added test coverage Added requirements.txt for pysftp Added in the roadmap the exact version of pysftp for this to work (0.2.8) Added in the roadmap the problem with list_db=False Adapted calls to message_post

--- auto_backup/tests/test_db_backup.py
+++ auto_backup/tests/test_db_backup.py
@@ -1,6 +1,6 @@
-# ?? 2015 Agile Business Group <http://www.agilebg.com>
-# ?? 2015 Alessio Gerace <alesiso.gerace@agilebg.com>
-# ?? 2016 Grupo ESOC Ingenier??a de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2015 Agile Business Group <http://www.agilebg.com>
+# Copyright 2015 Alessio Gerace <alesiso.gerace@agilebg.com>
+# Copyright 2016 Grupo ESOC Ingenieria de Servicios, S.L.U. - Jairo Llopis
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
