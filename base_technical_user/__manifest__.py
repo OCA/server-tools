b69a6e71ae533e587787e2f05f19843693b2ef8d
lily <lily@osbzr.com>
2018-10-13 15:24:45 +0800

    [MIG] base_technical_user: Migration to 12.0

--- base_technical_user/__manifest__.py
+++ base_technical_user/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': "http://acsone.eu",
     'category': 'Hidden/Dependency',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'base',
