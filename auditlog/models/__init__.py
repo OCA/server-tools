505271ed3e3892d384474cf68c05be6dceca2aee
lily <lily@osbzr.com>
2018-10-14 20:51:00 +0800

    mig pr for base_exception

--- auditlog/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import rule
-from . import http_session
-from . import http_request
-from . import log
-from . import autovacuum
