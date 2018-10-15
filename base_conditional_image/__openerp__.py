e34299e2590e5d12ea58ffe6b713c1525f3fd81a
Patrick Tombez <patrick.tombez@camptocamp.com>
2018-10-15 15:37:22 +0200

    Update manifest file

--- base_conditional_image/__openerp__.py
+++ base_conditional_image/__openerp__.py
@@ -3,9 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Image Storage',
+    'name': 'Conditional Images',
     'version': '9.0.1.0.0',
-    'author': 'Odoo Community Association (OCA), Camptocamp',
+    'author': 'Camptocamp, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'category': 'Misc',
     'depends': [
