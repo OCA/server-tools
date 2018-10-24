86f1c1590ae7b6bfa40c9797945f8bb9bd0328e0
hveficent <hector.villarreal@eficent.com>
2018-10-24 12:13:41 +0200

    [MIG] mail_log_message_to_process: Migration to 11.0

--- mail_log_message_to_process/__manifest__.py
+++ mail_log_message_to_process/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Mail Log Message to Process',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
     'summary': 'Log all messages received, before they start to be processed.',
     'author': "Eficent, "
--- mail_log_message_to_process/models/mail_thread.py
+++ mail_log_message_to_process/models/mail_thread.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #           (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import email
-import xmlrpclib
+import xmlrpc.client as xmlrpclib
 import logging
-from email.message import Message
 from odoo import api, models
-from odoo.tools import decode_smtp_header as decode
+from odoo.tools import pycompat
 
 _logger = logging.getLogger(__name__)
 
@@ -17,60 +15,23 @@ class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
     @api.model
-    def message_parse_basic_data(self, message):
-        """Parses a string or email.message.Message representing an RFC-2822
-        email, and returns a generic dict holding the message details.
-
-        :param message: the message to parse
-        :rtype: dict
-        :return: A dict with the following structure, where each field
-        may not be present if missing in original message:
-            { 'message_id': msg_id,
-              'subject': subject,
-              'from': from,
-              'to': to,
-              'cc': cc
-            }
-        """
-
-        msg_dict = {
-            'message_type': 'email',
-        }
-        if not isinstance(message, Message):
-            if isinstance(message, unicode):
-                # Warning: message_from_string doesn't always work
-                # correctly on unicode, we must use utf-8 strings here :-(
-                message = message.encode('utf-8')
-            message = email.message_from_string(message)
-
-        message_id = message['message-id']
-        if not message_id:
-            message_id = 'None'
-        msg_dict['message_id'] = message_id
-        if message.get('Subject'):
-            msg_dict['subject'] = decode(message.get('Subject'))
-
-        # Envelope fields not stored in mail.message but made available
-        # for message_new()
-        msg_dict['from'] = decode(message.get('from'))
-        msg_dict['to'] = decode(message.get('to'))
-        msg_dict['cc'] = decode(message.get('cc'))
-        msg_dict['email_from'] = decode(message.get('from'))
-        return msg_dict
-
-    @api.model
     def message_process(self, model, message, custom_values=None,
                         save_original=False, strip_attachments=False,
                         thread_id=None):
 
         if isinstance(message, xmlrpclib.Binary):
-            message = str(message.data)
-        # Warning: message_from_string doesn't always work correctly on
-        # unicode, we must use utf-8 strings here :-(
-        if isinstance(message, unicode):
+            message = bytes(message.data)
+            # message_from_string parses from a *native string*, except
+            # apparently sometimes message is ISO-8859-1 binary data or some
+            # shit and the straightforward version (pycompat.to_native) won't
+            # work right -> always encode message to bytes then use the
+            # relevant method depending on ~python version
+        if isinstance(message, pycompat.text_type):
             message = message.encode('utf-8')
-        msg_txt = email.message_from_string(message)
-        msg = self.message_parse_basic_data(msg_txt)
+        extract = getattr(email, 'message_from_bytes',
+                          email.message_from_string)
+        msg_txt = extract(message)
+        msg = self.message_parse(msg_txt)
         _logger.info(
             'Fetched mail from %s to %s with Message-Id %s',
             msg.get('from'), msg.get('to'), msg.get('message_id'))
