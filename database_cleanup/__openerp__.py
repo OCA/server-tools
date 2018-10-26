acd2703d40d11b158ba5e92315550ece78174a2b
Martin Trigaux <me@mart-e.be>
2018-10-26 19:18:12 +0200

    [MIG] database_cleanup: migration to 12.0
    Update version number
    Use correct import
    Update .pot to match new syntax
    Adapt icon to fontawesome
    Add models description
    Rename field external id to new syntax <model>__<field>

--- database_cleanup/__openerp__.py
+++ database_cleanup/__openerp__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Database cleanup',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': "Therp BV,Odoo Community Association (OCA)",
     'depends': ['base'],
     'license': 'AGPL-3',
