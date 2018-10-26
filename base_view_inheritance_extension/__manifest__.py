2ddfa364e5eca1d23df88010eb1595eb2181a386
Sergio Teruel <sergio.teruel@tecnativa.com>
2018-10-26 13:54:14 +0200

    [MIG] base_view_inheritance_extension: Migration to 11.0

--- base_view_inheritance_extension/__manifest__.py
+++ base_view_inheritance_extension/__manifest__.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
-# © 2016 Therp BV <http://therp.nl>
+# Copyright 2016 Therp BV <https://therp.nl>
+# Copyright 2018 Tecnativa - Sergio Teruel
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Extended view inheritance",
-    "version": "10.0.1.0.1",
+    "version": "11.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Hidden/Dependency",
