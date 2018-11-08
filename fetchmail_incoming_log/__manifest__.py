7d731895a7fed32077f94ec86d523cce8ba99b8f
hveficent <hector.villarreal@eficent.com>
2018-11-08 13:54:40 +0100

    [RENAME] mail_log_message_to_process -> fetchmail_incoming_log

---
+++ fetchmail_incoming_log/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2017 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Fetchmail Incoming Log',
+    'version': '11.0.1.0.0',
+    'category': 'Tools',
+    'summary': 'Log all messages received, before they start to be processed.',
+    'author': "Eficent, "
+              "Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'website': 'https://github.com/OCA/server-tools',
+    'depends': ['mail'],
+    'installable': True,
+}
