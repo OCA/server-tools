27ad43f9ff5b8ba68f127f56f2510da0e48c43d4
mreficent <miquel.raich@eficent.com>
2018-11-19 10:11:18 +0100

    [PATCH] [ADD] date_range: multicompany tests

--- date_range/tests/test_date_range_type.py
+++ date_range/tests/test_date_range_type.py
@@ -5,17 +5,29 @@
 from odoo.tests.common import TransactionCase
 from odoo.tools import mute_logger
 from psycopg2 import IntegrityError
+from odoo.exceptions import ValidationError
 
 
 class DateRangeTypeTest(TransactionCase):
 
+    def setUp(self):
+        super(DateRangeTypeTest, self).setUp()
+        self.type = self.env['date.range.type']
+        self.company = self.env['res.company'].create({
+            'name': 'Test company',
+        })
+        self.company_2 = self.env['res.company'].create({
+            'name': 'Test company 2',
+            'parent_id': self.company.id,
+        })
+
     def test_default_company(self):
-        drt = self.env['date.range.type'].create(
+        drt = self.type.create(
             {'name': 'Fiscal year',
              'allow_overlap': False})
         self.assertTrue(drt.company_id)
         # you can specify company_id to False
-        drt = self.env['date.range.type'].create(
+        drt = self.type.create(
             {'name': 'Fiscal year',
              'company_id': False,
              'allow_overlap': False})
@@ -34,3 +46,18 @@ class DateRangeTypeTest(TransactionCase):
         })
         with self.assertRaises(IntegrityError), mute_logger('odoo.sql_db'):
             drt.unlink()
+
+    def test_type_multicompany(self):
+        drt = self.type.create(
+            {'name': 'Fiscal year',
+             'company_id': False,
+             'allow_overlap': False})
+        self.env['date.range'].create({
+            'name': 'FS2016',
+            'date_start': '2015-01-01',
+            'date_end': '2016-12-31',
+            'type_id': drt.id,
+            'company_id': self.company.id,
+        })
+        with self.assertRaises(ValidationError):
+            drt.company_id = self.company_2
