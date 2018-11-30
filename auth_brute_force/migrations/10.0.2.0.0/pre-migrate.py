58d243d7398d5e70d3d718ebb89e5d42e332c4d5
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-11-30 17:01:13 +0100

    [FIX] auth_brute_force: flake8

--- auth_brute_force/migrations/10.0.2.0.0/pre-migrate.py
+++ auth_brute_force/migrations/10.0.2.0.0/pre-migrate.py
@@ -11,7 +11,7 @@ def migrate(cr, version):
     # Don't act when coming from version 9
     cr.execute("SELECT 1 FROM pg_class WHERE relname = 'res_banned_remote'")
     if not cr.fetchone():
-        return 
+        return
     # Fix typo across DB
     cr.execute(
         """ UPDATE res_authentication_attempt
