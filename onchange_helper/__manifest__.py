6beff1e64ad41c7c1ba2fbfdc757aa06523dda9a
sbejaoui <souheil.bejaoui@acsone.eu>
2018-12-17 19:29:54 +0100

    [IMP] - read may be harmful to performance

--- onchange_helper/__manifest__.py
+++ onchange_helper/__manifest__.py
@@ -2,13 +2,15 @@
 # © 2016-2017 Akretion (http://www.akretion.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-{'name': 'Onchange Helper',
- 'version': '10.0.1.0.0',
- 'summary': 'Technical module that ease execution of onchange in Python code',
- 'author': 'Akretion,Camptocamp,Odoo Community Association (OCA)',
- 'website': 'http://www.akretion.com',
- 'license': 'AGPL-3',
- 'category': 'Generic Modules',
- 'depends': ['base'],
- 'installable': True,
- }
+{
+    'name': 'Onchange Helper',
+    'version': '10.0.1.0.0',
+    'summary': 'Technical module that ease execution'
+    ' of onchange in Python code',
+    'author': 'Akretion,Camptocamp,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/server-tools',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules',
+    'depends': ['base'],
+    'installable': True,
+}
