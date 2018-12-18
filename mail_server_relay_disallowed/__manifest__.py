22a3361609f337bae0c1785d881582981b7a4218
Adrià Gil Sorribes <adria.gil@eficent.com>
2018-12-18 13:37:33 +0100

    [ADD] Add module mail_server_relay_disallowed to v12

---
+++ mail_server_relay_disallowed/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mail server relay disallowed",
+    "version": "12.0.1.0.0",
+    "author": "ForgeFlow, "
+              "Project Expert Team, "
+              "Odoo Community Association (OCA)",
+    "contributors": ["Jordi Ballester <jordi.ballester@forgeflow.com>"],
+    "website": "http://project.expert",
+    "license": "AGPL-3",
+    "category": "Social Network",
+    "depends": ["base"],
+    "installable": True,
+}
---
+++ mail_server_relay_disallowed/readme/CONTRIBUTORS.rst
@@ -0,0 +1,1 @@
+* ForgeFlow <https://www.forgeflow.com>
