f5fad26a4a88471e3b12e1782759bc36f39c4ddc
sbejaoui <souheil.bejaoui@acsone.eu>
2018-12-18 17:29:51 +0100

    [IMP] - get default values for non record onchange call
    other improvement:
    
    set onchange_specs to all fields as _onchange_spec()
    retrun onchange fields for default view
    return field value if it's set in onchange_fields (usuful to get default value for a field)

--- onchange_helper/models/models.py
+++ onchange_helper/models/models.py
@@ -22,9 +22,12 @@ class Base(models.AbstractModel):
         return new_values
 
     def play_onchanges(self, values, onchange_fields):
-        onchange_specs = self._onchange_spec()
+        # _onchange_spec() will return onchange fields from the default view
         # we need all fields in the dict even the empty ones
         # otherwise 'onchange()' will not apply changes to them
+        onchange_specs = {
+            field_name: '1' for field_name, field in self._fields.items()
+        }
         all_values = values.copy()
 
         # If self is a record (play onchange on existing record)
@@ -39,7 +42,8 @@ class Base(models.AbstractModel):
                 }
             )
         else:
-            record_values = {}
+            # We get default values, they may be used in onchange
+            record_values = self.default_get(self._fields.keys())
         for field in self._fields:
             if field not in all_values:
                 all_values[field] = record_values.get(field, False)
@@ -51,6 +55,8 @@ class Base(models.AbstractModel):
             all_values.update(new_values)
 
         return {
-            f: v for f, v in all_values.iteritems()
+            f: v
+            for f, v in all_values.items()
             if not (self._fields[f].compute and not self._fields[f].inverse)
-            and (f in values or f in new_values)}
+            and (f in values or f in new_values)
+        }
