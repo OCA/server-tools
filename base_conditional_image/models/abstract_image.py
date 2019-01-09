7c7bcf46b9741479be2d34e74cc3b0636e1cff1d
Patrick Tombez <patrick.tombez@camptocamp.com>
2019-01-09 15:31:14 +0100

    Improve readability

--- base_conditional_image/models/abstract_image.py
+++ base_conditional_image/models/abstract_image.py
@@ -22,32 +22,41 @@ class AbstractImageStorage(models.AbstractModel):
         store=False, readonly=True
     )
 
+    @staticmethod
+    def _compute_selector_test_without_company(image_selector, record):
+        return bool(
+            safe_eval(image_selector.selector or "True", {'object': record})
+        )
+
+    @staticmethod
+    def _compute_selector_test_with_company(image_selector, record):
+        if (image_selector.company_id == record.company_id or
+                record.company_id and not image_selector.company_id):
+            return AbstractImageStorage._compute_selector_test_without_company(
+                image_selector, record
+            )
+        return False
+
     def _compute_image(self):
         if 'company_id' in self._fields:
-            images = self.env['image'].search(
-                [('model_name', '=', self._name)],
-                order='company_id, selector'
-            )
-            def test_method(image, rec):
-                return ((image.company_id == rec.company_id or
-                         rec.company_id and not image.company_id) and
-                        safe_eval(image.selector or "True", {'object': rec}))
+            search_clause = [('model_name', '=', self._name)]
+            test_method = self._compute_selector_test_with_company
         else:
             # If inherited object doesn't have a `company_id` field,
             # remove the items with a company defined and the related checks
-            images = self.env['image'].search(
-                [('model_name', '=', self._name),
-                 ('company_id', '=', False)],
-                order='company_id, selector'
-            )
-            def test_method(image, rec):
-                return safe_eval(image.selector or "True", {'object': rec})
+            search_clause = [('model_name', '=', self._name),
+                             ('company_id', '=', False)]
+            test_method = self._compute_selector_test_without_company
+
+        image_selectors = self.env['image'].search(
+            search_clause, order='company_id, selector'
+        )
 
         for rec in self:
             found = None
-            for image in images:
-                if test_method(image, rec):
-                    found = image
+            for image_selector in image_selectors:
+                if test_method(image_selector, rec):
+                    found = image_selector
                     break
 
             rec.update({
