ac299809a76968ccbc0c31ee0a45febe9e827224
Benoit <benoit.guillot@akretion.com>
2019-01-21 14:06:40 +0100

    [FIX] don't remove computed fields with inverse method of the onchange values

--- onchange_helper/models/base.py
+++ onchange_helper/models/base.py
@@ -64,6 +64,6 @@ class Base(models.AbstractModel):
         return {
             f: v
             for f, v in all_values.items()
-            if not self._fields[f].compute
+            if not (self._fields[f].compute and not self._fields[f].inverse)
             and (f in values or f in new_values or f in onchange_fields)
         }
