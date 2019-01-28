68a12f49c0ea6a07a1125df1ac6ded55e4e1e4b7
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-01-28 11:41:58 +0100

    [UPD] server_tools: bump version

--- onchange_helper/__manifest__.py
+++ onchange_helper/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Onchange Helper',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'summary': 'Technical module that ease execution'
     ' of onchange in Python code',
     'author': 'Akretion,Camptocamp,Odoo Community Association (OCA)',
