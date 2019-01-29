0824ef11c96df5e59a6c6e2e77bce2e048fc6e79
Florent THOMAS <mailinglist@tdeo.fr>
2019-01-29 23:09:39 +0100

    [FIX] sale_exception: Make a defensive check before processing table and model rename (#1486)

--- base_exception/migrations/10.0.2.0.0/pre-migration.py
+++ base_exception/migrations/10.0.2.0.0/pre-migration.py
@@ -8,5 +8,6 @@ from openupgradelib import openupgrade
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     cr = env.cr
-    openupgrade.rename_tables(cr, [('sale_exception', 'exception_rule')])
-    openupgrade.rename_models(cr, [('sale.exception', 'exception.rule')])
+    if openupgrade.table_exists('sale_exception'):
+        openupgrade.rename_tables(cr, [('sale_exception', 'exception_rule')])
+        openupgrade.rename_models(cr, [('sale.exception', 'exception.rule')])
