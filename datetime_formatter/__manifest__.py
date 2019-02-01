9a0cbc1bef05648ea68c217956f4ecda9ec3017d
ernesto <ernesto.tejeda87@gmail.com>
2019-02-01 10:55:06 -0500

    [MIG] datetime_formatter: Migration to 12.0

--- datetime_formatter/__manifest__.py
+++ datetime_formatter/__manifest__.py
@@ -6,11 +6,11 @@
 {
     "name": "Date & Time Formatter",
     "summary": "Helper functions to give correct format to date[time] fields",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Tools",
-    "website": "https://www.tecnativa.com",
+    "website": "https://github.com/OCA/server-tools",
     "author": "Grupo ESOC Ingeniería de Servicios, "
-              "Tecnativa,"
+              "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
