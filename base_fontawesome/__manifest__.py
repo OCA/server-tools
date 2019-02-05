9326eddeccc3412d6422406e606a0d3a6191c263
Cesar Castro Cruz <cesar.castro@syspro.es>
2019-02-05 15:58:58 +0100

    Update to version 5.7.1

--- base_fontawesome/__manifest__.py
+++ base_fontawesome/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Base Fontawesome',
     'summary': """Up to date Fontawesome resources.""",
-    'version': '11.0.5.5.0',
+    'version': '11.0.5.7.1',
     'license': 'LGPL-3',
     'author': 'Camptocamp,Creu Blanca,Odoo Community Association (OCA)',
     'depends': [
