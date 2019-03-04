a6083ab95f11379a87a83b2c42f8253d5082f5b8
Cristina Martin <cristina.martin@tecnativa.com>
2019-03-04 11:39:36 +0100

    [12.0][MIG] scheduler_error_mailer: Migration to v12
    [UPD] README.rst
    
    [UPD] Update scheduler_error_mailer.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: server-tools-12.0/server-tools-12.0-scheduler_error_mailer
    Translate-URL: https://translation.odoo-community.org/projects/server-tools-12-0/server-tools-12-0-scheduler_error_mailer/
    
    [UPD] README.rst

--- scheduler_error_mailer/models/__init__.py
+++ scheduler_error_mailer/models/__init__.py
@@ -1,6 +1,3 @@
-# © 2012-2013 Akretion Sébastien BEAU,David Beal,Alexis de Lattre
-# © 2016 Sodexis
-# © 2018 bloopark systems (<http://bloopark.de>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import ir_cron
