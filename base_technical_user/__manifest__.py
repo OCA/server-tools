c90562e9941e4081e47a347d0f148db7fba8b791
Florian da Costa <florian.dacosta@akretion.com>
2019-04-04 22:12:43 +0200

    Migrate base_technical_user to version 12

--- base_technical_user/__manifest__.py
+++ base_technical_user/__manifest__.py
@@ -5,9 +5,9 @@
     'summary': """
         Add a technical user parameter on the company """,
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
-    'website': "http://acsone.eu",
+    'website': "https://github.com/OCA/server-tools/",
     'category': 'Hidden/Dependency',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'base',
