2d176465b33001ee8b1bf50cf59bcaa090d5a8c6
David Beal <david.beal@akretion.com>
2019-04-08 17:05:08 +0200

    PORT v12 sql_export

--- sql_export/__manifest__.py
+++ sql_export/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2015 Akretion (<http://www.akretion.com>)
 # @author: Florian da Costa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'SQL Export',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
     'license': 'AGPL-3',
