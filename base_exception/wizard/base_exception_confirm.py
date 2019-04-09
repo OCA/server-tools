80ac0908eee5ad8a81190fbb52f6775ec5cd9387
Iván Todorovich <ivan.todorovich@gmail.com>
2019-04-09 11:47:13 +0000

    [MIG] base_exception: Migration to 12.0

--- base_exception/wizard/base_exception_confirm.py
+++ base_exception/wizard/base_exception_confirm.py
@@ -2,19 +2,20 @@
 # Copyright 2017 Akretion (http://www.akretion.com)
 # Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 
 class ExceptionRuleConfirm(models.AbstractModel):
-
     _name = 'exception.rule.confirm'
+    _description = 'Exception Rule Confirm Wizard'
 
     related_model_id = fields.Many2one('base.exception',)
-    exception_ids = fields.Many2many('exception.rule',
-                                     string='Exceptions to resolve',
-                                     readonly=True)
+    exception_ids = fields.Many2many(
+        'exception.rule',
+        string='Exceptions to resolve',
+        readonly=True,
+    )
     ignore = fields.Boolean('Ignore Exceptions')
 
     @api.model
