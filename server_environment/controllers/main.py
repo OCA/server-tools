46210f8a877c9889db66ddbe792311b03b48cf32
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-04-25 12:59:00 +0200

    [FIX] typo ; [REF] use web.base.url instead of local_redirect

--- server_environment/controllers/main.py
+++ server_environment/controllers/main.py
@@ -3,11 +3,15 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import openerp.http as http
+from werkzeug.utils import redirect
+from urlparse import urljoin
+
 from openerp.tools.config import config
+from openerp import http
+from openerp.http import request, Controller
 
 
-class ServerEnvironmentController(http.Controller):
+class ServerEnvironmentController(Controller):
 
     @http.route(
         '/server_environment_<module_extension>'
@@ -16,9 +20,12 @@ class ServerEnvironmentController(http.Controller):
     def environment_redirect(self, module_extension, local_path, **kw):
         # Note: module_extension is present to make working
         # the module in normal configuration, with the folder
-        # server_environment_files and in demo configuration, withe the
+        # server_environment_files and in demo configuration, with the
         # module  server_environment_files_sample
         running_env = config.get('running_env', "default")
-        return http.local_redirect(
-            '/server_environment_%s/static/%s/%s' % (
-                module_extension, running_env, local_path))
+        IrConfigParameter = request.env['ir.config_parameter']
+        base_url = IrConfigParameter.get_param('web.base.url', '')
+        new_path = "/server_environment_%s/static/%s/%s" % (
+            module_extension, running_env, local_path)
+        url = urljoin(base_url, new_path)
+        return redirect(url, 303)
