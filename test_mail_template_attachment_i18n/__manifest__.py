82d34420acb1a6d4d812962652c969f1908c0df6
Dennis Sluijk <d.sluijk@onestein.nl>
2019-04-25 18:14:42 +0200

    [ADD] mail_template_attachment_i18n: Mail Template Language Dependent Attachments (#1367)

---
+++ test_mail_template_attachment_i18n/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': ' Tests for mail_template_attachment_i18n',
+    'summary': 'Test suite for mail_template_attachment_i18n.',
+    'author': 'Onestein,Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/server-tools',
+    'category': 'Generic Modules',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'mail_template_attachment_i18n',
+        'sale'
+    ],
+    'installable': True,
+}
