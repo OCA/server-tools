612f8a70a7170dceb40f6b1ed8e349fd0d6d6da9
Robin Hède <35724895+r-hede@users.noreply.github.com>
2019-05-03 14:33:24 +0200

    [10.0] [FIX] users_ldap_populate special characters (#1564)
    * [10.0] users_ldap_populate special characters

--- users_ldap_populate/tests/test_users_ldap_populate.py
+++ users_ldap_populate/tests/test_users_ldap_populate.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp.tests.common import TransactionCase
 from contextlib import contextmanager
+from odoo.tests.common import TransactionCase
 
 
 class PatchLDAPConnection(object):
