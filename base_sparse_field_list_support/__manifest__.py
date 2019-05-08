b7a427b87572e15e4c773ac7f35d45ce1c8e4240
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-05-08 16:12:21 +0200

    [IMP] Add pre-commit hook and reformat code thanks to black

--- base_sparse_field_list_support/__manifest__.py
+++ base_sparse_field_list_support/__manifest__.py
@@ -3,13 +3,11 @@
     "name": "Base Sparse Field List Support",
     "summary": "add list support to convert_to_cache()",
     "version": "10.0.1.0.0",
-    'category': 'Technical Settings',
+    "category": "Technical Settings",
     "website": "www.akretion.com",
     "author": "Akretion",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "base",
-    ],
+    "depends": ["base"],
 }
