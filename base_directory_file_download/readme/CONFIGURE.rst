862122b2f9c6eeb0258201eac6be55b04f609d96
Andrea <a.stirpe@onestein.nl>
2019-05-22 09:16:14 +0200

    Add readme fragments + code review

---
+++ base_directory_file_download/readme/CONFIGURE.rst
@@ -0,0 +1,3 @@
+To configure this module, you need to:
+
+#. Set the group "Download files of directory" for the users who need this functionality.
--- base_directory_file_download/tests/test_directory_files_download.py
+++ base_directory_file_download/tests/test_directory_files_download.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2018 Onestein (<http://www.onestein.eu>)
+# Copyright 2017-2018 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 import os
