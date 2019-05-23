2566b8bef9ae99d253597bf47defa04ce9633328
Robin Chatfield <robin.chatfield@vertel.se>
2019-05-23 15:23:31 +0000

    [12.0]letsencrypt: Updated letsencrypt to Odoo 12.0.

---
+++ letsencrypt/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Let's encrypt",
+    "version": "12.0.1.0.0",
+    "author": "Therp BV,"
+              "Tecnativa,"
+              "Vertel AB,"
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Request SSL certificates from letsencrypt.org",
+    "depends": [
+        'base',
+    ],
+    "data": [
+        "data/ir_config_parameter.xml",
+        "data/ir_cron.xml",
+        "demo/ir_cron.xml",
+    ],
+    "post_init_hook": 'post_init_hook',
+    'installable': True,
+    "external_dependencies": {
+        'bin': [
+            'openssl',
+        ],
+        'python': [
+            'acme_tiny',
+            'IPy',
+        ],
+    },
+}
