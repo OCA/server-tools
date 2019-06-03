d34d14d2767fe64cfad768026f7e8baeeb0e246e
Andrea <a.stirpe@onestein.nl>
2019-06-03 10:28:56 +0200

    [11.0][MIG] auto_backup_download

--- auto_backup_download/__manifest__.py
+++ auto_backup_download/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-2019 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
@@ -8,7 +7,7 @@
     'author': 'Onestein, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/server-tools',
     'category': 'Tools',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'base_setup',
