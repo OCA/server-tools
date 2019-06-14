0be04f42f47878225f79c7e96147f759deaf6d45
mreficent <miquel.raich@eficent.com>
2019-06-14 13:43:57 +0200

    [MIG] fetchmail_incoming_log: Migration to 12.0

--- fetchmail_incoming_log/__manifest__.py
+++ fetchmail_incoming_log/__manifest__.py
@@ -3,13 +3,15 @@
 
 {
     'name': 'Fetchmail Incoming Log',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Tools',
     'summary': 'Log all messages received, before they start to be processed.',
     'author': "Eficent, "
               "Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'website': 'https://github.com/OCA/server-tools',
-    'depends': ['mail'],
+    'depends': [
+        'mail', 'test_mail',
+    ],
     'installable': True,
 }
