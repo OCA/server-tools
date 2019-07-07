46d138b05f8a61b19a03e1d5f3b30fc94450133e
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-07-07 18:03:38 +0200

    [ADD] new module module_analysis
    fixup! [ADD] new module module_analysis
    
    fixup! fixup! [ADD] new module module_analysis
    
    fixup! fixup! fixup! [ADD] new module module_analysis
    
    fixup! fixup! fixup! fixup! [ADD] new module module_analysis
    
    IMP exception message
    
    fixup! fixup! fixup! fixup! fixup! [ADD] new module module_analysis
    
    [REF] remove use of cloc. use pygount librairy instead
    
    fixup! [REF] remove use of cloc. use pygount librairy instead
    
    fixup! fixup! [REF] remove use of cloc. use pygount librairy instead
    
    Apply suggestions from code review
    
    Co-Authored-By: David Beal <david.beal@akretion.com>
    
    Update module_analysis/views/menu.xml
    
    Co-Authored-By: David Beal <david.beal@akretion.com>
    
    Update module_analysis/tests/test_module.py
    
    Co-Authored-By: David Beal <david.beal@akretion.com>
    
    Update module_analysis/readme/CONFIGURE.rst
    
    Co-Authored-By: David Beal <david.beal@akretion.com>
    
    [IMP] handle encoding
    
    [UPD] Update module_analysis.pot
    
    [UPD] README.rst
    
    [UPD] README.rst

---
+++ module_analysis/post_init_hook.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2019-Today: GRAP (<http://www.grap.coop/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, SUPERUSER_ID
+
+
+def analyse_installed_modules(cr, registry):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        installed_modules = env['ir.module.module'].search(
+            [('state', '=', 'installed')])
+        installed_modules.button_analyse_code()
