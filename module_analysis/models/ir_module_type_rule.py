6615203c3052b67bb077a4b488fae48486cd867e
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-07-07 18:03:38 +0200

    [ADD] new module module_analysis
    fixup! [ADD] new module module_analysis
    
    fixup! fixup! [ADD] new module module_analysis
    
    fixup! fixup! fixup! [ADD] new module module_analysis
    
    fixup! fixup! fixup! fixup! [ADD] new module module_analysis
    
    IMP exception message
    
    fixup! fixup! fixup! fixup! fixup! [ADD] new module module_analysis
    
    [REF] remove use of cloc. use pygount librairy instead
    
    fixup! [REF] remove use of cloc. use pygount librairy instead
    
    fixup! fixup! [REF] remove use of cloc. use pygount librairy instead
    
    Apply suggestions from code review
    
    Co-Authored-By: David Beal <david.beal@akretion.com>
    
    Update module_analysis/views/menu.xml
    
    Co-Authored-By: David Beal <david.beal@akretion.com>
    
    Update module_analysis/tests/test_module.py
    
    Co-Authored-By: David Beal <david.beal@akretion.com>
    
    Update module_analysis/readme/CONFIGURE.rst
    
    Co-Authored-By: David Beal <david.beal@akretion.com>
    
    [IMP] handle encoding
    
    [UPD] Update module_analysis.pot
    
    [UPD] README.rst
    
    [UPD] README.rst

---
+++ module_analysis/models/ir_module_type_rule.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2019-Today: GRAP (<http://www.grap.coop/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+from odoo.tools.safe_eval import safe_eval
+
+
+class IrModuleType(models.Model):
+    _name = 'ir.module.type.rule'
+    _description = 'Modules Types Rules'
+    _order = 'sequence'
+
+    sequence = fields.Integer(string='Sequence')
+
+    module_domain = fields.Char(
+        string='Module Domain', required=True, default='[]')
+
+    module_type_id = fields.Many2one(
+        string='Module type', comodel_name='ir.module.type', required=True)
+
+    @api.multi
+    def _get_module_type_id_from_module(self, module):
+        IrModuleModule = self.env['ir.module.module']
+        for rule in self:
+            domain = safe_eval(rule.module_domain)
+            domain.append(('id', '=', module.id))
+            if IrModuleModule.search(domain):
+                return rule.module_type_id.id
+        return False
