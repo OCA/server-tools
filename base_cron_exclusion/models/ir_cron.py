b370da846e9f2d0cfffae328b38105b8c12b31dc
Lois Rilo <lois.rilo@eficent.com>
2019-08-05 12:53:54 +0200

    [12.0] base_cron_exclusion to LGPL
    Change license to LGPL to not limit the usage of this module
    in private customizations.

--- base_cron_exclusion/models/ir_cron.py
+++ base_cron_exclusion/models/ir_cron.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 import logging
 
