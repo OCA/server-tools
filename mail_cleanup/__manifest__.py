54742288bbc63cb48f840cecdf910c4e925e5cab
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2019-08-07 17:46:03 +0200

    [12.0][MIG] mail_cleanup: Migrate to 12.0

--- mail_cleanup/__manifest__.py
+++ mail_cleanup/__manifest__.py
@@ -1,14 +1,20 @@
-# Copyright 2015-2016 Matthieu Dietrich (Camptocamp SA)
+# Copyright 2015-2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Mail cleanup',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Tools',
     'summary': 'Mark as read or delete mails after a set time',
     'author': 'Camptocamp, '
               'Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'website': 'https://github.com/OCA/server-tools',
-    'depends': ['mail_environment'],
-    'data': ['views/mail_view.xml'],
+    'depends': [
+        'mail_environment',
+    ],
+    'data': [
+        'views/mail_view.xml',
+    ],
+    'installable': True,
+    'application': False,
 }
