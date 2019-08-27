0546fe417cea04fa7ed9364b989c611931f14bc9
Giovanni Francesco Capalbo <giovanni@therp.nl>
2019-08-27 11:23:17 +0200

    [FIX] remove a test

--- base_test_chrome/tests/test_base_test_chrome.py
+++ base_test_chrome/tests/test_base_test_chrome.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-import unittest
 from ..common import HttpCase
 
 
 class TestBaseTestChrome(HttpCase):
 
-    @unittest.skip(
-        'Memory leak in test lead to phantomjs crash, making it unreliable')
-    def test_base_test_chrome_web(self):
-        self.browser_js('/web/tests?mod=web', '', '', login='admin')
+    def test_base_test_chrome_basic(self):
+        self.browser_js('/web', 'console.log("ok")')
