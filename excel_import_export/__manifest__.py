048bb61df04d3fa8ef3769f8576ff07f5ceffacc
Saran <saranp502@hotmail.com>
2019-08-28 14:50:14 +0700

    [12.0][FIX] excel_import_export - Style sum footer to number

--- excel_import_export/__manifest__.py
+++ excel_import_export/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Excel Import/Export/Report',
     'summary': 'Base module for developing Excel import/export/report',
-    'version': '12.0.1.0.2',
+    'version': '12.0.1.0.4',
     'author': 'Ecosoft,Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'website': 'https://github.com/OCA/server-tools/',
