2145dce204291a5f4c6b7f3398092b3d3107f614
Dimitrios T. Tanis <dtanis@tanisfood.gr>
2019-09-10 09:06:19 +0300

    [UPD] base_custom_info backport from v9.0 to version 2.0

--- base_custom_info/models/__init__.py
+++ base_custom_info/models/__init__.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 # © 2015 Antiun Ingeniería S.L. - Sergio Teruel
 # © 2015 Antiun Ingeniería S.L. - Carlos Dauden
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
-from . import custom_info
+from . import custom_info_template
+from . import  custom_info_property
+from . import  custom_info_category
+from . import  custom_info_option
+from . import  custom_info_value
+from . import  custom_info
+from . import  res_partner
