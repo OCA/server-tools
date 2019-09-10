9eddce2370c91fc0aea17942a864852f776c065c
Dimitrios T. Tanis <dtanis@tanisfood.gr>
2019-09-10 09:19:22 +0300

    [FIX] Don't fail when having attribute values that are not ASCII
    Having an LDAP value with values out of the ASCII range fails because of
    str(). This fixes it by changing to a utf8 encoded string that works
    with international characters when used in attribute values.

--- users_ldap_push/models/res_users.py
+++ users_ldap_push/models/res_users.py
@@ -67,7 +67,7 @@ class ResUsers(models.Model):
             field_name = mapping.field_id.name
             if field_name not in values or not values[field_name]:
                 continue
-            result[str(mapping.attribute)] = [str(values[field_name])]
+            result[mapping.attribute] = [values[field_name].encode('utf-8')]
         if result:
             result['objectClass'] = conf.create_ldap_entry_objectclass\
                 .encode('utf-8').split(',')
