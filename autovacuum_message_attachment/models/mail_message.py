1d67ffd1ebd443d9de569da5f15b33ffb676a498
Florian da Costa <florian.dacosta@akretion.com>
2019-09-19 16:35:03 +0200

    Add possibility to delete attachments

--- autovacuum_message_attachment/models/mail_message.py
+++ autovacuum_message_attachment/models/mail_message.py
@@ -1,44 +1,10 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2018 Akretion
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-import logging
-
-import odoo
-from odoo import api, models
-
-_logger = logging.getLogger(__name__)
+from odoo import models
 
 
 class MailMessage(models.Model):
-    _inherit = "mail.message"
-
-    @api.multi
-    def batch_unlink(self):
-        with api.Environment.manage():
-            with odoo.registry(
-                    self.env.cr.dbname).cursor() as new_cr:
-                new_env = api.Environment(new_cr, self.env.uid,
-                                          self.env.context)
-                try:
-                    while self:
-                        batch_delete_messages = self[0:1000]
-                        self -= batch_delete_messages
-                        # do not attach new env to self because it may be
-                        # huge, and the cache is cleaned after each unlink
-                        # so we do not want to much record is the env in
-                        # which we call unlink because odoo would prefetch
-                        # fields, cleared right after.
-                        batch_delete_messages.with_env(new_env).unlink()
-                        new_env.cr.commit()
-                except Exception as e:
-                    _logger.exception(
-                        "Failed to delete messages : %s", str(e))
-
-    # Call by cron
-    @api.model
-    def autovacuum_mail_message(self):
-        rules = self.env['message.vacuum.rule'].search([])
-        for rule in rules:
-            domain = rule.get_message_domain()
-            messages = self.search(domain)
-            messages.batch_unlink()
+    _name = "mail.message"
+    _inherit = ["mail.message", "autovacuum.mixin"]
