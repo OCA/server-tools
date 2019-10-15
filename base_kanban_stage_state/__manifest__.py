179a3d43d1742033540d909b0770b0d73588aa67
mreficent <miquel.raich@eficent.com>
2019-10-15 19:14:56 +0200

    [MIG] base_kanban_stage_state: Migration to 12.0

--- base_kanban_stage_state/__manifest__.py
+++ base_kanban_stage_state/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Specialty Medical Drugstore
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Base Kanban Stage State",
     "summary": "Maps stages from base_kanban_stage to states",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Base",
-    "website": "https://odoo-community.org/",
+    "website": "https://github.com/OCA/server-tools",
     "author": "SMDrugstore, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
---
+++ base_kanban_stage_state/readme/DESCRIPTION.rst
@@ -0,0 +1,9 @@
+This module extends the functionality of base_kanban_stage to allow you to
+map stages from base_kanban_stage to states. The states are:
+
+* New (draft)
+* In Progress (open)
+* Pending (pending)
+* Done (done)
+* Cancelled (cancelled)
+* Exception (exception)
