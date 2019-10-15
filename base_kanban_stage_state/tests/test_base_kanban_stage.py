38aa463d0f4ec7807bfb795fec0e364c5cab9c93
mreficent <miquel.raich@eficent.com>
2019-10-15 19:14:56 +0200

    [MIG] base_kanban_stage_state: Migration to 12.0

--- base_kanban_stage_state/tests/test_base_kanban_stage.py
+++ base_kanban_stage_state/tests/test_base_kanban_stage.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Specialty Medical Drugstore
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
@@ -7,7 +6,7 @@ from odoo.tests.common import TransactionCase
 
 class TestBaseKanbanStage(TransactionCase):
     def test_get_states(self):
-        '''It should return a list of stages'''
+        """It should return a list of stages"""
         test_stage = self.env['base.kanban.stage'].with_context({})
 
         self.assertEqual(test_stage._get_states(),
