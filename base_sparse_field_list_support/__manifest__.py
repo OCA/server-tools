edfc67bf80344558b1e0b0ea8e072b1e6e131d2e
Laurent-Corron <laurent.corron@acsone.eu>
2019-10-22 09:51:56 +0200

    base_sparse_field_support: Migration to 13.0

--- base_sparse_field_list_support/__manifest__.py
+++ base_sparse_field_list_support/__manifest__.py
@@ -1,12 +1,12 @@
 {
     "name": "Base Sparse Field List Support",
     "summary": "add list support to convert_to_cache()",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Technical Settings",
     "website": "www.akretion.com",
     "author": "Akretion",
     "license": "AGPL-3",
     "application": False,
-    "installable": False,
+    "installable": True,
     "depends": ["base", "base_sparse_field"],
 }
