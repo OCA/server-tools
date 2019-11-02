2296bc2271de7b60097cfd689310df3ff1b9cb21
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-02 11:15:54 +0100

    base_jsonify 13.0.1.0.1

--- base_jsonify/__manifest__.py
+++ base_jsonify/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Base Jsonify",
     "summary": "Base module that provide the jsonify method on all models",
-    "version": "13.0.1.0.1",
+    "version": "13.0.1.0.2",
     "category": "Uncategorized",
     "website": "https://github.com/OCA/server-tools",
     "author": "Akretion, Odoo Community Association (OCA)",
