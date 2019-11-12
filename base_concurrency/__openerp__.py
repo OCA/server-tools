52d35e5b8d0bbab4a29ed34948f33510bdaea672
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-12 09:58:44 +0100

    [FIX] base_concurrency: RST syntax

--- base_concurrency/__openerp__.py
+++ base_concurrency/__openerp__.py
@@ -28,9 +28,10 @@
 Module to regroup all workarounds/fixes to avoid concurrency issues in SQL.
 
 * res.users login_date:
-the login date is now separated from res.users; on long transactions,
-"re-logging" by opening a new tab changes the current res.user row,
-which creates concurrency issues with PostgreSQL in the first transaction.
+
+  the login date is now separated from res.users; on long transactions,
+  "re-logging" by opening a new tab changes the current res.user row,
+  which creates concurrency issues with PostgreSQL in the first transaction.
 
 This creates a new table and a function field to avoid this. In order to
 avoid breaking modules which access via SQL the login_date column, a cron
