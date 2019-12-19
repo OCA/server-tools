e2060082bd21f798b4e9ba65920ab22900877a56
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-19 08:59:30 +0100

    [12.0][FIX] base_custom_info

--- base_custom_info/__manifest__.py
+++ base_custom_info/__manifest__.py
@@ -7,7 +7,7 @@
     'name': "Base Custom Info",
     'summary': "Add custom field in models",
     'category': 'Tools',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'depends': [
         'base_setup',
     ],
