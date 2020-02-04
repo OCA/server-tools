5d25263804f6158e574d5aee3334a01a0fd81524
Andrea <a.stirpe@onestein.nl>
2020-02-04 12:55:17 +0100

    [13.0][ADD] base_changeset

---
+++ base_changeset/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2015-2017 Camptocamp SA
+# Copyright 2020 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Track record changesets",
+    "version": "13.0.1.0.0",
+    "development_status": "Alpha",
+    "author": "Onestein, Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["astirpe"],
+    "license": "AGPL-3",
+    "category": "Tools",
+    "depends": ["web"],
+    "website": "https://github.com/OCA/server-tools/",
+    "data": [
+        "security/groups.xml",
+        "security/ir.model.access.csv",
+        "security/rules.xml",
+        "templates/assets.xml",
+        "views/record_changeset_views.xml",
+        "views/record_changeset_change_views.xml",
+        "views/changeset_field_rule_views.xml",
+        "views/menu.xml",
+    ],
+    "demo": ["demo/changeset_field_rule.xml"],
+    "qweb": ["static/src/xml/backend.xml"],
+    "installable": True,
+}
---
+++ base_changeset/models/__init__.py
@@ -0,0 +1,6 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import base
+from . import record_changeset
+from . import record_changeset_change
+from . import changeset_field_rule
