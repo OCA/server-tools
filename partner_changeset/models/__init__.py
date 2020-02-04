e9bd969dff62bebdad99b283aea683521b2679e8
Andrea <a.stirpe@onestein.nl>
2020-02-04 12:55:17 +0100

    [13.0][ADD] base_changeset

--- partner_changeset/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-2017 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import res_partner
-from . import res_partner_changeset
-from . import changeset_field_rule
