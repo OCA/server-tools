8a11b83e156cb3c3ff9d98058f183936d24933c3
Daniels Andersons <daniels.andersons@avoin.systems>
2020-02-27 14:40:31 +0200

    [MIG] sentry: Migration to 13.0

--- sentry/tests/test_logutils.py
+++ sentry/tests/test_logutils.py
@@ -9,69 +9,62 @@ from ..logutils import SanitizeOdooCookiesProcessor
 
 
 class TestOdooCookieSanitizer(unittest.TestCase):
-
     def test_cookie_as_string(self):
         data = {
-            'request': {
-                'cookies': 'website_lang=en_us;'
-                           'session_id=hello;'
-                           'Session_ID=hello;'
-                           'foo=bar',
-            },
+            "request": {
+                "cookies": "website_lang=en_us;"
+                "session_id=hello;"
+                "Session_ID=hello;"
+                "foo=bar"
+            }
         }
 
         proc = SanitizeOdooCookiesProcessor(mock.Mock())
         result = proc.process(data)
 
-        self.assertTrue('request' in result)
-        http = result['request']
+        self.assertTrue("request" in result)
+        http = result["request"]
         self.assertEqual(
-            http['cookies'],
-            'website_lang=en_us;'
-            'session_id={m};'
-            'Session_ID={m};'
-            'foo=bar'.format(
-                m=proc.MASK,
-            ),
+            http["cookies"],
+            "website_lang=en_us;"
+            "session_id={m};"
+            "Session_ID={m};"
+            "foo=bar".format(m=proc.MASK),
         )
 
     def test_cookie_as_string_with_partials(self):
-        data = {
-            'request': {
-                'cookies': 'website_lang=en_us;session_id;foo=bar',
-            },
-        }
+        data = {"request": {"cookies": "website_lang=en_us;session_id;foo=bar"}}
 
         proc = SanitizeOdooCookiesProcessor(mock.Mock())
         result = proc.process(data)
 
-        self.assertTrue('request' in result)
-        http = result['request']
+        self.assertTrue("request" in result)
+        http = result["request"]
         self.assertEqual(
-            http['cookies'],
-            'website_lang=en_us;session_id;foo=bar'.format(m=proc.MASK),
+            http["cookies"], "website_lang=en_us;session_id;foo=bar".format(m=proc.MASK)
         )
 
     def test_cookie_header(self):
         data = {
-            'request': {
-                'headers': {
-                    'Cookie': 'foo=bar;'
-                              'session_id=hello;'
-                              'Session_ID=hello;'
-                              'a_session_id_here=hello',
-                },
-            },
+            "request": {
+                "headers": {
+                    "Cookie": "foo=bar;"
+                    "session_id=hello;"
+                    "Session_ID=hello;"
+                    "a_session_id_here=hello"
+                }
+            }
         }
 
         proc = SanitizeOdooCookiesProcessor(mock.Mock())
         result = proc.process(data)
 
-        self.assertTrue('request' in result)
-        http = result['request']
+        self.assertTrue("request" in result)
+        http = result["request"]
         self.assertEqual(
-            http['headers']['Cookie'],
-            'foo=bar;'
-            'session_id={m};'
-            'Session_ID={m};'
-            'a_session_id_here={m}'.format(m=proc.MASK))
+            http["headers"]["Cookie"],
+            "foo=bar;"
+            "session_id={m};"
+            "Session_ID={m};"
+            "a_session_id_here={m}".format(m=proc.MASK),
+        )
