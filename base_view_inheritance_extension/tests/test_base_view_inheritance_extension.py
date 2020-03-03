923a752ae1dbc9121aa0d671152793344276c5c6
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-03-03 19:35:48 +0100

    [IMP] base_view_inheritance_extension: black, isort

--- base_view_inheritance_extension/tests/test_base_view_inheritance_extension.py
+++ base_view_inheritance_extension/tests/test_base_view_inheritance_extension.py
@@ -1,32 +1,29 @@
 # Copyright 2016 Therp BV <http://therp.nl>
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from lxml import etree
+
 from odoo.tests.common import TransactionCase
 
 
 class TestBaseViewInheritanceExtension(TransactionCase):
-
     def test_base_view_inheritance_extension(self):
-        view_id = self.env.ref('base.view_partner_simple_form').id
-        fields_view_get = self.env['res.partner'].fields_view_get(
-            view_id=view_id
-        )
-        view = etree.fromstring(fields_view_get['arch'])
+        view_id = self.env.ref("base.view_partner_simple_form").id
+        fields_view_get = self.env["res.partner"].fields_view_get(view_id=view_id)
+        view = etree.fromstring(fields_view_get["arch"])
         # verify normal attributes work
-        self.assertEqual(view.xpath('//form')[0].get('string'), 'Partner form')
+        self.assertEqual(view.xpath("//form")[0].get("string"), "Partner form")
         # verify our extra context key worked
         self.assertTrue(
-            'default_name' in
-            view.xpath('//field[@name="parent_id"]')[0].get('context')
+            "default_name" in view.xpath('//field[@name="parent_id"]')[0].get("context")
         )
         self.assertTrue(
-            "context.get('company_id', context.get('company'))" in
-            view.xpath('//field[@name="parent_id"]')[0].get('context')
+            "context.get('company_id', context.get('company'))"
+            in view.xpath('//field[@name="parent_id"]')[0].get("context")
         )
 
     def test_list_add(self):
-        view_model = self.env['ir.ui.view']
-        inherit_id = self.env.ref('base.view_partner_form').id
+        view_model = self.env["ir.ui.view"]
+        inherit_id = self.env.ref("base.view_partner_form").id
         source = etree.fromstring(
             """\
             <form>
@@ -45,15 +42,11 @@ class TestBaseViewInheritanceExtension(TransactionCase):
             </button>
             """
         )
-        modified_source = \
-            view_model.inheritance_handler_attributes_list_add(
-                source, specs, inherit_id
-            )
-        button_node = modified_source.xpath('//button[@name="test"]')[0]
-        self.assertEqual(
-            button_node.attrib['states'],
-            'draft,open,valid'
+        modified_source = view_model.inheritance_handler_attributes_list_add(
+            source, specs, inherit_id
         )
+        button_node = modified_source.xpath('//button[@name="test"]')[0]
+        self.assertEqual(button_node.attrib["states"], "draft,open,valid")
         # extend with list of values
         specs = etree.fromstring(
             """\
@@ -65,19 +58,15 @@ class TestBaseViewInheritanceExtension(TransactionCase):
             </button>
             """
         )
-        modified_source = \
-            view_model.inheritance_handler_attributes_list_add(
-                source, specs, inherit_id
-            )
-        button_node = modified_source.xpath('//button[@name="test"]')[0]
-        self.assertEqual(
-            button_node.attrib['states'],
-            'draft,open,valid,payable,paid'
+        modified_source = view_model.inheritance_handler_attributes_list_add(
+            source, specs, inherit_id
         )
+        button_node = modified_source.xpath('//button[@name="test"]')[0]
+        self.assertEqual(button_node.attrib["states"], "draft,open,valid,payable,paid")
 
     def test_list_remove(self):
-        view_model = self.env['ir.ui.view']
-        inherit_id = self.env.ref('base.view_partner_form').id
+        view_model = self.env["ir.ui.view"]
+        inherit_id = self.env.ref("base.view_partner_form").id
         source = etree.fromstring(
             """\
             <form>
@@ -96,12 +85,8 @@ class TestBaseViewInheritanceExtension(TransactionCase):
             </button>
             """
         )
-        modified_source = \
-            view_model.inheritance_handler_attributes_list_remove(
-                source, specs, inherit_id
-            )
-        button_node = modified_source.xpath('//button[@name="test"]')[0]
-        self.assertEqual(
-            button_node.attrib['states'],
-            'draft,valid,paid'
+        modified_source = view_model.inheritance_handler_attributes_list_remove(
+            source, specs, inherit_id
         )
+        button_node = modified_source.xpath('//button[@name="test"]')[0]
+        self.assertEqual(button_node.attrib["states"], "draft,valid,paid")
