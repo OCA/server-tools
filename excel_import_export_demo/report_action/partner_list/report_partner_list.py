9da0648bb90422fbed3738eb242326491081be82
kittiu <kittiu@gmail.com>
2020-04-03 12:05:47 +0700

    [IMP] : black, isort, prettier

--- excel_import_export_demo/report_action/partner_list/report_partner_list.py
+++ excel_import_export_demo/report_action/partner_list/report_partner_list.py
@@ -1,27 +1,21 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ReportPartnerList(models.TransientModel):
-    _name = 'report.partner.list'
-    _description = 'Wizard for report.partner.list'
+    _name = "report.partner.list"
+    _description = "Wizard for report.partner.list"
 
-    partner_ids = fields.Many2many(
-        comodel_name='res.partner',
-    )
-    supplier = fields.Boolean(
-        default=True,
-    )
-    customer = fields.Boolean(
-        default=True,
-    )
+    partner_ids = fields.Many2many(comodel_name="res.partner",)
+    supplier = fields.Boolean(default=True,)
+    customer = fields.Boolean(default=True,)
     results = fields.Many2many(
-        'res.partner',
-        string='Results',
-        compute='_compute_results',
-        help='Use compute fields, so there is nothing store in database',
+        "res.partner",
+        string="Results",
+        compute="_compute_results",
+        help="Use compute fields, so there is nothing store in database",
     )
 
     @api.multi
@@ -30,8 +24,11 @@ class ReportPartnerList(models.TransientModel):
         before export to excel by report_excel action
         """
         self.ensure_one()
-        domain = ['|', ('supplier', '=', self.supplier),
-                  ('customer', '=', self.customer)]
+        domain = [
+            "|",
+            ("supplier", "=", self.supplier),
+            ("customer", "=", self.customer),
+        ]
         if self.partner_ids:
-            domain.append(('id', 'in', self.partner_ids.ids))
-        self.results = self.env['res.partner'].search(domain, order='id')
+            domain.append(("id", "in", self.partner_ids.ids))
+        self.results = self.env["res.partner"].search(domain, order="id")
