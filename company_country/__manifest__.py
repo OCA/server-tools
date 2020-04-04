3657ca83a350e3ce9f509fe38df056fc64993019
Luis González [Vauxoo] <lgonzalez@vauxoo.com>
2020-04-04 15:16:19 -0600

    [ADD] company_country: Backport module from 12.0 (#1804)
    This module allows to set a country to the main company before the
    `account` module is installed, so the hook of that module installs the
    correct `l10n_***` module.
    
    This is useful because, if the company isn't already set correctly when
    the `account` module is installed, the generic accounting chart will be
    installed (`l10n_generic_coa`), which may be incorrect depending on your
    company's country.
    
    This is a backport from:
    https://github.com/OCA/server-tools/tree/12.0/company_country
    
    Co-authored-by: Moisés López <moylop260@vauxoo.com>
    
    Co-authored-by: Moisés López <moylop260@vauxoo.com>

---
+++ company_country/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2020  Vauxoo (<http://www.vauxoo.com/>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Company Country",
+    "summary": "Set country to main company",
+    "version": "11.0.1.0.0",
+    "category": "base",
+    "website": "https://github.com/OCA/server-tools/tree/11.0/company_country",
+    "maintainers": ['moylop260', 'luisg123v'],
+    "author": "Vauxoo, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [],
+    "data": ['data/res_config.xml'],
+    "sequence": 1,
+    "auto_install": False,
+    "installable": True,
+}
---
+++ company_country/readme/CONFIGURE.rst
@@ -0,0 +1,3 @@
+To configure this module, you need to:
+
+#. Set the environment variable ``COUNTRY`` using a two-letter ISO 3166 code.
