a619cdf9dfae7e60ff02caa3c588058283440e16
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2020-04-09 13:42:28 +0200

    [MIG] slow_statement_logger: Backport from 12.0

--- slow_statement_logger/__init__.py
+++ slow_statement_logger/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 # Copyright (c) ACSONE SA/NV 2019
 
@@ -19,23 +20,25 @@ if ENV_VAR in os.environ:
     LOG_MIN_DURATION_STATEMENT = int(os.environ.get(ENV_VAR, "-1"))
 
 
-class SlowStatementLoggingCursor(sql_db.Cursor):
-    def execute(self, query, params=None, log_exceptions=None):
-        if LOG_MIN_DURATION_STATEMENT >= 0:
-            start = time.perf_counter()
-            res = super().execute(query, params, log_exceptions)
-            duration = (time.perf_counter() - start) * 1000.0
-            if duration >= LOG_MIN_DURATION_STATEMENT:
-                # same logging technique as Odoo in sql_log mode
-                encoding = psycopg2.extensions.encodings[self.connection.encoding]
-                _logger.debug(
-                    "duration: %.3f ms  statement: %s",
-                    duration,
-                    self._obj.mogrify(query, params).decode(encoding, "replace"),
-                )
-            return res
-        else:
-            return super().execute(query, params, log_exceptions)
+initial_execute = sql_db.Cursor.execute
 
 
-sql_db.Cursor = SlowStatementLoggingCursor
+def execute(self, query, params=None, log_exceptions=None):
+    if LOG_MIN_DURATION_STATEMENT >= 0:
+        start = time.time()
+        res = initial_execute(self, query, params, log_exceptions)
+        duration = (time.time() - start) * 1000.0
+        if duration >= LOG_MIN_DURATION_STATEMENT:
+            # same logging technique as Odoo in sql_log mode
+            encoding = psycopg2.extensions.encodings[self.connection.encoding]
+            _logger.debug(
+                "duration: %.3f ms  statement: %s",
+                duration,
+                self._obj.mogrify(query, params).decode(encoding, "replace"),
+            )
+        return res
+    else:
+        return initial_execute(self, query, params, log_exceptions)
+
+
+sql_db.Cursor.execute = execute
