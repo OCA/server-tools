76c9573763e271920c718fcd20034522770261eb
Denis Valenchyts <denis@ventor.tech>
2020-04-17 13:40:28 +0300

    [IMP] fetchmail_notify_error_to_sender: black, isort, prettier

--- fetchmail_notify_error_to_sender/__manifest__.py
+++ fetchmail_notify_error_to_sender/__manifest__.py
@@ -4,24 +4,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Fetchmail Notify Error to Sender',
-    'summary': 'If fetching mails gives error, send an email to sender',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'author':
-        "Agile Business Group,Eficent,Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/server-tools',
-    'license': 'AGPL-3',
-    'depends': [
-        'fetchmail',
-        'test_mail',
-    ],
-    'data': [
-        'views/fetchmail_view.xml',
-        'data/email_template_data.xml',
-    ],
-    'qweb': [
-    ],
-    'installable': True,
-    'application': False,
+    "name": "Fetchmail Notify Error to Sender",
+    "summary": "If fetching mails gives error, send an email to sender",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "author": "Agile Business Group,Eficent,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-tools",
+    "license": "AGPL-3",
+    "depends": ["fetchmail", "test_mail",],
+    "data": ["views/fetchmail_view.xml", "data/email_template_data.xml",],
+    "qweb": [],
+    "installable": True,
+    "application": False,
 }
