b529f0aaa7527d583f41d8db7bbe09c70ccfca51
Denis Valenchyts <denis@ventor.tech>
2020-04-17 13:40:28 +0300

    [IMP] fetchmail_notify_error_to_sender: black, isort, prettier

--- fetchmail_notify_error_to_sender/models/mail_thread.py
+++ fetchmail_notify_error_to_sender/models/mail_thread.py
@@ -7,26 +7,31 @@ from odoo import api, models
 
 
 class MailThread(models.AbstractModel):
-    _inherit = 'mail.thread'
+    _inherit = "mail.thread"
 
     @api.model
-    def message_route(self, message, message_dict, model=None, thread_id=None,
-                      custom_values=None):
+    def message_route(
+        self, message, message_dict, model=None, thread_id=None, custom_values=None
+    ):
         try:
             res = super(MailThread, self).message_route(
-                message, message_dict, model=model, thread_id=thread_id,
-                custom_values=custom_values)
+                message,
+                message_dict,
+                model=model,
+                thread_id=thread_id,
+                custom_values=custom_values,
+            )
         except ValueError as ve:
-            fetchmail_server_id = self.env.context.get('fetchmail_server_id')
+            fetchmail_server_id = self.env.context.get("fetchmail_server_id")
             if not fetchmail_server_id:
                 raise ve
-            fetchmail_server = self.env['fetchmail.server'].with_context({
-                'sender_message': message,
-                'route_exception': ve,
-            }).browse(fetchmail_server_id)
+            fetchmail_server = (
+                self.env["fetchmail.server"]
+                .with_context({"sender_message": message, "route_exception": ve,})
+                .browse(fetchmail_server_id)
+            )
             if not fetchmail_server.error_notice_template_id:
                 raise ve
-            fetchmail_server.error_notice_template_id.send_mail(
-                fetchmail_server.id)
+            fetchmail_server.error_notice_template_id.send_mail(fetchmail_server.id)
             raise ve
         return res
