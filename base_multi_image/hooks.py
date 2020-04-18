9aaf646200ac14b6d2be158f3cad2a2498ba1d46
Luis Felipe Miléo <mileo@kmee.com.br>
2020-04-18 15:08:39 -0300

    [FIX] base_multi_image_image hook
    2020-04-18 18:00:52,359 1672245 WARNING db openerp.addons.base.ir.ir_translation: module product_m2mcategories: no translation for language pt_BR
    2020-04-18 18:00:52,387 1672245 ERROR db openerp.sql_db: Programming error: syntax error at or near "%"
    LINE 10:                     %s,
                                 ^
    , in query 
                    INSERT INTO base_multi_image_image (
                        owner_id,
                        owner_model,
                        storage,
                        file_db_store
                    )
                    SELECT
                        id,
                        %%s,
                        'db',
                        %(field)s
                    FROM
                        %(table)s
                    WHERE
                        %(field)s IS NOT NULL
                
    2020-04-18 18:00:52,388 1672245 CRITICAL db openerp.service.server: Failed to initialize database `db`.

--- base_multi_image/hooks.py
+++ base_multi_image/hooks.py
@@ -33,15 +33,14 @@ def pre_init_hook_for_submodules(cr, model, field):
                 )
                 SELECT
                     id,
-                    %%s,
+                    %(model)s,
                     'db',
                     %(field)s
                 FROM
                     %(table)s
                 WHERE
                     %(field)s IS NOT NULL
-            """, {"table": AsIs(env[model]._table), "field": AsIs(field)},
-            (model,)
+            """, {"table": AsIs(env[model]._table), "field": AsIs(field), "model": (model,)}
         )
 
 
