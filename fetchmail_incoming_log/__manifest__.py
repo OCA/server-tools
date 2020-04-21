04abcbd2b57027e6477c5a0b8058c452609997ee
mreficent <miquel.raich@eficent.com>
2020-04-21 16:51:35 +0200

    [IMP] fetchmail_incoming_log: black, isort, prettier

--- fetchmail_incoming_log/__manifest__.py
+++ fetchmail_incoming_log/__manifest__.py
@@ -2,16 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Fetchmail Incoming Log',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'summary': 'Log all messages received, before they start to be processed.',
-    'author': "Eficent, "
-              "Odoo Community Association (OCA)",
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/server-tools',
-    'depends': [
-        'mail', 'test_mail',
-    ],
-    'installable': True,
+    "name": "Fetchmail Incoming Log",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "summary": "Log all messages received, before they start to be processed.",
+    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-tools",
+    "depends": ["mail", "test_mail",],
+    "installable": True,
 }
