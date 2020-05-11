0f7403f33990959ef2211a42327ddc54713a9f60
Sébastien BEAU <sebastien.beau@akretion.com>
2020-05-11 21:51:34 +0200

    [REF] run black

--- attachment_synchronize/models/attachment.py
+++ attachment_synchronize/models/attachment.py
@@ -1,26 +1,27 @@
 # @ 2016 Florian DA COSTA @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
 import os
+from odoo import models, fields
 
 
 class AttachmentQueue(models.Model):
-    _inherit = 'attachment.queue'
+    _inherit = "attachment.queue"
 
-    task_id = fields.Many2one('attachment.synchronize.task', string='Task')
+    task_id = fields.Many2one("attachment.synchronize.task", string="Task")
     storage_backend_id = fields.Many2one(
-        'storage.backend', string='Storage Backend',
-        related='task_id.backend_id', store=True)
+        "storage.backend",
+        string="Storage Backend",
+        related="task_id.backend_id",
+        store=True,
+    )
     file_type = fields.Selection(
-        selection_add=[
-            ('export',
-             'Export File (External location)')
-        ])
+        selection_add=[("export", "Export File (External location)")]
+    )
 
     def _run(self):
         super()._run()
-        if self.file_type == 'export':
+        if self.file_type == "export":
             path = os.path.join(self.task_id.filepath, self.datas_fname)
             self.storage_backend_id._add_b64_data(path, self.datas)
 
