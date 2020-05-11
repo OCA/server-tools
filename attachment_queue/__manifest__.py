3ef496182ec4a7b1bf90a7b41f7dc939d1860077
Sébastien BEAU <sebastien.beau@akretion.com>
2020-05-11 14:41:08 +0200

    [REF] Rename to attachement_queue

---
+++ attachment_queue/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2015 Florian DA COSTA @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Attachment Queue',
+    'version': '12.0.1.0.0',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'summary': "Base module that add the concept of queue for processing file",
+    'website': 'https://github.com/OCA/server-tools',
+    'maintainers': ['florian-dacosta', 'sebastienbeau'],
+    'license': 'AGPL-3',
+    'category': 'Generic Modules',
+    'depends': [
+        'base',
+        'mail',
+    ],
+    'data': [
+        'views/attachment_queue_view.xml',
+        'security/ir.model.access.csv',
+        'data/cron.xml',
+        'data/ir_config_parameter.xml',
+        'data/mail_template.xml',
+    ],
+    'demo': [
+        'demo/attachment_queue_demo.xml'
+    ],
+    'installable': True,
+}
