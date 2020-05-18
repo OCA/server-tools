e5f4fd42f0d89e2ae7917dbf7dba2eb093eb0cf8
Eric Antones <eantones@nuobit.com>
2020-05-18 13:12:59 +0200

    [IMP] module_auto_update: black, isort, prettier

--- module_auto_update/migrations/12.0.2.0.5/post-migration.py
+++ module_auto_update/migrations/12.0.2.0.5/post-migration.py
@@ -6,7 +6,5 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.delete_records_safely_by_xml_id(
-        env, [
-            'module_auto_update.module_check_upgrades_cron',
-        ],
+        env, ["module_auto_update.module_check_upgrades_cron"],
     )
