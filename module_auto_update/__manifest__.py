8592e0e28598ee4f8cb1039e8f64ec4738e005d4
Eric Antones <eantones@nuobit.com>
2020-05-18 13:12:59 +0200

    [IMP] module_auto_update: black, isort, prettier

--- module_auto_update/__manifest__.py
+++ module_auto_update/__manifest__.py
@@ -3,24 +3,20 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Module Auto Update',
-    'summary': 'Automatically update Odoo modules',
-    'version': '12.0.2.0.5',
-    'category': 'Extra Tools',
-    'website': 'https://github.com/OCA/server-tools',
-    'author': 'LasLabs, '
-              'Juan José Scarafía, '
-              'Tecnativa, '
-              'ACSONE SA/NV, '
-              'Odoo Community Association (OCA)',
-    'license': 'LGPL-3',
-    'installable': True,
-    'uninstall_hook': 'uninstall_hook',
-    'depends': [
-        'base',
-    ],
-    'data': [
-        'views/ir_module_module.xml',
-    ],
-    'development_status': 'Production/Stable',
+    "name": "Module Auto Update",
+    "summary": "Automatically update Odoo modules",
+    "version": "12.0.2.0.5",
+    "category": "Extra Tools",
+    "website": "https://github.com/OCA/server-tools",
+    "author": "LasLabs, "
+    "Juan José Scarafía, "
+    "Tecnativa, "
+    "ACSONE SA/NV, "
+    "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "installable": True,
+    "uninstall_hook": "uninstall_hook",
+    "depends": ["base"],
+    "data": ["views/ir_module_module.xml"],
+    "development_status": "Production/Stable",
 }
