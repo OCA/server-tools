6a9dbeb57969baaf2c1916ca14b497d128bc1322
xavierjimenez <xavier.jimenez@qubiq.es>
2020-05-19 14:35:37 +0200

    [IMP] module_analysis: black, isort, prettier

--- module_analysis/tests/test_module.py
+++ module_analysis/tests/test_module.py
@@ -7,27 +7,26 @@ from odoo.tests.common import TransactionCase, post_install
 
 @post_install(True)
 class TestModule(TransactionCase):
-
     def setUp(self):
         super().setUp()
-        self.IrModuleModule = self.env['ir.module.module']
+        self.IrModuleModule = self.env["ir.module.module"]
 
     def test_installed_modules(self):
-        installed_modules = self.IrModuleModule.search(
-            [('state', '=', 'installed')])
+        installed_modules = self.IrModuleModule.search([("state", "=", "installed")])
         for module in installed_modules:
             self.assertTrue(
-                module.python_code_qty > 0 or
-                module.xml_code_qty > 0 or
-                module.js_code_qty > 0,
+                module.python_code_qty > 0
+                or module.xml_code_qty > 0
+                or module.js_code_qty > 0,
                 "module '%s' doesn't have code analysed defined, whereas it is"
-                " installed." % (module.name))
+                " installed." % (module.name),
+            )
 
     def test_uninstalled_modules(self):
-        uninstalled_modules = self.IrModuleModule.search(
-            [('state', '!=', 'installed')])
+        uninstalled_modules = self.IrModuleModule.search([("state", "!=", "installed")])
         for module in uninstalled_modules:
             self.assertTrue(
                 module.python_code_qty == 0,
                 "module '%s' has python lines defined, whereas it is"
-                " not installed." % (module.name))
+                " not installed." % (module.name),
+            )
