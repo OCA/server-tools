8f8849a7d28f629b226b20e47097824b8379b5ae
HviorForgeFlow <hector.villarreal@forgeflow.com>
2020-05-26 16:23:26 +0200

    [IMP] base_technical_user: black, isort, prettier

--- base_technical_user/__manifest__.py
+++ base_technical_user/__manifest__.py
@@ -1,19 +1,15 @@
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "Base Technical User",
-    'summary': """
+    "name": "Base Technical User",
+    "summary": """
         Add a technical user parameter on the company """,
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
-    'website': "https://github.com/OCA/server-tools/",
-    'category': 'Hidden/Dependency',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
-    ],
-    'data': [
-        'views/res_company_view.xml'
-    ],
-    'installable': True
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-tools/",
+    "category": "Hidden/Dependency",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/res_company_view.xml"],
+    "installable": True,
 }
