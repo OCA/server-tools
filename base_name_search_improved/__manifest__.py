60f23f2048b867be9d0e9e8c76df774f541bc6cb
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-06-11 10:00:02 -0300

    [IMP] base_name_search_improved: run pre commit again

--- base_name_search_improved/__manifest__.py
+++ base_name_search_improved/__manifest__.py
@@ -1,19 +1,15 @@
 # © 2016 Daniel Reis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Improved Name Search',
-    'summary': 'Friendlier search when typing in relation fields',
-    'version': '12.0.1.1.0',
-    'category': 'Uncategorized',
-    'website': 'https://odoo-community.org/',
-    'author': 'Daniel Reis, Odoo Community Association (OCA), ADHOC SA',
-    'license': 'AGPL-3',
-    'data': [
-        'views/ir_model_views.xml',
-    ],
-    'depends': [
-        'base',
-    ],
-    'installable': True,
+    "name": "Improved Name Search",
+    "summary": "Friendlier search when typing in relation fields",
+    "version": "12.0.1.1.0",
+    "category": "Uncategorized",
+    "website": "https://odoo-community.org/",
+    "author": "Daniel Reis, Odoo Community Association (OCA), ADHOC SA",
+    "license": "AGPL-3",
+    "data": ["views/ir_model_views.xml",],
+    "depends": ["base",],
+    "installable": True,
     "uninstall_hook": "uninstall_hook",
 }
