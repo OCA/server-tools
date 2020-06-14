4034ad378ff9bd739b642a989f539ed97caaaf1c
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-06-13 23:38:06 -0300

    [IMP] base_name_search_improved: patches

--- base_name_search_improved/hooks.py
+++ base_name_search_improved/hooks.py
@@ -1,6 +1,6 @@
 import logging
 
-from odoo import models
+from odoo import SUPERUSER_ID, api, models
 
 _logger = logging.getLogger(__name__)
 
@@ -8,4 +8,8 @@ _logger = logging.getLogger(__name__)
 def uninstall_hook(cr, registry):
     _logger.info("Reverting Patches...")
     models.BaseModel._revert_method("fields_view_get")
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env["ir.model.fields"].with_context(_force_unlink=True).search(
+        [("name", "=", "smart_search")]
+    ).unlink()
     _logger.info("Done!")
