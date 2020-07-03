a6af2892a4aafee830320ad9bf06d2192cccbcd2
Damien Crier <damien.crier@captivea.com>
2020-07-03 14:49:01 +0200

    [MIG] sql_export: Migration to 13.0

--- sql_export/readme/USAGE.rst
+++ sql_export/readme/USAGE.rst
@@ -6,5 +6,5 @@ Dashboards > Sql Export
 
 - `%(company_id)s` allows to set in the query the company id of the user
 - `%(user_id)s` allows to set in the query the user id
-- for any created field with `Sql Export Variables` menu, you can use it with `%(x_field_example)s` syntax 
+- for any created field with `Sql Export Variables` menu, you can use it with `%(x_field_example)s` syntax
   (Limitation for relational fields)
--- sql_export/tests/test_sql_query.py
+++ sql_export/tests/test_sql_query.py
@@ -3,27 +3,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import base64
-from odoo.tests.common import TransactionCase, post_install
+
 from odoo.exceptions import Warning as UserError
+from odoo.tests.common import TransactionCase, post_install
 
 
 @post_install(True)
 class TestExportSqlQuery(TransactionCase):
-
     def setUp(self):
         super(TestExportSqlQuery, self).setUp()
-        self.sql_export_obj = self.env['sql.export']
-        self.wizard_obj = self.env['sql.file.wizard']
-        self.sql_report_demo = self.env.ref('sql_export.sql_export_partner')
+        self.sql_export_obj = self.env["sql.export"]
+        self.wizard_obj = self.env["sql.file.wizard"]
+        self.sql_report_demo = self.env.ref("sql_export.sql_export_partner")
 
     def test_sql_query(self):
-        wizard = self.wizard_obj.create({
-            'sql_export_id': self.sql_report_demo.id,
-        })
+        wizard = self.wizard_obj.create({"sql_export_id": self.sql_report_demo.id})
         wizard.export_sql()
-        export = base64.b64decode(wizard.binary_file).decode('utf-8')
-        self.assertEqual(export.split(';')[0], 'name')
-        self.assertTrue(len(export.split(';')) > 6)
+        export = base64.b64decode(wizard.binary_file).decode("utf-8")
+        self.assertEqual(export.split(";")[0], "name")
+        self.assertTrue(len(export.split(";")) > 6)
 
     def test_prohibited_queries(self):
         prohibited_queries = [
@@ -38,9 +36,9 @@ class TestExportSqlQuery(TransactionCase):
         ]
         for query in prohibited_queries:
             with self.assertRaises(UserError):
-                sql_export = self.sql_export_obj.create({
-                    'name': 'test_prohibited',
-                    'query': query})
+                sql_export = self.sql_export_obj.create(
+                    {"name": "test_prohibited", "query": query}
+                )
                 sql_export.button_validate_sql_expression()
 
     def test_authorized_queries(self):
@@ -49,10 +47,10 @@ class TestExportSqlQuery(TransactionCase):
         ]
 
         for query in authorized_queries:
-            sql_export = self.sql_export_obj.create({
-                'name': 'test_authorized',
-                'query': query})
+            sql_export = self.sql_export_obj.create(
+                {"name": "test_authorized", "query": query}
+            )
             sql_export.button_validate_sql_expression()
             self.assertEqual(
-                sql_export.state, 'sql_valid',
-                "%s is a valid request" % (query))
+                sql_export.state, "sql_valid", "%s is a valid request" % (query)
+            )
