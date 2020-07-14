ae7389978c1b6ce1a62594bb7ea19c6f8fa76f81
victoralmau <victoralmau@gmail.com>
2020-07-14 13:52:51 +0200

    add try-except in slack message

--- slack/models/slack_message.py
+++ slack/models/slack_message.py
@@ -13,9 +13,9 @@ except (ImportError, IOError) as err:
 class SlackMessage(models.Model):
     _name = 'slack.message'
     _description = 'Slack Message'
-    
+
     @api.model
-    def create(self, values):        
+    def create(self, values):
         channel = self.env['ir.config_parameter'].sudo().get_param('slack_log_channel')
         api_token = tools.config.get('slack_bot_user_oauth_access_token')
         if api_token!=None:
@@ -27,25 +27,28 @@ class SlackMessage(models.Model):
             attachments = []
             #msg
             if 'msg' in values:
-                msg = values['msg']        
+                msg = values['msg']
             #attachments
             if 'attachments' in values:
                 attachments = values['attachments']
-            #channel            
+            #channel
             if 'channel' in values:
-                channel = values['channel']                    
+                channel = values['channel']
             #SlackClient
-            sc = slack.WebClient(token=api_token)        
-            result = sc.chat_postMessage(
-                channel=channel, 
-                text=msg, 
-                attachments=attachments, 
-                username='Odoo'
-            )            
-            if 'error' in result:
-                _logger.debug({
-                    'channel': channel,
-                    'error': result['error'] 
-                })
+            try:
+                sc = slack.WebClient(token=api_token)
+                result = sc.chat_postMessage(
+                    channel=channel,
+                    text=msg,
+                    attachments=attachments,
+                    username='Odoo'
+                )
+                if 'error' in result:
+                    _logger.debug({
+                        'channel': channel,
+                        'error': result['error']
+                    })
+            except:
+                _logger.debug('Slack Error')
             #return
             return False
