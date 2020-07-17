5315767545a72729a28bc441840e5a4151fb0de2
victoralmau <victoralmau@gmail.com>
2020-07-17 12:41:47 +0200

    website

--- slack_account/__manifest__.py
+++ slack_account/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Slack Account",
     "version": "12.0.1.0.0",
     "category": "Tools",
-    "website": "https://nodrizatech.com/",
+    "website": "https://github.com/OCA/server-tools",
     "author": "Odoo Nodriza Tech (ONT), "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
