e71ea39fc1c4c10666469a40196a1bfc93ac200e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-07-20 10:47:41 +0200

    [IMP] base_remote: black, isort, prettier

--- base_remote/models/base.py
+++ base_remote/models/base.py
@@ -1,17 +1,18 @@
 # Copyright 2018 Creu Blanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
 from threading import current_thread
+
+from odoo import models
 
 
 class Base(models.AbstractModel):
-    _inherit = 'base'
+    _inherit = "base"
 
     @property
     def remote(self):
         try:
             remote_addr = current_thread().environ["REMOTE_ADDR"]
         except (KeyError, AttributeError):
-            return self.env['res.remote']
-        return self.env['res.remote']._get_remote(remote_addr)
+            return self.env["res.remote"]
+        return self.env["res.remote"]._get_remote(remote_addr)
