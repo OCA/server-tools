d424fff800c5f2aaa5ee70e1920b0b3044e2e34d
Kitti U <kittiu@gmail.com>
2020-07-26 00:12:37 +0700

    [13.0][ADD] base_name_search_multi_lang

---
+++ base_name_search_multi_lang/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+{
+    "name": "Name Search Multi Lang",
+    "summary": "Name search by multiple active language",
+    "version": "13.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://github.com/OCA/server-tools",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/ir_model_views.xml"],
+    "maintainers": ["kittiu"],
+    "installable": True,
+}
