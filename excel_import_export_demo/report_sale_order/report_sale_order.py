48066f25eb6d34f4fc4357288d3bd623c9c85bcd
Kitti U <kittiu@gmail.com>
2020-08-24 17:57:03 +0700

    [IMP] : black, isort, prettier

--- excel_import_export_demo/report_sale_order/report_sale_order.py
+++ excel_import_export_demo/report_sale_order/report_sale_order.py
@@ -1,25 +1,22 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from openerp import models, fields, api
+from openerp import api, fields, models
 
 
 class ReportSaleOrder(models.TransientModel):
-    _name = 'report.sale.order'
-    _description = 'Wizard for report.sale.order'
-    _inherit = 'xlsx.report'
+    _name = "report.sale.order"
+    _description = "Wizard for report.sale.order"
+    _inherit = "xlsx.report"
 
     # Search Criteria
-    partner_id = fields.Many2one(
-        'res.partner',
-        string='Partner',
-    )
+    partner_id = fields.Many2one("res.partner", string="Partner",)
     # Report Result, sale.order
     results = fields.Many2many(
-        'sale.order',
-        string='Results',
-        compute='_compute_results',
-        help='Use compute fields, so there is nothing stored in database',
+        "sale.order",
+        string="Results",
+        compute="_compute_results",
+        help="Use compute fields, so there is nothing stored in database",
     )
 
     @api.multi
@@ -28,8 +25,8 @@ class ReportSaleOrder(models.TransientModel):
         before export to excel, by using xlsx.export
         """
         self.ensure_one()
-        Result = self.env['sale.order']
+        Result = self.env["sale.order"]
         domain = []
         if self.partner_id:
-            domain += [('partner_id', '=', self.partner_id.id)]
+            domain += [("partner_id", "=", self.partner_id.id)]
         self.results = Result.search(domain)
--- excel_import_export_demo/tests/test_xlsx_template.py
+++ excel_import_export_demo/tests/test_xlsx_template.py
@@ -1,11 +1,11 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 from ast import literal_eval
+
 from .test_common import TestExcelImportExport
 
 
 class TestXLSXTemplate(TestExcelImportExport):
-
     @classmethod
     def setUpClass(cls):
         super(TestExcelImportExport, cls).setUpClass()
@@ -17,46 +17,44 @@ class TestXLSXTemplate(TestExcelImportExport):
         self.assertDictEqual(
             instruction_dict,
             {
-                '__EXPORT__': {
-                    'sale_order': {
-                        '_HEAD_': {
-                            'B2': 'partner_id.display_name${value or ""}'
-                                  '#{align=left;style=text}#??',
-                            'B3': 'name${value or ""}'
-                                  '#{align=left;style=text}#??'},
-                        'order_line': {
-                            'A6': 'product_id.display_name${value or ""}'
-                                  '#{style=text}#??',
-                            'B6': 'name${value or ""}#{style=text}#??',
-                            'C6': 'product_uom_qty${value or 0}'
-                                  '#{style=number}#??',
-                            'D6': 'product_uom.name${value or ""}'
-                                  '#{style=text}#??',
-                            'E6': 'price_unit${value or 0}#{style=number}#??',
-                            'F6': 'tax_id${value and ",".join([x.display_name '
-                                  'for x in value]) or ""}#{}#??',
-                            'G6': 'price_subtotal${value or 0}'
-                                  '#{style=number}#??'
+                "__EXPORT__": {
+                    "sale_order": {
+                        "_HEAD_": {
+                            "B2": 'partner_id.display_name${value or ""}'
+                            "#{align=left;style=text}#??",
+                            "B3": 'name${value or ""}' "#{align=left;style=text}#??",
+                        },
+                        "order_line": {
+                            "A6": 'product_id.display_name${value or ""}'
+                            "#{style=text}#??",
+                            "B6": 'name${value or ""}#{style=text}#??',
+                            "C6": "product_uom_qty${value or 0}" "#{style=number}#??",
+                            "D6": 'product_uom.name${value or ""}' "#{style=text}#??",
+                            "E6": "price_unit${value or 0}#{style=number}#??",
+                            "F6": 'tax_id${value and ",".join([x.display_name '
+                            'for x in value]) or ""}#{}#??',
+                            "G6": "price_subtotal${value or 0}" "#{style=number}#??",
+                        },
+                    }
+                },
+                "__IMPORT__": {
+                    "sale_order": {
+                        "order_line": {
+                            "A6": "product_id",
+                            "B6": "name",
+                            "C6": "product_uom_qty",
+                            "D6": "product_uom",
+                            "E6": "price_unit",
+                            "F6": "tax_id",
                         }
                     }
                 },
-                '__IMPORT__': {
-                    'sale_order': {
-                        'order_line': {
-                            'A6': 'product_id',
-                            'B6': 'name',
-                            'C6': 'product_uom_qty',
-                            'D6': 'product_uom',
-                            'E6': 'price_unit',
-                            'F6': 'tax_id',
-                        }
-                    }
-                },
-                '__POST_IMPORT__': False
-            }
+                "__POST_IMPORT__": False,
+            },
         )
         # Finally load excel file into this new template
         self.assertFalse(self.sample_template.datas)  # Not yet loaded
-        self.template_obj.load_xlsx_template([self.sample_template.id],
-                                             addon='excel_import_export_demo')
+        self.template_obj.load_xlsx_template(
+            [self.sample_template.id], addon="excel_import_export_demo"
+        )
         self.assertTrue(self.sample_template.datas)  # Loaded successfully
