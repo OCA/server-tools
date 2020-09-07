2f6619a952dd171ac4259ab8b5c9796c67b9cfbe
nans <nans.lefebvre@acsone.eu>
2020-09-07 17:32:28 +0200

    [IMP] models: backport filtered_domain

--- base_partition/tests/test_partition.py
+++ base_partition/tests/test_partition.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import functools
+
 from odoo.tests.common import TransactionCase
 
 
@@ -14,33 +15,39 @@ class TestPartition(TransactionCase):
         self.c2 = self.Category.create({"name": "c2"})
         self.c3 = self.Category.create({"name": "c3"})
 
-        self.Partner = self.env['res.partner']
+        self.Partner = self.env["res.partner"]
         self.parent1 = self.Partner.create({"name": "parent1"})
         self.parent2 = self.Partner.create({"name": "parent2"})
         self.child1 = self.Partner.create({"name": "child1"})
         self.child2 = self.Partner.create({"name": "child2"})
         self.child3 = self.Partner.create({"name": "child3"})
-        self.x = self.Partner.create({
-            "name": "x",
-            "customer": True,
-            "category_id": [(6, 0, [self.c1.id, self.c2.id])],
-            "child_ids": [(6, 0, [self.child1.id, self.child2.id])],
-            "parent_id": self.parent1.id,
-        })
-        self.y = self.Partner.create({
-            "name": "y",
-            "customer": False,
-            "category_id": [(6, 0, [self.c2.id, self.c3.id])],
-            "child_ids": [(6, 0, [self.child2.id, self.child3.id])],
-            "parent_id": self.parent2.id,
-        })
-        self.z = self.Partner.create({
-            "name": "z",
-            "customer": False,
-            "category_id": [(6, 0, [self.c1.id, self.c3.id])],
-            "child_ids": [(6, 0, [self.child1.id, self.child3.id])],
-            "parent_id": self.parent2.id,
-        })
+        self.x = self.Partner.create(
+            {
+                "name": "x",
+                "customer": True,
+                "category_id": [(6, 0, [self.c1.id, self.c2.id])],
+                "child_ids": [(6, 0, [self.child1.id, self.child2.id])],
+                "parent_id": self.parent1.id,
+            }
+        )
+        self.y = self.Partner.create(
+            {
+                "name": "y",
+                "customer": False,
+                "category_id": [(6, 0, [self.c2.id, self.c3.id])],
+                "child_ids": [(6, 0, [self.child2.id, self.child3.id])],
+                "parent_id": self.parent2.id,
+            }
+        )
+        self.z = self.Partner.create(
+            {
+                "name": "z",
+                "customer": False,
+                "category_id": [(6, 0, [self.c1.id, self.c3.id])],
+                "child_ids": [(6, 0, [self.child1.id, self.child3.id])],
+                "parent_id": self.parent2.id,
+            }
+        )
         self.xyz = self.x + self.y + self.z
 
     def test_partition_many2many(self):
@@ -78,3 +85,60 @@ class TestPartition(TransactionCase):
 
         records = functools.reduce(sum, partition.values())
         self.assertEqual(self.xyz, records)  # we get the same recordset
+
+    def test_filtered_domain(self):
+        """Initially yo satisfy the coverage tools, this test actually documents
+           a number of pitfalls of filtered_domain and the differences with a search.
+           Commented examples would cause warnings, and even though these are edge-cases
+           these behaviours should be known.
+        """
+
+        records = self.xyz
+        empty_recordset = records.browse()
+
+        def filtered_search(domain):
+            search = self.xyz.search(domain)
+            return search.filtered(lambda r: r.id in self.xyz.ids)
+
+        self.assertEqual(records, records.filtered_domain([]))
+        self.assertEqual(empty_recordset, records.filtered_domain([(0, "=", 1)]))
+
+        for field in ["name"]:
+            for r in self.xyz:
+                domain = [(field, "=", r[field])]
+                self.assertEqual(self.xyz.filtered_domain(domain), r)
+                self.assertEqual(filtered_search(domain), r)
+
+                domain = [(field, "in", r[field])]
+                self.assertTrue(self.xyz.filtered_domain(domain), r)
+                with self.assertRaises(ValueError):
+                    filtered_search(domain)
+
+        for field in ["customer"]:
+            for r in [self.x, self.y | self.z]:
+                value = r[0][field]
+                domain = [(field, "=", value)]
+                self.assertEqual(self.xyz.filtered_domain(domain), r)
+                self.assertEqual(filtered_search(domain), r)
+                # domain = [(field, "in", value)]
+                # self.assertEqual(self.xyz.filtered_domain(domain), r)
+                # expected_result = r if value else empty_recordset  # !
+                # self.assertEqual(filtered_search(domain), expected_result)
+
+        for field in ["parent_id"]:
+            for r in [self.x, self.y | self.z]:
+                domain = [(field, "=", r[0][field].id)]
+                self.assertEqual(self.xyz.filtered_domain(domain), r)
+                self.assertEqual(filtered_search(domain), r)
+                domain = [(field, "in", r[0][field].ids)]
+                self.assertEqual(self.xyz.filtered_domain(domain), r)
+                self.assertEqual(filtered_search(domain), r)
+
+        for r in self.xyz:
+            field = "category_id"
+            in_domain = [(field, "in", r[field].ids)]
+            self.assertEqual(self.xyz.filtered_domain(in_domain), self.xyz)
+            self.assertEqual(self.xyz.search(in_domain), self.xyz)
+            # eq_domain = [(field, "=", r[field].ids)]
+            # self.assertEqual(self.xyz.search(eq_domain), self.xyz)
+            # self.assertEqual(self.xyz.filtered_domain(eq_domain), empty_recordset)
