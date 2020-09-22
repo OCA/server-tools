4e2441f370e544441fd09ad7e6f34535836a1922
Olar Alca [Vauxoo] <oscar@vauxoo.com>
2020-09-22 12:31:56 -0500

    Update server_environment/serv_config.py
    Co-authored-by: Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>

--- server_environment/serv_config.py
+++ server_environment/serv_config.py
@@ -59,8 +59,8 @@ _boolean_states = {
 
 def _load_running_env():
     if not system_base_config.get("running_env"):
-        _logger.warning("`running_env` not found. Using default = `test`.")
-        _logger.warning(
+        _logger.info("`running_env` not found. Using default = `test`.")
+        _logger.info(
             "We strongly recommend against using the rc file but instead use an "
             "explicit config file or env variable."
         )
