be24def02208a40ef5f6724780410d191d131332
Alexis de Lattre <alexis.delattre@akretion.com>
2020-11-24 00:08:58 +0100

    [MIG] base_view_inheritance_extension to v14

--- base_view_inheritance_extension/__manifest__.py
+++ base_view_inheritance_extension/__manifest__.py
@@ -3,11 +3,13 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Extended view inheritance",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Hidden/Dependency",
     "summary": "Adds more operators for view inheritance",
+    "website": "https://github.com/OCA/server-tools",
     "depends": ["base"],
+    "external_dependencies": {"python": ["pyyaml"]},
     "demo": ["demo/ir_ui_view.xml"],
 }
