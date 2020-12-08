7bf30de6d9488a0616bb1cc0ae6ae1efbb1d30e7
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-08 21:26:57 +0100

    migrate to version 14

--- base_sparse_field_list_support/__manifest__.py
+++ base_sparse_field_list_support/__manifest__.py
@@ -1,10 +1,10 @@
 {
     "name": "Base Sparse Field List Support",
     "summary": "add list support to convert_to_cache()",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.1",
     "category": "Technical Settings",
-    "website": "www.akretion.com",
-    "author": "Akretion",
+    "website": "https://github.com/OCA/server-tools",
+    "author": "Akretion,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
