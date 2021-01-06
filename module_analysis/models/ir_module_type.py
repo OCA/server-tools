4700038dca3cfbdea1316f75d3671ec1a0e28834
Telmo Santos <telmo.santos@camptocamp.com>
2021-01-06 15:19:29 +0100

    [IMP] module_analysis: black, isort, prettier

--- module_analysis/models/ir_module_type.py
+++ module_analysis/models/ir_module_type.py
@@ -6,25 +6,26 @@ from odoo import api, fields, models
 
 
 class IrModuleType(models.Model):
-    _name = 'ir.module.type'
-    _description = 'Modules Types'
-    _order = 'sequence'
+    _name = "ir.module.type"
+    _description = "Modules Types"
+    _order = "sequence"
 
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string="Name", required=True)
 
-    sequence = fields.Integer(string='Sequence')
+    sequence = fields.Integer(string="Sequence")
 
     installed_module_ids = fields.One2many(
-        string='Installed Modules', comodel_name='ir.module.module',
-        inverse_name='module_type_id')
+        string="Installed Modules",
+        comodel_name="ir.module.module",
+        inverse_name="module_type_id",
+    )
 
     installed_module_qty = fields.Integer(
-        string='Modules Quantity', compute='_compute_installed_module_qty',
-        store=True)
+        string="Modules Quantity", compute="_compute_installed_module_qty", store=True
+    )
 
     @api.multi
-    @api.depends('installed_module_ids.module_type_id')
+    @api.depends("installed_module_ids.module_type_id")
     def _compute_installed_module_qty(self):
         for module_type in self:
-            module_type.installed_module_qty = len(
-                module_type.installed_module_ids)
+            module_type.installed_module_qty = len(module_type.installed_module_ids)
