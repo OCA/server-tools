0ed90bc8ce613aa6d78457f8e4c228e964cc7d1a
Chafique <chafique.delli@akretion.com>
2021-01-08 19:54:00 +0100

    [FIX] pre-commit

--- scheduler_error_mailer/tests/test_scheduler_error_mailer.py
+++ scheduler_error_mailer/tests/test_scheduler_error_mailer.py
@@ -9,22 +9,25 @@ class TestSchedulerErrorMailer(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.test_scheduler_error_mailer = cls.env.ref(
-            "scheduler_error_mailer.test_scheduler_error_mailer")
+            "scheduler_error_mailer.test_scheduler_error_mailer"
+        )
 
     def test_scheduler_failure(self):
         try:
             self.test_scheduler_error_mailer.method_direct_trigger()
         except Exception:
             pass
-        lastcall = fields.Datetime.to_string(
-            self.test_scheduler_error_mailer.lastcall)
-        mails = self.env["mail.mail"].search([
-            ("res_id", "=", self.test_scheduler_error_mailer.id),
-            ("model", "=", "ir.cron"),
-            ("message_type", "=", "email"),
-            ("date", "=", lastcall)
-        ])
+        lastcall = fields.Datetime.to_string(self.test_scheduler_error_mailer.lastcall)
+        mails = self.env["mail.mail"].search(
+            [
+                ("res_id", "=", self.test_scheduler_error_mailer.id),
+                ("model", "=", "ir.cron"),
+                ("message_type", "=", "email"),
+                ("date", "=", lastcall),
+            ]
+        )
         self.assertEqual(len(mails), 1)
-        subject = ("[DB " + self.cr.dbname +
-                   "] Scheduler 'Test Scheduler Error Mailer' FAILED")
+        subject = (
+            "[DB " + self.cr.dbname + "] Scheduler 'Test Scheduler Error Mailer' FAILED"
+        )
         self.assertEqual(mails.subject, subject)
