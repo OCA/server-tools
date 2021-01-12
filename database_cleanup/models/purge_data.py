fb6bae4c59eefb09d65350ace9cd518c458f00ae
Stephane Mangin <stephane.mangin@camptocamp.com>
2021-01-12 14:15:12 +0100

    [IMP] database_cleanup: black, isort, prettier

--- database_cleanup/models/purge_data.py
+++ database_cleanup/models/purge_data.py
@@ -2,16 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
+
 from ..identifier_adapter import IdentifierAdapter
 
 
 class CleanupPurgeLineData(models.TransientModel):
-    _inherit = 'cleanup.purge.line'
-    _name = 'cleanup.purge.line.data'
+    _inherit = "cleanup.purge.line"
+    _name = "cleanup.purge.line.data"
 
-    data_id = fields.Many2one('ir.model.data', 'Data entry')
+    data_id = fields.Many2one("ir.model.data", "Data entry")
     wizard_id = fields.Many2one(
-        'cleanup.purge.wizard.data', 'Purge Wizard', readonly=True)
+        "cleanup.purge.wizard.data", "Purge Wizard", readonly=True
+    )
 
     @api.multi
     def purge(self):
@@ -19,18 +21,19 @@ class CleanupPurgeLineData(models.TransientModel):
         if self:
             objs = self
         else:
-            objs = self.env['cleanup.purge.line.data']\
-                .browse(self._context.get('active_ids'))
+            objs = self.env["cleanup.purge.line.data"].browse(
+                self._context.get("active_ids")
+            )
         to_unlink = objs.filtered(lambda x: not x.purged and x.data_id)
-        self.logger.info('Purging data entries: %s', to_unlink.mapped('name'))
-        to_unlink.mapped('data_id').unlink()
-        return to_unlink.write({'purged': True})
+        self.logger.info("Purging data entries: %s", to_unlink.mapped("name"))
+        to_unlink.mapped("data_id").unlink()
+        return to_unlink.write({"purged": True})
 
 
 class CleanupPurgeWizardData(models.TransientModel):
-    _inherit = 'cleanup.purge.wizard'
-    _name = 'cleanup.purge.wizard.data'
-    _description = 'Purge data'
+    _inherit = "cleanup.purge.wizard"
+    _name = "cleanup.purge.wizard.data"
+    _description = "Purge data"
 
     @api.model
     def find(self):
@@ -41,7 +44,7 @@ class CleanupPurgeWizardData(models.TransientModel):
         data_ids = []
         unknown_models = []
         self.env.cr.execute("""SELECT DISTINCT(model) FROM ir_model_data""")
-        for model, in self.env.cr.fetchall():
+        for (model,) in self.env.cr.fetchall():
             if not model:
                 continue
             if model not in self.env:
@@ -54,19 +57,35 @@ class CleanupPurgeWizardData(models.TransientModel):
                 AND res_id IS NOT NULL
                 AND NOT EXISTS (
                     SELECT id FROM %s WHERE id=ir_model_data.res_id)
-                """, (model, IdentifierAdapter(self.env[model]._table)))
+                """,
+                (model, IdentifierAdapter(self.env[model]._table)),
+            )
             data_ids.extend(data_row for data_row, in self.env.cr.fetchall())
-        data_ids += self.env['ir.model.data'].search([
-            ('model', 'in', unknown_models),
-        ]).ids
-        for data in self.env['ir.model.data'].browse(data_ids):
-            res.append((0, 0, {
-                        'data_id': data.id,
-                        'name': "%s.%s, object of type %s" % (
-                            data.module, data.name, data.model)}))
+        data_ids += (
+            self.env["ir.model.data"]
+            .search(
+                [
+                    ("model", "in", unknown_models),
+                ]
+            )
+            .ids
+        )
+        for data in self.env["ir.model.data"].browse(data_ids):
+            res.append(
+                (
+                    0,
+                    0,
+                    {
+                        "data_id": data.id,
+                        "name": "%s.%s, object of type %s"
+                        % (data.module, data.name, data.model),
+                    },
+                )
+            )
         if not res:
-            raise UserError(_('No orphaned data entries found'))
+            raise UserError(_("No orphaned data entries found"))
         return res
 
     purge_line_ids = fields.One2many(
-        'cleanup.purge.line.data', 'wizard_id', 'Data to purge')
+        "cleanup.purge.line.data", "wizard_id", "Data to purge"
+    )
