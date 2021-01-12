6bc6b2be77ca519ae192acd91e2b45bd72d9911c
Stephane Mangin <stephane.mangin@camptocamp.com>
2021-01-12 14:28:34 +0100

    [MIG] database_cleanup: Migration to 14.0

--- database_cleanup/models/purge_columns.py
+++ database_cleanup/models/purge_columns.py
@@ -1,4 +1,5 @@
 # Copyright 2014-2016 Therp BV <http://therp.nl>
+# Copyright 2021 Camptocamp <https://camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 # pylint: disable=consider-merging-classes-inherited
 from odoo import _, api, fields, models
@@ -10,14 +11,13 @@ from ..identifier_adapter import IdentifierAdapter
 class CleanupPurgeLineColumn(models.TransientModel):
     _inherit = "cleanup.purge.line"
     _name = "cleanup.purge.line.column"
-    _description = "Purge Column Wizard Lines"
+    _description = "Cleanup Purge Line Column"
 
     model_id = fields.Many2one("ir.model", "Model", required=True, ondelete="CASCADE")
     wizard_id = fields.Many2one(
         "cleanup.purge.wizard.column", "Purge Wizard", readonly=True
     )
 
-    @api.multi
     def purge(self):
         """
         Unlink columns upon manual confirmation.
@@ -123,7 +123,7 @@ class CleanupPurgeWizardColumn(models.TransientModel):
                 model_pool
             )
 
-        for table, model_spec in table2model.items():
+        for _table, model_spec in table2model.items():
             for column in self.get_orphaned_columns(model_spec[1]):
                 res.append((0, 0, {"name": column, "model_id": model_spec[0]}))
         if not res:
