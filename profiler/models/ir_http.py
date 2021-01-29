8fbb080205f5dbb96ca1ed53118192b83c060e76
Tom Blauwendraat <tom@sunflowerweb.nl>
2021-01-29 17:37:44 +0100

    [FIX] profiler: reraise user exceptions while still stopping profiler; save stats to file even if db is not possible

--- profiler/models/ir_http.py
+++ profiler/models/ir_http.py
@@ -2,11 +2,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from timeit import default_timer as timer
+from cProfile import Profile
 
 from odoo import models
 from odoo.http import request
-
-from .profiler_profile import ProfilerProfile
 
 
 class IrHttp(models.AbstractModel):
@@ -16,16 +15,28 @@ class IrHttp(models.AbstractModel):
     def _dispatch(cls):
         if request and request.httprequest and request.httprequest.session \
                 and getattr(request.httprequest.session, 'oca_profiler', False):
-            oca_profiler_id = request.httprequest.session.oca_profiler
-            profiler = request.env['profiler.profile'].browse(oca_profiler_id)
-            ProfilerProfile.enabled = True
-            start = timer()
-            with ProfilerProfile.profiling():
+            profile = Profile()
+            profile.enable()
+            try:
+                oca_profiler_id = request.httprequest.session.oca_profiler
+                RequestLine = request.env['profiler.profile.request.line']
+                profiler = request.env['profiler.profile'].browse(oca_profiler_id)
+                profile = Profile()
+                start = timer()
+                ret = False
+                profile.enable()
                 ret = super(IrHttp, cls)._dispatch()
-            end = timer()
-            ProfilerProfile.enabled = False
-            request_line = profiler.sudo().create_request_line()
-            request_line.dump_stats()
-            request_line.total_time = (end - start) * 1000.0
+                profile.disable()
+                end = timer()
+            except:  # noqa
+                profile.disable()
+                raise
+            try:
+                cprofile_fname, cprofile_path = RequestLine.dump_stats(profile)
+                request_line = profiler.sudo().create_request_line()
+                request_line.dump_stats_db(cprofile_fname, cprofile_path)
+                request_line.total_time = (end - start) * 1000.0
+            except:  # noqa
+                pass
             return ret
         return super(IrHttp, cls)._dispatch()
