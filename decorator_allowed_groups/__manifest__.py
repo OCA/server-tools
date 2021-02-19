ece8455492468929a4afdf239210d14a07d1a9ee
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-19 10:56:34 +0100

    fixup! fixup! fixup! [ADD] new module decorator_allowed_groups

--- decorator_allowed_groups/__manifest__.py
+++ decorator_allowed_groups/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2021-Today: GRAP (<http://www.grap.coop/>)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "Decorator - Allowed Groups",
