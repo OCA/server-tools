22765bd888edb9e30127f55a55f0fed8f74e78f7
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-24 09:20:42 +0100

    [IMP] remove pre-commit exception and fix various style in odoo_patch folder

--- upgrade_analysis/odoo_patch/addons/point_of_sale/__init__.py
+++ upgrade_analysis/odoo_patch/addons/point_of_sale/__init__.py
@@ -1,3 +1,4 @@
+# flake8: noqa: B902
 from odoo import api
 from odoo.addons.point_of_sale.models import pos_config
 from ...odoo_patch import OdooPatch
@@ -5,7 +6,7 @@ from ...odoo_patch import OdooPatch
 
 class PreInitHookPatch(OdooPatch):
     target = pos_config.PosConfig
-    method_names = ['post_install_pos_localisation']
+    method_names = ["post_install_pos_localisation"]
 
     @api.model
     def post_install_pos_localisation(cr):
--- upgrade_analysis/odoo_patch/odoo_patch.py
+++ upgrade_analysis/odoo_patch/odoo_patch.py
@@ -4,7 +4,7 @@ _logger = logging.getLogger(__name__)
 
 
 class OdooPatch(object):
-    """ Simple mechanism to apply a collection of monkeypatches using a
+    """Simple mechanism to apply a collection of monkeypatches using a
     context manager.
 
     Classes can register their monkeypatches by inheriting from this class.
@@ -39,21 +39,23 @@ class OdooPatch(object):
         do_something()
     ```
     """
+
     def __enter__(self):
         for cls in OdooPatch.__subclasses__():
             for method_name in cls.method_names:
                 method = getattr(cls, method_name)
-                setattr(method, '_original_method',
-                        getattr(cls.target, method_name))
+                method._original_method = getattr(cls.target, method_name)
                 setattr(cls.target, method_name, method)
 
     def __exit__(self, exc_type, exc_value, tb):
         for cls in OdooPatch.__subclasses__():
             for method_name in cls.method_names:
                 method = getattr(cls.target, method_name)
-                if hasattr(method, '_original_method'):
+                if hasattr(method, "_original_method"):
                     setattr(cls.target, method_name, method._original_method)
                 else:
                     _logger.warn(
-                        '_original_method not found on method %s of class %s',
-                        method_name, cls.target)
+                        "_original_method not found on method %s of class %s",
+                        method_name,
+                        cls.target,
+                    )
