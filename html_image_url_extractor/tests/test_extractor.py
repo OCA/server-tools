d02b24592a8fbad4823b42dc784b608e2769ccbb
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-24 14:49:59 +0100

    [IMP] html_image_url_extractor: black, isort, prettier

--- html_image_url_extractor/tests/test_extractor.py
+++ html_image_url_extractor/tests/test_extractor.py
@@ -1,8 +1,8 @@
 # Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tools import mute_logger
 from odoo.tests.common import TransactionCase
+from odoo.tools import mute_logger
 
 
 class ExtractorCase(TransactionCase):
@@ -42,8 +42,7 @@ class ExtractorCase(TransactionCase):
             self.assertEqual("/path/%d" % n, url)
         self.assertEqual(n, 0)
 
-    @mute_logger("odoo.addons.html_image_url_extractor" +
-                 ".models.ir_fields_converter")
+    @mute_logger("odoo.addons.html_image_url_extractor" + ".models.ir_fields_converter")
     def test_empty_html(self):
         """Empty HTML handled correctly."""
         for laps, text in self.imgs_from_html(""):
@@ -51,8 +50,7 @@ class ExtractorCase(TransactionCase):
         with self.assertRaises(Exception):
             list(self.imgs_from_html("", fail=True))
 
-    @mute_logger("odoo.addons.html_image_url_extractor" +
-                 ".models.ir_fields_converter")
+    @mute_logger("odoo.addons.html_image_url_extractor" + ".models.ir_fields_converter")
     def test_false_html(self):
         """``False`` HTML handled correctly."""
         for laps, text in self.imgs_from_html(False):
