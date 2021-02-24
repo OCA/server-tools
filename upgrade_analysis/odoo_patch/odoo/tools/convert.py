537fdf45606435e87c9ff0b5b3fa9cc6622bbc8c
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-24 09:20:42 +0100

    [IMP] remove pre-commit exception and fix various style in odoo_patch folder

--- upgrade_analysis/odoo_patch/odoo/tools/convert.py
+++ upgrade_analysis/odoo_patch/odoo/tools/convert.py
@@ -1,11 +1,12 @@
-from ...odoo_patch import OdooPatch
-from .... import upgrade_log
 from odoo.tools.convert import xml_import
+
+from .... import upgrade_log
+from ...odoo_patch import OdooPatch
 
 
 class XMLImportPatch(OdooPatch):
     target = xml_import
-    method_names = ['_test_xml_id']
+    method_names = ["_test_xml_id"]
 
     def _test_xml_id(self, xml_id):
         res = XMLImportPatch._test_xml_id._original_method(self, xml_id)
