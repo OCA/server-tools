4ead5e8f71dcd869b1859ac22aa7e2a106aa155d
SimoRubi <simone.rubino@agilebg.com>
2021-03-17 17:48:15 +0100

    [IMP] base_report_auto_create_qweb: black, isort, prettier

--- base_report_auto_create_qweb/tests/__init__.py
+++ base_report_auto_create_qweb/tests/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # (c) 2015 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
--- base_report_auto_create_qweb/wizard/report_duplicate.py
+++ base_report_auto_create_qweb/wizard/report_duplicate.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Authors: See README.RST for Contributors
 # Copyright 2015-2017
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -7,18 +6,18 @@ from odoo import api, fields, models
 
 
 class IrActionsReportDuplicate(models.TransientModel):
-    _name = 'ir.actions.report.xml.duplicate'
+    _name = "ir.actions.report.xml.duplicate"
 
     suffix = fields.Char(
-        string='Suffix', help='This suffix will be added to the report')
+        string="Suffix", help="This suffix will be added to the report"
+    )
 
     @api.multi
     def duplicate_report(self):
         self.ensure_one()
-        active_id = self.env.context.get('active_id')
-        model = self.env.context.get('active_model')
+        active_id = self.env.context.get("active_id")
+        model = self.env.context.get("active_model")
         if model:
             object = self.env[model].browse(active_id)
-            object.with_context(
-                suffix=self.suffix, enable_duplication=True).copy()
+            object.with_context(suffix=self.suffix, enable_duplication=True).copy()
         return {}
