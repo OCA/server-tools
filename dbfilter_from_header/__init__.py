fd8f0812b463ef2d3dad9790c9de33396ebc782e
PhilDL <philippe.lattention@hotmail.fr>
2021-04-21 16:10:08 +0400

    [14.0][MIG] dbfilter_from_header

---
+++ dbfilter_from_header/__init__.py
@@ -0,0 +1,2 @@
+from . import hooks
+from .hooks import post_load
---
+++ dbfilter_from_header/hooks.py
@@ -0,0 +1,32 @@
+# © 2013  Therp BV
+# © 2014  ACSONE SA/NV
+# © 2020  CodingDodo
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import logging
+import re
+
+from odoo import http
+from odoo.tools import config
+
+db_filter_org = http.db_filter
+
+
+def db_filter(dbs, httprequest=None):
+    dbs = db_filter_org(dbs, httprequest)
+    httprequest = httprequest or http.request.httprequest
+    db_filter_hdr = httprequest.environ.get("HTTP_X_ODOO_DBFILTER")
+    if db_filter_hdr:
+        dbs = [db for db in dbs if re.match(db_filter_hdr, db)]
+    return dbs
+
+
+def post_load():
+    if config.get("proxy_mode") and "dbfilter_from_header" in config.get(
+        "server_wide_modules"
+    ):
+        _logger = logging.getLogger(__name__)
+        _logger.info("monkey patching http.db_filter")
+        http._is_monkey_patch_filtered = True
+        http.db_filter = db_filter
