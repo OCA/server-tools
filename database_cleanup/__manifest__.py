045a69b66ec064f04d7e2344ba715c270d40cc30
Dimitrios T. Tanis <dtanis@tanisfood.gr>
2021-04-25 18:35:29 +0300

    [UPD]database_cleanup:Add ir.model.fields cleanup
    Clean up ir.model.fields that exist in ORM but not in database tables

--- database_cleanup/__manifest__.py
+++ database_cleanup/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Database cleanup',
-    'version': '12.0.1.0.4',
+    'version': '12.0.1.1.0',
     'author': "Therp BV,Odoo Community Association (OCA)",
     'depends': ['base'],
     'license': 'AGPL-3',
@@ -12,6 +12,7 @@
         'views/purge_menus.xml',
         'views/purge_modules.xml',
         'views/purge_models.xml',
+        'views/purge_model_fields.xml',
         'views/purge_columns.xml',
         'views/purge_tables.xml',
         'views/purge_data.xml',
