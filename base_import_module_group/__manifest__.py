d10eab5dbbc4dec2bc80c549799c0be7a37926e0
Akim Juillerat <akim.juillerat@camptocamp.com>
2021-05-07 14:09:11 +0200

    Add module base_import_module_group

---
+++ base_import_module_group/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Import Module Group",
+    "summary": "Restrict module importation to specific group",
+    "version": "12.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://github.com/OCA/server-tools",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto-install": True,
+    "depends": [
+        "base_import_module",
+    ],
+    "data": [
+        "security/res_groups.xml",
+        "views/import_module_group.xml",
+    ],
+}
