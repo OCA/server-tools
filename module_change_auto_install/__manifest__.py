cfd2f36b39cd9ec147e8c84242654889cc382db8
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-05-26 16:17:59 +0200

    [BACKPORT] module_change_auto_install (14.0 -> 12.0)

--- module_change_auto_install/__manifest__.py
+++ module_change_auto_install/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Change auto installable modules",
     "summary": "Customize auto installables modules by configuration",
-    "version": "14.0.1.0.1",
+    "version": "12.0.1.0.1",
     "category": "Tools",
     "maintainers": ["legalsylvain"],
     "author": "GRAP, Odoo Community Association (OCA)",
