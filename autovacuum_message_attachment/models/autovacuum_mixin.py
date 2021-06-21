f89dfab8c5f8fb6175553a0fae5fa7418fff708d
Enric Tobella <etobella@creublanca.es>
2021-06-21 14:39:03 +0200

    [IMP] autovacuum_message_attachment: black, isort, prettier

--- autovacuum_message_attachment/models/autovacuum_mixin.py
+++ autovacuum_message_attachment/models/autovacuum_mixin.py
@@ -1,12 +1,12 @@
 # Copyright (C) 2019 Akretion
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+import datetime
 import logging
 
 import odoo
 from odoo import api, models
 from odoo.tools.safe_eval import safe_eval
-import datetime
 
 _logger = logging.getLogger(__name__)
 
@@ -18,10 +18,8 @@ class AutovacuumMixin(models.AbstractModel):
     @api.multi
     def batch_unlink(self):
         with api.Environment.manage():
-            with odoo.registry(
-                    self.env.cr.dbname).cursor() as new_cr:
-                new_env = api.Environment(new_cr, self.env.uid,
-                                          self.env.context)
+            with odoo.registry(self.env.cr.dbname).cursor() as new_cr:
+                new_env = api.Environment(new_cr, self.env.uid, self.env.context)
                 try:
                     while self:
                         batch_delete = self[0:1000]
@@ -35,12 +33,13 @@ class AutovacuumMixin(models.AbstractModel):
                         new_env.cr.commit()
                 except Exception as e:
                     _logger.exception(
-                        "Failed to delete Ms : %s - %s" % (self._name, str(e)))
+                        "Failed to delete Ms : {} - {}".format(self._name, str(e))
+                    )
 
     # Call by cron
     @api.model
-    def autovacuum(self, ttype='message'):
-        rules = self.env['vacuum.rule'].search([('ttype', '=', ttype)])
+    def autovacuum(self, ttype="message"):
+        rules = self.env["vacuum.rule"].search([("ttype", "=", ttype)])
         for rule in rules:
             records = rule._search_autovacuum_records()
             records.batch_unlink()
@@ -55,8 +54,9 @@ class AutovacuumMixin(models.AbstractModel):
 
     def _get_autovacuum_records_model(self, rule):
         domain = self._get_autovacuum_domain(rule)
-        record_domain = safe_eval(rule.model_filter_domain,
-                                  locals_dict={'datetime': datetime})
+        record_domain = safe_eval(
+            rule.model_filter_domain, locals_dict={"datetime": datetime}
+        )
         autovacuum_relation = self._autovacuum_relation
         for leaf in domain:
             if not isinstance(leaf, (tuple, list)):
@@ -64,8 +64,7 @@ class AutovacuumMixin(models.AbstractModel):
                 continue
             field, operator, value = leaf
             record_domain.append(
-                ('%s.%s' % (autovacuum_relation, field), operator, value))
+                ("{}.{}".format(autovacuum_relation, field), operator, value)
+            )
         records = self.env[rule.model_id.model].search(record_domain)
-        return self.search(
-            domain + [('res_id', 'in', records.ids)]
-        )
+        return self.search(domain + [("res_id", "in", records.ids)])
