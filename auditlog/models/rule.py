f719ae9a855a90852b0e7eecf637772e14928a4a
Bhavesh Odedra <bodedra@opensourceintegrators.com>
2021-06-22 13:57:52 -0700

    [ADD] auditlog: enable to track Unlink record

--- auditlog/models/rule.py
+++ auditlog/models/rule.py
@@ -138,6 +138,9 @@ class AuditlogRule(models.Model):
         string="Action",
         states={"subscribed": [("readonly", True)]},
     )
+    capture_record = fields.Boolean(
+        "Capture Record", help="Select this if you want to keep track of Unlink Record",
+    )
 
     _sql_constraints = [
         (
@@ -474,6 +477,10 @@ class AuditlogRule(models.Model):
         for res_id in res_ids:
             model_model = self.env[res_model]
             name = model_model.browse(res_id).name_get()
+            model_id = self.pool._auditlog_model_cache[res_model]
+            auditlog_rule = self.env["auditlog.rule"].search(
+                [("model_id", "=", model_id)]
+            )
             res_name = name and name[0] and name[0][1]
             vals = {
                 "name": res_name,
@@ -498,6 +505,10 @@ class AuditlogRule(models.Model):
             elif method == "write":
                 self._create_log_line_on_write(
                     log, diff.changed(), old_values, new_values
+                )
+            elif method == "unlink" and auditlog_rule.capture_record:
+                self._create_log_line_on_read(
+                    log, list(old_values.get(res_id, EMPTY_DICT).keys()), old_values
                 )
 
     def _get_field(self, model, field_name):
