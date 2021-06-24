91ecc2ce4f2095383f8629ef97059d48927eb490
Maksym Yankin <yankinmk@gmail.com>
2021-06-24 12:56:28 +0300

    [MIG] base_import_module_group: Migration to 13.0

--- base_import_module_group/__manifest__.py
+++ base_import_module_group/__manifest__.py
@@ -1,16 +1,16 @@
-# Copyright 2020 Camptocamp SA
+# Copyright 2020-2021 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 {
     "name": "Import Module Group",
     "summary": "Restrict module importation to specific group",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/OCA/server-tools",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "auto-install": True,
-    "depends": ["base_import_module",],
-    "data": ["security/res_groups.xml", "views/import_module_group.xml",],
+    "auto_install": True,
+    "depends": ["base_import_module",],  # noqa: E231
+    "data": ["security/res_groups.xml", "views/import_module_group.xml",],  # noqa: E231
 }
--- base_import_module_group/models/ir_module.py
+++ base_import_module_group/models/ir_module.py
@@ -1,6 +1,6 @@
-# Copyright 2020 Camptocamp SA
+# Copyright 2020-2021 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import _, api, models
+from odoo import _, models
 from odoo.exceptions import AccessError
 
 
@@ -8,9 +8,8 @@ class IrModule(models.Model):
 
     _inherit = "ir.module.module"
 
-    @api.multi
     def _import_module(self, module, path, force=False):
-        group_xid = "import_module_group.group_module_import"
+        group_xid = "base_import_module_group.group_module_import"
         if not self.env.user.has_group(group_xid):
             group = self.env.ref(group_xid)
             raise AccessError(
