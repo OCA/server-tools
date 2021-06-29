7a3441c6484fd7689f099466da72cf01749db487
Denis Roussel <denis.roussel@acsone.eu>
2021-06-29 18:46:11 +0200

    [14.0][IMP] base_technical_user: Multi company context, add tests

--- base_technical_user/tests/test_sudo_tech.py
+++ base_technical_user/tests/test_sudo_tech.py
@@ -12,12 +12,27 @@ class SudoTechCase(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.company = cls.env.ref("base.main_company")
+        cls.company_2 = cls.env["res.company"].create(
+            {
+                "name": "Company 2 tech",
+            }
+        )
+        cls.env = cls.env(context=dict(cls.env.context,
+                          tracking_disable=True,
+                          no_reset_password=True)
+        )
         cls.user_tech = (
             cls.env["res.users"]
             .with_context(tracking_disable=True, no_reset_password=True)
             .create({"login": "tech", "name": "tech"})
         )
-        cls.company = cls.env.ref("base.main_company")
+        cls.user_tech_2 = (
+            cls.env["res.users"]
+            .with_context(tracking_disable=True, no_reset_password=True)
+            .with_company(cls.company_2)
+            .create({"login": "tech2", "name": "tech2", "company_id": cls.company_2.id})
+        )
         cls.env(user=cls.env.ref("base.user_demo").id)
 
     def test_sudo_tech(self):
@@ -32,3 +47,19 @@ class SudoTechCase(TransactionCase):
     def test_sudo_tech_missing_raise(self):
         with self.assertRaises(UserError):
             self.env["res.partner"].sudo_tech(raise_if_missing=True)
+
+    def test_sudo_tech_company_2(self):
+        self.company_2.user_tech_id = self.user_tech_2
+        self_tech = self.env["res.partner"].with_company(self.company_2).sudo_tech()
+        self.assertEqual(self_tech._uid, self.user_tech_2.id)
+
+    def test_sudo_tech_company_2_record(self):
+        # We switch company twice to fill in allowed_company_ids
+        user = self.env.user.with_company(self.company_2).with_company(self.company)
+        self.assertEqual(
+            self.company,
+            user.env.company,
+        )
+        self.company_2.user_tech_id = self.user_tech_2
+        self_tech = user.with_company(self.company_2).sudo_tech()
+        self.assertEqual(self_tech._uid, self.user_tech_2.id)
