f7d47fc13c136e1c598eeeb21742ad9584b1c2dd
Randall Castro <randall@vauxoo.com>
2021-07-02 21:06:57 -0600

    [IMP] attachment_unindex_content: black, isort, prettier

--- attachment_unindex_content/__manifest__.py
+++ attachment_unindex_content/__manifest__.py
@@ -2,18 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Attachment Unindex Content',
-    'summary': 'Disable indexing of attachments',
-    'version': '12.0.1.0.0',
-    'author': 'Vauxoo, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/server-tools',
-    'maintainers': ['moylop260', 'ebirbe'],
-    'license': 'AGPL-3',
-    'category': 'Tools',
-    'depends': [
-        'base',
-    ],
-    'installable': True,
-    'application': False,
-    'post_init_hook': 'post_init_hook',
+    "name": "Attachment Unindex Content",
+    "summary": "Disable indexing of attachments",
+    "version": "13.0.1.0.0",
+    "author": "Vauxoo, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-tools",
+    "maintainers": ["moylop260", "ebirbe"],
+    "license": "AGPL-3",
+    "category": "Tools",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+    "post_init_hook": "post_init_hook",
 }
