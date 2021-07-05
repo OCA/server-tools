48e29eb88fbff7f426aa875d1fb7c9f008836aa2
Nils Hamerlinck <nils@trobz.com>
2021-07-05 19:58:39 +0700

    [IMP] bus_alt_connection: black, isort, prettier

--- bus_alt_connection/__manifest__.py
+++ bus_alt_connection/__manifest__.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Bus Alt Connection',
-    'summary': 'Needed when using PgBouncer as a connection pooler',
-    'version': '12.0.1.0.0',
-    'author': 'Trobz,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/server-tools',
-    'category': 'Extra Tools',
-    'license': 'AGPL-3',
-    'depends': ['bus'],
-    'installable': True,
-    'auto_install': False,
+    "name": "Bus Alt Connection",
+    "summary": "Needed when using PgBouncer as a connection pooler",
+    "version": "12.0.1.0.0",
+    "author": "Trobz,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-tools",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": ["bus"],
+    "installable": True,
+    "auto_install": False,
 }
