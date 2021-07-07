0d5fae0e1a2c7be7a62ac3fa228df0163fe96a68
Alfredo Zamora <alfredo.zamora@agilebg.com>
2021-07-07 12:52:20 +0200

    [MIG] scheduler_error_mailer: Migration to 14.0

--- scheduler_error_mailer/models/ir_cron.py
+++ scheduler_error_mailer/models/ir_cron.py
@@ -25,7 +25,7 @@ class IrCron(models.Model):
     def _handle_callback_exception(
         self, cron_name, server_action_id, job_id, job_exception
     ):
-        res = super(IrCron, self)._handle_callback_exception(
+        super()._handle_callback_exception(
             cron_name, server_action_id, job_id, job_exception
         )
         my_cron = self.browse(job_id)
@@ -43,8 +43,6 @@ class IrCron(models.Model):
             self.env["mail.template"].browse(my_cron.email_template_id.id).with_context(
                 context
             ).sudo().send_mail(my_cron.id, force_send=True)
-
-        return res
 
     @api.model
     def _test_scheduler_failure(self):
--- scheduler_error_mailer/readme/CONTRIBUTORS.rst
+++ scheduler_error_mailer/readme/CONTRIBUTORS.rst
@@ -3,7 +3,10 @@
 * Alexis de Lattre <alexis.delattre@akretion.com>
 * Sodexis <dev@sodexis.com>
 * Achraf Mhadhbi <machraf@bloopark.de>
+* Alfredo Zamora <alfredo.zamora@agilebg.com>
 
 * `Tecnativa <https://www.tecnativa.com>`_:
 
   * Cristina Martin R.
+
+* Chafique Delli <chafique.delli@akretion.com>
