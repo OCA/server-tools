7137c311c4d3bcc1ad8058bac861ce7ddd50e818
hkpatel <helly.kapatel@initos.com>
2021-07-16 18:29:38 +0530

    [Fix]: Add header feature in latest v14

--- sql_request_abstract/__manifest__.py
+++ sql_request_abstract/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "SQL Request Abstract",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "GRAP,Akretion,Odoo Community Association (OCA)",
     "maintainers": ["legalsylvain"],
     "website": "https://github.com/OCA/server-tools",
