e2d180921374972c8d98ec3e54f9808bc7ac4100
Kitti U <kittiu@ecosoft.co.th>
2021-09-05 14:30:09 +0700

    [ENH] Add option 'Readonly User' to all models

--- base_model_restrict_update/models/ir_model_access.py
+++ base_model_restrict_update/models/ir_model_access.py
@@ -11,25 +11,56 @@ class IrModelAccess(models.Model):
 
     @api.model
     @tools.ormcache_context(
-        "self._uid", "model", "mode", "raise_exception", keys=("lang",)
+        "self.env.uid",
+        "self.env.su",
+        "model",
+        "mode",
+        "raise_exception",
+        keys=("lang",),
     )
     def check(self, model, mode="read", raise_exception=True):
         if self.env.su:
             return True
         res = super().check(model, mode, raise_exception)
-        self._cr.execute(
-            "SELECT restrict_update FROM ir_model WHERE model = %s", (model,)
-        )
-        query_res = self._cr.dictfetchone()
-        if (
-            query_res["restrict_update"]
-            and mode != "read"
-            and not self.env.user.unrestrict_model_update
-        ):
-            if raise_exception:
+        if mode != "read" and raise_exception:
+            if self._test_readonly(model) or self._test_restrict_update(model):
                 raise AccessError(
                     _("You are only allowed to read this record. (%s - %s)")
                     % (model, mode)
                 )
-            return False
         return res
+
+    @api.model
+    def _test_readonly(self, model):
+        exclude_models = self._readonly_exclude_models()
+        if model not in exclude_models and self.env.user.is_readonly_user:
+            return True
+        return False
+
+    @api.model
+    def _test_restrict_update(self, model):
+        self.env.cr.execute(
+            "SELECT restrict_update FROM ir_model WHERE model = %s", (model,)
+        )
+        query_res = self.env.cr.dictfetchone()
+        if query_res["restrict_update"] and not self.env.user.unrestrict_model_update:
+            return True
+        return False
+
+    @api.model
+    def _readonly_exclude_models(self):
+        """ Models updtate/create by system, and should be excluded from checking """
+        return (
+            self.sudo()
+            .search(
+                [
+                    ("group_id", "=", False),
+                    "|",
+                    ("perm_write", "=", True),
+                    "|",
+                    ("perm_create", "=", True),
+                    ("perm_unlink", "=", True),
+                ]
+            )
+            .mapped("model_id.model")
+        )
