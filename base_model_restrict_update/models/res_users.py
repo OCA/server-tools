b70360140d7356ff5b4e5a2f535c3e129a85d33c
Kitti U <kittiu@ecosoft.co.th>
2021-09-05 13:19:39 +0700

    [14.0][MIG] base_model_restrict_update

--- base_model_restrict_update/models/res_users.py
+++ base_model_restrict_update/models/res_users.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResUsers(models.Model):
@@ -12,7 +12,6 @@ class ResUsers(models.Model):
         help="Set to true and the user can update restricted model.",
     )
 
-    @api.multi
     def toggle_unrestrict_model_update(self):
         for record in self:
             record.unrestrict_model_update = not record.unrestrict_model_update
