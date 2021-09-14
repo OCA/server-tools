211bd87f8bd56020e22914fb9f8637d46e99e419
Alessandro Uffreduzzi <alessandro.uffreduzzi@associazionedigitaldreamers.it>
2021-09-14 17:24:33 +0200

    [FIX] scheduler_error_mailer: fixed email template fields
    [UPD] Update scheduler_error_mailer.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: server-tools-12.0/server-tools-12.0-scheduler_error_mailer
    Translate-URL: https://translation.odoo-community.org/projects/server-tools-12-0/server-tools-12-0-scheduler_error_mailer/

---
+++ scheduler_error_mailer/migrations/12.0.1.2.0/post-migration.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    template = env.ref("scheduler_error_mailer.scheduler_error_mailer")
+    template.body_html = template.body_html.replace(
+        "${object.model or ''}", "${object.model_id.name or ''}"
+    )
+    template.body_html = template.body_html.replace(
+        "<li>Method : ${object.function or ''}</li>", ""
+    )
+    template.body_html = template.body_html.replace(
+        "<li>Arguments : ${object.args or ''}</li>",
+        "<li>Python code : <code>${object.code or ''}</code></li>"
+    )
