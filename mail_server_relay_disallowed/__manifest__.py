7efceb7464aab4feb266fe0457dfad3ad833e988
joan <joan.sisquella@forgeflow.com>
2021-09-22 15:50:42 +0200

    [MIG] mail_server_relay_disallowed: Migration to 14.0

--- mail_server_relay_disallowed/__manifest__.py
+++ mail_server_relay_disallowed/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2020 ForgeFlow, S.L.
+# Copyright 2020-2021 ForgeFlow, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail Server Relay Disallowed",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "ForgeFlow, Project Expert Team, Odoo Community Association (OCA)",
     "contributors": ["Jordi Ballester <jordi.ballester@forgeflow.com>"],
     "website": "https://github.com/OCA/server-tools",
