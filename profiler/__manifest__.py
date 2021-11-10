1bcb112c37ba2ccee978befcef48ee7a2b546de1
Tom Blauwendraat <tom@sunflowerweb.nl>
2021-11-10 10:48:34 +0100

    [MIG] profiler: Migration to 12.0

--- profiler/__manifest__.py
+++ profiler/__manifest__.py
@@ -3,7 +3,7 @@
     'author': "Vauxoo, Sunflower IT, Odoo Community Association (OCA)",
     'website': "https://github.com/OCA/server-tools/tree/12.0/profiler",
     'category': 'Tests',
-    'version': '11.0.2.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': ["document", "web_tour"],
     'data': [
