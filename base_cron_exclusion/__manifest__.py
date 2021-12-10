fa09454d21bb572220fcf6ebd914ede644a2ec87
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-10 11:14:09 -0500

    [15.0][MIG] base_cron_exclusion

--- base_cron_exclusion/__manifest__.py
+++ base_cron_exclusion/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2017-19 ForgeFlow S.L. (https://www.forgeflow.com)
+# Copyright 2017-21 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Base Cron Exclusion",
     "summary": "Allow you to select scheduled actions that should not run "
     "simultaneously.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
-    "maintainers": ["LoisRForgeFlow"],
+    "maintainers": ["LoisRForgeFlow", "ChrisOForgeFlow"],
     "development_status": "Production/Stable",
     "website": "https://github.com/OCA/server-tools",
     "category": "Tools",
