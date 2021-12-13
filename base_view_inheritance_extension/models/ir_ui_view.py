eb816c105d823bb5ac77f947a2fa32bdaa19cab7
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-12-13 11:58:55 -0300

    [MIG] base_view_inheritance_extension: Migration to 15.0
    * Removed `list_add` and `list_remove`, they've been deprecated and
    implemented in Odoo core since several versions ago.
    
    * Removed `move`, as it has also already been implemented in core several
    versions ago.
    
    * Replaced `python_dict` by `update`, that performs an operation similar
    to :meth:`dict.update`, but on the ast.Dict.

--- base_view_inheritance_extension/models/ir_ui_view.py
+++ base_view_inheritance_extension/models/ir_ui_view.py
@@ -11,6 +11,45 @@ from lxml import etree
 from odoo import api, models
 
 
+def ast_dict_update(source, update):
+    """Perform a dict `update` on an ast.Dict
+
+    Behaves similar to :meth:`dict.update`, but on ast.Dict instead.
+    Only compares string-like ast.Dict keys (ast.Str or ast.Constant).
+
+    :returns: The updated ast.Dict
+    :rtype: ast.Dict
+    """
+    if not isinstance(source, ast.Dict):
+        raise TypeError("`source` must be an AST dict")
+    if not isinstance(update, ast.Dict):
+        raise TypeError("`update` must be an AST dict")
+
+    def ast_key_eq(k1, k2):
+        # python < 3.8 uses ast.Str; python >= 3.8 uses ast.Constant
+        if type(k1) != type(k2):
+            return False
+        elif isinstance(k1, ast.Str):
+            return k1.s == k2.s
+        elif isinstance(k1, ast.Constant):
+            return k1.value == k2.value
+
+    toadd_uidx = []
+    for uidx, ukey in enumerate(update.keys):
+        found = False
+        for sidx, skey in enumerate(source.keys):
+            if ast_key_eq(ukey, skey):
+                source.values[sidx] = update.values[uidx]
+                found = True
+                break
+        if not found:
+            toadd_uidx.append(uidx)
+    for uidx in toadd_uidx:
+        source.keys.append(update.keys[uidx])
+        source.values.append(update.values[uidx])
+    return source
+
+
 class IrUiView(models.Model):
     _inherit = "ir.ui.view"
 
@@ -41,14 +80,14 @@ class IrUiView(models.Model):
 
     @api.model
     def _get_inheritance_handler(self, node):
-        handler = super(IrUiView, self).apply_inheritance_specs
+        handler = super().apply_inheritance_specs
         if hasattr(self, "inheritance_handler_%s" % node.tag):
             handler = getattr(self, "inheritance_handler_%s" % node.tag)
         return handler
 
     @api.model
     def _get_inheritance_handler_attributes(self, node):
-        handler = super(IrUiView, self).apply_inheritance_specs
+        handler = super().apply_inheritance_specs
         if hasattr(self, "inheritance_handler_attributes_%s" % node.get("operation")):
             handler = getattr(
                 self, "inheritance_handler_attributes_%s" % node.get("operation")
@@ -56,75 +95,35 @@ class IrUiView(models.Model):
         return handler
 
     @api.model
-    def inheritance_handler_attributes_python_dict(self, source, specs):
-        """Implement
-        <$node position="attributes">
-            <attribute name="$attribute" operation="python_dict" key="$key">
-                $keyvalue
-            </attribute>
-        </$node>"""
+    def inheritance_handler_attributes_update(self, source, specs):
+        """Implement dict `update` operation on the attribute node.
+
+        .. code-block:: xml
+
+            <field position="attributes">
+                <attribute name="context" operation="update">
+                    {
+                        "key": "value",
+                    }
+                </attribute>
+            </field>
+        """
         node = self.locate_node(source, specs)
-        for attribute_node in specs:
-            attr_name = attribute_node.get("name")
-            attr_key = attribute_node.get("key")
-            str_dict = node.get(attr_name) or "{}"
-            ast_dict = ast.parse(str_dict, mode="eval").body
-            assert isinstance(ast_dict, ast.Dict), f"'{attr_name}' is not a dict"
-            assert attr_key, "No key specified for 'python_dict' operation"
-            # Find the ast dict key
-            # python < 3.8 uses ast.Str; python >= 3.8 uses ast.Constant
-            key_idx = next(
-                (
-                    i
-                    for i, k in enumerate(ast_dict.keys)
-                    if (isinstance(k, ast.Str) and k.s == attr_key)
-                    or (isinstance(k, ast.Constant) and k.value == attr_key)
-                ),
-                None,
-            )
-            # Update or create the key
-            value = ast.parse(attribute_node.text.strip(), mode="eval").body
-            if key_idx:
-                ast_dict.values[key_idx] = value
-            else:
-                ast_dict.keys.append(ast.Str(attr_key))
-                ast_dict.values.append(value)
+        for spec in specs:
+            attr_name = spec.get("name")
+            # Parse ast from both node and spec
+            source_ast = ast.parse(node.get(attr_name) or "{}", mode="eval").body
+            update_ast = ast.parse(spec.text.strip(), mode="eval").body
+            if not isinstance(source_ast, ast.Dict):
+                raise TypeError(f"Attribute `{attr_name}` is not a dict")
+            if not isinstance(update_ast, ast.Dict):
+                raise TypeError(f"Operation for attribute `{attr_name}` is not a dict")
+            # Update node ast dict
+            source_ast = ast_dict_update(source_ast, update_ast)
             # Dump the ast back to source
             # TODO: once odoo requires python >= 3.9; use `ast.unparse` instead
-            node.attrib[attribute_node.get("name")] = astor.to_source(
-                ast_dict, pretty_source=lambda s: "".join(s).strip()
+            node.attrib[attr_name] = astor.to_source(
+                source_ast,
+                pretty_source=lambda s: "".join(s).strip(),
             )
-        return source
-
-    @api.model
-    def inheritance_handler_attributes_list_add(self, source, specs):
-        """Implement
-        <$node position="attributes">
-            <attribute name="$attribute" operation="list_add">
-                $new_value
-            </attribute>
-        </$node>"""
-        node = self.locate_node(source, specs)
-        for attribute_node in specs:
-            attribute_name = attribute_node.get("name")
-            old_value = node.get(attribute_name) or ""
-            new_value = old_value + "," + attribute_node.text
-            node.attrib[attribute_name] = new_value
-        return source
-
-    @api.model
-    def inheritance_handler_attributes_list_remove(self, source, specs):
-        """Implement
-        <$node position="attributes">
-            <attribute name="$attribute" operation="list_remove">
-                $value_to_remove
-            </attribute>
-        </$node>"""
-        node = self.locate_node(source, specs)
-        for attribute_node in specs:
-            attribute_name = attribute_node.get("name")
-            old_values = (node.get(attribute_name) or "").split(",")
-            remove_values = attribute_node.text.split(",")
-            new_values = [x for x in old_values if x not in remove_values]
-            node.attrib[attribute_name] = ",".join([_f for _f in new_values if _f])
         return source
