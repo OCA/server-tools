ef1064e4ee48341a3ea4fcb712fdc1e299258bca
sonhd <sonhd@trobz.com>
2021-12-19 17:54:17 +0700

    [IMP] base_conditional_image: black, isort, prettier

--- base_conditional_image/__manifest__.py
+++ base_conditional_image/__manifest__.py
@@ -2,18 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Conditional Images',
-    'version': '12.0.1.0.1',
-    'author': 'Camptocamp, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'category': 'Misc',
-    'depends': [
-        'base',
+    "name": "Conditional Images",
+    "version": "12.0.1.0.1",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Misc",
+    "depends": [
+        "base",
     ],
-    'website': 'http://github.com/OCA/server-tools',
-    'data': [
-        'views/image_view.xml',
-        'security/ir.model.access.csv',
+    "website": "https://github.com/OCA/server-tools",
+    "data": [
+        "views/image_view.xml",
+        "security/ir.model.access.csv",
     ],
-    'installable': True,
+    "installable": True,
 }
