5732b47e12b60e69f48e971e2a81e65d046952f3
sonhd <sonhd@trobz.com>
2021-12-27 12:00:29 +0700

    [IMP] mail_cleanup: black, isort, prettier

--- mail_cleanup/__manifest__.py
+++ mail_cleanup/__manifest__.py
@@ -1,20 +1,19 @@
 # Copyright 2015-2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Mail cleanup',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'summary': 'Mark as read or delete mails after a set time',
-    'author': 'Camptocamp, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/server-tools',
-    'depends': [
-        'mail_environment',
+    "name": "Mail cleanup",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "summary": "Mark as read or delete mails after a set time",
+    "author": "Camptocamp, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-tools",
+    "depends": [
+        "mail_environment",
     ],
-    'data': [
-        'views/mail_view.xml',
+    "data": [
+        "views/mail_view.xml",
     ],
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
 }
