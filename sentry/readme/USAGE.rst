f176b8bd9dbf718dd8a0870db07cc4784653b4db
Fernanda Hernandez <fernanda@vauxoo.com>
2022-01-03 18:15:37 +0100

    [IMP] sentry: migrate sentry-raven to new api sentry-sdk

--- sentry/readme/USAGE.rst
+++ sentry/readme/USAGE.rst
@@ -3,4 +3,4 @@ above the configured Sentry logging level, no additional actions are necessary.
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/149/13.0
+   :target: https://runbot.odoo-community.org/runbot/149/14.0
--- sentry/tests/test_client.py
+++ sentry/tests/test_client.py
@@ -3,150 +3,148 @@
 
 import logging
 import sys
-import unittest
 
-import raven
 from mock import patch
+from sentry_sdk.integrations.logging import _IGNORED_LOGGERS
+from sentry_sdk.transport import HttpTransport
 
 from odoo import exceptions
+from odoo.tests import TransactionCase
+from odoo.tools import config
 
-from .. import initialize_raven
-from ..logutils import OdooSentryHandler
+from ..hooks import initialize_sentry
 
 GIT_SHA = "d670460b4b4aece5915caf5c68d12f560a9fe3e4"
 RELEASE = "test@1.2.3"
 
 
-def log_handler_by_class(logger, handler_cls):
-    for handler in logger.handlers:
-        if isinstance(handler, handler_cls):
-            yield handler
+def remove_handler_ignore(handler_name):
+    """Removes handlers of handlers ignored list."""
+    _IGNORED_LOGGERS.discard(handler_name)
 
 
-def remove_logging_handler(logger_name, handler_cls):
-    """Removes handlers of specified classes from a :class:`logging.Logger`
-    with a given name.
-
-    :param string logger_name: name of the logger
-
-    :param handler_cls: class of the handler to remove. You can pass a tuple of
-        classes to catch several classes
-    """
-    logger = logging.getLogger(logger_name)
-    for handler in log_handler_by_class(logger, handler_cls):
-        logger.removeHandler(handler)
+class TestException(exceptions.UserError):
+    pass
 
 
-class InMemoryClient(raven.Client):
-    """A :class:`raven.Client` subclass which simply stores events in a list.
+class InMemoryTransport(HttpTransport):
+    """A :class:`sentry_sdk.Hub.transport` subclass which simply stores events in a list.
 
     Extended based on the one found in raven-python to avoid additional testing
     dependencies: https://git.io/vyGO3
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.events = []
-        super(InMemoryClient, self).__init__(**kwargs)
 
-    def is_enabled(self):
-        return True
-
-    def send(self, **kwargs):
-        self.events.append(kwargs)
+    def capture_event(self, event, *args, **kwargs):
+        self.events.append(event)
 
     def has_event(self, event_level, event_msg):
         for event in self.events:
-            if event.get("level") == event_level and event.get("message") == event_msg:
+            if (
+                event.get("level") == event_level
+                and event.get("logentry", {}).get("message") == event_msg
+            ):
                 return True
         return False
 
+    def flush(self, *args, **kwargs):
+        pass
 
-class TestClientSetup(unittest.TestCase):
+    def kill(self, *args, **kwargs):
+        pass
+
+
+class TestClientSetup(TransactionCase):
     def setUp(self):
         super(TestClientSetup, self).setUp()
-        self.logger = logging.getLogger(__name__)
+        self.dsn = "http://public:secret@example.com/1"
+        config.options["sentry_enabled"] = True
+        config.options["sentry_dsn"] = self.dsn
+        self.client = initialize_sentry(config)._client
+        self.client.transport = InMemoryTransport({"dsn": self.dsn})
+        self.handler = self.client.integrations["logging"]._handler
 
-        # Sentry is enabled by default, so the default handler will be added
-        # when the module is loaded. After that, subsequent calls to
-        # setup_logging will not re-add our handler. We explicitly remove
-        # OdooSentryHandler handler so we can test with our in-memory client.
-        remove_logging_handler("", OdooSentryHandler)
+    def log(self, level, msg, exc_info=None):
+        record = logging.LogRecord(__name__, level, __file__, 42, msg, (), exc_info)
+        self.handler.emit(record)
 
     def assertEventCaptured(self, client, event_level, event_msg):
         self.assertTrue(
-            client.has_event(event_level, event_msg),
+            client.transport.has_event(event_level, event_msg),
             msg='Event: "%s" was not captured' % event_msg,
         )
 
     def assertEventNotCaptured(self, client, event_level, event_msg):
         self.assertFalse(
-            client.has_event(event_level, event_msg),
+            client.transport.has_event(event_level, event_msg),
             msg='Event: "%s" was captured' % event_msg,
         )
 
     def test_initialize_raven_sets_dsn(self):
-        config = {
-            "sentry_enabled": True,
-            "sentry_dsn": "http://public:secret@example.com/1",
-        }
-        client = initialize_raven(config, client_cls=InMemoryClient)
-        self.assertEqual(client.remote.base_url, "http://example.com")
+        self.assertEqual(self.client.dsn, self.dsn)
 
     def test_capture_event(self):
-        config = {
-            "sentry_enabled": True,
-            "sentry_dsn": "http://public:secret@example.com/1",
-        }
         level, msg = logging.WARNING, "Test event, can be ignored"
-        client = initialize_raven(config, client_cls=InMemoryClient)
-        self.logger.log(level, msg)
-        self.assertEventCaptured(client, level, msg)
+        self.log(level, msg)
+        level = "warning"
+        self.assertEventCaptured(self.client, level, msg)
+
+    def test_capture_event_exc(self):
+        level, msg = logging.WARNING, "Test event, can be ignored exception"
+        try:
+            raise TestException(msg)
+        except TestException:
+            exc_info = sys.exc_info()
+        self.log(level, msg, exc_info)
+        level = "warning"
+        self.assertEventCaptured(self.client, level, msg)
 
     def test_ignore_exceptions(self):
-        config = {
-            "sentry_enabled": True,
-            "sentry_dsn": "http://public:secret@example.com/1",
-            "sentry_ignore_exceptions": "odoo.exceptions.UserError",
-        }
-        level, msg = logging.WARNING, "Test UserError"
-        client = initialize_raven(config, client_cls=InMemoryClient)
-
-        handlers = list(log_handler_by_class(logging.getLogger(), OdooSentryHandler))
-        self.assertTrue(handlers)
-        handler = handlers[0]
+        config.options["sentry_ignore_exceptions"] = "odoo.exceptions.UserError"
+        client = initialize_sentry(config)._client
+        client.transport = InMemoryTransport({"dsn": self.dsn})
+        level, msg = logging.WARNING, "Test exception"
         try:
             raise exceptions.UserError(msg)
         except exceptions.UserError:
             exc_info = sys.exc_info()
-        record = logging.LogRecord(__name__, level, __file__, 42, msg, (), exc_info)
-        handler.emit(record)
+        self.log(level, msg, exc_info)
+        level = "warning"
         self.assertEventNotCaptured(client, level, msg)
 
-    @patch("odoo.addons.sentry.get_odoo_commit", return_value=GIT_SHA)
+    def test_exclude_logger(self):
+        config.options["sentry_enabled"] = True
+        config.options["sentry_exclude_loggers"] = __name__
+        client = initialize_sentry(config)._client
+        client.transport = InMemoryTransport({"dsn": self.dsn})
+        level, msg = logging.WARNING, "Test exclude logger %s" % __name__
+        self.log(level, msg)
+        level = "warning"
+        # Revert ignored logger so it doesn't affect other tests
+        remove_handler_ignore(__name__)
+        self.assertEventNotCaptured(client, level, msg)
+
+    @patch("odoo.addons.sentry.hooks.get_odoo_commit", return_value=GIT_SHA)
     def test_config_odoo_dir(self, get_odoo_commit):
-        config = {
-            "sentry_enabled": True,
-            "sentry_dsn": "http://public:secret@example.com/1",
-            "sentry_odoo_dir": "/opt/odoo/core",
-        }
-        client = initialize_raven(config, client_cls=InMemoryClient)
+        config.options["sentry_odoo_dir"] = "/opt/odoo/core"
+        client = initialize_sentry(config)._client
+
         self.assertEqual(
-            client.release,
+            client.options["release"],
             GIT_SHA,
             "Failed to use 'sentry_odoo_dir' parameter appropriately",
         )
 
-    @patch("odoo.addons.sentry.get_odoo_commit", return_value=GIT_SHA)
+    @patch("odoo.addons.sentry.hooks.get_odoo_commit", return_value=GIT_SHA)
     def test_config_release(self, get_odoo_commit):
-        config = {
-            "sentry_enabled": True,
-            "sentry_dsn": "http://public:secret@example.com/1",
-            "sentry_odoo_dir": "/opt/odoo/core",
-            "sentry_release": RELEASE,
-        }
-        client = initialize_raven(config, client_cls=InMemoryClient)
+        config.options["sentry_odoo_dir"] = "/opt/odoo/core"
+        config.options["sentry_release"] = RELEASE
+        client = initialize_sentry(config)._client
+
         self.assertEqual(
-            client.release,
+            client.options["release"],
             RELEASE,
             "Failed to use 'sentry_release' parameter appropriately",
         )
