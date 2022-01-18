6af336e000e100d6958f80163f04005f452666c9
Simone Orsi <simahawk@gmail.com>
2022-01-18 11:01:55 +0100

    base_jsonify: re-license to LGPL

--- base_jsonify/models/ir_exports_line.py
+++ base_jsonify/models/ir_exports_line.py
@@ -1,5 +1,5 @@
 # Copyright 2017 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
