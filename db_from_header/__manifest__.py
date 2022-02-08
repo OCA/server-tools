ba50b26a2c007c8e09cbcfd1e372e4d97478711a
Yann Papouin <ypa@decgroupe.com>
2022-02-08 14:49:11 +0100

    [ADD] db_from_header

---
+++ db_from_header/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "db_from_header",
+    "summary": "Pre-select a database with an HTTP header",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/OCA/server-tools",
+    "author": "DEC, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "complexity": "normal",
+    "category": "Tools",
+    "depends": ['web', ],
+    "auto_install": False,
+    'installable': True,
+}
