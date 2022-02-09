e79b3d382acd4d77e8e0a3e0b9586504a3d3ea72
Andrius Laukavičius <andrius@focusate.eu>
2022-02-09 18:31:41 +0200

    [MIG] base_jsonify

--- base_jsonify/tests/test_ir_exports_line.py
+++ base_jsonify/tests/test_ir_exports_line.py
@@ -2,10 +2,10 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo.exceptions import ValidationError
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestIrExportsLine(SavepointCase):
+class TestIrExportsLine(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
