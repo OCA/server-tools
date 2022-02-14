6ea49e7f5e33df8516a39fb32b80320f9c604bb5
eLBati <lb@takobi.online>
2022-02-14 18:07:29 +0100

    IMP base_model_restrict_update: readonly users can create wizards This is useful for reporting wizards typically

--- base_model_restrict_update/models/ir_model_access.py
+++ base_model_restrict_update/models/ir_model_access.py
@@ -50,8 +50,11 @@ class IrModelAccess(models.Model):
     @api.model
     def _readonly_exclude_models(self):
         """Models updtate/create by system, and should be excluded from checking"""
+        transient_models = self.env["ir.model"].sudo().search(
+            [("transient", "=", True)]).mapped("model")
+        # Models update/create by system, and should be excluded from checking
         return (
-            self.sudo()
+            transient_models + self.sudo()
             .search(
                 [
                     ("group_id", "=", False),
