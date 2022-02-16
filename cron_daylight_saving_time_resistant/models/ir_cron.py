8af4df7d56e75fc7b035c31faa131cb6551977d6
Florian da Costa <florian.dacosta@akretion.com>
2022-02-16 11:47:32 +0100

    [MIG] cron_daylight_saving_time_resistant: Migration to 14.0

--- cron_daylight_saving_time_resistant/models/ir_cron.py
+++ cron_daylight_saving_time_resistant/models/ir_cron.py
@@ -1,14 +1,13 @@
-# © 2018 Akretion - Raphaël Reverdy
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 import logging
 from datetime import datetime
 
 import pytz
-from openerp import api, fields, models
-from openerp.addons.base.ir.ir_cron import _intervalTypes
-from openerp.osv import fields as osv_fields
-from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+from odoo import api, fields, models
+
+from odoo.addons.base.models.ir_cron import _intervalTypes
 
 _logger = logging.getLogger(__name__)
 
@@ -37,55 +36,52 @@ class IrCron(models.Model):
         diff_offset = after_offset - before_offset
         return diff_offset
 
+    @classmethod
     def _process_job(cls, job_cr, job, cron_cr):
         """Add or remove the Daylight saving offset when needed."""
-        super(IrCron, cls)._process_job(job_cr, job, cron_cr)
+        super()._process_job(job_cr, job, cron_cr)
         # changing the date has to be after the super, else, e may add a hour
         # to next call, and the super will no run the cron, (because now will
         # be 1 hour too soon) and the date will just be incremented of 1
         # hour, each hour...until the changes time really occurs...
-        # if need to test this, use freeze_gun lib.
         if job["daylight_saving_time_resistant"]:
             with api.Environment.manage():
-                now = osv_fields.datetime.context_timestamp(
-                    job_cr, job["user_id"], datetime.now()
-                )
-                nextcall = osv_fields.datetime.context_timestamp(
-                    job_cr,
-                    job["user_id"],
-                    datetime.strptime(
-                        job["nextcall"],  # original nextcall
-                        DEFAULT_SERVER_DATETIME_FORMAT,
-                    ),
-                )
-                numbercall = job["numbercall"]
-                delta = _intervalTypes[job["interval_type"]](job["interval_number"])
-                diff_offset = cls._calculate_daylight_offset(
-                    nextcall, delta, numbercall, now
-                )
-                if diff_offset and nextcall < now and numbercall:
-                    cron_cr.execute(
-                        """
-                        SELECT nextcall FROM ir_cron WHERE id = %s
-                    """,
-                        (job["id"],),
-                    )
-                    res_sql = cron_cr.fetchall()
-                    new_nextcall = res_sql and res_sql[0][0]
-                    new_nextcall = osv_fields.datetime.context_timestamp(
+                try:
+                    cron = api.Environment(
                         job_cr,
                         job["user_id"],
-                        datetime.strptime(
-                            new_nextcall,  # original nextcall
-                            DEFAULT_SERVER_DATETIME_FORMAT,
-                        ),
+                        {"lastcall": fields.Datetime.from_string(job["lastcall"])},
+                    )[cls._name]
+                    now = fields.Datetime.context_timestamp(cron, datetime.now())
+                    # original nextcall
+                    nextcall = fields.Datetime.context_timestamp(cron, job["nextcall"])
+                    numbercall = job["numbercall"]
+                    delta = _intervalTypes[job["interval_type"]](job["interval_number"])
+                    diff_offset = cron._calculate_daylight_offset(
+                        nextcall, delta, numbercall, now
                     )
-                    new_nextcall -= diff_offset
-                    modified_next_call = fields.Datetime.to_string(
-                        new_nextcall.astimezone(pytz.UTC)
-                    )
-                    cron_cr.execute(
-                        "UPDATE ir_cron SET nextcall=%s WHERE id=%s",
-                        (modified_next_call, job["id"]),
-                    )
+                    if diff_offset and nextcall < now and numbercall:
+                        cron_cr.execute(
+                            """
+                            SELECT nextcall FROM ir_cron WHERE id = %s
+                        """,
+                            (job["id"],),
+                        )
+                        res_sql = cron_cr.fetchall()
+                        new_nextcall = res_sql and res_sql[0][0]
+                        new_nextcall = fields.Datetime.context_timestamp(
+                            cron, new_nextcall
+                        )
+                        new_nextcall -= diff_offset
+                        modified_next_call = fields.Datetime.to_string(
+                            new_nextcall.astimezone(pytz.UTC)
+                        )
+                        cron_cr.execute(
+                            "UPDATE ir_cron SET nextcall=%s WHERE id=%s",
+                            (modified_next_call, job["id"]),
+                        )
+                        cron.flush()
+                        cron.invalidate_cache()
+                finally:
+                    job_cr.commit()
                     cron_cr.commit()
--- cron_daylight_saving_time_resistant/tests/test_dst.py
+++ cron_daylight_saving_time_resistant/tests/test_dst.py
@@ -1,89 +1,62 @@
-# © 2018 Akretion <https://akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from datetime import datetime, timedelta
 
-from openerp.tests.common import TransactionCase
-from pytz import timezone
+from freezegun import freeze_time
+
+import odoo
+from odoo import fields
+from odoo.tests.common import TransactionCase
+from odoo.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT
 
 
 class TestDST(TransactionCase):
-    def test_dst(self):
-        """First test, caching some data."""
-        cron = self.env["ir.cron"]
-        brux = timezone("Europe/Brussels")
-        ncall = -1
-        winter_jan_12 = datetime(2018, 1, 1, 12, 0)
-        winter_feb_0 = datetime(2018, 1, 2, 0, 0)
-        summer_june_12 = datetime(2018, 6, 15, 12, 0)
-        summer_sep_3 = datetime(2018, 9, 17, 3, 0)
-        winter_jan_next_year = datetime(2019, 2, 3, 0, 0)
-        tests = [
-            {
-                "nextcall": brux.localize(winter_jan_12),
-                "delta": timedelta(days=5),
-                "now": brux.localize(winter_feb_0),
-                "expected": timedelta(hours=0),
-            },
-            {
-                "nextcall": brux.localize(winter_jan_12),
-                "delta": timedelta(days=6 * 30),
-                "now": brux.localize(winter_feb_0),
-                "expected": timedelta(hours=1),
-            },
-            {
-                "nextcall": brux.localize(winter_jan_12),
-                "delta": timedelta(days=5),
-                "now": brux.localize(summer_june_12),
-                "expected": timedelta(hours=1),
-            },
-            {
-                "nextcall": brux.localize(winter_jan_12),
-                "delta": timedelta(days=6 * 30),
-                "now": brux.localize(summer_june_12),
-                "expected": timedelta(hours=1),
-            },
-            {
-                "nextcall": brux.localize(winter_jan_12),
-                "delta": timedelta(days=6 * 365),
-                "now": brux.localize(winter_jan_next_year),
-                "expected": timedelta(hours=0),
-            },
-        ]
-        tests = tests + [
-            {
-                "nextcall": brux.localize(summer_june_12),
-                "delta": timedelta(days=5),
-                "now": brux.localize(winter_jan_next_year),
-                "expected": timedelta(hours=-1),
-            },
-            {
-                "nextcall": brux.localize(summer_june_12),
-                "delta": timedelta(days=4 * 30),
-                "now": brux.localize(winter_jan_next_year),
-                "expected": timedelta(hours=-1),
-            },
-            {
-                "nextcall": brux.localize(summer_june_12),
-                "delta": timedelta(days=5),
-                "now": brux.localize(summer_june_12),
-                "expected": timedelta(hours=0),
-            },
-            {
-                "nextcall": brux.localize(summer_june_12),
-                "delta": timedelta(days=6 * 30),
-                "now": brux.localize(summer_sep_3),
-                "expected": timedelta(hours=-1),
-            },
-            {
-                "nextcall": brux.localize(summer_june_12),
-                "delta": timedelta(days=6 * 365),
-                "now": brux.localize(summer_sep_3),
-                "expected": timedelta(hours=0),
-            },
-        ]
-        test = tests[0]
-        for test in tests:
-            res = cron._calculate_daylight_offset(
-                test["nextcall"], test["delta"], ncall, test["now"]
+    def setUp(self):
+        super().setUp()
+        self.registry.enter_test_mode(self.env.cr)
+
+    def tearDown(self):
+        self.registry.leave_test_mode()
+        super().tearDown()
+
+    def _check_cron_date_after_run(self, cron, datetime_str):
+        # add 10 sec to make sure cron will run
+        datetime_current = datetime.strptime(
+            datetime_str, DEFAULT_SERVER_DATETIME_FORMAT
+        ) + timedelta(seconds=10)
+        datetime_current_str = datetime_current.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        with freeze_time(datetime_current_str):
+            cron.write(
+                {"nextcall": datetime_str, "daylight_saving_time_resistant": True}
             )
-            self.assertEqual(res, test["expected"])
+            cron.flush()
+            self.env.cr.execute("SELECT * FROM ir_cron WHERE id = %s", (cron.id,))
+            job = self.env.cr.dictfetchall()[0]
+            timezone_date_orig = fields.Datetime.context_timestamp(cron, cron.nextcall)
+            with odoo.registry(self.env.cr.dbname).cursor() as new_cr:
+                registry = odoo.registry(new_cr.dbname)
+                registry["ir.cron"]._process_job(new_cr, job, new_cr)
+                day_after_date_orig = (timezone_date_orig + timedelta(days=1)).day
+            timezone_date_after = fields.Datetime.context_timestamp(cron, cron.nextcall)
+            # check the cron is really planned the next day (which mean it has run
+            # then check the planned hour is the same even in case of change of time
+            # (brussels summer time/ brussels winter time
+            self.assertEqual(day_after_date_orig, timezone_date_after.day)
+            self.assertEqual(timezone_date_orig.hour, timezone_date_after.hour)
+
+    def test_cron(self):
+        cron = self.env["ir.cron"].create(
+            {
+                "name": "TestCron",
+                "model_id": self.env.ref("base.model_res_partner").id,
+                "state": "code",
+                "code": "model.search([])",
+                "interval_number": 1,
+                "interval_type": "days",
+                "numbercall": -1,
+                "doall": False,
+            }
+        )
+        # from summer time to winter time
+        self._check_cron_date_after_run(cron, "2021-10-30 15:00:00")
+        # from winter time to summer time
+        self._check_cron_date_after_run(cron, "2021-03-27 15:00:00")
