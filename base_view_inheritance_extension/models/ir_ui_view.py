1382493f6ab5966e3e86b2d78eb84691492b786b
Simone Orsi <simahawk@gmail.com>
2022-03-01 15:03:18 +0100

    base_view_inheritance_extension: wrap import

--- base_view_inheritance_extension/models/ir_ui_view.py
+++ base_view_inheritance_extension/models/ir_ui_view.py
@@ -2,13 +2,18 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # Copyright 2021 Camptocamp SA (https://www.camptocamp.com).
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
 import ast
-import astor
+import logging
 
 from lxml import etree
 
 from odoo import api, models
+
+try:
+    import astor
+except ImportError as err:  # pragma: no cover
+    _logger = logging.getLogger(__name__)
+    _logger.debug(err)
 
 
 class IrUiView(models.Model):
