74db750ed30dc76932ceb55078e44c4c0fd03b02
Lorenzo Battistini <lb@takobi.online>
2022-03-17 15:43:00 +0100

    Let's combine the searches into one
    Co-authored-by: Yoshi Tashiro (Quartile) <tashiro@quartile.co>

--- base_model_restrict_update/models/ir_model_access.py
+++ base_model_restrict_update/models/ir_model_access.py
@@ -49,13 +49,11 @@ class IrModelAccess(models.Model):
 
     @api.model
     def _readonly_exclude_models(self):
-        transient_models = self.env["ir.model"].sudo().search(
-            [("transient", "=", True)]).mapped("model")
         skipped_models = self.env["ir.model"].sudo().search([
-            ("skip_check_for_readonly_users", "=", True)
+            "|", ("transient", "=", True), ("skip_check_for_readonly_users", "=", True)
         ]).mapped("model")
         # Models updtate/create by system, and should be excluded from checking
-        return skipped_models + transient_models + self.sudo().search([
+        return skipped_models + self.sudo().search([
             ("group_id", "=", False),
             "|", ("perm_write", "=", True),
             "|", ("perm_create", "=", True), ("perm_unlink", "=", True)
