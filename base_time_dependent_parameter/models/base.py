d841e3f87681fd60662897eb300ced4f5070d5ad
Henrik Norlin <henrik.norl@gmail.com>
2022-05-10 17:24:38 +0200

    [FIX] get_time_dependent_parameter

--- base_time_dependent_parameter/models/base.py
+++ base_time_dependent_parameter/models/base.py
@@ -1,13 +1,15 @@
 # Author Copyright (C) 2022 Nimarosa (Nicolas Rodriguez) (<nicolasrsande@gmail.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import datetime
 from odoo import models
 
 class Base(models.AbstractModel):
     _inherit = 'base'
 
     # This will make the parameters accessible from any Odoo model.
-    def get_time_dependent_parameter(self, code, date_field_name=None):
-        # TODO: set date_field based on date_field_name
-        date_field = self.dict.date_to
-        return self.env["base.time_parameter"]._get_parameter_from_code(code, date_field)
+    def get_time_dependent_parameter(self, code, date_field_name='date_to'):
+        date = getattr(self, date_field_name)
+        if type(date) is datetime.datetime:
+            date = date.date()
+        return self.env["base.time_parameter"]._get_parameter_from_code(code, date)
