ed45f32db5a261f171388f1dd2dc26f6dae50a67
Kev-Roche <kevin.roche@akretion.com>
2022-05-12 22:58:41 +0200

    [MIG] attachment_delete_restrict: Migration to  14.0

--- attachment_delete_restrict/models/ir_attachment.py
+++ attachment_delete_restrict/models/ir_attachment.py
@@ -1,40 +1,62 @@
 # Copyright 2021 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, models
+from odoo import _, models
 from odoo.exceptions import ValidationError
 
 
 class IrAttachment(models.Model):
     _inherit = "ir.attachment"
 
-    @api.multi
     def unlink(self):
-        for rec in self:
-            if not rec.res_model:
-                continue
-            model = self.env["ir.model"].search(
-                [
-                    ("model", "=", rec.res_model),
-                    ("restrict_delete_attachment", "=", True),
-                ]
-            )
-            if not model:
-                continue
-            groups = model.delete_attachment_group_ids
-            if groups and self.env.user in groups.mapped("users"):
-                continue
-            users = model.delete_attachment_user_ids
-            if users and self.env.user in users:
-                continue
-            user_names = "\n".join(
-                list(set(groups.mapped("users").mapped("name") + users.mapped("name")))
-            )
-            raise ValidationError(
-                _(
-                    "You are not allowed to delete this attachment.\n\nUsers with "
-                    "the delete permission:\n%s"
-                )
-                % (user_names or "None")
-            )
+        restriction = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("restrict_delete_attachment")
+        )
+        if restriction != "none":
+            for rec in self:
+                if restriction == "strict":
+                    if rec.create_uid == self.env.user or self.user_has_groups(
+                        "base.group_system"
+                    ):
+                        continue
+                    raise ValidationError(
+                        _(
+                            "You are not allowed to delete this attachment.\n"
+                            "Only the owner and administrators can delete it."
+                        )
+                    )
+                if restriction == "custom":
+                    if not rec.res_model:
+                        continue
+                    model = self.env["ir.model"].search(
+                        [
+                            ("model", "=", rec.res_model),
+                            ("is_restrict_delete_attachment", "=", True),
+                        ]
+                    )
+                    if not model:
+                        continue
+                    groups = model.delete_attachment_group_ids
+                    if groups and self.env.user in groups.mapped("users"):
+                        continue
+                    users = model.delete_attachment_user_ids
+                    if users and self.env.user in users:
+                        continue
+                    user_names = "\n".join(
+                        list(
+                            set(
+                                groups.mapped("users").mapped("name")
+                                + users.mapped("name")
+                            )
+                        )
+                    )
+                    raise ValidationError(
+                        _(
+                            "You are not allowed to delete this attachment.\n\nUsers with "
+                            "the delete permission:\n%s"
+                        )
+                        % (user_names or "None")
+                    )
         return super().unlink()
