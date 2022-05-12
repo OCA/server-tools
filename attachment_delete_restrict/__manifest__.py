7d2c4ede5ea5fb207cd924167d1863914ae0d5ca
Kev-Roche <kevin.roche@akretion.com>
2022-05-12 19:59:34 +0200

    [IMP] attachment_delete_restrict: black, isort, prettier

--- attachment_delete_restrict/__manifest__.py
+++ attachment_delete_restrict/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Restrict Deletion of Attachments",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": ["base"],
     "website": "https://github.com/OCA/server-tools",
     "author": "Quartile Limited, Odoo Community Association (OCA)",
