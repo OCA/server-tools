ce0235fa8c05eef21712d4c894d4364f3765c51c
Cesar Andres Sanchez <cesar.sanchez@tecnativa.com>
2022-06-28 23:24:27 +0200

    [IMP] Update development_status Mature

--- base_view_inheritance_extension/__manifest__.py
+++ base_view_inheritance_extension/__manifest__.py
@@ -3,7 +3,8 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Extended view inheritance",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
+    "development_status": "Mature",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Hidden/Dependency",
