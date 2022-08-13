1e30b1a3ad17b2ef3d4d058621aef2f3d236e4c0
Tom Blauwendraat <tom@sunflowerweb.nl>
2022-08-14 00:20:27 +0200

    [IMP] profiler: black, isort, prettier

--- profiler/hooks.py
+++ profiler/hooks.py
@@ -1,8 +1,7 @@
-
 import logging
 
-from odoo.http import WebRequest
 from odoo import http
+from odoo.http import WebRequest
 from odoo.sql_db import Cursor
 
 from .models.profiler_profile import ProfilerProfile
@@ -19,30 +18,33 @@ def patch_web_request_call_function():
 
     For instance, Odoo spawns a new thread for each request.
     """
-    _logger.info('Patching http.WebRequest._call_function')
+    _logger.info("Patching http.WebRequest._call_function")
     webreq_f_origin = WebRequest._call_function
 
     def webreq_f(*args, **kwargs):
         with ProfilerProfile.profiling():
             return webreq_f_origin(*args, **kwargs)
+
     WebRequest._call_function = webreq_f
 
 
 def patch_cursor_init():
-    _logger.info('Patching sql_dp.Cursor.__init__')
+    _logger.info("Patching sql_dp.Cursor.__init__")
     cursor_f_origin = Cursor.__init__
 
     def init_f(self, *args, **kwargs):
         cursor_f_origin(self, *args, **kwargs)
         enable = ProfilerProfile.activate_deactivate_pglogs
         if enable is not None:
-            self._obj.execute('SET log_min_duration_statement TO "%s"' %
-                              ((not enable) * -1,))
+            self._obj.execute(
+                'SET log_min_duration_statement TO "{}"'.format((not enable) * -1)
+            )
+
     Cursor.__init__ = init_f
 
 
 def patch_dispatch_rpc():
-    _logger.info('Patching Dispatch RPC http.dispatch_rpc')
+    _logger.info("Patching Dispatch RPC http.dispatch_rpc")
 
     dispatch_rpc = http.dispatch_rpc
 
--- profiler/tests/test_profiling_ui.py
+++ profiler/tests/test_profiling_ui.py
@@ -1,4 +1,3 @@
-
 # Copyright 2018 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -8,12 +7,11 @@ from odoo.tests.common import HttpCase, at_install, post_install
 @at_install(False)
 @post_install(True)
 class TestProfilingUI(HttpCase):
-
     def test_profile_creation_by_request(self):
         """We are testing the creation of a user session profile."""
         self.phantom_js(
             "/web",
             "odoo.__DEBUG__.services['web_tour.tour'].run('profiler_tour')",
             "odoo.__DEBUG__.services['web_tour.tour'].tours.profiler_tour.ready",
-            login="admin"
+            login="admin",
         )
