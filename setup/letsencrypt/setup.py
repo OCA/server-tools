87a69488a34a9a9ccd710fd40051e3d0db55bce5
Tom Blauwendraat <tom@sunflowerweb.nl>
2022-08-14 10:55:48 +0200

    [UPD] letsencrypt: python dependencies

--- setup/letsencrypt/setup.py
+++ setup/letsencrypt/setup.py
@@ -2,5 +2,14 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        'external_dependencies_override': {
+            'python': {
+                'acme': 'acme',
+                'cryptography': 'cryptography',
+                'dns': 'dnspython==1.16.0',
+                'josepy': 'josepy',
+            }
+        }
+    },
 )
