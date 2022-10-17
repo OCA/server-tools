cda5e638b0a8d8c6d636730f2e08a71e582c7586
Atte Isopuro <atte.isopuro@avoin.systems>
2022-10-17 13:28:49 +0300

    [MIG] sentry: Migration to 15.0

--- sentry/__manifest__.py
+++ sentry/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sentry",
     "summary": "Report Odoo errors to Sentry",
-    "version": "14.0.1.1.1",
+    "version": "15.0.1.0.0",
     "category": "Extra Tools",
     "website": "https://github.com/OCA/server-tools",
     "author": "Mohammed Barsi,"
