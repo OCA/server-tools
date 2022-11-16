47047034341d1713c60ba298856c66b5f41c7f82
matiasperalta1 <matias_peralta1@hotmail.com>
2022-11-16 17:25:07 -0300

    [MIG] base_exception: Migration to 16.0

--- base_exception/__manifest__.py
+++ base_exception/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Exception Rule",
-    "version": "15.0.1.0.2",
+    "version": "16.0.1.0.0",
     "development_status": "Mature",
     "category": "Generic Modules",
     "summary": """
