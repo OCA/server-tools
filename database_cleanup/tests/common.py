cbe9f9f7393b84121c3da83604d77f85191e439a
Joel Zilli <joz@adhoc.com.ar>
2022-11-24 09:50:23 -0300

    [MIG] database_cleanup: Migration to 16.0

--- database_cleanup/tests/common.py
+++ database_cleanup/tests/common.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.modules.registry import Registry
-from odoo.tests import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class Common(TransactionCase):
