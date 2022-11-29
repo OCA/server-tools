e44918649c8de819ee2aab95d07dde2e9753f64d
filoquin <filoquin@gmail.com>
2022-11-29 08:48:46 -0300

    [MIG] base_name_search_improved: Migration to 16.0

--- base_name_search_improved/hooks.py
+++ base_name_search_improved/hooks.py
@@ -1,13 +1,12 @@
 import logging
 
-from odoo import SUPERUSER_ID, api, models
+from odoo import SUPERUSER_ID, api
 
 _logger = logging.getLogger(__name__)
 
 
 def uninstall_hook(cr, registry):
     _logger.info("Reverting Patches...")
-    models.BaseModel._revert_method("fields_view_get")
     env = api.Environment(cr, SUPERUSER_ID, {})
     env["ir.model.fields"].with_context(_force_unlink=True).search(
         [("name", "=", "smart_search")]
