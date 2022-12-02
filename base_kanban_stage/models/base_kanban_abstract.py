c843bf545fdb2b89d504873503107cc70622f84e
AaronHForgeFlow <ahenriquez@eficent.com>
2022-12-02 23:13:12 +0100

    [MIG] base_kanban_stage: Migration to v15

--- base_kanban_stage/models/base_kanban_abstract.py
+++ base_kanban_stage/models/base_kanban_abstract.py
@@ -93,7 +93,6 @@ class BaseKanbanAbstract(models.AbstractModel):
             ("done", "Ready"),
             ("blocked", "Special Handling"),
         ],
-        string="Kanban Status",
         default="normal",
         tracking=True,
         required=True,
