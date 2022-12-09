d4e267cbbbedd363d80885af848e1da7cd3283da
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-12-09 17:30:04 +0100

    [MIG] session_db from 13.0

--- session_db/__manifest__.py
+++ session_db/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Store sessions in DB",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Odoo SA,ACSONE SA/NV,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/server-tools",
