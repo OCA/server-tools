c14559cb68f07895b4823cc67a2f838ece72cd95
FernandoRomera <fromera@astrohotel.es>
2022-12-19 09:19:47 +0100

    [16.0][MIG] iap_alternative_provider: Migration to 16.0

--- iap_alternative_provider/tests/test_iap_account.py
+++ iap_alternative_provider/tests/test_iap_account.py
@@ -1,7 +1,8 @@
 # Copyright 2022 Moka Tourisme (https://www.mokatourisme.fr).
 # @author Pierre Verkest <pierreverkest84@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-import mock
+
+from unittest import mock
 
 from odoo.tests import TransactionCase
 
