73115d1f73aa8846bfebaca98e19cee01244e54e
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-12-20 12:43:03 +0630

    [MIG] excel_import_export, excel_import_export_demo: Migration to 16.0

--- excel_import_export/wizard/import_xlsx_wizard.py
+++ excel_import_export/wizard/import_xlsx_wizard.py
@@ -1,6 +1,7 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
+
 from odoo import _, api, fields, models
 from odoo.exceptions import RedirectWarning, ValidationError
 
