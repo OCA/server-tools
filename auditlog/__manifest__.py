83461a537b1ded3109d921cad1083c3ae581bacd
Enric Tobella <etobella@creublanca.es>
2022-12-21 23:29:08 +0100

    [IMP] auditlog: make the line views using a non auto model

--- auditlog/__manifest__.py
+++ auditlog/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Audit Log",
-    "version": "16.0.1.0.0",
+    "version": "16.0.2.0.0",
     "author": "ABF OSIELL, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/server-tools",
