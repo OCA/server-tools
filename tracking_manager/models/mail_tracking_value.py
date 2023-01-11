5bd8845b54494e8525f2f61f5d2d88b165f88a7c
Sébastien BEAU <sebastien.beau@akretion.com>
2023-01-11 20:55:27 +0100

    tracking_manager: continue to refactor code
    - track m2m simplify using native mail_thread and process them like text
    - simplify code for track one2many
    - avoid useless code on model not impacted by the tracking

---
+++ tracking_manager/models/mail_tracking_value.py
@@ -0,0 +1,28 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+from ..tools import format_m2m
+
+
+class MailTrackingValue(models.Model):
+    _inherit = "mail.tracking.value"
+
+    def create_tracking_values(
+        self,
+        initial_value,
+        new_value,
+        col_name,
+        col_info,
+        tracking_sequence,
+        model_name,
+    ):
+        if col_info["type"] == "many2many":
+            col_info["type"] = "text"
+            initial_value = format_m2m(initial_value)
+            new_value = format_m2m(new_value)
+        return super().create_tracking_values(
+            initial_value, new_value, col_name, col_info, tracking_sequence, model_name
+        )
