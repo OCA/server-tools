022e7d3553a7e809d635d193376b07c9f9a5b75f
Sébastien BEAU <sebastien.beau@akretion.com>
2023-01-11 20:55:27 +0100

    tracking_manager: continue to refactor code
    - track m2m simplify using native mail_thread and process them like text
    - simplify code for track one2many
    - avoid useless code on model not impacted by the tracking

---
+++ tracking_manager/tools.py
@@ -0,0 +1,7 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+def format_m2m(records):
+    return "; ".join(records.mapped("display_name"))
