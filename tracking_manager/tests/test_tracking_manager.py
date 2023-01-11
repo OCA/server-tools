e9bdbdc399a820056791ed6cbcc9a6d8aa9d3850
Sébastien BEAU <sebastien.beau@akretion.com>
2023-01-11 20:55:27 +0100

    tracking_manager: continue to refactor code
    - track m2m simplify using native mail_thread and process them like text
    - simplify code for track one2many
    - avoid useless code on model not impacted by the tracking

--- tracking_manager/tests/test_tracking_manager.py
+++ tracking_manager/tests/test_tracking_manager.py
@@ -2,6 +2,8 @@
 # @author Kévin Roche <kevin.roche@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from odoo_test_helper import FakeModelLoader
+
 from odoo.tests.common import SavepointCase
 
 
@@ -9,6 +11,13 @@ class TestTrackingManager(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Load fake models ->/
+        cls.loader = FakeModelLoader(cls.env, cls.__module__)
+        cls.loader.backup_registry()
+        from .models import ResPartnerBank
+
+        cls.loader.update_registry([ResPartnerBank])
+
         cls.partner_categ_1, cls.partner_categ_2, cls.partner_categ_3 = cls.env[
             "res.partner.category"
         ].create(
@@ -25,9 +34,15 @@ class TestTrackingManager(SavepointCase):
                 "category_id": [(6, 0, [cls.partner_categ_1.id])],
             }
         )
-        cls.partner.message_ids.unlink()
         cls.partner_model = cls.env.ref("base.model_res_partner")
         cls._active_tracking(["bank_ids", "category_id"])
+        cls.flush_tracking()
+        cls.partner.message_ids.unlink()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.loader.restore_registry()
+        super().tearDownClass()
 
     @classmethod
     def _active_tracking(cls, fields_list):
@@ -56,8 +71,16 @@ class TestTrackingManager(SavepointCase):
         self.partner_model.update_custom_tracking()
         self.assertTrue(field.custom_tracking)
 
+    @classmethod
+    def flush_tracking(cls):
+        """Force the creation of tracking values."""
+        cls.env["base"].flush()
+        cls.env.cr.precommit.run()
+
     @property
     def messages(self):
+        # Force the creation of tracking values
+        self.flush_tracking()
         return self.partner.message_ids
 
     def test_m2m_add_line(self):
@@ -76,12 +99,18 @@ class TestTrackingManager(SavepointCase):
             }
         )
         self.assertEqual(len(self.messages), 1)
-        self.assertIn("New", self.messages.body)
+        tracking = self.messages.tracking_value_ids
+        self.assertEqual(len(tracking), 1)
+        self.assertEqual(tracking.old_value_text, "FOO")
+        self.assertEqual(tracking.new_value_text, "FOO; BAR")
 
     def test_m2m_delete_line(self):
         self.partner.write({"category_id": [(6, 0, [])]})
         self.assertEqual(len(self.messages), 1)
-        self.assertIn("Delete", self.messages.body)
+        tracking = self.messages.tracking_value_ids
+        self.assertEqual(len(tracking), 1)
+        self.assertEqual(tracking.old_value_text, "FOO")
+        self.assertEqual(tracking.new_value_text, "")
 
     def test_m2m_multi_line(self):
         self.partner.write(
@@ -99,8 +128,10 @@ class TestTrackingManager(SavepointCase):
             }
         )
         self.assertEqual(len(self.messages), 1)
-        self.assertEqual(self.messages.body.count("New"), 2)
-        self.assertEqual(self.messages.body.count("Delete"), 1)
+        tracking = self.messages.tracking_value_ids
+        self.assertEqual(len(tracking), 1)
+        self.assertEqual(tracking.old_value_text, "FOO")
+        self.assertEqual(tracking.new_value_text, "BAR; TOOH")
 
     def test_o2m_create_indirectly(self):
         self.partner.write({"bank_ids": [(0, 0, {"acc_number": "1234567890"})]})
@@ -146,6 +177,44 @@ class TestTrackingManager(SavepointCase):
         self.assertEqual(len(self.messages), 1)
         self.assertEqual(self.messages.body.count("New"), 1)
         self.assertEqual(self.messages.body.count("Delete"), 1)
+
+    def test_o2m_update_m2m_indirectly(self):
+        self.partner.write(
+            {
+                "bank_ids": [
+                    (
+                        1,
+                        self.partner.bank_ids[0].id,
+                        {
+                            "category_ids": [
+                                (
+                                    6,
+                                    0,
+                                    [self.partner_categ_1.id, self.partner_categ_2.id],
+                                )
+                            ]
+                        },
+                    ),
+                ]
+            }
+        )
+        self.assertEqual(len(self.messages), 1)
+        self.assertEqual(self.messages.body.count("Changed"), 1)
+
+    def test_o2m_update_m2o_indirectly(self):
+        self.partner.write(
+            {
+                "bank_ids": [
+                    (
+                        1,
+                        self.partner.bank_ids[0].id,
+                        {"bank_id": self.env["res.bank"].create({"name": "DOO"}).id},
+                    ),
+                ]
+            }
+        )
+        self.assertEqual(len(self.messages), 1)
+        self.assertEqual(self.messages.body.count("Changed"), 1)
 
     def test_o2m_create_directly(self):
         self.env["res.partner.bank"].create(
