01ff98281113eef33735fbfa5cd3b889499667ea
Simone Rubino <sir@takobi.online>
2023-01-23 16:06:47 +0100

    [FIX] base_external_dbsource_mysql: Add Python dependencies

--- base_external_dbsource_mysql/__manifest__.py
+++ base_external_dbsource_mysql/__manifest__.py
@@ -15,13 +15,12 @@
     'depends': [
         'base_external_dbsource_sqlite',
     ],
-    # Uncomment this for v11
-    # 'external_dependencies': [
-    #     'python': [
-    #         'sqlalchemy',
-    #         'MySQLdb',
-    #     ]
-    # ],
+    'external_dependencies': {
+        'python': [
+            'sqlalchemy',
+            'MySQLdb',
+        ],
+    },
     'demo': [
         'demo/base_external_dbsource.xml',
     ],
