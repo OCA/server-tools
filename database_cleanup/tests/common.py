83fa16b92445e451ec899ec1323967c538bb430e
Maksym Yankin <yankinmk@gmail.com>
2023-02-09 14:39:14 +0200

    [IMP] database_cleanup: improve tests

--- database_cleanup/tests/common.py
+++ database_cleanup/tests/common.py
@@ -1,14 +1,27 @@
 # Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.modules.registry import Registry
-from odoo.tests.common import TransactionCase
+from contextlib import contextmanager
+
+import odoo
+from odoo.tests import common
+from odoo.tests.common import BaseCase, tagged
+
+ADMIN_USER_ID = common.ADMIN_USER_ID
 
 
-class Common(TransactionCase):
+@contextmanager
+def environment():
+    """Return an environment with a new cursor for the current database; the
+    cursor is committed and closed after the context block.
+    """
+    registry = odoo.registry(common.get_db_name())
+    with registry.cursor() as cr:
+        yield odoo.api.Environment(cr, ADMIN_USER_ID, {})
+
+
+# Use post_install to get all models loaded more info: odoo/odoo#13458
+@tagged("post_install", "-at_install")
+class Common(BaseCase):
     def setUp(self):
-        super(Common, self).setUp()
-        # this reloads our registry, and we don't want to run tests twice
-        # we also need the original registry for further tests, so save a
-        # reference to it
-        self.original_registry = Registry.registries[self.env.cr.dbname]
+        super().setUp()
--- database_cleanup/tests/test_purge_tables.py
+++ database_cleanup/tests/test_purge_tables.py
@@ -5,21 +5,19 @@ from psycopg2 import ProgrammingError
 from odoo.tests.common import tagged
 from odoo.tools import mute_logger
 
-from .common import Common
+from .common import Common, environment
 
 
 # Use post_install to get all models loaded more info: odoo/odoo#13458
 @tagged("post_install", "-at_install")
 class TestCleanupPurgeLineTable(Common):
-    def setUp(self):
-        super(TestCleanupPurgeLineTable, self).setUp()
-        # create an orphaned table
-        self.env.cr.execute("create table database_cleanup_test (test int)")
-
     def test_empty_table(self):
-        wizard = self.env["cleanup.purge.wizard.table"].create({})
-        wizard.purge_all()
-        with self.assertRaises(ProgrammingError):
-            with self.env.registry.cursor() as cr:
-                with mute_logger("odoo.sql_db"):
-                    cr.execute("select * from database_cleanup_test")
+        with environment() as env:
+            # create an orphaned table
+            env.cr.execute("create table database_cleanup_test (test int)")
+            wizard = env["cleanup.purge.wizard.table"].create({})
+            wizard.purge_all()
+            with self.assertRaises(ProgrammingError):
+                with env.registry.cursor() as cr:
+                    with mute_logger("odoo.sql_db"):
+                        cr.execute("select * from database_cleanup_test")
