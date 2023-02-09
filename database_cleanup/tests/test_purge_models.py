0785ef0208e292ab9014bf8032f3615bcd56d090
Maksym Yankin <yankinmk@gmail.com>
2023-02-09 14:39:14 +0200

    [IMP] database_cleanup: improve tests

--- database_cleanup/tests/test_purge_models.py
+++ database_cleanup/tests/test_purge_models.py
@@ -3,44 +3,38 @@
 
 from odoo.tests.common import tagged
 
-from .common import Common
+from .common import Common, environment
 
 
 # Use post_install to get all models loaded more info: odoo/odoo#13458
 @tagged("post_install", "-at_install")
 class TestCleanupPurgeLineColumn(Common):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        # create a nonexistent model
-        cls.model_name = "x_database.cleanup.test.model"
-        cls.model_values = {
-            "name": "Database cleanup test model",
-            "model": cls.model_name,
-        }
-        cls.model = cls.env["ir.model"].create(cls.model_values)
-        cls.env.cr.execute(
-            "insert into ir_attachment (name, res_model, res_id, type) values "
-            "('test attachment', %s, 42, 'binary')",
-            [cls.model_name],
-        )
-        cls.env.registry.models.pop(cls.model_name)
-
-    @classmethod
-    def tearDownClass(cls):
-        """We recreate the model to avoid registry Exception at loading"""
-        super().tearDownClass()
-        # FIXME: issue origin is not clear but it must be addressed.
-        cls.model = cls.env["ir.model"].create(cls.model_values)
+    def setUp(self):
+        super().setUp()
+        with environment() as env:
+            # create a nonexistent model
+            self.model_name = "x_database.cleanup.test.model"
+            self.model_values = {
+                "name": "Database cleanup test model",
+                "model": self.model_name,
+            }
+            self.model = env["ir.model"].create(self.model_values)
+            env.cr.execute(
+                "insert into ir_attachment (name, res_model, res_id, type) values "
+                "('test attachment', %s, 42, 'binary')",
+                [self.model_name],
+            )
+            env.registry.models.pop(self.model_name)
 
     def test_empty_model(self):
-        wizard = self.env["cleanup.purge.wizard.model"].create({})
-        wizard.purge_all()
-        # must be removed by the wizard
-        self.assertFalse(
-            self.env["ir.model"].search(
-                [
-                    ("model", "=", self.model_name),
-                ]
+        with environment() as env:
+            wizard = env["cleanup.purge.wizard.model"].create({})
+            wizard.purge_all()
+            # must be removed by the wizard
+            self.assertFalse(
+                env["ir.model"].search(
+                    [
+                        ("model", "=", self.model_name),
+                    ]
+                )
             )
-        )
