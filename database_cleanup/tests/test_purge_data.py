276f6bdca4be63578db58de5b46b25344eed50c9
Maksym Yankin <yankinmk@gmail.com>
2023-02-09 14:39:14 +0200

    [IMP] database_cleanup: improve tests

--- database_cleanup/tests/test_purge_data.py
+++ database_cleanup/tests/test_purge_data.py
@@ -3,28 +3,30 @@
 
 from odoo.tests.common import tagged
 
-from .common import Common
+from .common import Common, environment
 
 
 # Use post_install to get all models loaded more info: odoo/odoo#13458
 @tagged("post_install", "-at_install")
 class TestCleanupPurgeLineData(Common):
     def setUp(self):
-        super(TestCleanupPurgeLineData, self).setUp()
-        # create a data entry pointing nowhere
-        self.env.cr.execute("select max(id) + 1 from res_users")
-        self.env["ir.model.data"].create(
-            {
-                "module": "database_cleanup",
-                "name": "test_no_data_entry",
-                "model": "res.users",
-                "res_id": self.env.cr.fetchone()[0],
-            }
-        )
+        super().setUp()
+        with environment() as env:
+            # create a data entry pointing nowhere
+            env.cr.execute("select max(id) + 1 from res_users")
+            env["ir.model.data"].create(
+                {
+                    "module": "database_cleanup",
+                    "name": "test_no_data_entry",
+                    "model": "res.users",
+                    "res_id": env.cr.fetchone()[0],
+                }
+            )
 
     def test_pointing_nowhere(self):
-        wizard = self.env["cleanup.purge.wizard.data"].create({})
-        wizard.purge_all()
-        # must be removed by the wizard
-        with self.assertRaises(ValueError):
-            self.env.ref("database_cleanup.test_no_data_entry")
+        with environment() as env:
+            wizard = env["cleanup.purge.wizard.data"].create({})
+            wizard.purge_all()
+            # must be removed by the wizard
+            with self.assertRaises(ValueError):
+                env.ref("database_cleanup.test_no_data_entry")
