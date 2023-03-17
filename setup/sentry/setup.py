bba08208441b188d733008297ae06246c799c03b
Deivis Laya <deivis@vauxoo.com>
2023-03-17 17:47:37 +0000

    [FIX] sentry: change with_locals to include_local_variables variable
    Currently, version 1.17.0 of sentry_sdk is causing the following error:
    
    SentryOption("with_locals", DEFAULT_OPTIONS["with_locals"], None),
    KeyError: 'with_locals'.
    
    Where the with_locals key is not found in the dictionary, generating an
    error, stopping the installation of the sentry module.
    
    In version 1.17.0 rename 'with_locals'  to 'include_local_variables'
    https://github.com/getsentry/sentry-python/commit/79e33169aa629ec67cf9636b8440f64bf0a6d566
    
    This commit adjust the  get_sentry_options() method in
    https://github.com/Vauxoo/server-tools/blob/14.0/sentry/const.py file, set the new variable.

--- setup/sentry/setup.py
+++ setup/sentry/setup.py
@@ -2,5 +2,11 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        'external_dependencies_override': {
+            'python': {
+                'sentry_sdk': 'sentry_sdk>=1.17.0',
+            }
+        }
+    },
 )
