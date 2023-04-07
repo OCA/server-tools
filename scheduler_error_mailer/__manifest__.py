beb51ffd601050a2a1377d74cf3a905684d4187a
Holger Brunn <mail@hunki-enterprises.com>
2023-04-07 20:51:51 +0200

    [MIG] scheduler_error_mailer

--- scheduler_error_mailer/__manifest__.py
+++ scheduler_error_mailer/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Scheduler Error Mailer",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Extra Tools",
     "license": "AGPL-3",
     "author": "Akretion,Sodexis,Odoo Community Association (OCA)",
