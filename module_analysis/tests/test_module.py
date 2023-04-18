11c3b44a897c5f37b5e3749b430e130dd3e79782
Florian da Costa <florian.dacosta@akretion.com>
2023-04-18 16:37:48 +0200

    [FIX] Playing test after install must not be mandatory anymore because there is no post install hook anymore

--- module_analysis/tests/test_module.py
+++ module_analysis/tests/test_module.py
@@ -2,11 +2,9 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import TransactionCase, at_install, post_install
+from odoo.tests.common import TransactionCase
 
 
-@at_install(False)
-@post_install(True)
 class TestModule(TransactionCase):
     def setUp(self):
         super().setUp()
