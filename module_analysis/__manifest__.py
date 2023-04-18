8adb4c0f943bcbddd94eb507ac2bec8fb1bd6eab
Florian da Costa <florian.dacosta@akretion.com>
2023-04-18 14:42:19 +0200

    [16.0][MIG] module_analysis : migrate to 16

--- module_analysis/__manifest__.py
+++ module_analysis/__manifest__.py
@@ -9,7 +9,7 @@
     " custom modules",
     "author": "GRAP, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-tools",
-    "version": "15.0.1.1.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base"],
     "data": [
