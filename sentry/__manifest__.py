b7ed7cc9602c6fa113d97fa1b4606148cf90e163
Peter Clark <peter.clark@otrium.com>
2023-04-25 15:19:51 +0100

    [ADD] sentry module backported from OCA Odoo 16 module
    Backported the sentry module for Odoo 16 created by OCA,
    so that the latest sentry-sdk Python package could be used
    rather than the outdated raven package.
    
    Following changes made (tracked in Readme):
    * Patched Sentry middleware into `odoo.service.wsgi_server`
    * Removed Sentry log message to say Odoo was up and running
    * Use git tag name as release identifier rather than commit hash
    * Set unit tests to run post install

--- sentry/__manifest__.py
+++ sentry/__manifest__.py
@@ -1,24 +1,26 @@
 # Copyright 2016-2017 Versada <https://versada.eu/>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sentry',
-    'summary': 'Report Odoo errors to Sentry',
-    'version': '11.0.1.1.0',
-    'category': 'Extra Tools',
-    'website': 'https://odoo-community.org/',
-    'author': 'Mohammed Barsi,'
-              'Versada,'
-              'Nicolas JEUDY,'
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'application': False,
-    'installable': True,
-    'external_dependencies': {
-        'python': [
-            'raven',
-        ]
+    "name": "Sentry",
+    "summary": "Report Odoo errors to Sentry. Based on Odoo 16 version backported to Odoo 11 by Otrium.",
+    "version": "11.0.0.0.1",
+    "category": "Extra Tools",
+    "website": "https://github.com/OCA/server-tools",
+    "author": "Mohammed Barsi,"
+    "Versada,"
+    "Nicolas JEUDY,"
+    "Odoo Community Association (OCA),"
+    "Vauxoo",
+    "Otrium"
+    "maintainers": ["barsi", "naglis", "versada", "moylop260", "fernandahf"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": ["sentry_sdk"]
     },
-    'depends': [
-        'base',
+    "depends": [
+        "base",
     ],
+    "post_load": "post_load",
 }
--- sentry/logutils.py
+++ sentry/logutils.py
@@ -1,105 +1,80 @@
 # Copyright 2016-2017 Versada <https://versada.eu/>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
+import os.path
 import urllib.parse
+import subprocess
 
-import odoo.http
+from sentry_sdk._compat import text_type
+from werkzeug import datastructures
 
-_logger = logging.getLogger(__name__)
-try:
-    from raven.handlers.logging import SentryHandler
-    from raven.processors import SanitizePasswordsProcessor
-    from raven.utils.wsgi import get_environ, get_headers
-except ImportError:
-    _logger.debug('Cannot import "raven". Please make sure it is installed.')
-    SentryHandler = object
-    SanitizePasswordsProcessor = object
+from .generalutils import get_environ
+from .processor import SanitizePasswordsProcessor
 
 
 def get_request_info(request):
-    '''
+    """
     Returns context data extracted from :param:`request`.
 
     Heavily based on flask integration for Sentry: https://git.io/vP4i9.
-    '''
+    """
     urlparts = urllib.parse.urlsplit(request.url)
     return {
-        'url': '%s://%s%s' % (urlparts.scheme, urlparts.netloc, urlparts.path),
-        'query_string': urlparts.query,
-        'method': request.method,
-        'headers': dict(get_headers(request.environ)),
-        'env': dict(get_environ(request.environ)),
+        "url": "{}://{}{}".format(urlparts.scheme, urlparts.netloc, urlparts.path),
+        "query_string": urlparts.query,
+        "method": request.method,
+        "headers": dict(datastructures.EnvironHeaders(request.environ)),
+        "env": dict(get_environ(request.environ)),
     }
 
 
-def get_extra_context():
-    '''
+def get_extra_context(request):
+    """
     Extracts additional context from the current request (if such is set).
-    '''
-    request = odoo.http.request
+    """
     try:
-        session = getattr(request, 'session', {})
+        session = getattr(request, "session", {})
     except RuntimeError:
         ctx = {}
     else:
         ctx = {
-            'tags': {
-                'database': session.get('db', None),
+            "tags": {
+                "database": session.get("db", None),
             },
-            'user': {
-                'login': session.get('login', None),
-                'uid': session.get('uid', None),
+            "user": {
+                "email": session.get("login", None),
+                "id": session.get("uid", None),
             },
-            'extra': {
-                'context': session.get('context', {}),
+            "extra": {
+                "context": session.get("context", {}),
             },
         }
         if request.httprequest:
-            ctx.update({
-                'request': get_request_info(request.httprequest),
-            })
+            ctx.update({"request": get_request_info(request.httprequest)})
     return ctx
 
 
-class LoggerNameFilter(logging.Filter):
-    '''
-    Custom :class:`logging.Filter` which allows to filter loggers by name.
-    '''
-
-    def __init__(self, loggers, name=''):
-        super(LoggerNameFilter, self).__init__(name=name)
-        self._exclude_loggers = set(loggers)
-
-    def filter(self, event):
-        return event.name not in self._exclude_loggers
-
-
-class OdooSentryHandler(SentryHandler):
-    '''
-    Customized :class:`raven.handlers.logging.SentryHandler`.
-
-    Allows to add additional Odoo and HTTP request data to the event which is
-    sent to Sentry.
-    '''
-
-    def __init__(self, include_extra_context, *args, **kwargs):
-        super(OdooSentryHandler, self).__init__(*args, **kwargs)
-        self.include_extra_context = include_extra_context
-
-    def emit(self, record):
-        if self.include_extra_context:
-            self.client.context.merge(get_extra_context())
-        return super(OdooSentryHandler, self).emit(record)
-
-
 class SanitizeOdooCookiesProcessor(SanitizePasswordsProcessor):
-    '''
-    Custom :class:`raven.processors.Processor`.
-
+    """Custom :class:`raven.processors.Processor`.
     Allows to sanitize sensitive Odoo cookies, namely the "session_id" cookie.
-    '''
+    """
 
-    KEYS = FIELDS = frozenset([
-        'session_id',
-    ])
+    KEYS = frozenset(
+        [
+            "session_id",
+        ]
+    )
+
+
+class InvalidGitRepository(Exception):
+    pass
+
+
+def fetch_git_tag(path):
+    """Return tag for supplied git path"""
+    if not os.path.exists(path):
+        raise InvalidGitRepository("Invalid git path: '%s' provided" % (path))
+    os.chdir(path)
+    git_tag = subprocess.check_output(["git", "describe", "--tags"])
+    git_tag = git_tag.decode("utf-8").strip()
+    return git_tag
