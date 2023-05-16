c8c0b841533ad7ebaf0eb9a5b8f4dcb455fdce9f
Holger Brunn <mail@hunki-enterprises.com>
2023-05-16 15:38:26 +0200

    [IMP] set default template

---
+++ scheduler_error_mailer/hooks.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import SUPERUSER_ID, api
+
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env["ir.cron"].with_context(active_test=False).search([]).write(
+        {
+            "email_template_id": env.ref(
+                "scheduler_error_mailer.scheduler_error_mailer"
+            ).id
+        }
+    )
