5af588724e4cfc227d8b58024640ec3a84b29285
Florian da Costa <florian.dacosta@akretion.com>
2023-05-25 18:03:34 +0200

    [MIG] attachment_synchronize : Migration to v16

--- attachment_synchronize/tests/common.py
+++ attachment_synchronize/tests/common.py
@@ -2,34 +2,36 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import os
+import base64
 
-import mock
-
-from odoo.addons.storage_backend.tests.common import CommonCase
+from odoo.tests.common import TransactionCase
 
 
-class SyncCommon(CommonCase):
+class SyncCommon(TransactionCase):
     def _clean_testing_directory(self):
         for test_dir in [
             self.directory_input,
             self.directory_output,
             self.directory_archived,
         ]:
-            for filename in self.backend.list_files(test_dir):
-                self.backend.delete(os.path.join(test_dir, filename))
+            fs = self.backend.fs
+            if not fs.exists(test_dir):
+                fs.makedirs(test_dir)
+            for filename in fs.ls(test_dir, detail=False):
+                fs.rm(filename)
 
     def _create_test_file(self):
-        self.backend._add_b64_data(
-            os.path.join(self.directory_input, "bar.txt"),
-            self.filedata,
-            mimetype="text/plain",
-        )
+        fs = self.backend.fs
+        path = fs.sep.join([self.directory_input, "bar.txt"])
+        with fs.open(path, "wb") as f:
+            f.write(self.filedata)
 
     def setUp(self):
         super().setUp()
-        self.env.cr.commit = mock.Mock()
-        self.registry.enter_test_mode(self.env.cr)
+        #        self.env.cr.commit = mock.Mock()
+        #        self.registry.enter_test_mode(self.env.cr)
+        self.backend = self.env.ref("fs_storage.default_fs_storage")
+        self.filedata = base64.b64encode(b"This is a simple file")
         self.directory_input = "test_import"
         self.directory_output = "test_export"
         self.directory_archived = "test_archived"
@@ -38,6 +40,5 @@ class SyncCommon(CommonCase):
         self.task = self.env.ref("attachment_synchronize.import_from_filestore")
 
     def tearDown(self):
-        self.registry.leave_test_mode()
         self._clean_testing_directory()
         super().tearDown()
