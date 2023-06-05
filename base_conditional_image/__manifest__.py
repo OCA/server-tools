f59672bcd34bc6cf8408ada8b41f1142a8b551f7
pol <pol.reig@qubiq.es>
2023-06-05 13:36:25 +0200

    [MIG] base_conditional_image: Migration to 16

--- base_conditional_image/__manifest__.py
+++ base_conditional_image/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Conditional Images",
     "summary": "This module extends the functionality to support conditional images",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Misc",
