347d3c1f420f3dfb3e52b2f56ebaa0d3b915a663
pol <pol.reig@qubiq.es>
2023-06-05 12:23:22 +0200

    [MIG] base_conditional_image: migration module

---
+++ base_conditional_image/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019-2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Conditional Images",
+    "summary": "This module extends the functionality to support conditional images",
+    "version": "16.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Misc",
+    "depends": [
+        "base",
+    ],
+    "website": "https://github.com/OCA/server-tools",
+    "data": [
+        "views/image_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
---
+++ base_conditional_image/readme/CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+* `Camptocamp <https://camptocamp.com/>`_:
+    * Patrick Tombez <patrick.tombez@camptocamp.com>
+    * Denis Leemann <denis.leemann@camptocamp.com>
+* `Trobz <https://trobz.com>`_:
+    * Son Ho <sonhd@trobz.com>
