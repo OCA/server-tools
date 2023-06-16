ForgeFlow/server-ux

[14.0][FIX] excel_import_export, make new xml_id not deletable on update process

excel_import_export/models/xlsx_import.py
@@ -50,7 +50,7 @@ class XLSXImport(models.AbstractModel):
             ModelData.create(
                 {
                     "name": "{}_{}".format(record._table, record.id),
-                    "module": "excel_import_export",
+                    "module": "__export__",
                     "model": record._name,
                     "res_id": record.id,
                 }

OCA/server-tools

excel_import_export/models/xlsx_import.py
@@ -50,7 +50,7 @@ class XLSXImport(models.AbstractModel):
             ModelData.create(
                 {
                     "name": "{}_{}".format(record._table, record.id),
-                    "module": "excel_import_export",
+                    "module": "__excel_import_export__",
                     "model": record._name,
                     "res_id": record.id,
                 }