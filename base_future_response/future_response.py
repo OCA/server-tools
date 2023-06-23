14ad6a444745540ad22f02c3722552404046d338
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2023-06-23 20:05:47 +0200

    [ADD] base_future_response

---
+++ base_future_response/future_response.py
@@ -0,0 +1,74 @@
+# Copyright 2021 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import functools
+
+import werkzeug
+from werkzeug.wrappers import Response
+
+from odoo import http
+
+
+class FutureResponse:
+    """
+    werkzeug.Response mock class that only serves as placeholder for
+    headers to be injected in the final response.
+    """
+
+    # used by werkzeug.Response.set_cookie
+    charset = "utf-8"
+    max_cookie_size = 4093
+
+    def __init__(self):
+        self.headers = werkzeug.datastructures.Headers()
+
+    @functools.wraps(werkzeug.Response.set_cookie)
+    def set_cookie(
+        self,
+        key,
+        value="",
+        max_age=None,
+        expires=None,
+        path="/",
+        domain=None,
+        secure=False,
+        httponly=False,
+        samesite=None,
+    ):
+        werkzeug.Response.set_cookie(
+            self,
+            key,
+            value=value,
+            max_age=max_age,
+            expires=expires,
+            path=path,
+            domain=domain,
+            secure=secure,
+            httponly=httponly,
+            samesite=samesite,
+        )
+
+
+_original_get_request = http.root.__class__.get_request
+
+
+def get_request(self, httprequest):
+    request = _original_get_request(self, httprequest)
+    request.future_response = FutureResponse()
+    return request
+
+
+http.root.__class__.get_request = get_request
+
+
+_original_get_response = http.root.__class__.get_response
+
+
+def get_response(self, httprequest, result, explicit_session):
+    response = _original_get_response(self, httprequest, result, explicit_session)
+    if isinstance(response, Response):
+        response.headers.extend(http.request.future_response.headers)
+    return response
+
+
+http.root.__class__.get_response = get_response
---
+++ base_future_response/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This is a backport of Odoo 16 ``request.future_response`` mechanism, to allow
+setting cookies in the response before the response is constructed.
