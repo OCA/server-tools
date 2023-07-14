82d3afd3fc19d9ac2e0348d917624bc3a2a4be14
Ruben Ortlam <ruben.ortlam@initos.com>
2023-07-14 15:10:05 +0200

    [IMP] Improve JS and XML formatting

--- import_processor/static/src/js/processor_menu.js
+++ import_processor/static/src/js/processor_menu.js
@@ -1,13 +1,12 @@
-odoo.define('base_generic_import.ImportMenu', function (require) {
+odoo.define("base_generic_import.ImportMenu", function (require) {
     "use strict";
 
-    const core = require('web.core');
-    const DropdownMenuItem = require('web.DropdownMenuItem');
-    const FavoriteMenu = require('web.FavoriteMenu');
-    const { useModel } = require('web/static/src/js/model.js');
+    const core = require("web.core");
+    const DropdownMenuItem = require("web.DropdownMenuItem");
+    const FavoriteMenu = require("web.FavoriteMenu");
+    const {useModel} = require("web/static/src/js/model.js");
 
     const _t = core._t;
-
 
     /**
      * Import Records menu
@@ -19,7 +18,7 @@ odoo.define('base_generic_import.ImportMenu', function (require) {
     class GenImportMenu extends DropdownMenuItem {
         constructor() {
             super(...arguments);
-            this.model = useModel('searchModel');
+            this.model = useModel("searchModel");
         }
 
         /**
@@ -27,17 +26,17 @@ odoo.define('base_generic_import.ImportMenu', function (require) {
          */
         _onImportClick() {
             const action = {
-                type: 'ir.actions.act_window',
-                name: _t('Import Processor'),
-                target: 'new',
-                view_mode: 'form',
-                res_model: 'import.processor.wizard',
-                views: [[false, 'form']],
+                type: "ir.actions.act_window",
+                name: _t("Import Processor"),
+                target: "new",
+                view_mode: "form",
+                res_model: "import.processor.wizard",
+                views: [[false, "form"]],
                 context: {
                     default_model: this.model.config.modelName,
                 },
             };
-            this.trigger('do-action', {action: action});
+            this.trigger("do-action", {action: action});
         }
 
         /**
@@ -45,18 +44,20 @@ odoo.define('base_generic_import.ImportMenu', function (require) {
          * @returns {Boolean}
          */
         static shouldBeDisplayed(env) {
-            return env.view &&
-                ['kanban', 'list'].includes(env.view.type) &&
+            return (
+                env.view &&
+                ["kanban", "list"].includes(env.view.type) &&
                 !env.device.isMobile &&
-                Boolean(JSON.parse(env.view.arch.attrs.import || '1')) &&
-                Boolean(JSON.parse(env.view.arch.attrs.create || '1'));
+                Boolean(JSON.parse(env.view.arch.attrs.import || "1")) &&
+                Boolean(JSON.parse(env.view.arch.attrs.create || "1"))
+            );
         }
     }
 
     GenImportMenu.props = {};
-    GenImportMenu.template = 'import_processor.ProcessorMenu';
+    GenImportMenu.template = "import_processor.ProcessorMenu";
 
-    FavoriteMenu.registry.add('importer', GenImportMenu, 1);
+    FavoriteMenu.registry.add("importer", GenImportMenu, 1);
 
     return GenImportMenu;
 });
