906b39ba18e17e66095581f309421d927632eda6
Xander De Matteis <xander.dematteis@dynapps.be>
2023-07-26 15:37:52 +0200

    [IMP] test_mail_template_attachment_i18n: pre-commit stuff

--- test_mail_template_attachment_i18n/__manifest__.py
+++ test_mail_template_attachment_i18n/__manifest__.py
@@ -1,14 +1,11 @@
 {
-    'name': ' Tests for mail_template_attachment_i18n',
-    'summary': 'Test suite for mail_template_attachment_i18n.',
-    'author': 'Onestein,Odoo Community Association (OCA)',
-    'website': 'https://github.com/oca/server-tools',
-    'category': 'Generic Modules',
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'mail_template_attachment_i18n',
-        'sale'
-    ],
-    'installable': True,
+    "name": " Tests for mail_template_attachment_i18n",
+    "summary": "Test suite for mail_template_attachment_i18n.",
+    "author": "Onestein,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-tools",
+    "category": "Generic Modules",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mail_template_attachment_i18n", "sale"],
+    "installable": True,
 }
