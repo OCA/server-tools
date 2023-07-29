3a2cf42dd9ea6f24e8e921a5cea5699ac97d35d6
Tom Blauwendraat <tom@sunflowerweb.nl>
2023-07-29 02:35:35 +0200

    [ADD] new module: base_order_by_related

---
+++ base_order_by_related/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2023 Therp (<http://www.therp.nl>).
+# @author Tom Blauwendraat <tblauwendraat@therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Base order by related",
+    "summary": "Order by non-stored related fields",
+    "version": "14.0.1.0.0",
+    "category": "Tools",
+    "website": "https://github.com/OCA/server-tools",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "maintainers": ["thomaspaulb"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["base"],
+    "data": [],
+}
