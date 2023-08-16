6fc2081e48bbc253d9b46bc0ce1aab8f6b01e73e
Lois Rilo <lois.rilo@forgeflow.com>
2023-08-16 11:46:42 +0200

    [IMP] black, isort, prettier

--- profiler/static/src/js/tour.js
+++ profiler/static/src/js/tour.js
@@ -1,4 +1,4 @@
-odoo.define("profiler.tour", function(require) {
+odoo.define("profiler.tour", function (require) {
     "use strict";
 
     var core = require("web.core");
@@ -23,9 +23,9 @@ odoo.define("profiler.tour", function(require) {
             },
             {
                 trigger: ".o_list_view",
-                run: function() {
+                run: function () {
                     // Hack to wait for 500 ms before entering next step
-                    _.delay(function() {
+                    _.delay(function () {
                         $(".o_list_button_add").addClass("ready");
                     }, 500);
                 },
