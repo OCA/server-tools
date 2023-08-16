6171bf30679a408bba2a0893f6f3a59620d5eabe
Lois Rilo <lois.rilo@forgeflow.com>
2023-08-16 11:47:05 +0200

    [MIG] profiler: Migration to 14.0

--- profiler/__manifest__.py
+++ profiler/__manifest__.py
@@ -3,7 +3,7 @@
     "author": "Vauxoo, Sunflower IT, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-tools",
     "category": "Tests",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["web_tour"],
     "data": [
