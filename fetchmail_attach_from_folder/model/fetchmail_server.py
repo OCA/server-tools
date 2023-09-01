1343e994c47d4c3368618ffee1cfa6c2ef4b2f96
Ronald Portier (Therp BV) <ronald@therp.nl>
2023-09-01 17:10:31 +0200

    [FIX] Do not repeatedly get message with no message-id
    Also another step in agreeing to code conventions.

--- fetchmail_attach_from_folder/model/fetchmail_server.py
+++ fetchmail_attach_from_folder/model/fetchmail_server.py
@@ -1,16 +1,43 @@
 # -*- coding: utf-8 -*-
-# Copyright 2013-2020 Therp BV - <https://therp.nl>.
+# Copyright 2013-2023 Therp BV - <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 # pylint: disable=invalid-name,missing-docstring,too-many-arguments
+# pylint: disable=super-with-arguments,import-error,protected-access
+# pylint: disable=too-many-locals
 import base64
+import email
+import logging
 import simplejson
 from lxml import etree
 
 from openerp.osv.orm import Model, except_orm
 from openerp.tools.translate import _
 from openerp.osv import fields
-from openerp.addons.fetchmail.fetchmail import logger
 from openerp.tools.misc import UnquoteEvalContext
+
+_logger = logging.getLogger(__name__)
+
+
+def get_message_id(message):
+    """Utility function to quickly retrieve message-id without full parse.
+
+    The code is actually derived from parse_message in mail.message model.
+    """
+    msg_txt = message
+    if isinstance(message, str):
+        msg_txt = email.message_from_string(message)
+
+    # Warning: message_from_string doesn't always work correctly on unicode,
+    # we must use utf-8 strings here :-(
+
+    # pylint: disable=undefined-variable
+    # For some reason unicode seen as variable
+    if isinstance(message, unicode):
+        message = message.encode('utf-8')
+        msg_txt = email.message_from_string(message)
+
+    message_id = msg_txt.get('message-id', False)
+    return message_id
 
 
 class fetchmail_server(Model):
@@ -20,7 +47,6 @@ class fetchmail_server(Model):
         'folder_ids': fields.one2many(
             'fetchmail.server.folder', 'server_id', 'Folders'),
     }
-
     _defaults = {
         'type': 'imap',
     }
@@ -40,49 +66,50 @@ class fetchmail_server(Model):
         return retval
 
     def fetch_mail(self, cr, uid, ids, context=None):
+        """Fetch mail from folders in incoming mail servers."""
         if context is None:
             context = {}
-
         check_original = []
-
         for this in self.browse(cr, uid, ids, context):
             if this.object_id:
                 check_original.append(this.id)
-
             context.update(
                 {
                     'fetchmail_server_id': this.id,
                     'server_type': this.type
-                })
-
+                }
+            )
             connection = this.connect()
             for folder in this.folder_ids:
                 this.handle_folder(connection, folder)
-
             connection.close()
-
         return super(fetchmail_server, self).fetch_mail(
             cr, uid, check_original, context)
 
     def handle_folder(self, cr, uid, ids, connection, folder, context=None):
-        '''Return ids of objects matched'''
+        """Return ids of objects matched"""
         for this in self.browse(cr, uid, ids, context=context):
-            logger.info('start checking for emails in %s server %s',
-                        folder.path, this.name)
+            _logger.info(
+                "start checking for emails in %s server %s",
+                folder.path,
+                this.name
+            )
             match_algorithm = folder.get_algorithm()
-            if connection.select(folder.path)[0] != 'OK':
-                logger.error(
-                    'Could not open mailbox %s on %s',
+            if connection.select(folder.path)[0] != "OK":
+                _logger.error(
+                    "Could not open mailbox %s on %s",
                     folder.path,
                     this.server
                 )
                 connection.select()
                 continue
-            result, msgids = this.get_msgids(connection)
-            if result != 'OK':
-                logger.error(
-                    'Could not search mailbox %s on %s' % (
-                        folder.path, this.server))
+            result, msgids = connection.search(None, "UNDELETED")
+            if result != "OK":
+                _logger.error(
+                    "Could not search mailbox %s on %s",
+                    folder.path,
+                    this.server
+                )
                 continue
             for msgid in msgids[0].split():
                 # We will accept exceptions for single messages
@@ -97,56 +124,89 @@ class fetchmail_server(Model):
                 except Exception:  # pylint: disable=broad-except
                     cr.rollback = rollback_org
                     cr.execute('rollback to savepoint apply_matching')
-                    logger.exception(
+                    _logger.exception(
                         "Failed to fetch mail %s from %s",
                         msgid,
                         this.name
                     )
-            logger.info(
+            _logger.info(
                 'finished checking for emails in %s server %s',
                 folder.path,
                 this.name
             )
 
-    def get_msgids(self, cr, uid, ids, connection, context=None):
-        '''Return imap ids of messages to process'''
-        return connection.search(None, 'UNDELETED')
-
     def apply_matching(
             self, cr, uid, ids,
             connection, folder, msgid, match_algorithm, context=None):
         """Match retrieved mail with one or only record, else flag message."""
+        message_model = self.pool["mail.message"]
         for this in self.browse(cr, uid, ids, context=context):
-            result, msgdata = connection.fetch(msgid, '(RFC822)')
-            if result != 'OK':
-                logger.error(
-                    'Could not fetch %s in %s on %s',
-                    msgid, folder.path, this.server)
+            # Fetch the complete message from the server.
+            result, msgdata = connection.fetch(msgid, "(RFC822)")
+            if result != "OK":
+                _logger.error(
+                    "Could not fetch %s in %s on %s",
+                    msgid,
+                    folder.path,
+                    this.server
+                )
                 continue
 
-            mail_message = self.pool.get('mail.message').parse_message(
-                msgdata[0][1], this.original)
-            if self.pool.get('mail.message').search(
-                    cr, uid,
-                    [('message_id', '=', mail_message['message-id'])]):
-                # Ignore mails that have been handled already
+            # Get raw message from message data.
+            raw_message = msgdata[0][1]
+
+            # Get message-id, also to prevent generation of random one
+            message_id = get_message_id(raw_message)
+
+            # Ignore mails that have been handled already
+            if message_id:
+                if message_model.search_count(
+                    cr, uid, [("message_id", "=", message_id)]
+                ):
+                    continue
+
+            # Parse message.
+            mail_message = message_model.parse_message(raw_message, this.original)
+
+            # Ignore messages without id, otherwise we would retrieve
+            # those again and again, until deleted on server.
+            if not message_id:
+                subject = mail_message.get("subject", "No subject")
+                _logger.error(
+                    "Message %s in %s on %s, with subject %s, had no message-id",
+                    msgid,
+                    folder.path,
+                    this.server,
+                    subject
+                )
                 continue
 
             found_ids = match_algorithm.search_matches(
-                cr, uid, folder,
-                mail_message, msgdata[0][1])
+                cr,
+                uid,
+                folder,
+                mail_message,
+                raw_message
+            )
             if found_ids and (len(found_ids) == 1 or folder.match_first):
                 match_algorithm.handle_match(
-                    cr, uid, connection,
-                    found_ids[0], folder, mail_message,
-                    msgdata[0][1], msgid, context)
+                    cr,
+                    uid,
+                    connection,
+                    found_ids[0],
+                    folder,
+                    mail_message,
+                    raw_message,
+                    msgid,
+                    context=context
+                )
             elif folder.flag_nonmatching:
-                connection.store(msgid, '+FLAGS', '\\FLAGGED')
+                connection.store(msgid, "+FLAGS", "\\FLAGGED")
 
     def attach_mail(
             self, cr, uid, ids, connection, object_id, folder,
             mail_message, msgid, context=None):
-        '''Return ids of messages created'''
+        """Return ids of messages created"""
 
         mail_message_ids = []
 
@@ -164,6 +224,7 @@ class fetchmail_server(Model):
             if this.attach and mail_message.get('attachments'):
                 for attachment in mail_message['attachments']:
                     fname, fcontent = attachment
+                    # pylint: disable=undefined-variable
                     if isinstance(fcontent, unicode):
                         fcontent = fcontent.encode('utf-8')
                     data_attach = {
@@ -206,10 +267,9 @@ class fetchmail_server(Model):
         return mail_message_ids
 
     def button_confirm_login(self, cr, uid, ids, context=None):
-        retval = super(fetchmail_server, self).button_confirm_login(cr, uid,
-                                                                    ids,
-                                                                    context)
-
+        retval = super(fetchmail_server, self).button_confirm_login(
+            cr, uid, ids, context=context
+        )
         for this in self.browse(cr, uid, ids, context):
             this.write({'state': 'draft'})
             connection = this.connect()
