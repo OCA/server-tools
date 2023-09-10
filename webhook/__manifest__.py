90f83d088d6b053ed3f926f935324553db51f258
Ilyass Jaatit <ilyassjaatit@gmail.com>
2023-09-10 22:43:10 +0200

    Change of version in the manifest file

--- webhook/__manifest__.py
+++ webhook/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Webhook',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Vauxoo, Odoo Community Association (OCA)',
     'category': 'Server Tools',
     'website': 'https://www.vauxoo.com',
