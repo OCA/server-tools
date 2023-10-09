64b6cf52539c33ce600453c5d37865035d538a10
Yhael S <yhaelopez@gmail.com>
2023-10-09 17:56:14 -0500

    session store file

---
+++ sentry/session_store.py
@@ -0,0 +1,16 @@
+import logging
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
+except ImportError:
+    _logger.debug(
+        "Cannot import 'sentry-sdk'.\
+                        Please make sure it is installed."
+    )
+
+class CustomSentryWsgiMiddleware(SentryWsgiMiddleware):
+    def __init__(self, application, session_store=None):
+        super().__init__(application)
+        self.session_store = session_store
