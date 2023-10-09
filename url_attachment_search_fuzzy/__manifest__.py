08ae170e72375c46aedf9c3359206f9cfe85d4e1
Jaume Bernaus <jaume.bernaus@forgeflow.com>
2023-10-09 10:21:06 +0200

    [BKP] url_attachment_search_fuzzy: Backport to 13.0

--- url_attachment_search_fuzzy/__manifest__.py
+++ url_attachment_search_fuzzy/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Fuzzy Search of URL in Attachments",
-    "version": "14.0.1.0.0",
+    "version": "13.0.1.0.0",
     "depends": [
         "base_search_fuzzy",
     ],
