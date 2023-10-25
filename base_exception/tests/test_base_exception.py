68d23990ec1e6d85407e95c10a9e1ec7b69fe17f
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-25 10:28:11 +0200

    [IMP] base_exception: test performance improvement - Include context keys for avoiding mail operations overhead.

--- base_exception/tests/test_base_exception.py
+++ base_exception/tests/test_base_exception.py
@@ -13,7 +13,16 @@ class TestBaseException(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.loader = FakeModelLoader(cls.env, cls.__module__)
         cls.loader.backup_registry()
         from .purchase_test import ExceptionRule, LineTest, PurchaseTest, WizardTest
