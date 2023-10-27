738333f8bcee3d7dcb7a11807e96d33301dcdc2c
Ronald Portier <ronald@therp.nl>
2023-10-27 10:25:22 +0200

    [IMP] base_external_system: black

--- base_external_system/models/external_system_os.py
+++ base_external_system/models/external_system_os.py
@@ -14,9 +14,9 @@ class ExternalSystemOs(models.AbstractModel):
     system interface. This is still a fully usable implementation, however.
     """
 
-    _name = 'external.system.os'
-    _inherit = 'external.system.adapter'
-    _description = 'External System OS'
+    _name = "external.system.os"
+    _inherit = "external.system.adapter"
+    _description = "External System OS"
 
     previous_dir = None
 
