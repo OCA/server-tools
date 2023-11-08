f0a3744c9c00601222c2f1fd85fee5360669e84e
Alessio Renda <renda855@gmail.com>
2023-11-08 16:38:44 +0100

    [IMP] module_auto_update: confirm message before updating

--- module_auto_update/__manifest__.py
+++ module_auto_update/__manifest__.py
@@ -17,6 +17,9 @@
     "installable": True,
     "uninstall_hook": "uninstall_hook",
     "depends": ["base"],
-    "data": ["views/ir_module_module.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/module_auto_update_views.xml",
+    ],
     "development_status": "Production/Stable",
 }
