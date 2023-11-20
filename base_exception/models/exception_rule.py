bc734e67d92cd5356bff081596f239cb00a543aa
Antoni Marroig Campomar <amarroig@apsl.net>
2023-11-20 10:54:14 +0100

    [MIG] base_exception: Migration to 17.0

--- base_exception/models/exception_rule.py
+++ base_exception/models/exception_rule.py
@@ -124,15 +124,15 @@ class ExceptionRule(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         res = super().create(vals_list)
-        self._get_cached_rules_for_domain.clear_cache(self)
+        self._get_cached_rules_for_domain.cache.clear(self)
         return res
 
     def write(self, vals):
         res = super().write(vals)
-        self._get_cached_rules_for_domain.clear_cache(self)
+        self._get_cached_rules_for_domain.cache.clear(self)
         return res
 
     def unlink(self):
         res = super().unlink()
-        self._get_cached_rules_for_domain.clear_cache(self)
+        self._get_cached_rules_for_domain.cache.clear(self)
         return res
