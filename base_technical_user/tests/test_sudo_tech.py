719f2236b2e4c6fddda91b2765a575b7271cd655
François Honoré <francois.honore@acsone.eu>
2023-11-28 07:38:24 +0100

    [FIX] base_technical_user - Error message when no technical user. When we use use_tech(raise_if_missing=True) and the technical user (user_tech_id) is not defined on the current company, the error message who said that field is empty should be related to the active company and not related to the default user's company.

--- base_technical_user/tests/test_sudo_tech.py
+++ base_technical_user/tests/test_sudo_tech.py
@@ -13,23 +13,26 @@ class SudoTechCase(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.company = cls.env.ref("base.main_company")
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.company_2 = cls.env["res.company"].create(
             {
                 "name": "Company 2 tech",
             }
         )
-        cls.env = cls.env(context=dict(cls.env.context,
-                          tracking_disable=True,
-                          no_reset_password=True)
+        cls.company_3 = cls.env["res.company"].create(
+            {
+                "name": "Company 3 NO tech",
+                "user_tech_id": False,
+            }
         )
         cls.user_tech = (
             cls.env["res.users"]
-            .with_context(tracking_disable=True, no_reset_password=True)
+            .with_context(no_reset_password=True)
             .create({"login": "tech", "name": "tech"})
         )
         cls.user_tech_2 = (
             cls.env["res.users"]
-            .with_context(tracking_disable=True, no_reset_password=True)
+            .with_context(no_reset_password=True)
             .with_company(cls.company_2)
             .create({"login": "tech2", "name": "tech2", "company_id": cls.company_2.id})
         )
@@ -63,3 +66,16 @@ class SudoTechCase(TransactionCase):
         self.company_2.user_tech_id = self.user_tech_2
         self_tech = user.with_company(self.company_2).sudo_tech()
         self.assertEqual(self_tech._uid, self.user_tech_2.id)
+
+    def test_sudo_tech_company_3(self):
+        """
+        Ensure the error message is related to the
+        active company when there is no technical user.
+        """
+        user = self.env.user.with_company(self.company_3)
+        self.assertEqual(self.company_3, user.env.company)
+        self.assertNotEqual(user.env.company, user.company_id)
+        with self.assertRaises(UserError) as em:
+            user.sudo_tech(raise_if_missing=True)
+        self.assertIn(self.company_3.name, em.exception.args[0])
+        self.assertNotIn(user.company_id.name, em.exception.args[0])
