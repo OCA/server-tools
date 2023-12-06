491e4ad9d1c607f62d3bf5015d46e886fcb398f0
Raf Ven <raf.ven@dynapps.be>
2023-12-06 19:22:49 +0100

    [MIG] module_change_auto_install: Migration to 17.0

--- module_change_auto_install/__manifest__.py
+++ module_change_auto_install/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Change auto installable modules",
     "summary": "Customize auto installables modules by configuration",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Tools",
     "maintainers": ["legalsylvain"],
     "author": "GRAP, Odoo Community Association (OCA)",
