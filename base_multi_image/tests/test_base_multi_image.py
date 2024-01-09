742af869e946c713ffe0afcf9472b1db41a5a4f1
Omal Bastin <omalbastin@gmail.com>
2024-01-09 23:38:17 +0530

    [IMP] : pre-commit auto fixes

--- base_multi_image/tests/test_base_multi_image.py
+++ base_multi_image/tests/test_base_multi_image.py
@@ -2,11 +2,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import base64
-from odoo.tools.misc import file_open
+
 from odoo_test_helper import FakeModelLoader
 
-from odoo.exceptions import UserError, ValidationError
 from odoo.tests import TransactionCase
+from odoo.tools.misc import file_open
 
 
 class TestBaseException(TransactionCase):
@@ -16,7 +16,7 @@ class TestBaseException(TransactionCase):
 
         cls.loader = FakeModelLoader(cls.env, cls.__module__)
         cls.loader.backup_registry()
-        img_path = 'product/static/img/product_product_11-image.png'
+        img_path = "product/static/img/product_product_11-image.png"
         img_content = base64.b64encode(file_open(img_path, "rb").read())
         cls.transparent_image = (  # 1x1 Transparent GIF
             b"R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
@@ -28,22 +28,35 @@ class TestBaseException(TransactionCase):
             b"R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
         )
         from .test_images import ImageOwnerTest
-        cls.loader.update_registry((ImageOwnerTest,))
-        cls.img_owner = cls.env['base_multi_image.owner.test'].create({
-            'name': 'Test Multiple Imges',
-            'image_ids': [(0, 0, {
-                "storage": "filestore",
-                "name": "Image 1",
-                "attachment_image": img_content,
-                "owner_model": "base_multi_image.owner.test",
 
-            }), (0, 0, {
-                "storage": "filestore",
-                "name": "Image 2",
-                "attachment_image": cls.black_image,
-                "owner_model": "product.template",
-            }),]
-        })
+        cls.loader.update_registry((ImageOwnerTest,))
+        cls.img_owner = cls.env["base_multi_image.owner.test"].create(
+            {
+                "name": "Test Multiple Imges",
+                "image_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "storage": "filestore",
+                            "name": "Image 1",
+                            "attachment_image": img_content,
+                            "owner_model": "base_multi_image.owner.test",
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "storage": "filestore",
+                            "name": "Image 2",
+                            "attachment_image": cls.black_image,
+                            "owner_model": "product.template",
+                        },
+                    ),
+                ],
+            }
+        )
 
     def test_all_images(self):
         self.assertEqual(len(self.img_owner.image_ids), 2)
@@ -71,4 +84,3 @@ class TestBaseException(TransactionCase):
         self.img_owner.image_ids[0].name = text
         self.img_owner.refresh()
         self.assertEqual(self.img_owner.image_ids[0].name, text)
-
--- base_multi_image/tests/test_images.py
+++ base_multi_image/tests/test_images.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Omal Bastin (o4odoo@gmail.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ImageOwnerTest(models.Model):
