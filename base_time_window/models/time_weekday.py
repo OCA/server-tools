eb31d1a39fc49df8727bef443d4debf661d6a180
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-01-26 18:07:49 +0100

    base_time_window: Add helper functions to get next Date matching weekday

--- base_time_window/models/time_weekday.py
+++ base_time_window/models/time_weekday.py
@@ -60,3 +60,30 @@ class TimeWeekday(models.Model):
         result = super().unlink()
         self._get_id_by_name.clear_cache(self)
         return result
+
+    def _get_next_weekday_date(self, date_from=False, include_date_from=True):
+        """Returns the next Date matching weekday
+
+        :param date_from: Date object from which we start searching for next weekday
+        :param include_date_from: Allows to return date from if it's same weekday
+        :return Date object matching the weekday
+        """
+        self.ensure_one()
+        if not date_from:
+            date_from = fields.Date.today()
+        next_weekday_delta = int(self.name) - date_from.weekday()
+        if next_weekday_delta < 0:
+            next_weekday_delta += 7
+        elif next_weekday_delta == 0 and not include_date_from:
+            next_weekday_delta += 7
+        return fields.Date.add(date_from, days=next_weekday_delta)
+
+    def _get_next_weekdays_date(self, date_from=False, include_date_from=True):
+        return min(
+            [
+                weekday._get_next_weekday_date(
+                    date_from=date_from, include_date_from=include_date_from
+                )
+                for weekday in self
+            ]
+        )
