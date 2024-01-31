87315c1a4c1a57cf3bfb44ba11186581e1aba9f9
Rocío Vega <rov@adhoc.com.ar>
2024-01-31 08:55:49 -0300

    [MIG] base_name_search_improved: Migration to 17.0

--- base_name_search_improved/__manifest__.py
+++ base_name_search_improved/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Improved Name Search",
     "summary": "Friendlier search when typing in relation fields",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/OCA/server-tools",
     "author": "Daniel Reis, Odoo Community Association (OCA), ADHOC SA",
