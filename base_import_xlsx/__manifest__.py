12ac9539d2976777de7fadedbc3e1230671f0bbb
Ronald Portier <ronald@therp.nl>
2024-02-05 12:08:11 +0100

    [FIX] base_import_xlsx: better initial version number

--- base_import_xlsx/__manifest__.py
+++ base_import_xlsx/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Base import Excel",
-    "version": "13.0.0.0.0",
+    "version": "13.0.1.0.0",
     "category": "Generic Modules",
     "summary": "Replace support for excel import by one based on openpyxl",
     "author": "Therp BV, Odoo Community Association (OCA)",
