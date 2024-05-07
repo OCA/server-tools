603aeb4923e449c370d0111f587d79c70cdb4a23
Andrea Stirpe <and.stirpe@gmail.com>
2024-05-07 19:38:39 +0000

    [MIG] adapt code for V16

--- database_cleanup/__manifest__.py
+++ database_cleanup/__manifest__.py
@@ -14,7 +14,7 @@
         "views/purge_menus.xml",
         "views/purge_modules.xml",
         "views/purge_models.xml",
-        'views/purge_fields.xml',
+        "views/purge_fields.xml",
         "views/purge_columns.xml",
         "views/purge_tables.xml",
         "views/purge_data.xml",
--- database_cleanup/readme/CONTRIBUTORS.rst
+++ database_cleanup/readme/CONTRIBUTORS.rst
@@ -2,3 +2,5 @@
 * Holger Brunn <hbrunn@therp.nl>
 * Stéphane Mangin <stephane.mangin@camptocamp.com>
 * Mark Schuit <mark@gig.solutions>
+* `360ERP <https://www.360erp.com>`_:
+   *  Andrea Stirpe
