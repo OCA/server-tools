44bab1cee4a574b3d764cb9145545d018d56ec24
Christopher Ormaza <chris.ormaza@forgeflow.com>
2024-05-31 14:18:44 -0500

    [16.0][MIG] fetchmail_incoming_log

--- fetchmail_incoming_log/__manifest__.py
+++ fetchmail_incoming_log/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Fetchmail Incoming Log",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "summary": "Log all messages received, before they start to be processed.",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
