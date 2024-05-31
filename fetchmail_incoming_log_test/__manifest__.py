1352160f5f3c8187ceeb069d2018302304ae592f
Christopher Ormaza <chris.ormaza@forgeflow.com>
2024-05-31 14:19:45 -0500

    [IMP] fetchmail_incoming_log_test: : pre-commit stuff

--- fetchmail_incoming_log_test/__manifest__.py
+++ fetchmail_incoming_log_test/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Fetchmail Incoming Log Test",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "summary": "Tests for 'fetchmail_incoming_log' module.",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
