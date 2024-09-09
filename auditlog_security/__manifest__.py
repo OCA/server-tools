f99f33a345d8f015c6bcaae86f44ea9964e906e5
KKamaa <kevkamaa96@gmail.com>
2024-09-09 11:53:04 +0300

    Draft: [16.0][MIG] #10965 auditlog_security

---
+++ auditlog_security/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2021 Therp B.V. <https://www.therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Audit Log User Permissions",
+    "version": "11.0.1.1.4",
+    "author": "Therp B.V.,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-tools/",
+    "category": "Tools",
+    "description": """Allow regular users to view Audit log lines
+                   via the form view of the relevant model""",
+    "depends": [
+        "auditlog",
+        "contacts",
+    ],
+    "data": [
+        "security/res_groups.xml",
+        "views/auditlog_view.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule.xml",
+    ],
+    "application": True,
+    "installable": True,
+}
