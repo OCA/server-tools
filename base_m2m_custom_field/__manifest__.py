603dba8a7b8909444095558d701cf64bf6b218f8
trisdoan <doanminhtri8183@gmail.com>
2024-09-17 17:24:12 +0700

    [MIG] base_m2m_custom_field: Migration to 17.0

--- base_m2m_custom_field/__manifest__.py
+++ base_m2m_custom_field/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base Many2many Custom Field",
     "summary": "Customizations of Many2many",
-    "version": "14.0.1.1.0",
+    "version": "17.0.1.0.0",
     "category": "Technical Settings",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
