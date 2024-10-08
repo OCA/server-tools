a6ef22a3ca4d39b34efde082fadcf12a3a5ecc63
ntsirintanis <ntsirintanis@therp.nl>
2024-10-08 16:20:30 +0200

    [MIG] mail_cleanup: Migration to 16.0

--- mail_cleanup/__manifest__.py
+++ mail_cleanup/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Mail cleanup",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "summary": "Mark as read or delete mails after a set time",
     "author": "Camptocamp, " "Odoo Community Association (OCA)",
