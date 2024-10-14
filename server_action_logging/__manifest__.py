b9075b26e54220fe17a8d152dbdcef1358c693e1
karizma <m.arajdal@karizma.ma>
2024-10-14 14:53:25 +0100

    [MIG] server_action_logging: Migration to 18.0

--- server_action_logging/__manifest__.py
+++ server_action_logging/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Server Action Logging",
     "summary": "Module that provides a logging mechanism for server actions",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Hidden",
     "website": "https://github.com/OCA/server-tools",
     "author": "Camptocamp, Odoo Community Association (OCA)",
