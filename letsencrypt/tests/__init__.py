9caade2db29169c8ff88a9ed6155202a26322e9b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-10-22 15:57:35 +0200

    [FIX] letsencrypt: Disable tests
    We are getting these continuous errors:
    
    There were too many requests of a given type :: Service busy; retry later.

--- letsencrypt/tests/__init__.py
+++ letsencrypt/tests/__init__.py
@@ -1,3 +1,4 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import test_http
-from . import test_letsencrypt
+
+# from . import test_letsencrypt  # Commented due to hammering exception
