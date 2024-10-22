5a6a412e38f130cf830c3e378d28025e4df783f4
Ronald Portier <ronald@therp.nl>
2024-10-22 14:48:38 +0200

    [FIX] base_external...: prevent excessive logging

--- base_external_system_http/models/external_system_adapter_http.py
+++ base_external_system_http/models/external_system_adapter_http.py
@@ -10,7 +10,6 @@ from odoo.exceptions import UserError
 
 # You must initialize logging, otherwise you'll not see debug output.
 logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
 requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
