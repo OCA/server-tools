1937f853c8b05eaf61cfc8e76aaa0cdea9ee88ee
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2024-12-24 13:33:53 +0900

    [IMP] base_model_restrict_update: Enable group-based exemptions and allow read-only exception models Previously, the exemption for the model update restriction was given by setting unrestrict_model_update to the relevant users. However, this design had the limitation of not being able to control the permitted users per model.
    This commit addresses this limitation by adding the update_allowed_group_ids
    field to ir.model, removing the unrestrict_model_update field from
    res.users.
    
    This commit also contains the following changes:
    
    - Make the logic of _check_is_readonly_user() more complete.
    - Refactor tests to make them more comprehensive.
    - Add the flexibility of allowing certain models to be
      excluded from being read-only even when the user is set as read-only.

--- base_model_restrict_update/models/ir_model_access.py
+++ base_model_restrict_update/models/ir_model_access.py
@@ -1,13 +1,63 @@
-# Copyright 2021 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2021-2024 Quartile
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import api, models, tools
+from odoo import api, models
 from odoo.exceptions import AccessError
 from odoo.tools.translate import _
 
 
 class IrModelAccess(models.Model):
     _inherit = "ir.model.access"
+
+    @api.model
+    def _readonly_exclude_models(self):
+        """Models update/create by system, and should be excluded from checking"""
+        self.env.cr.execute(
+            """
+            SELECT m.model
+            FROM ir_model_access ma
+            JOIN ir_model m ON ma.model_id = m.id
+            WHERE ma.group_id IS NULL
+              AND (
+                ma.perm_write = TRUE
+                OR ma.perm_create = TRUE
+                OR ma.perm_unlink = TRUE
+              )
+            """,
+        )
+        return self.env.cr.fetchall()
+
+    @api.model
+    def _test_readonly(self, model):
+        if not self.env.user.is_readonly_user:
+            return False
+        if (model,) in self._readonly_exclude_models():
+            return False
+        models_to_exclude = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("base_model_restrict_update.excluded_models_from_readonly", "")
+        )
+        if model in models_to_exclude:
+            return False
+        return True
+
+    @api.model
+    def _test_restrict_update(self, model):
+        # Get the IDs of unresticted users for the model if it's restricted
+        self.env.cr.execute(
+            """
+            SELECT gurel.uid
+            FROM ir_model m
+            LEFT JOIN ir_model_res_groups_update_allowed_rel mgrel ON m.id = mgrel.ir_model_id
+            LEFT JOIN res_groups_users_rel gurel ON mgrel.res_groups_id = gurel.gid
+            WHERE m.model = %s
+              AND m.restrict_update = true
+            """,
+            (model,),
+        )
+        query_res = self.env.cr.fetchall()
+        return bool(query_res) and (self.env.uid,) not in query_res
 
     @api.model
     def check(self, model, mode="read", raise_exception=True):
@@ -23,38 +73,3 @@ class IrModelAccess(models.Model):
                     % {"model": model, "mode": mode}
                 )
         return res
-
-    @api.model
-    def _test_readonly(self, model):
-        exclude_models = self._readonly_exclude_models()
-        if model not in exclude_models and self.env.user.is_readonly_user:
-            return True
-        return False
-
-    @api.model
-    def _test_restrict_update(self, model):
-        self.env.cr.execute(
-            "SELECT restrict_update FROM ir_model WHERE model = %s", (model,)
-        )
-        query_res = self.env.cr.dictfetchone()
-        if query_res["restrict_update"] and not self.env.user.unrestrict_model_update:
-            return True
-        return False
-
-    @api.model
-    def _readonly_exclude_models(self):
-        """Models updtate/create by system, and should be excluded from checking"""
-        return (
-            self.sudo()
-            .search(
-                [
-                    ("group_id", "=", False),
-                    "|",
-                    ("perm_write", "=", True),
-                    "|",
-                    ("perm_create", "=", True),
-                    ("perm_unlink", "=", True),
-                ]
-            )
-            .mapped("model_id.model")
-        )
