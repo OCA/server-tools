36bc3986135e94d06c09422bfb34d28e1eaf4b1a
abdessamad salim <abdessamad.salim@smile-maroc.com>
2025-02-03 09:56:38 +0100

    [MIG] iap_alternative_provider: Migration to 18.0

--- iap_alternative_provider/tests/test_iap_account.py
+++ iap_alternative_provider/tests/test_iap_account.py
@@ -8,15 +8,26 @@ from odoo.tests import TransactionCase
 
 
 class IapAccountCase(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.service = self.env["iap.service"].create(
+            {
+                "name": "Some Service",
+                "technical_name": "some-service",
+                "description": "Test service",
+                "unit_name": "unit",
+                "integer_balance": 0,
+            }
+        )
+
     def test_create_odoo_iap(self):
         account = self.env["iap.account"].create(
             {
                 "name": "Odoo IAP",
-                "provider": "odoo",
-                "service_name": "some-service",
+                "service_id": self.service.id,
             }
         )
-        self.assertEqual(account.service_name, "some-service")
+        self.assertEqual(account.service_id.id, self.service.id)
 
     def test_create_with_mock(self):
         with mock.patch(
@@ -27,51 +38,27 @@ class IapAccountCase(TransactionCase):
             account = self.env["iap.account"].create(
                 {
                     "name": "Odoo IAP",
-                    "provider": "odoo",
-                    "service_name": "some-service",
+                    "service_id": self.service.id,
                 }
             )
-            self.assertEqual(account.service_name, "other-service")
+            self.assertEqual(account.service_id.id, self.service.id)
 
     def test_write_odoo_iap(self):
         account = self.env["iap.account"].create(
             {
                 "name": "Odoo IAP",
-                "provider": "odoo",
-                "service_name": "",
+                "service_id": self.service.id,
             }
         )
-        self.assertEqual(account.service_name, "")
-        account.write({"service_name": "some-service"})
-        self.assertEqual(account.service_name, "some-service")
-        with mock.patch(
-            "odoo.addons.iap_alternative_provider.models."
-            "iap_account.IapAccount._get_service_from_provider",
-            return_value="other-service",
-        ):
-            account.write({"service_name": "some-service-2"})
-            self.assertEqual(account.service_name, "other-service")
-
-    def test_unlink_odoo_iap(self):
-        account = self.env["iap.account"].create(
+        self.assertEqual(account.service_id.id, self.service.id)
+        new_service = self.env["iap.service"].create(
             {
-                "name": "Odoo IAP",
-                "provider": "odoo",
-                "service_name": "some-service",
+                "name": "New Service",
+                "technical_name": "new-service",
+                "description": "Another service",
+                "unit_name": "new_unit",
+                "integer_balance": 0,
             }
         )
-        account.unlink()
-        self.assertFalse(account.exists(), "Account should be deleted")
-
-    def test_search_odoo_iap(self):
-        account = self.env["iap.account"].create(
-            {
-                "name": "Odoo IAP",
-                "provider": "odoo",
-                "service_name": "some-service",
-            }
-        )
-        results = self.env["iap.account"].search([("name", "=", "Odoo IAP")])
-        self.assertIn(
-            account.id, results.ids, "Account should be found in search results"
-        )
+        account.write({"service_id": new_service.id})
+        self.assertEqual(account.service_id.id, new_service.id)
