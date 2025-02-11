16795d7b92ac27ea140ba64e5bd4bf4023eccef0
Enric Tobella <enric.tobella@dixmit.com>
2025-02-12 00:02:18 +0100

    [FIX] base_remote: RunTime error is raised on tests

--- base_remote/models/base.py
+++ base_remote/models/base.py
@@ -13,6 +13,6 @@ class Base(models.AbstractModel):
     def remote(self):
         try:
             remote_addr = http.request.httprequest.remote_addr
-        except (KeyError, AttributeError):
+        except (KeyError, AttributeError, RuntimeError):
             return self.env["res.remote"]
         return self.env["res.remote"]._get_remote(remote_addr)
