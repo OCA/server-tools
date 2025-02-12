7e21893eeb87a52e1dafd1f4b411706af37656b2
Ruchir Shukla <ruchir@bizzappdev.com>
2025-02-12 19:28:59 +0530

    [MIG] attachment_unindex_content: Migration to 17.0

--- attachment_unindex_content/__manifest__.py
+++ attachment_unindex_content/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Attachment Unindex Content",
     "summary": "Disable indexing of attachments",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Vauxoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-tools",
     "maintainers": [
