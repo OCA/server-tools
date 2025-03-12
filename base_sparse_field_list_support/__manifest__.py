09f127040c7bd48fc5793b809631787cf6a6bc68
Simone Orsi <simahawk@gmail.com>
2025-03-12 17:40:30 +0100

    [mig] base_sparse_field_list_support: migrate to v18

--- base_sparse_field_list_support/__manifest__.py
+++ base_sparse_field_list_support/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Base Sparse Field List Support",
     "summary": "add list support to convert_to_cache()",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Technical Settings",
     "website": "https://github.com/OCA/server-tools",
     "author": "Akretion,Odoo Community Association (OCA)",
