c182d832d49dbc3954897e8aba7f21b69503eed0
natuan9 <tuan3032001@gmail.com>
2025-03-25 16:08:10 +0700

    [MIG] iap_alternative_provider: Migration to 18.0

--- iap_alternative_provider/__manifest__.py
+++ iap_alternative_provider/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "IAP Alternative Provider",
     "summary": "Base module for providing alternative provider for iap apps",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/OCA/server-tools",
     "author": "Akretion, Odoo Community Association (OCA)",
