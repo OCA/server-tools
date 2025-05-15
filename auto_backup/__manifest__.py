ae695c94aeb5b56d7caf5bda73b0bff80573e969
Eugene Molotov <molotov@it-projects.info>
2025-05-15 10:11:37 +0500

    [REV][FIX] Pin cryptography to keep compatibility with Odoo's pyopenssl==19.0.0"
    Reverted commit disallows to install auto_backup, when using offical Odoo 15.0
    docker image, which is based on bullseye [1].
    The bullseye has following python packages:
    
    - cryptography 3.3.2 with security patches [2]
    - pyopenssl 20.0.1 [3]
    
    [1] https://github.com/odoo/docker/blob/5fb6a842747c296099d9384587cd89640eb7a615/15.0/Dockerfile#L1
    [2] https://packages.debian.org/bullseye/python3-cryptography
    [3] https://packages.debian.org/bullseye/python3-openssl

--- auto_backup/__manifest__.py
+++ auto_backup/__manifest__.py
@@ -24,5 +24,5 @@
         "view/db_backup_view.xml",
     ],
     "installable": True,
-    "external_dependencies": {"python": ["pysftp", "cryptography==2.6.1"]},
+    "external_dependencies": {"python": ["pysftp"]},
 }
