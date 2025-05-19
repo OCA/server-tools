ea2b92af4ee5bc021ea9672ccbecfb8e8dda3189
Nikos Tsirintanis <ntsirintanis@therp.nl>
2025-05-19 14:27:08 +0200

    [MIG] base_order_by_related: Migration to 16.0

--- base_order_by_related/__manifest__.py
+++ base_order_by_related/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright (C) 2023 Therp (<http://www.therp.nl>).
+# Copyright (C) 2025 Therp (<http://www.therp.nl>).
 # @author Tom Blauwendraat <tblauwendraat@therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Base order by related",
     "summary": "Order by non-stored related fields",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/OCA/server-tools",
     "author": "Therp BV, Odoo Community Association (OCA)",
