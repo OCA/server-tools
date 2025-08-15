81fa3e394f0dd861509026fe3cc2f470452f90a2
Aungkokolin1997 <lin@quartile.co>
2025-08-15 03:48:17 +0000

    [MIG] attachment_delete_restrict: Migration to 17.0

--- attachment_delete_restrict/__manifest__.py
+++ attachment_delete_restrict/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Restrict Deletion of Attachments",
-    "version": "18.0.1.0.0",
+    "version": "17.0.1.0.0",
     "depends": [
         "base",
         "base_setup",
