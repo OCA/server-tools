0ded25f53ed7865fd0784504ea5615bbe07360ed
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2025-08-20 12:06:55 +0200

    [IMP] base_view_inheritance_extension: drop unused dependency
    Drops ``astor``: unused external dependency

--- base_view_inheritance_extension/__manifest__.py
+++ base_view_inheritance_extension/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Extended view inheritance",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "development_status": "Mature",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "LGPL-3",
@@ -11,6 +11,5 @@
     "summary": "Adds more operators for view inheritance",
     "website": "https://github.com/OCA/server-tools",
     "depends": ["base"],
-    "external_dependencies": {"python": ["astor"]},
     "demo": ["demo/ir_ui_view.xml"],
 }
