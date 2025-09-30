6c5260dd260d81ac94023e574157f44088596608
Florian Mounier <paradoxxx.zero@gmail.com>
2025-09-30 15:18:45 +0200

    [MIG] odoo_test_xmlrunner: Migration to 18.0

--- odoo_test_xmlrunner/__manifest__.py
+++ odoo_test_xmlrunner/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Unittest xUnit reports",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "depends": ["base"],
     "author": "Smile, Odoo Community Association (OCA)",
     "license": "AGPL-3",
