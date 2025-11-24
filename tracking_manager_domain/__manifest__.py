5a97930c515f37866d6fbb0e219f2da1456f5127
Christopher Rogos <crogos@gmail.com>
2025-11-24 12:34:08 +0000

    [FIX] tracking_manager_domain: prevent error when tracking properties

--- tracking_manager_domain/__manifest__.py
+++ tracking_manager_domain/__manifest__.py
@@ -6,10 +6,10 @@
     "summary": "This module extends the tracking manager to"
     " allow to define a domain on fields to track changes "
     "only when certain conditions apply.",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Tools",
     "website": "https://github.com/OCA/server-tools",
-    "author": "Akretion, Odoo Community Association (OCA)",
+    "author": "glueckkanja AG, Odoo Community Association (OCA)",
     "maintainers": ["CRogos"],
     "license": "AGPL-3",
     "application": False,
