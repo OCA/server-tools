bf1c7bddb410a39061cfc6d979774158eb5cef94
Ronald Portier <ronald@therp.nl>
2026-02-09 10:22:53 +0100

    [RFR] fetch..folder: put each test class in own file

--- fetchmail_attach_from_folder/tests/__init__.py
+++ fetchmail_attach_from_folder/tests/__init__.py
@@ -1,3 +1,4 @@
-# Copyright - 2015-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import test_attach_mail_manually
 from . import test_match_algorithms
