b6e85a61ff3bf180b2491d3d4017ae19085142a8
Sergij Pfaifer <sergiipf@gmail.com>
2026-02-10 15:47:12 +0200

    [IMP] onchange_helper: pre-commit auto fixes

--- onchange_helper/__manifest__.py
+++ onchange_helper/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Onchange Helper",
     "version": "18.0.1.0.1",
-    "summary": "Technical module that ease execution" " of onchange in Python code",
+    "summary": "Technical module that ease execution of onchange in Python code",
     "author": "Akretion,Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-tools",
     "license": "LGPL-3",
