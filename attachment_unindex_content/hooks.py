33e67a4665aa4a609ddeb909b0ccf6e1c29c5613
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2026-02-13 10:59:00 -0600

    [IMP] attachment_unindex_content: Update only rows with null values

--- attachment_unindex_content/hooks.py
+++ attachment_unindex_content/hooks.py
@@ -1,3 +1,8 @@
 def post_init_hook(env):
     """Clear the indexed data for records already in database"""
-    env.cr.execute("UPDATE ir_attachment SET index_content=NULL")
+    env.cr.execute(
+        """
+        UPDATE ir_attachment SET index_content=NULL
+        WHERE index_content IS NOT NULL
+        """
+    )
