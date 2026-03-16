69f99865327f953c359dc04d806e154c8a939580
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2026-03-16 15:24:20 +0100

    [MIG] mail_template_attachment_i18n: Migration to 17.0

--- mail_template_attachment_per_lang/models/mail_template.py
+++ mail_template_attachment_per_lang/models/mail_template.py
@@ -21,37 +21,44 @@ class MailTemplate(models.Model):
         inverse_name="mail_template_id",
     )
 
-    def generate_email(self, res_ids, fields=None):
+    def _generate_template_attachments(
+        self, res_ids, render_fields, render_results=None
+    ):
         self.ensure_one()
-        multi = True
-        if isinstance(res_ids, int):
-            res_ids = [res_ids]
-            multi = False
-        res = super().generate_email(res_ids, fields)
+        res = super()._generate_template_attachments(
+            res_ids, render_fields, render_results=render_results
+        )
+        recipient_values = {}  # Get recipients (to work with partner_lang)
+        if self.ir_attachment_language_method == "partner_lang":
+            recipient_fields = {"email_cc", "email_to", "partner_to"}
+            self._generate_template_recipients(
+                res_ids,
+                recipient_fields,
+                render_results=recipient_values,
+            )
         lang_codes = dict(self._render_lang(res_ids))
-        for res_id in res.keys():
+        for res_id in res_ids:
+            values = res.setdefault(res_id, {})
             attached = []
             lang_code_list = []
-            if self.ir_attachment_language_method == "partner_lang":
-                mail = res[res_id]
-                partner_ids = "partner_ids" in mail and mail["partner_ids"] or False
+            if self.env.context.get("template_preview_lang"):
+                lang = self.env.context.get("template_preview_lang")
+                lang_codes = {res_id: lang for res_id in res_ids}
+                lang_code_list = [lang_codes.get(res_id)]
+            elif self.ir_attachment_language_method == "partner_lang":
+                partner_ids = recipient_values.get(res_id, {}).get("partner_ids", [])
                 partners = self.env["res.partner"].browse(partner_ids)
                 lang_code_list = [p.lang for p in partners]
             elif self.ir_attachment_language_method == "template_lang":
                 lang_code_list = [lang_codes.get(res_id)]
             for lang_code in lang_code_list:
                 for lang_attach in self.ir_attachment_language_ids.filtered(
-                    lambda a: a.lang == lang_code
+                    lambda a, lc=lang_code: a.lang == lc
                 ):
                     if lang_attach.id in attached:
                         continue
-                    if not res[res_id].get("attachments"):
-                        res[res_id]["attachments"] = []
-                    res[res_id]["attachments"].append(
-                        (
-                            lang_attach.attachment_id.name,
-                            lang_attach.attachment_id.datas,
-                        )
-                    )
+                    if "attachment_ids" not in values:
+                        values["attachment_ids"] = []
+                    values["attachment_ids"].append(lang_attach.attachment_id.id)
                     attached.append(lang_attach.id)
-        return multi and res or res[res_ids[0]]
+        return res
