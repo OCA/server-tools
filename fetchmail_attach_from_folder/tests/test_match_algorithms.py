b69650a4ceb78534e63f2365fc297be1d23c0dfb
Ronald Portier <ronald@therp.nl>
2026-03-19 12:48:55 +0100

    [FIX] fetch..folder: make manual attach work again
    In the past manual attachment was limited to the model defined in the
    folder, by overriding the selection for the object in a fields_view_get
    method. This no longer works as the method is not called any more. Setting
    the selection to just this model through other methods also does not work,
    neither setting a default model on a Reference field.
    
    So basically now attaching a mail to any object is allowed.

--- fetchmail_attach_from_folder/tests/test_match_algorithms.py
+++ fetchmail_attach_from_folder/tests/test_match_algorithms.py
@@ -9,43 +9,11 @@ from odoo.fields import Command
 from odoo.tests.common import TransactionCase
 
 from ..match_algorithm import email_domain
+from .common import get_message_body
 
 TEST_EMAIL = "reynaert@dutchsagas.nl"
 TEST_SUBJECT = "Test subject"
 MAIL_MESSAGE = {"subject": TEST_SUBJECT, "to": "demo@yourcompany.example.com"}
-
-
-def get_message_body(email, subject):
-    """Get Message Body, as returned by fetch() from connection.
-
-    fetch returns a list of tuples with the message information.
-    """
-    return [
-        (
-            "1 (RFC822 {1149}",
-            "Return-Path: <ronald@acme.com>\r\n"
-            "Delivered-To: demo@yourcompany.example.com\r\n"
-            "Received: from localhost (localhost [127.0.0.1])\r\n"
-            "\tby vanaheim.acme.com (Postfix) with ESMTP id 14A3183163\r\n"
-            "\tfor <demo@yourcompany.example.com>;"
-            " Mon, 26 Mar 2018 16:03:52 +0200 (CEST)\r\n"
-            "To: Test User <nonexistingemail@yourcompany.example.com>\r\n"
-            "From: Reynaert de Vos <%(test_email)s>\r\n"
-            "Subject: %(test_subject)s\r\n"
-            "Message-ID: <485a8041-d560-a981-5afc-d31c1f136748@acme.com>\r\n"
-            "Date: Mon, 26 Mar 2018 16:03:51 +0200\r\n"
-            "User-Agent: Mock Test\r\n"
-            "MIME-Version: 1.0\r\n"
-            "Content-Type: text/plain; charset=utf-8\r\n"
-            "Content-Language: en-US\r\n"
-            "Content-Transfer-Encoding: 7bit\r\n\r\n"
-            "Hallo Wereld!\r\n"
-            % {
-                "test_email": email,
-                "test_subject": subject,
-            },
-        )
-    ]
 
 
 class MockConnection:
