9cbd62e3c98d5ab7d3f8c103ecf383d1318de645
Don Kendall <dkendall@ledoweb.com>
2026-05-20 10:13:34 -0400

    [MIG] attachment_queue: Migration to 19.0
    * self._cr → self.env.cr in SQL execs (T01)
    * Delete migrations/18.0.1.0.0/ (T08)
    * Search view: <field> elements re-ordered before <filter>/<separator>/
      <group>; <group string="Group By"> wrapper removed (19 RelaxNG)
    * Mail template body: ${object.x} → {{object.x}} (Mako removed in 19)
    * Tests refactored from odoo_test_helper.FakeModelLoader → native
      odoo.orm.model_classes.add_to_registry
    * W8161: bare _() → self.env._() in test_models.py
    
    Signed-off-by: Don Kendall <dkendall@ledoweb.com>

--- attachment_queue/tests/test_models.py
+++ attachment_queue/tests/test_models.py
@@ -1,6 +1,6 @@
 # Copyright 2023 Akretion
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import _, models
+from odoo import models
 from odoo.exceptions import UserError
 
 
@@ -9,7 +9,7 @@ class AttachmentQueue(models.Model):
     _name = "attachment.queue"
 
     def mock_run_fail(self):
-        raise UserError(_("boom"))
+        raise UserError(self.env._("boom"))
 
     def mock_run_create_partners(self):
         for x in range(10):
@@ -17,7 +17,7 @@ class AttachmentQueue(models.Model):
 
     def mock_run_create_partners_and_fail(self):
         self.mock_run_create_partners()
-        raise UserError(_("boom"))
+        raise UserError(self.env._("boom"))
 
     def _get_failure_emails(self):
         return "test@test.com"
