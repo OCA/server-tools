ec9eff3b00261985ce89ac4002d6767f9fa8fe0d
Aungkokolin1997 <lin@quartile.co>
2026-06-30 08:10:01 +0000

    [IMP] attachment_delete_restrict: add summary to manifest

--- attachment_delete_restrict/__manifest__.py
+++ attachment_delete_restrict/__manifest__.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Restrict Deletion of Attachments",
+    "summary": "Restrict deletion of attachments at different levels",
     "version": "17.0.1.0.0",
     "depends": [
         "base",
