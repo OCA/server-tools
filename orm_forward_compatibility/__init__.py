61abc07eb32923bdab7051756aeebe7307a5bf15
David Gallay <david.gallay.96@gmail.com>
2026-08-14 15:18:57 +0200

    [ADD] orm_forward_compatibility: Odoo 19+ ORM helpers backported to 18.0
    Minimal `Domain` shim (delegates to odoo.osv.expression) plus the typed
    `ir.config_parameter` getters get_str/get_int/get_float/get_bool.
    
    Purpose: cut the effort of backporting 19/20 modules to 18.0 by keeping
    their call sites 1-1 with upstream instead of rewriting them.

---
+++ orm_forward_compatibility/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2026 Camptocamp SA
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+from . import models
+from .domain import Domain
---
+++ orm_forward_compatibility/readme/CONTRIBUTORS.md
@@ -0,0 +1,2 @@
+- [Camptocamp](https://www.camptocamp.com):
+  - David Gallay \<<david.gallay@camptocamp.com>\>
